benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 069af64f0abd9bdb368b51561db34aaf7c859b1e

Split the IPC code into ipc/
This splits the ipc code from the common project.  The 'common' project pulls in
all of webkit, the v8 bindings, skia, googleurl, and a number of other projects
which makes it very difficult to deal with especially for external projects
wanting just to use some of Chromium's infrastructure.  This puts the ipc code
into its top-level ipc/ directory with a dependency only on base.  The common
project depends on the new ipc/ipc.gyp:ipc target so that all projects currently
pulling common in to get the IPC code still have it available.  This mostly
follows agl's pre-gyp attempt to do this which was r13062.

Known issues:
- Currently a number of projects depend on chrome/chrome.gyp:common in order to
use the IPC infrastructure.  Rather than fixing all of these dependencies I have
made common depend on ipc/ipc.gyp:ipc and added "ipc" to the include_rules
section of DEPS so that checkdeps.py doesn't complain.  Over time projects that
need IPC should depend on the IPC project themselves and dependencies on common
removed, although I don't think many projects that need IPC will be able to get
away without common currently.
- ipc/ipc_message_macros.h still has #include "chrome/common/..." inside of a
ipc/ should not refer to files in chrome/... now.  I'm not sure how to resolve
this since it's really an IDE bug
- the named pipe name (windows+linux) and the logging event name (all) + env
variable (posix) refer explicitly to 'Chrome' which somewhat hurts the illusion
of ipc/ being an independent library.  I think this should be examined in a
subsequent, much smaller patch.
- I've eliminated the IPC.SendMsgCount counter since it was implemented in a way
to create a dependency from ipc/ to chrome/common/chrome_counters. This is the
same approach that r13062 took.

http://codereview.chromium.org/155905

(Patch from James Robinson)


git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21342 0039d316-1c4b-4281-b951-d872f2087c98

chrome/chrome.gyp
@@ -301,6 +301,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         '../base/base.gyp:base',
         '../base/base.gyp:base_gfx',
         '../build/temp_gyp/googleurl.gyp:googleurl',
+        '../ipc/ipc.gyp:ipc',
         '../net/net.gyp:net',
         '../skia/skia.gyp:skia',
         '../third_party/icu38/icu38.gyp:icui18n',
@@ -362,6 +363,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'common/chrome_constants.h',
         'common/chrome_counters.cc',
         'common/chrome_counters.h',
+        'common/chrome_descriptors.h',
         'common/chrome_paths.cc',
         'common/chrome_paths.h',
         'common/chrome_paths_internal.h',
@@ -378,14 +380,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'common/classfactory.cc',
         'common/classfactory.h',
         'common/common_glue.cc',
+        'common/common_param_traits.cc',
+        'common/common_param_traits.h',
         'common/debug_flags.cc',
         'common/debug_flags.h',
         'common/devtools_messages.h',
         'common/devtools_messages_internal.h',
         'common/env_vars.cc',
         'common/env_vars.h',
-        'common/file_descriptor_set_posix.cc',
-        'common/file_descriptor_set_posix.h',
         'common/filter_policy.h',
         'common/gears_api.h',
         'common/gtk_util.cc',
@@ -394,25 +396,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'common/histogram_synchronizer.h',
         'common/important_file_writer.cc',
         'common/important_file_writer.h',
-        'common/ipc_channel.h',
-        'common/ipc_channel_handle.h',
-        'common/ipc_channel_posix.cc',
-        'common/ipc_channel_posix.h',
-        'common/ipc_channel_proxy.cc',
-        'common/ipc_channel_proxy.h',
-        'common/ipc_channel_win.cc',
-        'common/ipc_channel_win.h',
-        'common/ipc_logging.cc',
-        'common/ipc_logging.h',
-        'common/ipc_message.cc',
-        'common/ipc_message.h',
-        'common/ipc_message_macros.h',
-        'common/ipc_message_utils.cc',
-        'common/ipc_message_utils.h',
-        'common/ipc_sync_channel.cc',
-        'common/ipc_sync_channel.h',
-        'common/ipc_sync_message.cc',
-        'common/ipc_sync_message.h',
         'common/json_value_serializer.cc',
         'common/json_value_serializer.h',
         'common/jstemplate_builder.cc',
@@ -3480,35 +3463,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       ],
     },
     {
-      'target_name': 'ipc_tests',
-      'type': 'executable',
-      'msvs_guid': 'B92AE829-E1CD-4781-824A-DCB1603A1672',
-      'dependencies': [
-        'common',
-        'test_support_unit',
-        '../base/base.gyp:base',
-        '../testing/gtest.gyp:gtest',
-      ],
-      'sources': [
-        'common/ipc_fuzzing_tests.cc',
-        'common/ipc_send_fds_test.cc',
-        'common/ipc_tests.cc',
-        'common/ipc_tests.h',
-      ],
-      'conditions': [
-        ['OS=="linux"', {
-          'dependencies': [
-            '../build/linux/system.gyp:gtk',
-          ],
-        }],
-        ['OS=="linux" and toolkit_views==1', {
-          'dependencies': [
-            '../views/views.gyp:views',
-          ],
-        }],
-      ],
-    },
-    {
       'target_name': 'ui_tests',
       'type': 'executable',
       'msvs_guid': '76235B67-1C27-4627-8A33-4B2E1EF93EDE',
@@ -3661,6 +3615,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'test_support_unit',
         'utility',
         '../app/app.gyp:app_resources',
+        '../ipc/ipc.gyp:ipc',
         '../net/net.gyp:net_resources',
         '../net/net.gyp:net_test_support',
         '../printing/printing.gyp:printing',
@@ -3884,15 +3839,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'common/bzip2_unittest.cc',
         'common/child_process_logging_mac_unittest.mm',
         'common/chrome_plugin_unittest.cc',
+        'common/common_param_traits_unittest.cc',
         'common/extensions/extension_unittest.cc',
         'common/extensions/url_pattern_unittest.cc',
         'common/extensions/user_script_unittest.cc',
-        'common/file_descriptor_set_unittest.cc',
         'common/important_file_writer_unittest.cc',
-        'common/ipc_message_unittest.cc',
-        'common/ipc_sync_channel_unittest.cc',
-        'common/ipc_sync_message_unittest.cc',
-        'common/ipc_sync_message_unittest.h',
         'common/json_value_serializer_unittest.cc',
         'common/mru_cache_unittest.cc',
         'common/net/url_util_unittest.cc',
@@ -4030,7 +3981,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           ],
           'sources!': [
             'browser/gtk/tabs/tab_renderer_gtk_unittest.cc',
-            'common/file_descriptor_set_unittest.cc',
             'common/net/url_util_unittest.cc',
           ],
           'link_settings': {

nwjs/chromium.src

commit 15aa819dcf5200c013d33afb2379aac147ee1364

Merge 21369 - Build fix: ipc_tests.exe is not getting built for Windows release.
(Broke in r21342)

<jamesr> agl: chromium XP release builder builds chrome.sln and then tries to
run ipc_tests.exe. nothing in chrome.gyp depends on ipc_tests now so the
executable doesn't exist on that builder
<jamesr> agl/jrg: adding a dependency on ../ipc/ipc.gyp:ipc_tests to the
'coverage' target would solve both issues
<jrg> jamesr: agl's change will only impact the coverage bots.  Sounds like
that't not what you want, right?
<jamesr> jrg: i think it's correct but won't resolve the chromium XP issue.
coverage should run for those tests either way
<agl> jamesr: how about I tie it to unit_tests?
<agl> jamesr: there's a long term solution and then there's getting the builder
green.
<jamesr> agl: yeah. trying to check super fast if getting that builder to not
run ipc_tests.exe is easy

http://codereview.chromium.org/155977


TBR=agl@chromium.org

Review URL: http://codereview.chromium.org/160195

git-svn-id: svn://svn.chromium.org/chrome/branches/195/src@21696 0039d316-1c4b-4281-b951-d872f2087c98

chrome/chrome.gyp
@@ -3540,6 +3540,9 @@
         'utility',
         '../app/app.gyp:app_resources',
         '../ipc/ipc.gyp:ipc',
+        # TODO(jamesr): this is required for the release builders to build
+        # ipc_tests.exe. However, it's probably not what we really want.
+        '../ipc/ipc.gyp:ipc_tests',
         '../net/net.gyp:net_resources',
         '../net/net.gyp:net_test_support',
         '../printing/printing.gyp:printing',
@@ -5019,6 +5022,7 @@
           # E.g. build/{linux|mac}/chrome_linux.croc
           'dependencies': [
             '../base/base.gyp:base_unittests',
+            '../ipc/ipc.gyp:ipc_tests',
             '../media/media.gyp:media_unittests',
             '../net/net.gyp:net_unittests',
             '../printing/printing.gyp:printing_unittests',