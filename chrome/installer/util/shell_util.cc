96da71c66537e1bf8acb144cb2fa5924e34c1d09
mal@chromium.org <mal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-12 07:26:50 +0000

    Merge 22314 - Use alternate icon for Chrome shortcuts if specified in master preferences.
    This change depends on change 159539 that is currently under review and a new chrome2.ico that is yet to be added to the repository.
    
    BUG=12701
    TEST=Specify an alternate icon in installerdata file and make sure that icon is used for shortcuts and it not changed later by updates.
    
    Review URL: http://codereview.chromium.org/159618
    
    TBR=kuchhal@chromium.org
    
    Review URL: http://codereview.chromium.org/164390
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/195/src@23180 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/shell_util.cc
+++ chrome/installer/util/shell_util.cc
@@ -27,6 +27,7 @@
 #include "chrome/installer/util/browser_distribution.h"
 #include "chrome/installer/util/create_reg_key_work_item.h"
 #include "chrome/installer/util/install_util.h"
+#include "chrome/installer/util/master_preferences.h"
 #include "chrome/installer/util/l10n_string_util.h"
 #include "chrome/installer/util/set_reg_value_work_item.h"
 #include "chrome/installer/util/util_constants.h"
@@ -691,14 +692,23 @@ bool ShellUtil::UpdateChromeShortcut(const std::wstring& chrome_exe,
                                      const std::wstring& description,
                                      bool create_new) {
   std::wstring chrome_path = file_util::GetDirectoryFromPath(chrome_exe);
+
   if (create_new) {
+    FilePath prefs_path(chrome_path);
+    prefs_path = prefs_path.Append(installer_util::kDefaultMasterPrefs);
+    scoped_ptr<DictionaryValue> prefs(
+        installer_util::ParseDistributionPreferences(prefs_path));
+    int icon_index = 0;
+    installer_util::GetDistroIntegerPreference(prefs.get(),
+        installer_util::master_preferences::kChromeShortcutIconIndex,
+        &icon_index);
     return file_util::CreateShortcutLink(chrome_exe.c_str(),      // target
                                          shortcut.c_str(),        // shortcut
                                          chrome_path.c_str(),     // working dir
                                          NULL,                    // arguments
                                          description.c_str(),     // description
                                          chrome_exe.c_str(),      // icon file
-                                         0);                      // icon index
+                                         icon_index);             // icon index
   } else {
     return file_util::UpdateShortcutLink(chrome_exe.c_str(),      // target
                                          shortcut.c_str(),        // shortcut
