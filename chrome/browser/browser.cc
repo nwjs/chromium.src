9cea61c73811f971a65fdde0c260e0f0eb2a8f72
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-19 16:04:25 +0000

    Merge 32413 - Tweaks and fixes for NTP extension promo.
    Don't show promo for extensions on Mac, as they're not available yet. Decrement promo counter even when NTP is not home page, and do not remove puzzle piece when promo line is closed. When message bar is closed, make it zip off bottom of screen instead of just disappearing.
    
    BUG= 27814, 25258, 27815
    TEST= see various bugs.
    
    Review URL: http://codereview.chromium.org/385135
    
    TBR=mirandac@chromium.org
    Review URL: http://codereview.chromium.org/399115
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/249/src@32512 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser.cc
+++ chrome/browser/browser.cc
@@ -1344,7 +1344,8 @@ void Browser::RegisterUserPrefs(PrefService* prefs) {
   prefs->RegisterIntegerPref(prefs::kDeleteTimePeriod, 0);
   prefs->RegisterBooleanPref(prefs::kCheckDefaultBrowser, true);
   prefs->RegisterBooleanPref(prefs::kShowOmniboxSearchHint, true);
-  prefs->RegisterIntegerPref(prefs::kNTPPromoRemaining, 5);
+  prefs->RegisterIntegerPref(prefs::kNTPPromoLineRemaining, 5);
+  prefs->RegisterIntegerPref(prefs::kNTPPromoImageRemaining, 5);
   prefs->RegisterBooleanPref(prefs::kShowExtensionShelf, true);
 }
 
