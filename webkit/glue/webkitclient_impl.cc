0142283f0d40e6676beeb25fb2f249539bda7094
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-20 21:02:05 +0000

    Merge 32373 - Rename policy_url to first_party_for_cookies in cookie related code.
    R=abarth
    BUG=25133
    TEST=none. A cosmetic change.
    Review URL: http://codereview.chromium.org/403011
    
    TBR=wtc@chromium.org
    Review URL: http://codereview.chromium.org/418025
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/249/src@32664 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webkitclient_impl.cc
+++ webkit/glue/webkitclient_impl.cc
@@ -124,7 +124,7 @@ WebThemeEngine* WebKitClientImpl::themeEngine() {
 }
 
 bool WebKitClientImpl::rawCookies(const WebURL& url,
-                                  const WebURL& policy_url,
+                                  const WebURL& first_party_for_cookies,
                                   WebVector<WebCookie>* raw_cookies) {
   NOTREACHED();
   return false;
