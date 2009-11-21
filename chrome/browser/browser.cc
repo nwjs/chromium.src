7e78a585d5d76b2aedad47204c1417efb371ac01
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-21 21:45:42 +0000

    Merge 32616 - Fix window.close() for page action popups. Convert mappy to a page action popup. Also fix a bug where disabling an extension with a page action didn't cause the icon to hide.
    BUG=27519
    TEST=none
    
    Review URL: http://codereview.chromium.org/414019
    
    TBR=erikkay@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/249/src@32755 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser.cc
+++ chrome/browser/browser.cc
@@ -137,6 +137,8 @@ Browser::Browser(Type type, Profile* profile)
                  NotificationService::AllSources());
   registrar_.Add(this, NotificationType::EXTENSION_UNLOADED,
                  NotificationService::AllSources());
+  registrar_.Add(this, NotificationType::EXTENSION_UNLOADED_DISABLED,
+                 NotificationService::AllSources());
   registrar_.Add(this, NotificationType::EXTENSION_PROCESS_CRASHED,
                  NotificationService::AllSources());
   registrar_.Add(this, NotificationType::BROWSER_THEME_CHANGED,
@@ -2254,8 +2256,9 @@ void Browser::Observe(NotificationType type,
       break;
     }
 
-    case NotificationType::EXTENSION_UNLOADED: {
-      window()->GetLocationBar()->InvalidatePageActions();
+    case NotificationType::EXTENSION_UNLOADED:
+    case NotificationType::EXTENSION_UNLOADED_DISABLED: {
+      window()->GetLocationBar()->UpdatePageActions();
 
       // Close any tabs from the unloaded extension.
       Extension* extension = Details<Extension>(details).ptr();
