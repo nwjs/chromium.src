8f608b2c8e3e11325f14b6306bc616aedc0fbac3
jorlow@chromium.org <jorlow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-26 03:03:18 +0000

    Merge 33144 - RESUBMIT of http://codereview.chromium.org/404025/show
    Start the WebKit thread when we initialize the resource
    dispatcher host.
    
    NOTE: It's very possible this will have a startup
    performance impact. I'm going to watch the bots after
    committing.
    
    TODO: Clean up shutdown logic that exists simply
    because we don't know whether or not we'll ever start
    the WebKit thread.
    
    Originally landed in r33063, reverted in r33101 due to
    coincidence with Modules Linux startup test failures.
    
    TBR=jorlow, darin
    TEST=The WebKit thread gets spun up early in the
    initialization process.
    BUG=24144,28364
    
    Review URL: http://codereview.chromium.org/441025
    
    TBR=chase@chromium.org
    Review URL: http://codereview.chromium.org/443014
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/249/src@33170 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/resource_dispatcher_host.cc
+++ chrome/browser/renderer_host/resource_dispatcher_host.cc
@@ -293,6 +293,7 @@ ResourceDispatcherHost::~ResourceDispatcherHost() {
 
 void ResourceDispatcherHost::Initialize() {
   DCHECK(ChromeThread::CurrentlyOn(ChromeThread::UI));
+  webkit_thread_->Initialize();
   safe_browsing_->Initialize();
   ChromeThread::PostTask(
       ChromeThread::IO, FROM_HERE,
