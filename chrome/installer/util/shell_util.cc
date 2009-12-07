92e32cf40f29e1228568018e5c9615e06ae222ef
gwilson@chromium.org <gwilson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-07 19:54:05 +0000

    Revert 33895 - Revert 33874  Merge 33732  Chrome updates should not change shortcut icon if there is an index specified in prefs file.
    Reverting revert.  Fix for alternate icons going back in.
    
    BUG=none
    TEST=Specify a chrome with alternative icon (using master prefs), install an update and make sure the icon doesnt change.
    
    Review URL: http://codereview.chromium.org/455041
    
    TBR=kuchhal@chromium.org,laforge@chromium.org
    
    Review URL: http://codereview.chromium.org/466044
    
    TBR=stuartmorgan@chromium.org
    Review URL: http://codereview.chromium.org/469008
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/195/src@33979 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/shell_util.cc
+++ chrome/installer/util/shell_util.cc
@@ -314,7 +314,7 @@ bool IsChromeRegistered(const std::wstring& chrome_exe,
   return registered;
 }
 
-// This method registers Chrome on Vista by launching eleavated setup.exe. 
+// This method registers Chrome on Vista by launching eleavated setup.exe.
 // That will show user standard Vista elevation prompt. If user accepts it
 // the new process will make the necessary changes and return SUCCESS that
 // we capture and return.
@@ -772,15 +772,15 @@ bool ShellUtil::UpdateChromeShortcut(const std::wstring& chrome_exe,
                                      bool create_new) {
   std::wstring chrome_path = file_util::GetDirectoryFromPath(chrome_exe);
 
+  FilePath prefs_path(chrome_path);
+  prefs_path = prefs_path.Append(installer_util::kDefaultMasterPrefs);
+  scoped_ptr<DictionaryValue> prefs(
+      installer_util::ParseDistributionPreferences(prefs_path));
+  int icon_index = 0;
+  installer_util::GetDistroIntegerPreference(prefs.get(),
+      installer_util::master_preferences::kChromeShortcutIconIndex,
+      &icon_index);
   if (create_new) {
-    FilePath prefs_path(chrome_path);
-    prefs_path = prefs_path.Append(installer_util::kDefaultMasterPrefs);
-    scoped_ptr<DictionaryValue> prefs(
-        installer_util::ParseDistributionPreferences(prefs_path));
-    int icon_index = 0;
-    installer_util::GetDistroIntegerPreference(prefs.get(),
-        installer_util::master_preferences::kChromeShortcutIconIndex,
-        &icon_index);
     return file_util::CreateShortcutLink(chrome_exe.c_str(),      // target
                                          shortcut.c_str(),        // shortcut
                                          chrome_path.c_str(),     // working dir
@@ -795,6 +795,6 @@ bool ShellUtil::UpdateChromeShortcut(const std::wstring& chrome_exe,
                                          NULL,                    // arguments
                                          description.c_str(),     // description
                                          chrome_exe.c_str(),      // icon file
-                                         0);                      // icon index
+                                         icon_index);             // icon index
   }
 }
