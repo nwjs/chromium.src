c3688425693c1af2a312f6bd1599560530e1576b
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-09 01:54:52 +0000

    Merge 33770 - Add ExtensionsQuotaService to limit abusive amounts of requests to mutating extension functions, as discussed on chromiumdev and in Extensions quotaserver design doc.
    Add a hook in the dispatcher to have the quota service assess the request.
    
    Wire up bookmarks.{create, move, remove, update} to the service.
    
    BUG=19899
    TEST=ExtensionsQuotaServiceTest, QuotaLimitHeuristicTest (both new)
    
    Review URL: http://codereview.chromium.org/441006
    
    Review URL: http://codereview.chromium.org/464076
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/249/src@34128 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_function_dispatcher.cc
+++ chrome/browser/extensions/extension_function_dispatcher.cc
@@ -23,6 +23,8 @@
 #include "chrome/browser/extensions/extension_tabs_module_constants.h"
 #include "chrome/browser/extensions/extension_test_api.h"
 #include "chrome/browser/extensions/extension_toolstrip_api.h"
+#include "chrome/browser/extensions/extensions_quota_service.h"
+#include "chrome/browser/extensions/extensions_service.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/renderer_host/render_process_host.h"
 #include "chrome/browser/renderer_host/render_view_host.h"
@@ -147,6 +149,7 @@ void FactoryRegistry::ResetFunctions() {
   RegisterFunction<ExtensionTestPassFunction>();
   RegisterFunction<ExtensionTestFailFunction>();
   RegisterFunction<ExtensionTestLogFunction>();
+  RegisterFunction<ExtensionTestQuotaResetFunction>();
 }
 
 void FactoryRegistry::GetAllNames(std::vector<std::string>* names) {
@@ -263,7 +266,16 @@ void ExtensionFunctionDispatcher::HandleRequest(const std::string& name,
   function->SetArgs(args);
   function->set_request_id(request_id);
   function->set_has_callback(has_callback);
-  function->Run();
+
+  ExtensionsService* service = profile()->GetExtensionsService();
+  DCHECK(service);
+  ExtensionsQuotaService* quota = service->quota_service();
+  if (quota->Assess(extension_id(), function, args, base::TimeTicks::Now())) {
+    function->Run();
+  } else {
+    render_view_host_->SendExtensionResponse(function->request_id(), false,
+        std::string(), QuotaLimitHeuristic::kGenericOverQuotaError);
+  }
 }
 
 void ExtensionFunctionDispatcher::SendResponse(ExtensionFunction* function,
