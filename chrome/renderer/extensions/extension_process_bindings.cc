80bf2baa213a125b82c255cf37ed39a8ac6bd810
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-06 22:54:16 +0000

    Merge 32120: Refactoring of the chrome.experimental.popup API implementation to allow display of pop-ups for extensions viewed through a tab-contents view.I added a new class, ExtensionPopupHost. This class implements the necessary environment for managing child popup windows from either an ExtensionHost, or an ExtensionDOMUI. Note that this class is added as a member to ExtensionHost and ExtensionDOMUI.
    I decided to take this approach to prevent multiple inheritance of the NotificationObserver class: Both ExtensionPopupHost and ExtensionHost must inherit from this class, and I was uncertain of how the system would behave wrt virtual inheritance. Please comment on if I should have used the inheritance approach. 
    
    I also removed the customHandler tag (in extension_api.json) that I had added in the initial submission. The arguments in the schema are now those that users of the API see. The nodocs tags were also removed. 
    
    The api experimental.popup.getAnchorWindow() has been renamed to popup.getParentWindow, as per a suggestion from Erik K. 
    
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/249/src@35662 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/extension_process_bindings.cc
+++ chrome/renderer/extensions/extension_process_bindings.cc
@@ -225,8 +225,8 @@ class ExtensionImpl : public ExtensionBase {
       return v8::FunctionTemplate::New(GetRenderViewId);
     } else if (name->Equals(v8::String::New("GetPopupView"))) {
       return v8::FunctionTemplate::New(GetPopupView);
-    } else if (name->Equals(v8::String::New("GetPopupAnchorView"))) {
-      return v8::FunctionTemplate::New(GetPopupAnchorView);
+    } else if (name->Equals(v8::String::New("GetPopupParentWindow"))) {
+      return v8::FunctionTemplate::New(GetPopupParentWindow);
     } else if (name->Equals(v8::String::New("SetExtensionActionIcon"))) {
       return v8::FunctionTemplate::New(SetExtensionActionIcon);
     }
@@ -287,7 +287,7 @@ class ExtensionImpl : public ExtensionBase {
     return PopupViewFinder(args, ViewType::EXTENSION_POPUP);
   }
 
-  static v8::Handle<v8::Value> GetPopupAnchorView(const v8::Arguments& args) {
+  static v8::Handle<v8::Value> GetPopupParentWindow(const v8::Arguments& args) {
     return PopupViewFinder(args, ViewType::EXTENSION_TOOLSTRIP);
   }
 
