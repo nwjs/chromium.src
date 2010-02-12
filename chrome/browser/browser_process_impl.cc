eb8a70fb223073813bc6da36f5e9c4edfcc21986
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-12 20:28:02 +0000

    Merge 38296 - Delete HTML5 database in BrowsingDataRemover, part 1.
    This part deletes databases except for when they're currently used by a renderer.
    
    BUG=34633
    TEST=open the webkit html5 database demo, create some notes. close the tab. clear browsing data. open the demo again. notes should be gone.
    
    Review URL: http://codereview.chromium.org/570032
    
    TBR=jochen@chromium.org
    Review URL: http://codereview.chromium.org/605013
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/249/src@38928 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_process_impl.cc
+++ chrome/browser/browser_process_impl.cc
@@ -315,8 +315,7 @@ void BrowserProcessImpl::ClearLocalState(const FilePath& profile_path) {
   SQLitePersistentCookieStore::ClearLocalState(profile_path.Append(
       chrome::kCookieFilename));
   DOMStorageContext::ClearLocalState(profile_path, chrome::kExtensionScheme);
-  webkit_database::DatabaseTracker::ClearLocalState(profile_path,
-      chrome::kExtensionScheme);
+  webkit_database::DatabaseTracker::ClearLocalState(profile_path);
   // TODO(jochen): clear app cache local state.
 }
 
