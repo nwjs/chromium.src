2d27a370aac0e85d2e6cadb1f16227c12708e40f
nick@chromium.org <nick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-17 20:58:15 +0000

    Merge 38490 - Mark dirty in PutParentIdPropertyOnly.
    BUG=33081
    TEST=none
    
    Review URL: http://codereview.chromium.org/594010
    
    TBR=nick@chromium.org
    Review URL: http://codereview.chromium.org/621009
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/307/src@39263 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/syncable/syncable.cc
+++ chrome/browser/sync/syncable/syncable.cc
@@ -1105,6 +1105,7 @@ bool MutableEntry::Put(IdField field, const Id& value) {
 
 void MutableEntry::PutParentIdPropertyOnly(const Id& parent_id) {
   dir()->ReindexParentId(kernel_, parent_id);
+  kernel_->mark_dirty();
 }
 
 bool MutableEntry::Put(BaseVersion field, int64 value) {
