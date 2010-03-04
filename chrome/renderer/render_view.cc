d48b5a6f9fc310e90cfbc9725e6e74e69b2bdb61
abarth@chromium.org <abarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-04 04:23:42 +0000

    Merge 40593 - Only show the blocked plugin UI when we actually fail to instantiate a plugin.
    BUG=36879
    TEST=This code review does not have have a blocked plugin icon but http://kb2.adobe.com/cps/155/tn_15507.html does.
    
    Review URL: http://codereview.chromium.org/661363
    
    TBR=abarth@chromium.org
    Review URL: http://codereview.chromium.org/668040
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/249/src@40600 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.cc
+++ chrome/renderer/render_view.cc
@@ -1926,11 +1926,7 @@ void RenderView::willClose(WebFrame* frame) {
 bool RenderView::allowPlugins(WebFrame* frame, bool enabled_per_settings) {
   if (!enabled_per_settings)
     return false;
-  if (!AllowContentType(CONTENT_SETTINGS_TYPE_PLUGINS)) {
-    DidBlockContentType(CONTENT_SETTINGS_TYPE_PLUGINS);
-    return false;
-  }
-  return true;
+  return AllowContentType(CONTENT_SETTINGS_TYPE_PLUGINS);
 }
 
 bool RenderView::allowImages(WebFrame* frame, bool enabled_per_settings) {
@@ -2598,6 +2594,10 @@ bool RenderView::allowScript(WebFrame* frame, bool enabled_per_settings) {
 
 void RenderView::didNotAllowScript(WebKit::WebFrame* frame) {
   DidBlockContentType(CONTENT_SETTINGS_TYPE_JAVASCRIPT);
+}
+
+void RenderView::didNotAllowPlugins(WebKit::WebFrame* frame) {
+  DidBlockContentType(CONTENT_SETTINGS_TYPE_PLUGINS);
 }
 
 void RenderView::didExhaustMemoryAvailableForScript(WebFrame* frame) {
