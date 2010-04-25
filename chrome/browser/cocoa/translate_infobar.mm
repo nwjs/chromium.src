13cb776cd46f5aecb2edbfb1b7406b0f567fa227
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-25 09:19:53 +0000

    Merge 45553 - Mac: Fix useafterfree in translate infobar options menu.
    The "About Translate" command opens a new tab which closes the infobar, we where trying to update the options menu which acccessed an already released object.
    
    The fix is to match Windows behavior and only rebuild the options menu when clicked.
    
    Thanks to Rohit for diagnosing the problem!
    
    BUG=42030
    TEST=repro in bug description.
    
    Review URL: http://codereview.chromium.org/1756006
    
    TBR=jeremy@chromium.org
    Review URL: http://codereview.chromium.org/1745013
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/375/src@45554 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/translate_infobar.mm
+++ chrome/browser/cocoa/translate_infobar.mm
@@ -259,9 +259,6 @@ class TranslateNotificationObserverBridge :
 }
 
 - (void)languageModified {
-  // If necessary, update state and translate.
-  [self rebuildOptionsMenu];
-
   // Selecting an item from the "from language" menu in the before translate
   // phase shouldn't trigger translation - http://crbug.com/36666
   TranslateInfoBarDelegate* delegate = [self delegate];
@@ -566,6 +563,10 @@ class TranslateNotificationObserverBridge :
   // Instantiate additional controls.
   [self constructViews];
 
+  // Set ourselves as the delegate for the options menu so we can populate it
+  // dynamically.
+  [[optionsPopUp_ menu] setDelegate:self];
+
   // Replace label_ with label1_ so we get a consistent look between all the
   // labels we display in the translate view.
   [[label_ superview] replaceSubview:label_ with:label1_.get()];
@@ -644,10 +645,20 @@ class TranslateNotificationObserverBridge :
 - (void)menuItemSelected:(id)item {
   if ([item respondsToSelector:@selector(tag)]) {
     int cmd = [item tag];
+    // Danger Will Robinson! : This call can release the infobar (e.g. invoking
+    // "About Translate" can open a new tab).
+    // Do not access member variables after this line!
     menu_model_->ExecuteCommand(cmd);
   } else {
     NOTREACHED();
   }
+}
+
+#pragma mark NSMenuDelegate
+
+// Invoked by virtue of us being set as the delegate for the options menu.
+- (void)menuNeedsUpdate:(NSMenu *)menu {
+  [self rebuildOptionsMenu];
 }
 
 #pragma mark TestingAPI
