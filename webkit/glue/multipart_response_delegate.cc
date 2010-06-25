43fc5a7140515e88a2247708aa06150b7b93d593
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-25 02:03:05 +0000

    Merge 50676 - Fix a crash when the content type of a multipart request changes to a content type that we ignore the load of.
    For example, if the content type of a multipart request changes
    from text/html to foo/bar, we would crash because we would
    delete the multipart delegate while it was still on the stack.
    
    BUG=47105
    
    Review URL: http://codereview.chromium.org/2850023
    
    TBR=tony@chromium.org
    Review URL: http://codereview.chromium.org/2806033
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/375/src@50810 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/multipart_response_delegate.cc
+++ webkit/glue/multipart_response_delegate.cc
@@ -125,7 +125,7 @@ void MultipartResponseDelegate::OnReceivedData(const char* data,
 
   size_t boundary_pos;
   while ((boundary_pos = FindBoundary()) != std::string::npos) {
-    if (boundary_pos > 0) {
+    if (boundary_pos > 0 && client_) {
       // Send the last data chunk.
       client_->didReceiveData(loader_,
                               data_.data(),
@@ -154,7 +154,7 @@ void MultipartResponseDelegate::OnReceivedData(const char* data,
 void MultipartResponseDelegate::OnCompletedRequest() {
   // If we have any pending data and we're not in a header, go ahead and send
   // it to WebCore.
-  if (!processing_headers_ && !data_.empty()) {
+  if (!processing_headers_ && !data_.empty() && !stop_sending_ && client_) {
     client_->didReceiveData(loader_,
                             data_.data(),
                             static_cast<int>(data_.length()));
@@ -235,7 +235,8 @@ bool MultipartResponseDelegate::ParseHeaders() {
   response.setIsMultipartPayload(has_sent_first_response_);
   has_sent_first_response_ = true;
   // Send the response!
-  client_->didReceiveResponse(loader_, response);
+  if (client_)
+    client_->didReceiveResponse(loader_, response);
 
   return true;
 }
