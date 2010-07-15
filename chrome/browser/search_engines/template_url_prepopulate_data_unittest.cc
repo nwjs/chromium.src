40473445b8be28cc524ad255f453c3122326ba06
gwilson@chromium.org <gwilson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-15 22:54:01 +0000

    Attempt to fix official builders.  Changes reference to refactored PrefService() constructor that doesn't exist on 375.
    Last attempt before revert.
    
    TBR=laforge
    BUG=none
    TEST=builders green
    
    Review URL: http://codereview.chromium.org/2847053
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/375/src@52558 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search_engines/template_url_prepopulate_data_unittest.cc
+++ chrome/browser/search_engines/template_url_prepopulate_data_unittest.cc
@@ -102,10 +102,7 @@ TEST_F(TemplateURLPrepopulateDataTest, ProvidersFromPrefs) {
   FilePath preferences_file = temp_dir.path().AppendASCII("Preferences");
   file_util::WriteFile(preferences_file, pref_data, sizeof(pref_data));
 
-  scoped_ptr<PrefService> prefs(new PrefService(new PrefValueStore(
-      NULL, new JsonPrefStore(preferences_file,
-      ChromeThread::GetMessageLoopProxyForThread(ChromeThread::FILE)),
-      NULL, NULL)));
+  scoped_ptr<PrefService> prefs(new PrefService(preferences_file));
 
   TemplateURLPrepopulateData::RegisterUserPrefs(prefs.get());
 
