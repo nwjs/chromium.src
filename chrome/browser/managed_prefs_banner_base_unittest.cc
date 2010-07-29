0897873902a2f312d30d69a56ce9aaf7f623e1cc
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-29 12:51:41 +0000

    Merge 53460 - Properly handle local state prefs for the managed prefs warning banner.
    BUG=49655
    TEST=Configure MetricsReportingEnabled policy and verify advanced tab in options shows the banner.
    
    Review URL: http://codereview.chromium.org/3057007
    
    TBR=mnissler@chromium.org
    
    Review URL: http://codereview.chromium.org/2856073
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/472/src@54122 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/managed_prefs_banner_base_unittest.cc
+++ chrome/browser/managed_prefs_banner_base_unittest.cc
@@ -6,67 +6,76 @@
 #include "chrome/browser/managed_prefs_banner_base.h"
 #include "chrome/browser/pref_service.h"
 #include "chrome/common/pref_names.h"
+#include "chrome/test/testing_pref_service.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
-
-namespace {
-
-const wchar_t* kDummyPref = L"dummy";
-
-}  // namespace
 
 // Tests whether managed preferences banner base functionality correctly
 // determines banner visiblity.
 class ManagedPrefsBannerBaseTest : public testing::Test {
  public:
   virtual void SetUp() {
-    managed_prefs_ = new DummyPrefStore;
-    extension_prefs_ = new DummyPrefStore;
-    command_line_prefs_ = new DummyPrefStore;
-    user_prefs_ = new DummyPrefStore;
-    default_prefs_ = new DummyPrefStore;
-    pref_service_.reset(new PrefService(new PrefValueStore(managed_prefs_,
-                                                           extension_prefs_,
-                                                           command_line_prefs_,
-                                                           user_prefs_,
-                                                           default_prefs_)));
-    pref_service_->RegisterStringPref(prefs::kHomePage, "http://google.com");
-    pref_service_->RegisterBooleanPref(prefs::kHomePageIsNewTabPage, false);
-    pref_service_->RegisterBooleanPref(kDummyPref, false);
+    user_prefs_.reset(new TestingPrefService);
+    user_prefs_->RegisterBooleanPref(prefs::kHomePageIsNewTabPage, false);
+    user_prefs_->RegisterBooleanPref(prefs::kSearchSuggestEnabled, false);
+    local_state_.reset(new TestingPrefService);
+    local_state_->RegisterBooleanPref(prefs::kHomePageIsNewTabPage, false);
+    local_state_->RegisterBooleanPref(prefs::kMetricsReportingEnabled, false);
   }
 
-  scoped_ptr<PrefService> pref_service_;
-  DummyPrefStore* managed_prefs_;
-  DummyPrefStore* extension_prefs_;
-  DummyPrefStore* command_line_prefs_;
-  DummyPrefStore* user_prefs_;
-  DummyPrefStore* default_prefs_;
+  scoped_ptr<TestingPrefService> local_state_;
+  scoped_ptr<TestingPrefService> user_prefs_;
 };
 
 TEST_F(ManagedPrefsBannerBaseTest, VisibilityTest) {
-  ManagedPrefsBannerBase banner(pref_service_.get(), OPTIONS_PAGE_GENERAL);
+  ManagedPrefsBannerBase banner(local_state_.get(),
+                                user_prefs_.get(),
+                                OPTIONS_PAGE_ADVANCED);
+  banner.AddLocalStatePref(prefs::kMetricsReportingEnabled);
   EXPECT_FALSE(banner.DetermineVisibility());
-  managed_prefs_->prefs()->SetBoolean(kDummyPref, true);
+  user_prefs_->SetManagedPref(prefs::kHomePageIsNewTabPage,
+                              Value::CreateBooleanValue(true));
   EXPECT_FALSE(banner.DetermineVisibility());
-  user_prefs_->prefs()->SetString(prefs::kHomePage, "http://foo.com");
+  user_prefs_->SetUserPref(prefs::kSearchSuggestEnabled,
+                           Value::CreateBooleanValue(true));
   EXPECT_FALSE(banner.DetermineVisibility());
-  managed_prefs_->prefs()->SetString(prefs::kHomePage, "http://bar.com");
+  user_prefs_->SetManagedPref(prefs::kSearchSuggestEnabled,
+                              Value::CreateBooleanValue(false));
   EXPECT_TRUE(banner.DetermineVisibility());
+  local_state_->SetManagedPref(prefs::kMetricsReportingEnabled,
+                               Value::CreateBooleanValue(true));
+  EXPECT_TRUE(banner.DetermineVisibility());
+  user_prefs_->RemoveManagedPref(prefs::kSearchSuggestEnabled);
+  EXPECT_TRUE(banner.DetermineVisibility());
+  local_state_->RemoveManagedPref(prefs::kMetricsReportingEnabled);
+  EXPECT_FALSE(banner.DetermineVisibility());
+  local_state_->SetManagedPref(prefs::kHomePageIsNewTabPage,
+                               Value::CreateBooleanValue(true));
+  EXPECT_FALSE(banner.DetermineVisibility());
 }
 
 // Mock class that allows to capture the notification callback.
 class ManagedPrefsBannerBaseMock : public ManagedPrefsBannerBase {
  public:
-  ManagedPrefsBannerBaseMock(PrefService* pref_service, OptionsPage page)
-      : ManagedPrefsBannerBase(pref_service, page) { }
+  ManagedPrefsBannerBaseMock(PrefService* local_state,
+                             PrefService* user_prefs,
+                             OptionsPage page)
+      : ManagedPrefsBannerBase(local_state, user_prefs, page) { }
 
   MOCK_METHOD0(OnUpdateVisibility, void());
 };
 
 TEST_F(ManagedPrefsBannerBaseTest, NotificationTest) {
-  ManagedPrefsBannerBaseMock banner(pref_service_.get(), OPTIONS_PAGE_GENERAL);
+  ManagedPrefsBannerBaseMock banner(local_state_.get(),
+                                    user_prefs_.get(),
+                                    OPTIONS_PAGE_ADVANCED);
+  banner.AddLocalStatePref(prefs::kMetricsReportingEnabled);
   EXPECT_CALL(banner, OnUpdateVisibility()).Times(0);
-  pref_service_->SetBoolean(kDummyPref, true);
+  user_prefs_->SetBoolean(prefs::kHomePageIsNewTabPage, true);
   EXPECT_CALL(banner, OnUpdateVisibility()).Times(1);
-  pref_service_->SetString(prefs::kHomePage, "http://foo.com");
+  user_prefs_->SetManagedPref(prefs::kSearchSuggestEnabled,
+                              Value::CreateBooleanValue(false));
+  EXPECT_CALL(banner, OnUpdateVisibility()).Times(1);
+  local_state_->SetManagedPref(prefs::kMetricsReportingEnabled,
+                               Value::CreateBooleanValue(true));
 }
