524e31ce705d6bf622835af8441edc287b3cc023
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-03 01:51:28 +0000

    Merge 54174 - Browser actions container layout changes for M6 theme.
    In short, this:
    * Eliminates the divider on the right (the new chevron has a built-in divider)
    * Changes the padding between items (see updated comments in header for precise layout details)
    * Updates the imagery for the new theme
    * Makes the chevron respond to theme changes
    
    BUG=50107
    TEST=none
    Review URL: http://codereview.chromium.org/3057016
    
    TBR=pkasting@chromium.org
    Review URL: http://codereview.chromium.org/3015048
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/472/src@54683 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/toolbar_view.cc
+++ chrome/browser/views/toolbar_view.cc
@@ -182,6 +182,7 @@ void ToolbarView::Init(Profile* profile) {
 
   location_bar_->Init();
   show_home_button_.Init(prefs::kShowHomeButton, profile->GetPrefs(), this);
+  browser_actions_->Init();
 
   SetProfile(profile);
 }
