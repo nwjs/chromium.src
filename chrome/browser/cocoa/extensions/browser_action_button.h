35d62b51af7303a04ad28794d05870b526a90875
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-03 23:29:05 +0000

    Merge 54782 - [Mac] Adjust toolbar spacing of browser actions for M6.
    Toolbar.xib change brings browser-action container view out from under
    Omnibox.
    
    Push browser-action sizing code into controller to reduce exposed
    constants.  Make size consistent with other toolbar buttons.  4px
    visual spacing from omnibox to first browser action (grippy in that
    area), between browser actions, and last action to wrench menu.
    
    browser_actions_overflow_Template.pdf resource for browser-action
    chevron.  Chevron same height as browser actions.  Get rid of divider
    between browser actions and wrench (future CL will integrate it into
    chevron button).  Chevron 4px from Omnibox or 2px from last browser
    action, then 4px to wrench menu.  Chevron button has hover like other
    buttons.
    
    BUG=50575
    TEST=Browser-actions area still works right WRT spacing and rearranging and everything else.
    
    Review URL: http://codereview.chromium.org/3012044
    
    TBR=shess@chromium.org
    Review URL: http://codereview.chromium.org/3076027
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/472/src@54840 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/extensions/browser_action_button.h
+++ chrome/browser/cocoa/extensions/browser_action_button.h
@@ -25,8 +25,6 @@ extern NSString* const kBrowserActionButtonDraggingNotification;
 // Fired when the user drops the button.
 extern NSString* const kBrowserActionButtonDragEndNotification;
 
-extern const CGFloat kBrowserActionWidth;
-
 @interface BrowserActionButton : NSButton {
  @private
   // Bridge to proxy Chrome notifications to the Obj-C class as well as load the
@@ -57,9 +55,10 @@ extern const CGFloat kBrowserActionWidth;
   BOOL dragCouldStart_;
 }
 
-- (id)initWithExtension:(Extension*)extension
-                profile:(Profile*)profile
-                  tabId:(int)tabId;
+- (id)initWithFrame:(NSRect)frame
+          extension:(Extension*)extension
+            profile:(Profile*)profile
+              tabId:(int)tabId;
 
 - (void)setFrame:(NSRect)frameRect animate:(BOOL)animate;
 
