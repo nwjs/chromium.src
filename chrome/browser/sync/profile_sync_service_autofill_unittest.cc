ace8eeed961acf3de0299275199238b40d179d2e
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-11 21:32:36 +0000

    Merge 55644 - sync: Fix backend configuration to support sync db refresh. There were conflicts in sync_backend_host due to use_ssl_tcp parameter.
    Removes the has_new bit in favor of checking if initial_sync_ended for requested
    types, and recreates the sync db on corruption (as we used to).  These together
    mean we refresh the sync db on corruption.  Also added some defensive dchecks.
    
    To test this, I ripped out the test-only #ifdefs in SyncBackendHost so the tests
    use the real Initialize code path, and added a toolbox of stuff to control when
    we set initial sync ended bits during testing.
    
    BUG=50965
    TEST=DirectoryBackingStoreTest, ProfileSyncService* tests.
    
    Review URL: http://codereview.chromium.org/3099001
    
    TBR=tim@chromium.org
    
    Please review this at http://codereview.chromium.org/3141008/show
    
    Review URL: http://codereview.chromium.org/3109007
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/472/src@55788 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service_autofill_unittest.cc
+++ chrome/browser/sync/profile_sync_service_autofill_unittest.cc
@@ -42,6 +42,7 @@ using base::WaitableEvent;
 using browser_sync::AutofillChangeProcessor;
 using browser_sync::AutofillDataTypeController;
 using browser_sync::AutofillModelAssociator;
+using browser_sync::SyncBackendHostForProfileSyncTest;
 using syncable::WriteTransaction;
 using testing::_;
 using testing::DoAll;
@@ -144,6 +145,9 @@ class ProfileSyncServiceAutofillTest : public AbstractProfileSyncServiceTest {
           new AutofillDataTypeController(&factory_,
                                          &profile_,
                                          service_.get());
+
+     SyncBackendHostForProfileSyncTest::
+         SetDefaultExpectationsForWorkerCreation(&profile_);
 
       EXPECT_CALL(factory_, CreateAutofillSyncComponents(_, _, _, _)).
           WillOnce(MakeAutofillSyncComponents(service_.get(),
