4256fc4b5fe7790cece41b18a759e9b84a0d50a0
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-27 03:48:58 +0000

    Merge 57620 - Add --disable-translate to help Selenium/WebDriver dudes make Chrome more predictable for automated testing.
    BUG=none
    TEST=Launch Chrome.  Go to http://www.diplomatie.gouv.fr/fr/
    See translate info bar?  Good.
    Now launch Chrome with --disable-translate and go to that page again.
    Make sure translate info bar never shows up.
    
    Review URL: http://codereview.chromium.org/3117044
    
    TBR=jrg@chromium.org
    Review URL: http://codereview.chromium.org/3241001
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/472/src@57634 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -83,6 +83,7 @@ extern const char kDisableSyncPreferences[];
 extern const char kDisableSyncThemes[];
 extern const char kDisableSyncTypedUrls[];
 extern const char kDisableTabCloseableStateWatcher[];
+extern const char kDisableTranslate[];
 extern const char kDisableWebResources[];
 extern const char kDisableWebSecurity[];
 extern const char kDisableWebSockets[];
