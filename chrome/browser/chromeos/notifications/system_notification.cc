068498ad250e086b3fdf84f9472873d074ca8d21
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-15 21:44:51 +0000

    Merge 62666 - Do not call SystemNotification::Hide() in SystemNotification dtor. The bug report feedback notification is a Singleton. When it gets destroyed in atExit(), it calls Hide() which touches a non-existant global balloon collection object. We avoid this by requiring that Hide() be explicitly called (which it is everywhere except in bug_report_util.cc). BUG=6895 TEST=none
    Review URL: http://codereview.chromium.org/3803005
    
    TBR=achuith@google.com
    Review URL: http://codereview.chromium.org/3825007
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/552/src@62796 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/notifications/system_notification.cc
+++ chrome/browser/chromeos/notifications/system_notification.cc
@@ -29,7 +29,6 @@ SystemNotification::SystemNotification(Profile* profile, std::string id,
 }
 
 SystemNotification::~SystemNotification() {
-  Hide();
 }
 
 void SystemNotification::Show(const string16& message,
