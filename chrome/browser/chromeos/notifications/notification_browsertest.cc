20515e5df48594e98af8547a8742b6719ca023d5
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-19 21:04:26 +0000

    Merge 62592 - Use DesktopNotificationService::CreateDataUrl for notification without link.  I also changed the test to make the cause of failure clear.
    TBR=chocobo@chromium.org
    BUG=59195
    TEST=TestActivateDeactivate will pass
    
    Review URL: http://codereview.chromium.org/3796005
    
    TBR=oshima@chromium.org
    Review URL: http://codereview.chromium.org/3914001
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/552/src@63105 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/notifications/notification_browsertest.cc
+++ chrome/browser/chromeos/notifications/notification_browsertest.cc
@@ -522,6 +522,7 @@ IN_PROC_BROWSER_TEST_F(NotificationTest, TestActivateDeactivate) {
   // Wait until all renderers get size.
   WaitForResize(view1);
   WaitForResize(view2);
+  EXPECT_LT(view2->size().height(), 50) << "view size is bigger than expected";
 
   panel->OnMouseMotion(gfx::Point(10, 50));
   EXPECT_TRUE(tester->IsActive(view1));
