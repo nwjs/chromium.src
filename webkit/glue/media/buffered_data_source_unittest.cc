a859991203b9dec915ff24595c06bd1adbfbc4ab
vrk@google.com <vrk@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-26 01:34:49 +0000

    Merge 63817 - Fixes ReadInternal() memory leak from BufferedDataSource tests
    Makes Read() calls from the loader class automatically delete the given
    ReadCallback by default, so as to prevent memory leaks when a test does
    not explicitly delete the ReadCallback. Also deletes valgrind and heapcheck
    suppressions associated with this fix.
    
    BUG=60381
    TEST=test_shell_tests
    
    Review URL: http://codereview.chromium.org/4116001
    
    TBR=vrk@google.com
    Review URL: http://codereview.chromium.org/4157001
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/552/src@63841 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/media/buffered_data_source_unittest.cc
+++ webkit/glue/media/buffered_data_source_unittest.cc
@@ -630,6 +630,11 @@ class BufferedDataSourceTest : public testing::Test {
                            &BufferedDataSourceTest::InvokeStartCallback)));
     }
 
+    // Attach a static function that deletes the memory referred by the
+    // "callback" parameter.
+    ON_CALL(*loader_, Read(_, _, _ , _))
+        .WillByDefault(DeleteArg<3>());
+
     StrictMock<media::MockFilterCallback> callback;
     EXPECT_CALL(*loader_, instance_size())
         .WillRepeatedly(Return(instance_size));
