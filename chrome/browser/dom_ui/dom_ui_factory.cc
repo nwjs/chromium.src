a2a1b32f4203867e97503d92215c83edee00afb7
johnnyg@chromium.org <johnnyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-27 20:32:48 +0000

    Fix branch by adding a dependency from an unmerged change.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/4118006
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/552/src@64137 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/dom_ui_factory.cc
+++ chrome/browser/dom_ui/dom_ui_factory.cc
@@ -9,6 +9,7 @@
 #include "chrome/browser/browser_thread.h"
 #include "chrome/browser/dom_ui/bookmarks_ui.h"
 #include "chrome/browser/dom_ui/bug_report_ui.h"
+#include "chrome/browser/dom_ui/constrained_html_ui.h"
 #include "chrome/browser/dom_ui/downloads_ui.h"
 #include "chrome/browser/dom_ui/devtools_ui.h"
 #include "chrome/browser/dom_ui/history_ui.h"
