943d768f3529566242c1215d152cfc2aec9e788b
chocobo@chromium.org <chocobo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-17 01:30:54 +0000

    Merge 66356 - Don't allow connection to networks that are not connected. For example, if Google-A is not configure, it will be disabled in the network menu and not selectable in the options pages. And from oobe/login, Google-A will always be disabled b/c the certificates will not be there until the user logs in. BUG=chromium-os:8293 TEST=Manual. From oobe, make sure google-a is disabled. Before setting up google-a, make sure google-a is disabled in network menu and options pages. Then set up google-a and see that it's now enabled in both network menu and options page. Log out and check that google-a is disabled. Review URL: http://codereview.chromium.org/4976007
    TBR=chocobo@chromium.org
    Review URL: http://codereview.chromium.org/5127001
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/552d/src@66357 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/dom_ui/internet_options_handler.cc
+++ chrome/browser/chromeos/dom_ui/internet_options_handler.cc
@@ -816,13 +816,15 @@ void InternetOptionsHandler::RefreshCellularPlanCallback(
 
 ListValue* InternetOptionsHandler::GetNetwork(const std::string& service_path,
     const SkBitmap& icon, const std::string& name, bool connecting,
-    bool connected, chromeos::ConnectionType connection_type, bool remembered,
-    chromeos::ActivationState activation_state, bool restricted_ip) {
-
+    bool connected, bool connectable, chromeos::ConnectionType connection_type,
+    bool remembered, chromeos::ActivationState activation_state,
+    bool restricted_ip) {
   ListValue* network = new ListValue();
 
   int connection_state = IDS_STATUSBAR_NETWORK_DEVICE_DISCONNECTED;
-  if (connecting)
+  if (!connectable)
+    connection_state = IDS_STATUSBAR_NETWORK_DEVICE_NOT_CONFIGURED;
+  else if (connecting)
     connection_state = IDS_STATUSBAR_NETWORK_DEVICE_CONNECTING;
   else if (connected)
     connection_state = IDS_STATUSBAR_NETWORK_DEVICE_CONNECTED;
@@ -859,6 +861,8 @@ ListValue* InternetOptionsHandler::GetNetwork(const std::string& service_path,
                     static_cast<int>(activation_state)));
   // restricted
   network->Append(Value::CreateBooleanValue(restricted_ip));
+  // connectable
+  network->Append(Value::CreateBooleanValue(connectable));
   return network;
 }
 
@@ -885,6 +889,7 @@ ListValue* InternetOptionsHandler::GetWiredList() {
           l10n_util::GetStringUTF8(IDS_STATUSBAR_NETWORK_DEVICE_ETHERNET),
           ethernet_network->connecting(),
           ethernet_network->connected(),
+          ethernet_network->connectable(),
           chromeos::TYPE_ETHERNET,
           false,
           chromeos::ACTIVATION_STATE_UNKNOWN,
@@ -915,6 +920,7 @@ ListValue* InternetOptionsHandler::GetWirelessList() {
         (*it)->name(),
         (*it)->connecting(),
         (*it)->connected(),
+        (*it)->connectable(),
         chromeos::TYPE_WIFI,
         false,
         chromeos::ACTIVATION_STATE_UNKNOWN,
@@ -935,6 +941,7 @@ ListValue* InternetOptionsHandler::GetWirelessList() {
         (*it)->name(),
         (*it)->connecting(),
         (*it)->connected(),
+        (*it)->connectable(),
         chromeos::TYPE_CELLULAR,
         false,
         (*it)->activation_state(),
@@ -949,6 +956,7 @@ ListValue* InternetOptionsHandler::GetWirelessList() {
         l10n_util::GetStringUTF8(IDS_OPTIONS_SETTINGS_OTHER_NETWORKS),
         false,
         false,
+        true,
         chromeos::TYPE_WIFI,
         false,
         chromeos::ACTIVATION_STATE_UNKNOWN,
@@ -979,6 +987,7 @@ ListValue* InternetOptionsHandler::GetRememberedList() {
         (*it)->name(),
         (*it)->connecting(),
         (*it)->connected(),
+        true,
         chromeos::TYPE_WIFI,
         true,
         chromeos::ACTIVATION_STATE_UNKNOWN,
