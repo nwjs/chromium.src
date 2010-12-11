62de808235ce6e0c2c0360a88e824c417d38fc03
maf@chromium.org <maf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-11 00:01:45 +0000

    Merge 68890 - Nuke the about:flag for Mac Instant now that there is a real pref. Fixes bug where Instant is on even when checkbox in prefs is cleared. BUG=66089 Review URL: http://codereview.chromium.org/5745004
    TBR=maf@chromium.org
    Review URL: http://codereview.chromium.org/5800001
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/597/src@68913 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -169,13 +169,6 @@ const Experiment kExperiments[] = {
     switches::kEnableCrxlessWebApps
   },
   {
-    "match-preview",  // FLAGS:RECORD_UMA
-    IDS_FLAGS_PREDICTIVE_INSTANT_NAME,
-    IDS_FLAGS_PREDICTIVE_INSTANT_DESCRIPTION,
-    kOsMac,
-    switches::kEnablePredictiveInstant
-  },
-  {
     "gpu-compositing",
      IDS_FLAGS_ACCELERATED_COMPOSITING_NAME,
      IDS_FLAGS_ACCELERATED_COMPOSITING_DESCRIPTION,
