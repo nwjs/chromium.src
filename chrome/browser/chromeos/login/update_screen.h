60ab4d1df184f0dc9ac9654d3564037e4a4a826a
oritm@chromium.org <oritm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-18 00:31:47 +0000

    Merge 68995 - [cros] Show 'Checking for updates' state all that time during update check.
    BUG=chromium-os:10163
    TEST=Manual.
    
    Review URL: http://codereview.chromium.org/5760003
    
    TBR=nkostylev@chromium.org
    Review URL: http://codereview.chromium.org/6025001
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/597/src@69594 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/update_screen.h
+++ chrome/browser/chromeos/login/update_screen.h
@@ -38,10 +38,6 @@ class UpdateScreen: public DefaultViewScreen<chromeos::UpdateView>,
   // Reports update results to the ScreenObserver.
   virtual void ExitUpdate();
 
-  // Maximal curtain time get/set, in seconds.
-  int maximal_curtain_time() const { return maximal_curtain_time_; }
-  void SetMaximalCurtainTime(int seconds);
-
   // Reboot check delay get/set, in seconds.
   int reboot_check_delay() const { return reboot_check_delay_; }
   void SetRebootCheckDelay(int seconds);
@@ -56,11 +52,7 @@ class UpdateScreen: public DefaultViewScreen<chromeos::UpdateView>,
 
  private:
   // Timer notification handlers.
-  void OnMaximalCurtainTimeElapsed();
   void OnWaitForRebootTimeElapsed();
-
-  // Timer for the interval while curtain view is shown.
-  base::OneShotTimer<UpdateScreen> maximal_curtain_time_timer_;
 
   // Timer for the interval to wait for the reboot.
   // If reboot didn't happen - ask user to reboot manually.
@@ -68,10 +60,6 @@ class UpdateScreen: public DefaultViewScreen<chromeos::UpdateView>,
 
   // True if in the process of checking for update.
   bool checking_for_update_;
-
-  // Maximal time (secs) the curtain view is shown. Updating progress is shown
-  // to the user when this time elapsed.
-  int maximal_curtain_time_;
 
   // Time in seconds after which we decide that the device has not rebooted
   // automatically. If reboot didn't happen during this interval, ask user to
