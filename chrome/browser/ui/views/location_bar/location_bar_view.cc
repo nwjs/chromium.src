481d223275152e40408ca325adb1753005b47267
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-18 00:49:28 +0000

    Merge 69572 - Fixes bug where tab-to-search wouldn't work with instant enabled.
    TEST=see bug
    BUG=67258
    
    Review URL: http://codereview.chromium.org/6020001
    
    TBR=sky@chromium.org
    Review URL: http://codereview.chromium.org/5989003
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/597/src@69610 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/location_bar/location_bar_view.cc
+++ chrome/browser/ui/views/location_bar/location_bar_view.cc
@@ -1050,16 +1050,16 @@ bool LocationBarView::SkipDefaultKeyEventProcessing(const views::KeyEvent& e) {
       // Return true so that the edit sees the tab and commits the suggestion.
       return true;
     }
+    if (keyword_hint_view_->IsVisible() && !e.IsShiftDown()) {
+      // Return true so the edit gets the tab event and enters keyword mode.
+      return true;
+    }
     InstantController* instant = delegate_->GetInstant();
     if (instant && instant->IsCurrent()) {
       // Tab while showing instant commits instant immediately.
       instant->CommitCurrentPreview(INSTANT_COMMIT_PRESSED_ENTER);
       // Return true so that focus traversal isn't attempted. The edit ends
       // up doing nothing in this case.
-      return true;
-    }
-    if (keyword_hint_view_->IsVisible() && !e.IsShiftDown()) {
-      // Return true so the edit gets the tab event and enters keyword mode.
       return true;
     }
   }
