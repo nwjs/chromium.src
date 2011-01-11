74bd5a0bf2355f7ec7f1f5dd3e5be661e1263673
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-11 05:01:35 +0000

    Merge 69758 - Do not show apps section until default apps are installed. The referenced bugs were side-effects of this underlying issue.
    BUG=67073,67075
    TEST=Run chrome on new profile. Apps section should not show up
    initially. Go to chrome://extensions/ -> developer mode, and
    click 'update now'. Apps should show up along with promo.
    Uninstall all apps. Apps section should keep showing up.
    
    Review URL: http://codereview.chromium.org/5958002
    
    TBR=aa@chromium.org
    Review URL: http://codereview.chromium.org/6183005
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/597/src@71006 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/app_launcher_handler.cc
+++ chrome/browser/dom_ui/app_launcher_handler.cc
@@ -186,6 +186,16 @@ void AppLauncherHandler::FillAppDictionary(DictionaryValue* dictionary) {
   dictionary->SetBoolean("disableAppWindowLaunch", true);
   dictionary->SetBoolean("disableCreateAppShortcut", true);
 #endif
+
+  // We always show the launcher on Chrome OS. On Desktop Chrome, only show it
+  // if we've installed our default apps.
+#if defined(OS_CHROMEOS)
+  dictionary->SetBoolean("showLauncher", true);
+#else
+  dictionary->SetBoolean(
+      "showLauncher",
+      extensions_service_->default_apps()->GetDefaultAppsInstalled());
+#endif
 }
 
 void AppLauncherHandler::HandleGetApps(const ListValue* args) {
