bloomberg/chromium.bb

commit 39763829f16d0ccbf41037a8604f7b4e51a93bb3

Reland r73760: Move most of chrome-extension:// request checks into renderer. One cannot be moved because we don't have the bit of state we need in the renderer. This should have fixed the bug 57263, but it doesn't. So maybe something else is going on?
BUG=57263
TBR=mpcomplete@chromium.org

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73784 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 78d5cfe21612aa1382c983e8b6467cd7bc54c44c

chrome/browser/extensions/extension_resource_request_policy_apitest.cc
@@ -1,0 +1,95 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/logging.h"
+#include "chrome/browser/extensions/extension_apitest.h"
+#include "chrome/browser/tab_contents/tab_contents.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/test/ui_test_utils.h"
+#include "googleurl/src/gurl.h"
+#include "net/base/mock_host_resolver.h"
+
+class ExtensionResourceRequestPolicyTest : public ExtensionApiTest {
+};
+
+// Note, this mostly tests the logic of chrome/renderer/extensions/
+// extension_resource_request_policy.*, but we have it as a browser test so that
+// can make sure it works end-to-end.
+IN_PROC_BROWSER_TEST_F(ExtensionResourceRequestPolicyTest, OriginPrivileges) {
+  host_resolver()->AddRule("*", "127.0.0.1");
+  ASSERT_TRUE(test_server()->Start());
+  ASSERT_TRUE(LoadExtension(test_data_dir_
+      .AppendASCII("extension_resource_request_policy")
+      .AppendASCII("extension")));
+
+  GURL web_resource(
+      test_server()->GetURL(
+          "files/extensions/api_test/extension_resource_request_policy/"
+          "index.html"));
+
+  std::string host_a("a.com");
+  GURL::Replacements make_host_a_com;
+  make_host_a_com.SetHostStr(host_a);
+
+  std::string host_b("b.com");
+  GURL::Replacements make_host_b_com;
+  make_host_b_com.SetHostStr(host_b);
+
+  // A web host that has permission.
+  ui_test_utils::NavigateToURL(
+      browser(), web_resource.ReplaceComponents(make_host_a_com));
+  std::string result;
+  ASSERT_TRUE(ui_test_utils::ExecuteJavaScriptAndExtractString(
+    browser()->GetSelectedTabContents()->render_view_host(), L"",
+      L"window.domAutomationController.send(document.title)",
+    &result));
+  EXPECT_EQ(result, "Loaded");
+
+  // A web host that does not have permission.
+  ui_test_utils::NavigateToURL(
+      browser(), web_resource.ReplaceComponents(make_host_b_com));
+  ASSERT_TRUE(ui_test_utils::ExecuteJavaScriptAndExtractString(
+      browser()->GetSelectedTabContents()->render_view_host(), L"",
+      L"window.domAutomationController.send(document.title)",
+      &result));
+  EXPECT_EQ(result, "Image failed to load");
+
+  // A data URL. Data URLs should always be able to load chrome-extension://
+  // resources.
+  std::string file_source;
+  ASSERT_TRUE(file_util::ReadFileToString(
+      test_data_dir_.AppendASCII("extension_resource_request_policy")
+                    .AppendASCII("index.html"), &file_source));
+  ui_test_utils::NavigateToURL(browser(),
+      GURL(std::string("data:text/html;charset=utf-8,") + file_source));
+  ASSERT_TRUE(ui_test_utils::ExecuteJavaScriptAndExtractString(
+      browser()->GetSelectedTabContents()->render_view_host(), L"",
+      L"window.domAutomationController.send(document.title)",
+      &result));
+  EXPECT_EQ(result, "Loaded");
+
+  // A different extension. Extensions should always be able to load each
+  // other's resources.
+  ASSERT_TRUE(LoadExtension(test_data_dir_
+      .AppendASCII("extension_resource_request_policy")
+      .AppendASCII("extension2")));
+  ui_test_utils::NavigateToURL(
+      browser(),
+      GURL("chrome-extension://pbkkcbgdkliohhfaeefcijaghglkahja/index.html"));
+  ASSERT_TRUE(ui_test_utils::ExecuteJavaScriptAndExtractString(
+      browser()->GetSelectedTabContents()->render_view_host(), L"",
+      L"window.domAutomationController.send(document.title)",
+      &result));
+  EXPECT_EQ(result, "Loaded");
+}
+
+IN_PROC_BROWSER_TEST_F(ExtensionResourceRequestPolicyTest, Audio) {
+  EXPECT_TRUE(RunExtensionSubtest("extension_resource_request_policy/media",
+                                  "audio.html"));
+}
+
+IN_PROC_BROWSER_TEST_F(ExtensionResourceRequestPolicyTest, Video) {
+  EXPECT_TRUE(RunExtensionSubtest("extension_resource_request_policy/media",
+                                  "video.html"));
+}

chrome/renderer/extensions/extension_resource_request_policy.cc
@@ -1,0 +1,63 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/renderer/extensions/extension_resource_request_policy.h"
+
+#include "base/logging.h"
+#include "chrome/common/url_constants.h"
+#include "chrome/common/extensions/extension.h"
+#include "chrome/common/extensions/extension_set.h"
+#include "googleurl/src/gurl.h"
+
+// static
+bool ExtensionResourceRequestPolicy::CanRequestResource(
+    const GURL& resource_url,
+    const GURL& frame_url,
+    const ExtensionSet* loaded_extensions) {
+  CHECK(resource_url.SchemeIs(chrome::kExtensionScheme));
+
+  // chrome:// URLs are always allowed to load chrome-extension:// resources.
+  // The app launcher in the NTP uses this feature, as does dev tools.
+  if (frame_url.SchemeIs(chrome::kChromeDevToolsScheme) ||
+      frame_url.SchemeIs(chrome::kChromeUIScheme))
+    return true;
+
+  // Disallow loading of packaged resources for hosted apps. We don't allow
+  // hybrid hosted/packaged apps. The one exception is access to icons, since
+  // some extensions want to be able to do things like create their own
+  // launchers.
+  const Extension* extension = loaded_extensions->GetByURL(resource_url);
+  if (extension && extension->is_hosted_app() &&
+      !extension->icons().ContainsPath(resource_url.path())) {
+    LOG(ERROR) << "Denying load of " << resource_url.spec() << " from "
+               << "hosted app.";
+    return false;
+  }
+
+  // Otherwise, pages are allowed to load resources from extensions if the
+  // extension has host permissions to (and therefore could be running script
+  // in, which might need access to the extension resources).
+  //
+  // Exceptions are:
+  // - empty origin (needed for some edge cases when we have empty origins)
+  // - chrome-extension:// (for legacy reasons -- some extensions interop)
+  // - data: (basic HTML notifications use data URLs internally)
+  if (frame_url.is_empty() ||
+      frame_url.SchemeIs(chrome::kExtensionScheme) |
+      frame_url.SchemeIs(chrome::kDataScheme)) {
+    return true;
+  } else {
+    if (extension->GetEffectiveHostPermissions().ContainsURL(frame_url)) {
+      return true;
+    } else {
+      LOG(ERROR) << "Denying load of " << resource_url.spec() << " from "
+                 << frame_url.spec() << " because the extension does not have "
+                 << "access to the requesting page.";
+      return false;
+    }
+  }
+}
+
+ExtensionResourceRequestPolicy::ExtensionResourceRequestPolicy() {
+}

chrome/test/data/extensions/api_test/extension_resource_request_policy/media/audio.html
@@ -1,0 +1,1 @@
+<audio src="bear.wav" oncanplay="chrome.test.notifyPass()">

nwjs/chromium.src

commit 32d36df4fa483c36adba87da55613027216642e1

Merge 73956 - Fix regression caused by r73784, add regression test and do some minor test organization.
BUG=57263,71955
TEST=Install https://chrome.google.com/webstore/detail/odmpalfplhaahlgnkkonchfhpegdcgjm and a few apps from the web store. Open popup. Popup should contain icons for each app and should not crash.

Review URL: http://codereview.chromium.org/6410088

TBR=aa@chromium.org
Review URL: http://codereview.chromium.org/6246163

git-svn-id: svn://svn.chromium.org/chrome/branches/648/src@74064 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/extensions/extension_resource_request_policy_apitest.cc
@@ -84,12 +84,25 @@ IN_PROC_BROWSER_TEST_F(ExtensionResourceRequestPolicyTest, OriginPrivileges) {
   EXPECT_EQ(result, "Loaded");
 }
 
+IN_PROC_BROWSER_TEST_F(ExtensionResourceRequestPolicyTest,
+                       ExtensionCanLoadHostedAppIcons) {
+  ASSERT_TRUE(LoadExtension(test_data_dir_
+      .AppendASCII("extension_resource_request_policy")
+      .AppendASCII("extension")));
+
+  ASSERT_TRUE(RunExtensionSubtest(
+      "extension_resource_request_policy/extension2/",
+      "can_load_icons_from_hosted_apps.html"));
+}
+
 IN_PROC_BROWSER_TEST_F(ExtensionResourceRequestPolicyTest, Audio) {
-  EXPECT_TRUE(RunExtensionSubtest("extension_resource_request_policy/media",
-                                  "audio.html"));
+  EXPECT_TRUE(RunExtensionSubtest(
+      "extension_resource_request_policy/extension2",
+      "audio.html"));
 }
 
 IN_PROC_BROWSER_TEST_F(ExtensionResourceRequestPolicyTest, Video) {
-  EXPECT_TRUE(RunExtensionSubtest("extension_resource_request_policy/media",
-                                  "video.html"));
+  EXPECT_TRUE(RunExtensionSubtest(
+      "extension_resource_request_policy/extension2",
+      "video.html"));
 }

chrome/common/extensions/extension_icon_set.cc
@@ -15,7 +15,7 @@ void ExtensionIconSet::Clear() {
 }
 
 void ExtensionIconSet::Add(int size, const std::string& path) {
-  DCHECK(path.size() > 0 && path[0] != '/');
+  CHECK(path.size() > 0 && path[0] != '/');
   map_[size] = path;
 }
 
@@ -50,10 +50,14 @@ std::string ExtensionIconSet::Get(int size, MatchType match_type) const {
 }
 
 bool ExtensionIconSet::ContainsPath(const std::string& path) const {
-  DCHECK(path.size() > 0 && path[0] != '/');
+  if (path.empty())
+    return false;
+
+  CHECK(path[0] != '/') <<
+      "ExtensionIconSet stores icon paths without leading slash.";
+
   for (IconMap::const_iterator iter = map_.begin(); iter != map_.end();
        ++iter) {
-    LOG(ERROR) << iter->second << " , " << path;
     if (iter->second == path)
       return true;
   }

chrome/renderer/extensions/extension_resource_request_policy.cc
@@ -28,8 +28,10 @@ bool ExtensionResourceRequestPolicy::CanRequestResource(
   // some extensions want to be able to do things like create their own
   // launchers.
   const Extension* extension = loaded_extensions->GetByURL(resource_url);
+  std::string resource_root_relative_path =
+      resource_url.path().empty() ? "" : resource_url.path().substr(1);
   if (extension && extension->is_hosted_app() &&
-      !extension->icons().ContainsPath(resource_url.path())) {
+      !extension->icons().ContainsPath(resource_root_relative_path)) {
     LOG(ERROR) << "Denying load of " << resource_url.spec() << " from "
                << "hosted app.";
     return false;

chrome/common/extensions/extension_icon_set_unittest.cc
@@ -44,6 +44,7 @@ TEST(ExtensionIconSet, Values) {
   EXPECT_TRUE(icons.ContainsPath("foo"));
   EXPECT_TRUE(icons.ContainsPath("bar"));
   EXPECT_FALSE(icons.ContainsPath("baz"));
+  EXPECT_FALSE(icons.ContainsPath(""));
 
   icons.Clear();
   EXPECT_FALSE(icons.ContainsPath("foo"));

chrome/test/data/extensions/api_test/extension_resource_request_policy/extension2/can_load_icons_from_hosted_apps.html
@@ -1,3 +1,3 @@
-<title>Unmodified</title>
-
-<img onload="document.title='Loaded'" onerror="document.title='Image failed to load'" src="chrome-extension://ggmldgjhdenlnjjjmehkomheglpmijnf/test.png">
+<img src="chrome-extension://fnbdbepgnidhjejikpionpfohdjjogpm/test.png"
+    onload="chrome.test.notifyPass()"
+    onerror="chrome.test.notifyFail()">

chrome/test/data/extensions/api_test/extension_resource_request_policy/extension2/cant_load_packaged_resources_from_hosted_apps.html
@@ -1,3 +1,0 @@
-<title>Unmodified</title>
-
-<img onload="document.title='Loaded'" onerror="document.title='Image failed to load'" src="chrome-extension://ggmldgjhdenlnjjjmehkomheglpmijnf/test2.png">

chrome/test/data/extensions/api_test/extension_resource_request_policy/extension2/extensions_can_load_other_extension_resources.html
@@ -1,3 +1,0 @@
-<title>Unmodified</title>
-
-<img onload="document.title='Loaded'" onerror="document.title='Image failed to load'" src="chrome-extension://fnbdbepgnidhjejikpionpfohdjjogpm/test.png">