07c0bfd72e2c45d90bdd8eeb48acab41850ad8cd
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-10 15:40:32 +0000

    Merge 74428 - Restore old title in WebViewPlugin only when loading the plugin.
    BUG=72437
    TEST=see bug for manual test
    
    Review URL: http://codereview.chromium.org/6476006
    
    TBR=bauerb@chromium.org
    Review URL: http://codereview.chromium.org/6483014
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/648/src@74434 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/blocked_plugin.cc
+++ chrome/renderer/blocked_plugin.cc
@@ -160,6 +160,7 @@ void BlockedPlugin::LoadPlugin() {
   WebPlugin* new_plugin =
       render_view()->CreatePluginNoCheck(frame_, plugin_params_);
   if (new_plugin && new_plugin->initialize(container)) {
+    plugin_->RestoreTitleText();
     container->setPlugin(new_plugin);
     container->invalidate();
     container->reportGeometry();
