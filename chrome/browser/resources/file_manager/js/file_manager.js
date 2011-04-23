4655234f325bb3ff103d117d76451503f07a8af6
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-23 05:12:36 +0000

    Merge 82741 - Added extra data to chrome.fileBrowserPrivate.onDiskChanged even payload that will give us more data about the volume and really tell us what operation happened here.BUG=chromium-os:14368TEST=noneReview URL: http://codereview.chromium.org/6879105 TBR=zelidrag@chromium.org Review URL: http://codereview.chromium.org/6895040
    git-svn-id: svn://svn.chromium.org/chrome/branches/742/src@82783 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -954,6 +954,7 @@ FileManager.prototype = {
   };
 
   FileManager.prototype.onTasksFound_ = function(tasksList) {
+    this.taskButtons_.innerHTML = '';
     for (var i = 0; i < tasksList.length; i++) {
       var task = tasksList[i];
 
@@ -1267,8 +1268,9 @@ FileManager.prototype = {
    *
    * @param {string} path The path that has been mounted or unmounted.
    */
-  FileManager.prototype.onDiskChanged_ = function(path) {
-    this.changeDirectory(path);
+  FileManager.prototype.onDiskChanged_ = function(event) {
+    // TODO(rginda): Please check and wire handling of other data passed here.
+    this.changeDirectory(event.volumeInfo.mountPath);
   };
 
   /**
