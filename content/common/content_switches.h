fdb49052d992013fc4238c9c4cffc06c73c7a6d0
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-05 21:49:34 +0000

    Revert 84299 - Merge 84218 - Mac: Remove switches::kDisableHolePunching
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/6929026
    
    TBR=thakis@chromium.org
    Review URL: http://codereview.chromium.org/6930045
    
    TBR=thakis@chromium.org
    Review URL: http://codereview.chromium.org/6927059
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/742/src@84322 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/content_switches.h
+++ content/common/content_switches.h
@@ -29,6 +29,7 @@ extern const char kDisableGLMultisampling[];
 extern const char kDisableGLSLTranslator[];
 extern const char kDisableGpuSandbox[];
 extern const char kDisableGpuWatchdog[];
+extern const char kDisableHolePunching[];
 extern const char kDisableIndexedDatabase[];
 extern const char kDisableJava[];
 extern const char kDisableJavaScript[];
