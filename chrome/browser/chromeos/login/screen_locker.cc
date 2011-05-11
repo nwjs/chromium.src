a3e3d1f3365ec95b47035b11a8d191606100377a
oshima@google.com <oshima@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-11 01:32:25 +0000

    Merge 84904 - Disable context menu in screen locker Don't show image behind screen locker.
    BUG=chromium-os:14985, chromium-os:13154
    TEST=lock the screen, and alt-click or right click passworld field.
    
    Review URL: http://codereview.chromium.org/6997007
    
    TBR=oshima@google.com
    Review URL: http://codereview.chromium.org/7003017
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/742/src@84905 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/screen_locker.cc
+++ chrome/browser/chromeos/login/screen_locker.cc
@@ -796,12 +796,6 @@ void ScreenLocker::Init() {
                               GTK_WINDOW(lock_window_->GetNativeView()));
   g_object_unref(window_group);
 
-  // Don't let X draw default background, which was causing flash on
-  // resume.
-  gdk_window_set_back_pixmap(lock_window_->GetNativeView()->window,
-                             NULL, false);
-  gdk_window_set_back_pixmap(lock_widget_->GetNativeView()->window,
-                             NULL, false);
   lock_window->set_toplevel_focus_widget(lock_widget_->window_contents());
 
   // Create the SystemKeyEventListener so it can listen for system keyboard
