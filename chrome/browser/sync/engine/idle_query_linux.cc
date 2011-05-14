f5b8cc68f3f96a7402a7b561f10d2a89659adc4e
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-14 00:06:17 +0000

    Merge 85138 - Use GetXDisplay() instead of XOpenDisplay() in IdleQueryLinux.
    We weren't checking that XOpenDisplay was actually succeeding before
    attempting to use the display.
    
    BUG=82308
    TEST=none
    
    Review URL: http://codereview.chromium.org/7015007
    
    TBR=levin@chromium.org
    Review URL: http://codereview.chromium.org/6973065
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/742/src@85353 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/engine/idle_query_linux.cc
+++ chrome/browser/sync/engine/idle_query_linux.cc
@@ -1,10 +1,11 @@
 // Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 #include "chrome/browser/sync/engine/idle_query_linux.h"
 
-#include <X11/Xlib.h>
 #include <X11/extensions/scrnsaver.h>
+#include "ui/base/x/x11_util.h"
 
 namespace browser_sync {
 
@@ -13,8 +14,8 @@ class IdleData {
   IdleData() {
     int event_base;
     int error_base;
-    display = XOpenDisplay(NULL);
-    if (XScreenSaverQueryExtension(display, &event_base, &error_base)) {
+    if (XScreenSaverQueryExtension(ui::GetXDisplay(), &event_base,
+                                   &error_base)) {
       mit_info = XScreenSaverAllocInfo();
     } else {
       mit_info = NULL;
@@ -22,36 +23,28 @@ class IdleData {
   }
 
   ~IdleData() {
-    if (display) {
-      XCloseDisplay(display);
-      display = NULL;
-    }
-    if (mit_info) {
+    if (mit_info)
       XFree(mit_info);
-    }
   }
 
   XScreenSaverInfo *mit_info;
-  Display *display;
 };
 
-IdleQueryLinux::IdleQueryLinux() : idle_data_(new IdleData()) {
-}
+IdleQueryLinux::IdleQueryLinux() : idle_data_(new IdleData()) {}
 
-IdleQueryLinux::~IdleQueryLinux() {
-}
+IdleQueryLinux::~IdleQueryLinux() {}
 
 int IdleQueryLinux::IdleTime() {
-  if (!idle_data_->mit_info || !idle_data_->display) {
+  if (!idle_data_->mit_info)
     return 0;
-  }
 
-  if (XScreenSaverQueryInfo(idle_data_->display,
-                            RootWindow(idle_data_->display, 0),
+  if (XScreenSaverQueryInfo(ui::GetXDisplay(),
+                            RootWindow(ui::GetXDisplay(), 0),
                             idle_data_->mit_info)) {
     return (idle_data_->mit_info->idle) / 1000;
   } else {
     return 0;
   }
 }
+
 }  // namespace browser_sync
