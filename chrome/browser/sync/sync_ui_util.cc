6f6a6ef15d6e158fee0a7fa3f60e6170f384f2f7
lipalani@chromium.org <lipalani@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-19 20:24:33 +0000

    Show error in the bookmarks bar if passphrase is not valid.
    BUG=81492
    TEST=
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=85721
    
    Review URL: http://codereview.chromium.org/7019023
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/742/src@85962 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/sync_ui_util.cc
+++ chrome/browser/sync/sync_ui_util.cc
@@ -240,8 +240,11 @@ MessageType GetStatus(ProfileSyncService* service) {
 }
 
 bool ShouldShowSyncErrorButton(ProfileSyncService* service) {
-  return service && !service->IsManaged() && service->HasSyncSetupCompleted() &&
-      (GetStatus(service) == sync_ui_util::SYNC_ERROR);
+  return service &&
+         ((!service->IsManaged() &&
+           service->HasSyncSetupCompleted()) &&
+         (GetStatus(service) == sync_ui_util::SYNC_ERROR ||
+          service->observed_passphrase_required()));
 }
 
 string16 GetSyncMenuLabel(ProfileSyncService* service) {
