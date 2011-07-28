cdcfa90884feaccb65f135228b6fcbce999ea8a1
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-28 23:10:20 +0000

    Merge 94556 - Fix memory corruption by calling clear_owner() earlier.  The Notify() calls can result in the InfoBarDelegate being deleted, so it's wrong to call clear_owner() after them; call before them instead.
    BUG=89915
    TEST=none
    TBR=erg
    Review URL: http://codereview.chromium.org/7531008
    
    TBR=pkasting@chromium.org
    Review URL: http://codereview.chromium.org/7534003
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/835/src@94574 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/tab_contents/tab_contents_wrapper.cc
+++ chrome/browser/ui/tab_contents/tab_contents_wrapper.cc
@@ -522,6 +522,7 @@ void TabContentsWrapper::ReplaceInfoBar(InfoBarDelegate* old_delegate,
 
   infobars_.insert(infobars_.begin() + i, new_delegate);
 
+  old_delegate->clear_owner();
   InfoBarReplacedDetails replaced_details(old_delegate, new_delegate);
   NotificationService::current()->Notify(
       chrome::NOTIFICATION_TAB_CONTENTS_INFOBAR_REPLACED,
@@ -529,7 +530,6 @@ void TabContentsWrapper::ReplaceInfoBar(InfoBarDelegate* old_delegate,
       Details<InfoBarReplacedDetails>(&replaced_details));
 
   infobars_.erase(infobars_.begin() + i + 1);
-  old_delegate->clear_owner();
 }
 
 InfoBarDelegate* TabContentsWrapper::GetInfoBarDelegateAt(size_t index) {
@@ -673,6 +673,7 @@ void TabContentsWrapper::RemoveInfoBarInternal(InfoBarDelegate* delegate,
   DCHECK_LT(i, infobars_.size());
   InfoBarDelegate* infobar = infobars_[i];
 
+  infobar->clear_owner();
   InfoBarRemovedDetails removed_details(infobar, animate);
   NotificationService::current()->Notify(
       chrome::NOTIFICATION_TAB_CONTENTS_INFOBAR_REMOVED,
@@ -680,7 +681,6 @@ void TabContentsWrapper::RemoveInfoBarInternal(InfoBarDelegate* delegate,
       Details<InfoBarRemovedDetails>(&removed_details));
 
   infobars_.erase(infobars_.begin() + i);
-  delegate->clear_owner();
   // Remove ourselves as an observer if we are tracking no more InfoBars.
   if (infobars_.empty()) {
     registrar_.Remove(this, content::NOTIFICATION_NAV_ENTRY_COMMITTED,
