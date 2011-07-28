5f1b886b356f1cb63fa19923c1e4abcee201f71b
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-28 19:05:40 +0000

    Merge 94369 - GTK: Give up on ever having icons in Unity Global Menus.
    This removes all code I added to the history menu to "conditionally" load
    icons. The only code difference between having the flag off and the previous
    version with no image support at all (that users said was a vast improvement) was declaring things to be
    GtkImageMenuItems vs. GtkMenuItems, which looks like it's enough to stall up dbusmenu.
    
    This reverts commit r93575, which was a rework revert of commit r91615.
    
    BUG=86715
    TEST=none
    
    Review URL: http://codereview.chromium.org/7461122
    
    TBR=erg@chromium.org
    Review URL: http://codereview.chromium.org/7493058
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/835/src@94522 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -340,7 +340,6 @@ extern const char kKioskMode[];
 
 #if defined(TOOLKIT_GTK)
 extern const char kEnableGlobalBookmarkMenu[];
-extern const char kEnableIconsInGlobalHistoryMenu[];
 #endif
 
 #if defined(TOOLKIT_VIEWS)
