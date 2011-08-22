57543f093f34fef7e35342f844214dbf90041b8b
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-22 17:06:40 +0000

    Merge 97223 - [ChromeOS] Pass app locale info to auth extension.
    BUG=chromium-os:19284
    TEST=Verify fix for chromium-os:19284.
    
    
    Review URL: http://codereview.chromium.org/7669027
    
    TBR=xiyuan@chromium.org
    Review URL: http://codereview.chromium.org/7712004
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/835/src@97665 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
@@ -8,6 +8,7 @@
 #include "base/stringprintf.h"
 #include "base/task.h"
 #include "base/values.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/io_thread.h"
 #include "chrome/browser/chromeos/cros/cros_library.h"
 #include "chrome/browser/chromeos/cros/power_library.h"
@@ -277,7 +278,15 @@ void SigninScreenHandler::ShowSigninScreenIfReady() {
 
   DictionaryValue params;
   params.SetString("email", email_);
-  params.SetString("startUrl", kGaiaExtStartPage);
+
+  std::string url = kGaiaExtStartPage;
+  const std::string app_locale = g_browser_process->GetApplicationLocale();
+  if (!app_locale.empty()) {
+    url.append("?hl=");
+    url.append(app_locale);
+  }
+  params.SetString("startUrl", url);
+
   params.SetBoolean("createAccount",
       UserCrosSettingsProvider::cached_allow_new_user());
   params.SetBoolean("guestSignin",
