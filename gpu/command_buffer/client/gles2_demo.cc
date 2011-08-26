3c6e7cf9a6a148da568c8f837bc6c3c1ae1fc53f
vangelis@google.com <vangelis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-26 18:32:25 +0000

    Manually merging trunk revs 95836 and 96904 to 835 branch.
    BUG=92260
    Review URL: http://codereview.chromium.org/7758004
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/835/src@98456 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/client/gles2_demo.cc
+++ gpu/command_buffer/client/gles2_demo.cc
@@ -56,6 +56,7 @@ bool GLES2Demo::Setup(void* hwnd, int32 size) {
   if (!command_buffer->Initialize(size))
     return NULL;
 
+  gpu::gles2::ContextGroup::Ref group(new gpu::gles2::ContextGroup(true));
   GpuScheduler* gpu_scheduler = GpuScheduler::Create(command_buffer.get(),
                                                      NULL,
                                                      NULL);
@@ -92,7 +93,8 @@ bool GLES2Demo::Setup(void* hwnd, int32 size) {
                                               transfer_buffer.size,
                                               transfer_buffer.ptr,
                                               transfer_buffer_id,
-                                              false));
+                                              false,
+                                              true));
 
   GLFromCPPInit();
 
