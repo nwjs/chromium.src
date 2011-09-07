fccff8423b2076a5271d40c4f8c7f3e411f3170b
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-07 01:20:25 +0000

    Merge 99883 - Completely disable about:flags
    This is an attempt to improve stability on the m14 branch. I will revert this CL right after merging it to m14.
    
    BUG=95592
    TEST=Go to about:flags and chrome://flags. Nothing should be there.
    Go to about:version, no --begin-flags and --end-flags should be there.
    
    Review URL: http://codereview.chromium.org/7837006
    
    TBR=thakis@chromium.org
    Review URL: http://codereview.chromium.org/7795042
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/835/src@99886 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_about_handler.cc
+++ chrome/browser/browser_about_handler.cc
@@ -119,7 +119,8 @@ const char* const kChromePaths[] = {
   chrome::kChromeUIDNSHost,
   chrome::kChromeUIDownloadsHost,
   chrome::kChromeUIExtensionsHost,
-  chrome::kChromeUIFlagsHost,
+  // TODO(thakis): Reenable.
+  //chrome::kChromeUIFlagsHost,
   chrome::kChromeUIFlashHost,
   chrome::kChromeUIGpuInternalsHost,
   chrome::kChromeUIHistogramsHost,
