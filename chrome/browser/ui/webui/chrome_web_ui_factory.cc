61e2629dd9882342ed1bcb34f616a6fb675c5d89
laforge@chromium.org <laforge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-10 23:26:09 +0000

    Revert 99886 - Merge 99883 - Completely disable about:flags
    This is an attempt to improve stability on the m14 branch. I will revert this CL right after merging it to m14.
    
    BUG=95592
    TEST=Go to about:flags and chrome://flags. Nothing should be there.
    Go to about:version, no --begin-flags and --end-flags should be there.
    
    Review URL: http://codereview.chromium.org/7837006
    
    TBR=thakis@chromium.org
    Review URL: http://codereview.chromium.org/7795042
    
    TBR=thakis@chromium.org
    Review URL: http://codereview.chromium.org/7869002
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/835/src@100602 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chrome_web_ui_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_factory.cc
@@ -170,9 +170,8 @@ static WebUIFactoryFunction GetWebUIFactoryFunction(Profile* profile,
     return &NewWebUI<HistoryUI>;
   if (url.host() == chrome::kChromeUIHistory2Host)
     return &NewWebUI<HistoryUI2>;
-  // TODO(thakis): Reenable.
-  //if (url.host() == chrome::kChromeUIFlagsHost)
-  //  return &NewWebUI<FlagsUI>;
+  if (url.host() == chrome::kChromeUIFlagsHost)
+    return &NewWebUI<FlagsUI>;
   if (url.host() == chrome::kChromeUIFlashHost)
     return &NewWebUI<FlashUI>;
 #if defined(TOUCH_UI)
@@ -377,9 +376,8 @@ RefCountedMemory* ChromeWebUIFactory::GetFaviconResourceBytes(
   if (page_url.host() == chrome::kChromeUIHistory2Host)
     return HistoryUI2::GetFaviconResourceBytes();
 
-  // TODO(thakis): Reenable.
-  //if (page_url.host() == chrome::kChromeUIFlagsHost)
-  //  return FlagsUI::GetFaviconResourceBytes();
+  if (page_url.host() == chrome::kChromeUIFlagsHost)
+    return FlagsUI::GetFaviconResourceBytes();
 
   if (page_url.host() == chrome::kChromeUISessionsHost)
     return SessionsUI::GetFaviconResourceBytes();
