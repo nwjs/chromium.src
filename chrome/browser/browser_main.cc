5464111269422714c7c32563c6d1ab1e0c619a6c
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-10 22:32:22 +0000

    Merge 100591 - Rearrange and validate clean-shutdown UMA stat
    NOTE: Includes hand-merge of changes in chrome_browser_main.cc
    intot the old browser_main.cc. I did not include the CHECK()
    in order to redcu risk on the branch
    END OF MANUAL NOTE
    
    Don't start monitoring pref setting until after all
    premature-returns in browser main.
    
    Check that whatever path we took, we properly maintained
    the status of the pref flag (to prevent regressions).
    
    r=eroman,rtenneti
    BUG=96124
    Review URL: http://codereview.chromium.org/7780020
    
    TBR=jar@chromium.org
    Review URL: http://codereview.chromium.org/7780024
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/835/src@100597 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_main.cc
+++ chrome/browser/browser_main.cc
@@ -1966,6 +1966,9 @@ int BrowserMain(const MainFunctionParams& parameters) {
     }
   }
 
+  // Start watching for a hang.
+  MetricsService::LogNeedForCleanShutdown();
+
 #if defined(OS_WIN)
   // We check this here because if the profile is OTR (chromeos possibility)
   // it won't still be accessible after browser is destroyed.
