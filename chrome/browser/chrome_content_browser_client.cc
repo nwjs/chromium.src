b25bb233c9b7df6f4c95a1b8d602559dd6c9bcc6
mihaip@chromium.org <mihaip@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-16 18:26:20 +0000

    Merge 101152 - Remove flag guard for inline installs (web store changes have been pushed).
    BUG=96526
    R=aa@chromium.org
    
    Review URL: http://codereview.chromium.org/7892023
    
    TBR=mihaip@chromium.org
    Review URL: http://codereview.chromium.org/7923013
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/874/src@101534 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -316,7 +316,6 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
       switches::kEnableCrxlessWebApps,
       switches::kEnableExperimentalExtensionApis,
       switches::kEnableInBrowserThumbnailing,
-      switches::kEnableInlineWebstoreInstall,
       switches::kEnableIPCFuzzing,
       switches::kEnableNaCl,
       switches::kEnablePrintPreview,
