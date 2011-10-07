6138ae4f1ff3be3d911ad78d11be920ab8129f04
creis@chromium.org <creis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-07 18:23:58 +0000

    Revert 104517 - Merge 104010 - Add a second line of defense for receiving a bad message in the renderer.
    BUG=88949
    TEST=none
    
    Review URL: http://codereview.chromium.org/8142009
    
    TBR=creis@chromium.org
    Review URL: http://codereview.chromium.org/8199014
    
    TBR=creis@chromium.org
    Review URL: http://codereview.chromium.org/8205007
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/874/src@104521 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view.cc
+++ content/renderer/render_view.cc
@@ -630,8 +630,7 @@ bool RenderView::OnMessageReceived(const IPC::Message& message) {
       return true;
 
   bool handled = true;
-  bool msg_is_ok = true;
-  IPC_BEGIN_MESSAGE_MAP_EX(RenderView, message, msg_is_ok)
+  IPC_BEGIN_MESSAGE_MAP(RenderView, message)
     IPC_MESSAGE_HANDLER(ViewMsg_Navigate, OnNavigate)
     IPC_MESSAGE_HANDLER(ViewMsg_Stop, OnStop)
     IPC_MESSAGE_HANDLER(ViewMsg_ReloadFrame, OnReloadFrame)
@@ -741,13 +740,6 @@ bool RenderView::OnMessageReceived(const IPC::Message& message) {
     // Have the super handle all other messages.
     IPC_MESSAGE_UNHANDLED(handled = RenderWidget::OnMessageReceived(message))
   IPC_END_MESSAGE_MAP()
-
-  if (!msg_is_ok) {
-    // The message had a handler, but its deserialization failed.
-    // Kill the renderer to avoid potential spoofing attacks.
-    CHECK(false) << "Unable to deserialize message in RenderView.";
-  }
-
   return handled;
 }
 
