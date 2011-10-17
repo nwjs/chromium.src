8498ea7ea85752cd0b43ccd7e187b3b62620c437
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-17 18:03:10 +0000

    Merge bugfixes for 98611 to M15.
    This CL should incude all the dependencies and tests broken by the original change.
    
    BUG=98433
    Review URL: http://codereview.chromium.org/8316006
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/874/src@105846 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -325,6 +325,7 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
       switches::kExperimentalSpellcheckerFeatures,
       switches::kMemoryProfiling,
       switches::kMessageLoopHistogrammer,
+      switches::kNoRunningInsecureContent,
       switches::kPpapiFlashArgs,
       switches::kPpapiFlashInProcess,
       switches::kPpapiFlashPath,
