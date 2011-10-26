e1003c047c7868ef850d1d50807476d1257995df
rogerta@chromium.org <rogerta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-26 17:39:45 +0000

    Merge 107144 - Remove race condition when installing default apps into a new profile.
    BUG=99547
    TEST=Install chrome and stop it as quickly as possible when the window opens.
    Or create a new profile and close all windows as soon as the new profile's
    window opens.  Do that as often as you like.  Make sure that eventually,
    after leaving the windows open, that all default apps are correctly installed.
    
    Review URL: http://codereview.chromium.org/8245018
    
    TBR=rogerta@chromium.org
    Review URL: http://codereview.chromium.org/8393062
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/912/src@107375 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -88,6 +88,8 @@
 #include "chrome/browser/chromeos/status/input_method_menu.h"
 #include "chrome/browser/chromeos/status/network_menu_button.h"
 #include "chrome/browser/chromeos/user_cros_settings_provider.h"
+#else
+#include "chrome/browser/extensions/default_apps.h"
 #endif
 
 namespace browser {
@@ -187,6 +189,9 @@ void RegisterUserPrefs(PrefService* user_prefs) {
 #endif
   SyncPromoUI::RegisterUserPrefs(user_prefs);
   chrome_browser_net::HttpServerPropertiesManager::RegisterPrefs(user_prefs);
+#if !defined(OS_CHROMEOS)
+  default_apps::RegisterUserPrefs(user_prefs);
+#endif
 }
 
 void MigrateBrowserPrefs(PrefService* user_prefs, PrefService* local_state) {
