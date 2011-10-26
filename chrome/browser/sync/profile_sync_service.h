9f5c35c493f5027c31cd5ca83a75e7ac5df84f24
lipalani@chromium.org <lipalani@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-26 00:59:45 +0000

    Merge 106373 - Prevent crash by posting a task to call unrecoverable error handler. The task is posted on a weak pointer to PSS so as to prevent crashes on shut down.
    BUG=100698
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8345026
    
    TBR=lipalani@chromium.org
    Review URL: http://codereview.chromium.org/8395029
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/912/src@107265 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service.h
+++ chrome/browser/sync/profile_sync_service.h
@@ -105,7 +105,11 @@ struct UserShare;
 class ProfileSyncService : public browser_sync::SyncFrontend,
                            public browser_sync::SyncPrefObserver,
                            public browser_sync::UnrecoverableErrorHandler,
-                           public NotificationObserver {
+                           public NotificationObserver,
+                           // TODO(lipalani): crbug.com/100829. Instead of
+                           // doing this vend weak pointers from a factory.
+                           public base::SupportsWeakPtr<ProfileSyncService> {
+
  public:
   typedef ProfileSyncServiceObserver Observer;
   typedef browser_sync::SyncBackendHost::Status Status;
