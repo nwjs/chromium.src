909037279adad29041e08282984fcbb8cb0cb758
yusukes@google.com <yusukes@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-09 06:08:04 +0000

    Merge 108848 - Set NumLock on by default when chrome starts.
    - This CL depends on http://codereview.chromium.org/8356040/.
    - Committed with TBR since the CL is already reviewed by djkurtz as a part of 8356040 (see Patch Set #5 in the CL).
    
    BUG=crosbug.com/p/5795
    BUG=crosbug.com/p/6245
    TEST=On Mario, 1) power off, plug an external USB keyboard, power on, confirm that NumLock LED on the keyboard is lit on login screen, confirm that the NumPad on the USB keyboard works as intended (i.e. can enter numbers). 2) power off, unplug all external USB keyboards, power on, on login screen plug an external USB keyboard, confirm that NumLock LED on the keyboard is lit, confirm that the NumPad on the USB keyboard works as intended. 3) then, unplug and plug the keyboard, confirm LED and NumPad again (i.e. LED=on, can enter numbers), press Num Lock key on the keyboard, confirm that the LED turns off and NumPad (2/4/6/8) is now works as cursor key. 4) then, unplug and plug the keyboard, check LED and NumPad again (i.e. LED is still off, can move cursor). 5) Do the same tests with S3/Resume instead of unplug/plug a keyboard. 6) Do the same with a USB Hub, just in case.
    TEST=Do the same tests on Alex. Note that Alex turns off USB power supply on suspend.
    TBR=djkurtz
    
    Review URL: http://codereview.chromium.org/8491005
    
    TBR=yusukes@google.com
    Review URL: http://codereview.chromium.org/8505003
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/912/src@109192 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -13,7 +13,10 @@
 #include "chrome/browser/chromeos/brightness_observer.h"
 #include "chrome/browser/chromeos/cros/cros_library.h"
 #include "chrome/browser/chromeos/dbus/dbus_thread_manager.h"
+#include "chrome/browser/chromeos/input_method/input_method_manager.h"
+#include "chrome/browser/chromeos/input_method/xkeyboard.h"
 #include "chrome/browser/chromeos/net/cros_network_change_notifier_factory.h"
+#include "chrome/browser/chromeos/system/runtime_environment.h"
 #include "chrome/browser/chromeos/system/statistics_provider.h"
 #include "chrome/browser/defaults.h"
 #include "chrome/common/chrome_switches.h"
@@ -139,4 +142,11 @@ void ChromeBrowserMainPartsChromeos::PostMainMessageLoopStart() {
   brightness_observer_.reset(new chromeos::BrightnessObserver());
   chromeos::DBusThreadManager::Get()->power_manager_client()->AddObserver(
       brightness_observer_.get());
+
+  // For http://crosbug.com/p/5795 and http://crosbug.com/p/6245.
+  // Enable Num Lock on X start up.
+  if (chromeos::system::runtime_environment::IsRunningOnChromeOS()) {
+    chromeos::input_method::InputMethodManager::GetInstance()->GetXKeyboard()->
+        SetNumLockEnabled(true);
+  }
 }
