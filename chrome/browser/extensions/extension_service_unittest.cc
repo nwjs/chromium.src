50b0e410f69419f4c8ae052b71e9874007320135
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-15 10:09:13 +0000

    Merge 109011 - Fix a crash in PageActionImageView when extensions are reloaded due to incognito settings changing.
    BUG=99736
    TEST=Precise reproduction steps can be found in the bug description (first comment).
    Review URL: http://codereview.chromium.org/8333021
    
    TBR=finnur@chromium.org
    Review URL: http://codereview.chromium.org/8468002
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/912/src@110064 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_service_unittest.cc
+++ chrome/browser/extensions/extension_service_unittest.cc
@@ -448,6 +448,10 @@ void ExtensionServiceTestBase::InitializeEmptyExtensionService() {
   InitializeExtensionServiceHelper(false);
 }
 
+void ExtensionServiceTestBase::InitializeExtensionProcessManager() {
+  profile_->CreateExtensionProcessManager();
+}
+
 void ExtensionServiceTestBase::InitializeExtensionServiceWithUpdater() {
   InitializeExtensionServiceHelper(true);
   service_->updater()->Start();
@@ -3967,6 +3971,7 @@ TEST_F(ExtensionServiceTest, ProcessSyncDataWrongType) {
 
 TEST_F(ExtensionServiceTest, ProcessSyncDataSettings) {
   InitializeEmptyExtensionService();
+  InitializeExtensionProcessManager();
   TestSyncProcessorStub processor;
   service_->MergeDataAndStartSyncing(syncable::EXTENSIONS, SyncDataList(),
       &processor);
