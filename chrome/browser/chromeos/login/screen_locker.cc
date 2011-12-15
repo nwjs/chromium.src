92a50abdb7dadb3a0643aa1e61d836df9535720b
stevenjb@google.com <stevenjb@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-15 00:38:56 +0000

    Merge 114540 - Set the status area to browser mode when unlocking the screen locker.
    Change-Id: I43866084657d7d9c9ebcdd466684ac6f3e993f1e
    
    BUG=chromium-os:23912
    TEST=Log in, lock the screen, and unlock it. Ensure that the network menu ends with 'Network settings...' + IP, not 'More...'
    
    Review URL: http://codereview.chromium.org/8913017
    
    TBR=stevenjb@google.com
    Review URL: http://codereview.chromium.org/8949004
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/963/src@114545 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/screen_locker.cc
+++ chrome/browser/chromeos/login/screen_locker.cc
@@ -29,6 +29,7 @@
 #include "chrome/browser/chromeos/login/login_utils.h"
 #include "chrome/browser/chromeos/login/user_manager.h"
 #include "chrome/browser/chromeos/login/webui_screen_locker.h"
+#include "chrome/browser/chromeos/status/status_area_view_chromeos.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/sync/profile_sync_service.h"
@@ -86,6 +87,9 @@ class ScreenLockObserver : public chromeos::ScreenLockLibrary::Observer,
   virtual void UnlockScreen(chromeos::ScreenLockLibrary* obj) OVERRIDE {
     RestoreInputMethods();
     chromeos::ScreenLocker::Hide();
+    chromeos::StatusAreaViewChromeos::SetScreenMode(
+        chromeos::StatusAreaViewChromeos::BROWSER_MODE);
+
   }
 
   virtual void UnlockScreenFailed(chromeos::ScreenLockLibrary* obj) OVERRIDE {
