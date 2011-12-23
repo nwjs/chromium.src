6506c64184a5ea8a1850f9695af8327e720b07e7
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-23 03:07:31 +0000

    Merge 115468 - Missing plugin : Fix null mimetype issue in embed tag
    In chrome 16,17,18 if you have an embed tag, say flash that has no type information, 
    like:
    
    <embed width="100%" name="plugin" src="foo.swf" >
    
    without the type attribute, like  type="application/x-shockwave-flash"
    
    Chrome shows the "missing plugin" element instead of flash.
    
    This uses chrome's knowledge of the mime type instead of just passing webkit's notion of
    the mime type which in this case is empty ("").
    
    
    BUG=108228
    TEST=see bug for easy repro case.
    Review URL: http://codereview.chromium.org/9016030
    
    TBR=cpu@chromium.org
    Review URL: http://codereview.chromium.org/9030004
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/912/src@115677 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -358,6 +358,14 @@ WebPlugin* ChromeContentRendererClient::CreatePlugin(
       break;
     }
   }
+  
+  if (params.mimeType.isNull() && (actual_mime_type.size() > 0)) {
+    // Webkit might say that mime type is null while we already know the
+    // actual mime type via ChromeViewHostMsg_GetPluginInfo. In that case
+    // we should use what we know since WebpluginDelegateProxy does some
+    // specific initializations based on this information.
+    params.mimeType = WebString::fromUTF8(actual_mime_type.c_str());
+  }
 
   ContentSetting outdated_policy = CONTENT_SETTING_ASK;
   ContentSetting authorize_policy = CONTENT_SETTING_ASK;
