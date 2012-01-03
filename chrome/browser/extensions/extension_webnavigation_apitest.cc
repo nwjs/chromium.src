9224b2cea9465fdf5b4baaedd7db2221f42a3a2c
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-03 20:05:48 +0000

    Fix compile breakage. On M17, the method is still called render_view_host()
    BUG=none
    TEST=none
    TBR=jochen@chromium.org
    Review URL: http://codereview.chromium.org/9075010
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/963/src@116182 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_webnavigation_apitest.cc
+++ chrome/browser/extensions/extension_webnavigation_apitest.cc
@@ -215,9 +215,9 @@ IN_PROC_BROWSER_TEST_F(ExtensionApiTest, WebNavigationTargetBlank) {
   mouse_event.x = 7;
   mouse_event.y = 7;
   mouse_event.clickCount = 1;
-  tab->GetRenderViewHost()->ForwardMouseEvent(mouse_event);
+  tab->render_view_host()->ForwardMouseEvent(mouse_event);
   mouse_event.type = WebKit::WebInputEvent::MouseUp;
-  tab->GetRenderViewHost()->ForwardMouseEvent(mouse_event);
+  tab->render_view_host()->ForwardMouseEvent(mouse_event);
 
   ASSERT_TRUE(catcher.GetNextResult()) << catcher.message();
 }
@@ -251,9 +251,9 @@ IN_PROC_BROWSER_TEST_F(ExtensionApiTest, WebNavigationTargetBlankIncognito) {
   mouse_event.x = 7;
   mouse_event.y = 7;
   mouse_event.clickCount = 1;
-  tab->GetRenderViewHost()->ForwardMouseEvent(mouse_event);
+  tab->render_view_host()->ForwardMouseEvent(mouse_event);
   mouse_event.type = WebKit::WebInputEvent::MouseUp;
-  tab->GetRenderViewHost()->ForwardMouseEvent(mouse_event);
+  tab->render_view_host()->ForwardMouseEvent(mouse_event);
 
   ASSERT_TRUE(catcher.GetNextResult()) << catcher.message();
 }
