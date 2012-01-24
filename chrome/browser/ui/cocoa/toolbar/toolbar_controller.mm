35157685385262c6b2b6fe412fc4953a1528875e
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-24 21:36:35 +0000

    Merge 116692 - Change omnibox behavior when stripping javascript schema to navigate after stripping the schema on drag drop.
    BUG=109245
    TEST=N/A
    Review URL: http://codereview.chromium.org/9116016
    
    TBR=cdn@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9285029
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/963/src@118905 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/toolbar/toolbar_controller.mm
+++ chrome/browser/ui/cocoa/toolbar/toolbar_controller.mm
@@ -753,7 +753,6 @@ class NotificationBridge : public content::NotificationObserver {
   if (url.SchemeIs(chrome::kJavaScriptScheme)) {
     browser_->window()->GetLocationBar()->location_entry()->SetUserText(
         OmniboxView::StripJavascriptSchemas(UTF8ToUTF16(url.spec())));
-    return;
   }
   browser_->GetSelectedTabContents()->OpenURL(url, GURL(), CURRENT_TAB,
                                               content::PAGE_TRANSITION_TYPED);
