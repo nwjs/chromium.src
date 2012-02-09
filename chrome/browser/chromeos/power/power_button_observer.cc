2240256fd2e66ffb592788f027178a03c571b75b
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-09 21:50:30 +0000

    Merge 121026 - Log the user out when locking before they have completed the new user flow.
    BUG=112225
    TEST=Log in as a new user. Close the lid and verify that after a few seconds opening again you are logged out.
    
    Review URL: https://chromiumcodereview.appspot.com/9349022
    
    TBR=flackr@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9373040
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1025/src@121307 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/power/power_button_observer.cc
+++ chrome/browser/chromeos/power/power_button_observer.cc
@@ -23,7 +23,7 @@ PowerButtonObserver::PowerButtonObserver() {
 
   registrar_.Add(
       this,
-      chrome::NOTIFICATION_SESSION_STARTED,
+      chrome::NOTIFICATION_LOGIN_USER_CHANGED,
       content::NotificationService::AllSources());
   registrar_.Add(
       this,
@@ -48,7 +48,7 @@ void PowerButtonObserver::Observe(int type,
                                   const content::NotificationSource& source,
                                   const content::NotificationDetails& details) {
   switch (type) {
-    case chrome::NOTIFICATION_SESSION_STARTED: {
+    case chrome::NOTIFICATION_LOGIN_USER_CHANGED: {
       const User* user = &UserManager::Get()->logged_in_user();
       ash::Shell::GetInstance()->power_button_controller()->
           OnLoginStateChange(true /* logged_in */, user->is_guest());
