fed8ddd4f87706888be6b06ca81d1a0f1bfa921b
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-15 20:43:13 +0000

    Merge 122053 - Properly parse UTF8Strings in certificates on Windows.
    BUG=114168
    TEST=https://www.verisign.co.jp appears correctly regardless of system
    locale. Additionally, net_unittests:X509TypesTest* should cover this.
    
    Review URL: https://chromiumcodereview.appspot.com/9358080
    
    TBR=rsleevi@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9347050
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/963/src@122139 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/x509_cert_types.h
+++ net/base/x509_cert_types.h
@@ -51,10 +51,12 @@ struct NET_EXPORT CertPrincipal {
   explicit CertPrincipal(const std::string& name);
   ~CertPrincipal();
 
-#if defined(OS_MACOSX)
+#if defined(OS_MACOSX) || defined(OS_WIN)
   // Parses a BER-format DistinguishedName.
   bool ParseDistinguishedName(const void* ber_name_data, size_t length);
+#endif
 
+#if defined(OS_MACOSX)
   // Parses a CSSM_X509_NAME struct.
   void Parse(const CSSM_X509_NAME* name);
 
@@ -69,7 +71,7 @@ struct NET_EXPORT CertPrincipal {
   // order: CN, O and OU and returns the first non-empty one found.
   std::string GetDisplayName() const;
 
-  // The different attributes for a principal.  They may be "".
+  // The different attributes for a principal, stored in UTF-8.  They may be "".
   // Note that some of them can have several values.
 
   std::string common_name;
