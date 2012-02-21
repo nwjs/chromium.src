3a5a2a6db3dc977bfe9f13dfb978675bb4a491e0
dpolukhin@chromium.org <dpolukhin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-21 06:35:59 +0000

    Merge 122061 - Escape URL on offline error page.
    BUG=chromium-os:26001
    TEST=manual
    
    
    Review URL: http://codereview.chromium.org/9372019
    
    TBR=dpolukhin@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9355056
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/963/src@122798 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/offline/offline_load_page.cc
+++ chrome/browser/chromeos/offline/offline_load_page.cc
@@ -29,6 +29,7 @@
 #include "content/public/browser/notification_types.h"
 #include "grit/browser_resources.h"
 #include "grit/generated_resources.h"
+#include "net/base/escape.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 
@@ -125,7 +126,7 @@ void OfflineLoadPage::GetAppOfflineStrings(
   strings->SetString(
       "msg",
       l10n_util::GetStringFUTF16(IDS_APP_OFFLINE_LOAD_DESCRIPTION,
-                                 failed_url));
+                                 net::EscapeForHTML(failed_url)));
 }
 
 void OfflineLoadPage::GetNormalOfflineStrings(
@@ -139,7 +140,7 @@ void OfflineLoadPage::GetNormalOfflineStrings(
   strings->SetString(
       "msg",
       l10n_util::GetStringFUTF16(IDS_SITE_OFFLINE_LOAD_DESCRIPTION,
-                                 failed_url));
+                                 net::EscapeForHTML(failed_url)));
 }
 
 void OfflineLoadPage::CommandReceived(const std::string& cmd) {
