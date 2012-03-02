241a2af5308d183b02199310ad14dc594b16d8db
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-02 02:35:11 +0000

    Merge 124139 - Apply a rate limit on a per-plug-in basis to plug-in crashes.
    BUG=115758
    Review URL: https://chromiumcodereview.appspot.com/9460038
    
    TBR=cevans@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9568051
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1025/src@124576 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/browser/plugin_service.h
+++ content/public/browser/plugin_service.h
@@ -116,6 +116,10 @@ class PluginService {
   virtual void SetFilter(PluginServiceFilter* filter) = 0;
   virtual PluginServiceFilter* GetFilter() = 0;
 
+  // Used to monitor plug-in stability. An unstable plug-in is one that has
+  // crashed more than a set number of times in a set time period.
+  virtual bool IsPluginUnstable(const FilePath& plugin_path) = 0;
+
   // The following functions are wrappers around webkit::npapi::PluginList.
   // These must be used instead of those in order to ensure that we have a
   // single global list in the component build and so that we don't
