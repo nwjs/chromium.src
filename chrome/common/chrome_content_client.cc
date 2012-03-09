afa34e5598a6c1618225c47f01bcd8fe18ca9ba5
karen@chromium.org <karen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-09 01:03:38 +0000

    Revert 124510 - Merge 121478 - Restrict Flash sandbox to one process
    Add single-process job object restriction (no UI restrictions).
    Review URL: https://chromiumcodereview.appspot.com/9380010
    
    TBR=jschuh@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9577001
    
    TBR=jschuh@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9653015
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1025/src@125748 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_content_client.cc
+++ chrome/common/chrome_content_client.cc
@@ -418,13 +418,7 @@ bool ChromeContentClient::SandboxPlugin(CommandLine* command_line,
 
   // Spawn the flash broker and apply sandbox policy.
   if (LoadFlashBroker(plugin_path, command_line)) {
-    // UI job restrictions break windowless Flash, so just pick up single
-    // process limit for now.
-    policy->SetJobLevel(sandbox::JOB_LIMITED_USER,
-                        JOB_OBJECT_UILIMIT_DISPLAYSETTINGS |
-                        JOB_OBJECT_UILIMIT_SYSTEMPARAMETERS |
-                        JOB_OBJECT_UILIMIT_DESKTOP |
-                        JOB_OBJECT_UILIMIT_EXITWINDOWS);
+    policy->SetJobLevel(sandbox::JOB_UNPROTECTED, 0);
     policy->SetTokenLevel(sandbox::USER_RESTRICTED_SAME_ACCESS,
                           sandbox::USER_LIMITED);
     // Allow the Flash plugin to forward some messages back to Chrome.
