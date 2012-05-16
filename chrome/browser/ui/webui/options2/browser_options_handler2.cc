cdbbe0f85aae9c49a56faab64e85d517ab3e5f0c
atwilson@chromium.org <atwilson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-16 19:34:09 +0000

    Merge 137359 - Treat SyncCredentialsLost as an auth error Break up AreCredentialsAvailable() into two routines, IsSyncEnabledAndLoggedIn() and IsSyncTokenAvailable(). ProfileSyncService now will start up the sync backend even if there's no sync token available. GetCredentials() will make up fake/invalid tokens for the sync backend to use, which will eventually yield an auth error for the user.
    BUG=121755
    TEST=see bug
    
    
    Review URL: https://chromiumcodereview.appspot.com/10335015
    
    TBR=atwilson@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10388169
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1132/src@137484 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options2/browser_options_handler2.cc
+++ chrome/browser/ui/webui/options2/browser_options_handler2.cc
@@ -1027,9 +1027,10 @@ scoped_ptr<DictionaryValue> BrowserOptionsHandler::GetSyncStateDictionary() {
   sync_status->SetBoolean("managed", service->IsManaged());
   sync_status->SetBoolean("hasUnrecoverableError",
                           service->unrecoverable_error_detected());
-  sync_status->SetBoolean("autoLoginVisible",
+  sync_status->SetBoolean(
+      "autoLoginVisible",
       CommandLine::ForCurrentProcess()->HasSwitch(switches::kEnableAutologin) &&
-      service->AreCredentialsAvailable());
+      service->IsSyncEnabledAndLoggedIn() && service->IsSyncTokenAvailable());
 
   return sync_status.Pass();
 }
