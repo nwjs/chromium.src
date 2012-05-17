e84a5c83487e549b59c0c1c33bdd7c73b7bca3f2
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-17 15:24:20 +0000

    Merge 137512 - Construct a TCW for the login view's WebView. It needs this for Password Manager. It could be that we could just construct the Password Manager for this type, but this is the simplest fix.
    http://crbug.com/127110
    TEST=See bug, configure proxy with auth required and open GAIA frame at sign in screen.
    
    Review URL: https://chromiumcodereview.appspot.com/10388166
    
    TBR=ben@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10399064
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1132/src@137660 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/captive_portal_view.h
+++ chrome/browser/chromeos/login/captive_portal_view.h
@@ -19,6 +19,7 @@
 
 class Profile;
 class ReloadButton;
+class TabContentsWrapper;
 class ToolbarModel;
 
 namespace views {
@@ -113,6 +114,8 @@ class CaptivePortalView : public views::ButtonListener,
   ReloadButton* reload_;
   LocationBarView* location_bar_;
   views::WebView* web_view_;
+  // TODO: remove, needed to create a password manager for web_view_'s WC.
+  scoped_ptr<TabContentsWrapper> wrapper_;
 
   // Contains |web_view_| while it isn't owned by the view.
   scoped_ptr<views::WebView> web_view_container_;
