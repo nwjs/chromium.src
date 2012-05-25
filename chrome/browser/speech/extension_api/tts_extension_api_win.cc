d43c46292e632778c45836bef79880b5474c2178
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-25 23:17:29 +0000

    Merge 138393 - Make ExtensionTtsPlatformImplWin a leaky singleton to avoid a crash in the destructor.
    BUG=122026
    TEST=Does not crash on shutdown on XP. Speech still stops if you load TTS Demo and exit in the middle of a long sentencexsxc.
    
    Review URL: https://chromiumcodereview.appspot.com/10413054
    
    TBR=dmazzoni@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10447049
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1084/src@139140 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/speech/extension_api/tts_extension_api_win.cc
+++ chrome/browser/speech/extension_api/tts_extension_api_win.cc
@@ -213,7 +213,8 @@ ExtensionTtsPlatformImplWin::ExtensionTtsPlatformImplWin()
 
 // static
 ExtensionTtsPlatformImplWin* ExtensionTtsPlatformImplWin::GetInstance() {
-  return Singleton<ExtensionTtsPlatformImplWin>::get();
+  return Singleton<ExtensionTtsPlatformImplWin,
+                   LeakySingletonTraits<ExtensionTtsPlatformImplWin> >::get();
 }
 
 // static
