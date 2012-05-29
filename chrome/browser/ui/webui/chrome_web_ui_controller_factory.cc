c11d16248dd29526bb4f78dd302a20b8824b3c2c
dharani@chromium.org <dharani@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-29 18:18:13 +0000

    Since 1155 is going for dev, reverting this #1 crasher on branch 1155
    Revert 139085 - Restrict WebUI calls to the chrome:// origins.
    
    BUG=129288
    Review URL: https://chromiumcodereview.appspot.com/10332305
    
    TBR=cevans@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10451069
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1155/src@139322 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -236,7 +236,12 @@ WebUIFactoryFunction GetWebUIFactoryFunction(WebUI* web_ui,
   if (url.host() == chrome::kChromeUIPrintHost &&
       !g_browser_process->local_state()->GetBoolean(
           prefs::kPrintPreviewDisabled)) {
-    return &NewWebUI<PrintPreviewUI>;
+    printing::PrintPreviewTabController* controller =
+        printing::PrintPreviewTabController::GetInstance();
+    bool valid = controller && controller->is_creating_print_preview_tab();
+
+    if (valid)
+      return &NewWebUI<PrintPreviewUI>;
   }
   // Android does not support plugins for now.
   if (url.host() == chrome::kChromeUIPluginsHost)
