3a0f4484b76a41279c60e20dd07d7832453c31e5
dharani@chromium.org <dharani@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-31 21:53:16 +0000

    Reverting only on 1159 branch to test renderer regression
    Revert 138261 - PPAPI/NaCl: Fix IPC proxy build; pthread_t changed
    
    We broke when NaCl DEPS were rolled, giving us this change:
    r8650: (mseaborn) libpthread: Make pthread_t a pointer instead of a small integer ID
    
    BUG=116317
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/10417026
    
    TBR=dmichael@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10456066
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1159/src@139882 0039d316-1c4b-4281-b951-d872f2087c98

--- base/threading/platform_thread_posix.cc
+++ base/threading/platform_thread_posix.cc
@@ -160,11 +160,8 @@ PlatformThreadId PlatformThread::CurrentId() {
   return syscall(__NR_gettid);
 #elif defined(OS_ANDROID)
   return gettid();
-#elif defined(OS_SOLARIS)
+#elif defined(OS_NACL) || defined(OS_SOLARIS)
   return pthread_self();
-#elif defined(OS_NACL)
-  // Pointers are 32-bits in NaCl.
-  return reinterpret_cast<int32>(pthread_self());
 #elif defined(OS_POSIX)
   return reinterpret_cast<int64>(pthread_self());
 #endif
