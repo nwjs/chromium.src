deb089d9e1ffaf754f4b0c6a83064734b7ef0078
rbyers@chromium.org <rbyers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-04 20:05:01 +0000

    Merge 132051 - Fix bookmark deleting (removed 'splice' listeners in list)
    It appears to me that the 'splice' events are redundant because they
    are only generated in the ArrayListDataModel.splice() method, which
    also generates a 'permuted' event.  Receiving both events causes the
    list to be redraw()'n twice.  Also, I don't think that list's
    handleDataModelChange_ method is suitable for handling the 'splice'
    event because it only invalidates one cachedItems_, ie.
          delete this.cachedItems_[e.index];
    But the 'splice' event could represent changes to multiple rows.
    BUG=105181,125019
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9816020
    
    TBR=kgr@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10521009
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1084/src@140352 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/shared/js/cr/ui/list.js
+++ chrome/browser/resources/shared/js/cr/ui/list.js
@@ -126,8 +126,6 @@ cr.define('cr.ui', function() {
               this.boundHandleDataModelPermuted_);
           this.dataModel_.removeEventListener('change',
                                               this.boundHandleDataModelChange_);
-          this.dataModel_.removeEventListener('splice',
-                                              this.boundHandleDataModelChange_);
         }
 
         this.dataModel_ = dataModel;
@@ -143,8 +141,6 @@ cr.define('cr.ui', function() {
               'permuted',
               this.boundHandleDataModelPermuted_);
           this.dataModel_.addEventListener('change',
-                                           this.boundHandleDataModelChange_);
-          this.dataModel_.addEventListener('splice',
                                            this.boundHandleDataModelChange_);
         }
 
@@ -1060,6 +1056,8 @@ cr.define('cr.ui', function() {
         return;
       }
 
+      console.time('list:redraw');
+
       // Save the previous positions before any manipulation of elements.
       var scrollTop = this.scrollTop;
       var clientHeight = this.clientHeight;
@@ -1149,6 +1147,8 @@ cr.define('cr.ui', function() {
           }
         });
       }
+
+      console.timeEnd('list:redraw');
     },
 
     /**
