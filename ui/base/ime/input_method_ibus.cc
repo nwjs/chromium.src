58a800f4e3a9bc5e4a1dccf4dcdd8bb42169a141
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-19 00:59:10 +0000

    Merge 142705 - chromeos: Send 'keypress' for the content when composing a character with dead keys
    This change leaves characters outside BMP unable to be typed on docs.google.com, but surely fixes the problem for most use cases.
    
    BUG=132668
    TEST=Create a document on docs.google.com and try typing a character with dead keys (e.g. type '^'+'a' with keyboard layout "English - US international")
    
    Review URL: https://chromiumcodereview.appspot.com/10565032
    
    TBR=hashimoto@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10579011
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1132/src@142883 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/ime/input_method_ibus.cc
+++ ui/base/ime/input_method_ibus.cc
@@ -582,8 +582,15 @@ void InputMethodIBus::ProcessUnfilteredKeyPressEvent(
     string16 composed = character_composer_.composed_character();
     if (!composed.empty()) {
       client = GetTextInputClient();
-      if (client)
-        client->InsertText(composed);
+      if (client) {
+        // TODO(hashimoto): Send correct DOM event for characters which cannot
+        // be inserted with InsertChar(). Without sending those events, composed
+        // character will not be shown on docs.google.com. crbug.com/133269
+        if (composed.size() == 1)
+          client->InsertChar(composed[0], state);
+        else
+          client->InsertText(composed);
+      }
     }
     return;
   }
@@ -620,8 +627,15 @@ void InputMethodIBus::ProcessUnfilteredFabricatedKeyPressEvent(
     string16 composed = character_composer_.composed_character();
     if (!composed.empty()) {
       client = GetTextInputClient();
-      if (client)
-        client->InsertText(composed);
+      if (client) {
+        // TODO(hashimoto): Send correct DOM event for characters which cannot
+        // be inserted with InsertChar(). Without sending those events, composed
+        // character will not be shown on docs.google.com. crbug.com/133269
+        if (composed.size() == 1)
+          client->InsertChar(composed[0], flags);
+        else
+          client->InsertText(composed);
+      }
     }
     return;
   }
