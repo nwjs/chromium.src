61f301a8cdfcbf044186642a61b8d3f37bb76fe5
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-09 18:35:14 +0000

    Manual merge of http://src.chromium.org/viewvc/chrome?view=rev&revision=145346
    BUG=135583
    TBR=jorgelo
    Review URL: https://chromiumcodereview.appspot.com/10757003
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1132/src@145703 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/sandbox_init_linux.cc
+++ content/common/sandbox_init_linux.cc
@@ -297,6 +297,7 @@ static void ApplyGPUPolicy(std::vector<struct sock_filter>* program) {
   EmitAllowSyscall(__NR_shutdown, program);  // Virtual driver.
   EmitAllowSyscall(__NR_rt_sigaction, program);  // Breakpad signal handler.
   EmitFailSyscall(__NR_socket, EACCES, program);  // Nvidia binary driver.
+  EmitFailSyscall(__NR_fchmod, EPERM, program);  // ATI binary driver.
   EmitAllowSignalSelf(program);  // GPU watchdog.
 
   // Generally, filename-based syscalls will fail with ENOENT to behave
@@ -348,6 +349,8 @@ static void ApplyFlashPolicy(std::vector<struct sock_filter>* program) {
   EmitAllowSyscall(__NR_sched_yield, program);
   EmitAllowSyscall(__NR_shutdown, program);
   EmitAllowSyscall(__NR_sched_getaffinity, program);  // 3D
+  EmitAllowSyscall(__NR_sched_setscheduler, program);
+  EmitFailSyscall(__NR_socket, EACCES, program);
   EmitAllowSignalSelf(program);
 
   // These are under investigation, and hopefully not here for the long term.
