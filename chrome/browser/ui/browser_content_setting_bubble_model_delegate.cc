2193a6543e67fdf71a524ea4e8cad38fccadff38
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-10 17:45:23 +0000

    Merge 143429 - Move the mixed scripting infobar to a page action icon / content setting.
    BUG=133189
    Review URL: https://chromiumcodereview.appspot.com/10577028
    
    TBR=cevans@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1180/src@145906 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_content_setting_bubble_model_delegate.cc
+++ chrome/browser/ui/browser_content_setting_bubble_model_delegate.cc
@@ -4,7 +4,13 @@
 
 #include "chrome/browser/ui/browser_content_setting_bubble_model_delegate.h"
 
+#include "chrome/browser/google/google_util.h"
 #include "chrome/browser/ui/browser.h"
+
+// The URL for when the user clicks "learn more" on the mixed scripting page
+// icon bubble.
+const char kInsecureScriptHelpUrl[] =
+    "https://support.google.com/chrome/bin/answer.py?answer=1342714";
 
 BrowserContentSettingBubbleModelDelegate::
 BrowserContentSettingBubbleModelDelegate(Browser* browser) : browser_(browser) {
@@ -21,5 +27,13 @@ void BrowserContentSettingBubbleModelDelegate::ShowCollectedCookiesDialog(
 
 void BrowserContentSettingBubbleModelDelegate::ShowContentSettingsPage(
     ContentSettingsType type) {
-  browser_->ShowContentSettingsPage(type);
+  if (type == CONTENT_SETTINGS_TYPE_MIXEDSCRIPT) {
+    // We don't (yet?) implement user-settable exceptions for mixed script
+    // blocking, so bounce to an explanatory page for now.
+    GURL url(google_util::AppendGoogleLocaleParam(
+        GURL(kInsecureScriptHelpUrl)));
+    browser_->AddSelectedTabWithURL(url, content::PAGE_TRANSITION_LINK);
+  } else {
+    browser_->ShowContentSettingsPage(type);
+  }
 }
