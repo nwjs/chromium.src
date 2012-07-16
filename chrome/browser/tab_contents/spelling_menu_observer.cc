f4a4a19ea826b2e1d4d0c23638a8794b0e41fd71
karen@chromium.org <karen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-16 17:25:12 +0000

    Revert 142102 - Add SpellingServiceClient::IsAvailable(). This change adds a static function SpellingServiceClient::IsAvailable() and use it instead of checking conditions directly.
    BUG=none
    TEST=SpellingServiceClientTest.AvailableServices
    
    Review URL: https://chromiumcodereview.appspot.com/10536141
    
    TBR=hbono@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10793013
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1180/src@146822 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/spelling_menu_observer.cc
+++ chrome/browser/tab_contents/spelling_menu_observer.cc
@@ -59,9 +59,12 @@ void SpellingMenuObserver::InitMenu(const content::ContextMenuParams& params) {
   }
 
   // Append a placeholder item for the suggestion from the Spelling serivce and
-  // send a request to the service if we can retrieve suggestions from it.
-  if (SpellingServiceClient::IsAvailable(profile,
-                                         SpellingServiceClient::SUGGEST)) {
+  // send a request to the service if we have enabled the spellchecking and
+  // integrated the Spelling service.
+  PrefService* pref = profile->GetPrefs();
+  if (params.spellcheck_enabled &&
+      pref->GetBoolean(prefs::kEnableSpellCheck) &&
+      pref->GetBoolean(prefs::kSpellCheckUseSpellingService)) {
     // Retrieve the misspelled word to be sent to the Spelling service.
     string16 text = params.misspelled_word;
     if (!text.empty()) {
