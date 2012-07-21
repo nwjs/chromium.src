7bca17853c511e2064fb7246cf468289d95d8c3e
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-21 04:18:48 +0000

    Merge 145475 - HiDPI assets for product_logo_16 / _32
    Use the new logo on about:chrome
    
    BUG=134372,135179
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10692069
    
    TBR=thakis@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1180/src@147770 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/spelling_bubble_model.cc
+++ chrome/browser/tab_contents/spelling_bubble_model.cc
@@ -13,7 +13,7 @@
 #include "chrome/common/url_constants.h"
 #include "googleurl/src/gurl.h"
 #include "grit/generated_resources.h"
-#include "grit/theme_resources.h"
+#include "grit/theme_resources_standard.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/image/image.h"
