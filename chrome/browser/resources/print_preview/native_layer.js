68f595a66a0bae7ed0e4af968f68fc9f2684a6fa
abodenha@google.com <abodenha@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-23 19:44:44 +0000

    Merge 146438 - Sets print preview document title.
    BUG=136924
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/10693159
    
    TBR=rltoscano@google.com
    Review URL: https://chromiumcodereview.appspot.com/10802076
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1180/src@147901 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/native_layer.js
+++ chrome/browser/resources/print_preview/native_layer.js
@@ -134,7 +134,7 @@ cr.define('print_preview', function() {
           printTicketStore.getPageNumberSet().getPageRanges() : [];
 
       var ticket = {
-        'pageRange': pageRanges, // pageRanges,
+        'pageRange': pageRanges,
         'landscape': printTicketStore.isLandscapeEnabled(),
         'color': printTicketStore.isColorEnabled() ?
             NativeLayer.ColorMode_.COLOR : NativeLayer.ColorMode_.GRAY,
@@ -354,6 +354,7 @@ cr.define('print_preview', function() {
           numberFormatSymbols[1] || '.',
           unitType,
           initialSettings['previewModifiable'] || false,
+          initialSettings['initiatorTabTitle'] || '',
           marginsType,
           customMargins,
           initialSettings['duplex'] || false,
@@ -570,6 +571,7 @@ cr.define('print_preview', function() {
    *     local machine's measurement system.
    * @param {boolean} isDocumentModifiable Whether the document to print is
    *     modifiable.
+   * @param {string} documentTitle Title of the document.
    * @param {print_preview.ticket_items.MarginsType.Value} marginsType Initial
    *     margins type.
    * @param {print_preview.Margins} customMargins Initial custom margins.
@@ -586,6 +588,7 @@ cr.define('print_preview', function() {
       decimalDelimeter,
       unitType,
       isDocumentModifiable,
+      documentTitle,
       marginsType,
       customMargins,
       isDuplexEnabled,
@@ -626,6 +629,13 @@ cr.define('print_preview', function() {
      * @private
      */
     this.isDocumentModifiable_ = isDocumentModifiable;
+
+    /**
+     * Title of the document.
+     * @type {string}
+     * @private
+     */
+    this.documentTitle_ = documentTitle;
 
     /**
      * Initial margins type.
@@ -692,6 +702,11 @@ cr.define('print_preview', function() {
     /** @return {boolean} Whether the document to print is modifiable. */
     get isDocumentModifiable() {
       return this.isDocumentModifiable_;
+    },
+
+    /** @return {string} Document title. */
+    get documentTitle() {
+      return this.documentTitle_;
     },
 
     /**
