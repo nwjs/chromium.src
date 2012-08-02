5d2418a21914d0a8e009770517bf3f29db2750c4
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-02 21:28:35 +0000

    Deal gracefully with removing content settings for unregistered extensions.
    This is a repeat of r141415 for M21. I'll leave it in on trunk to continue to investigate.
    
    BUG=128652
    
    Review URL: https://chromiumcodereview.appspot.com/10830082
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1180/src@149703 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/content_settings/content_settings_store.cc
+++ chrome/browser/extensions/api/content_settings/content_settings_store.cc
@@ -235,8 +235,8 @@ void ContentSettingsStore::ClearContentSettingsForExtension(
       char ext_id_buffer[33];
       base::strlcpy(ext_id_buffer, ext_id.c_str(), sizeof(ext_id_buffer));
       base::debug::Alias(ext_id_buffer);
-      // Do a clean crash.
-      CHECK(false);
+      // Gracefully return instead of crashing below.
+      return;
     }
     notify = !map->empty();
     map->clear();
