CyanogenMod/android_external_chromium_org

commit 0d71f6286969cf17062489ad0d8bb6e95a178332

Change return type of FaviconTabHelper::GetFavicon() to gfx::Image.
No intended behavior change for now.
Depends on http://codereview.chromium.org/10832128/
Part of supporting hidpi favicons in the tab strip.

BUG=138550
TBR=joi

Review URL: https://chromiumcodereview.appspot.com/10837090

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149891 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 6172089d610e92345a897c9bb05f7f8c7279d39b

chrome/browser/ui/cocoa/tab_contents/favicon_util_mac.mm
@@ -19,9 +19,7 @@ namespace mac {
 
 NSImage* FaviconForTabContents(TabContents* contents) {
   if (contents && contents->favicon_tab_helper()->FaviconIsValid()) {
-    CGColorSpaceRef color_space = base::mac::GetSystemColorSpace();
-    NSImage* image = gfx::SkBitmapToNSImageWithColorSpace(
-        contents->favicon_tab_helper()->GetFavicon(), color_space);
+    NSImage* image = contents->favicon_tab_helper()->GetFavicon().ToNSImage();
     // The |image| could be nil if the bitmap is null. In that case, fallback
     // to the default image.
     if (image) {

ui/gfx/image/image.cc
@@ -337,6 +337,14 @@ NSImage* Image::ToNSImage() const {
 }
 #endif
 
+SkBitmap Image::AsBitmap() const {
+  return IsEmpty() ? SkBitmap() : *ToSkBitmap();
+}
+
+ImageSkia Image::AsImageSkia() const {
+  return IsEmpty() ? ImageSkia(SkBitmap()) : *ToImageSkia();
+}
+
 ImageSkia* Image::CopyImageSkia() const {
   return new ImageSkia(*ToImageSkia());
 }

ui/gfx/image/image.h
@@ -107,6 +107,13 @@ class UI_EXPORT Image {
   NSImage* ToNSImage() const;
 #endif
 
+  // Same as ToSkBitmap(), but returns a null SkBitmap if this image is empty.
+  SkBitmap AsBitmap() const;
+
+  // Same as ToSkBitmap(), but returns a ImageSkia with a null SkBitmap if this
+  // image is empty.
+  ImageSkia AsImageSkia() const;
+
   // Performs a conversion, like above, but returns a copy of the result rather
   // than a weak pointer. The caller is responsible for deleting the result.
   // Note that the result is only a copy in terms of memory management; the

chrome/browser/favicon/favicon_tab_helper.cc
@@ -49,18 +49,18 @@ void FaviconTabHelper::FetchFavicon(const GURL& url) {
     touch_icon_handler_->FetchFavicon(url);
 }
 
-SkBitmap FaviconTabHelper::GetFavicon() const {
+gfx::Image FaviconTabHelper::GetFavicon() const {
   // Like GetTitle(), we also want to use the favicon for the last committed
   // entry rather than a pending navigation entry.
   const NavigationController& controller = web_contents()->GetController();
   NavigationEntry* entry = controller.GetTransientEntry();
   if (entry)
-    return entry->GetFavicon().AsBitmap();
+    return entry->GetFavicon().image;
 
   entry = controller.GetLastCommittedEntry();
   if (entry)
-    return entry->GetFavicon().AsBitmap();
-  return SkBitmap();
+    return entry->GetFavicon().image;
+  return gfx::Image();
 }
 
 bool FaviconTabHelper::FaviconIsValid() const {
@@ -112,7 +112,8 @@ void FaviconTabHelper::SaveFavicon() {
   }
   std::vector<unsigned char> image_data;
   // TODO: Save all representations.
-  gfx::PNGCodec::EncodeBGRASkBitmap(favicon.AsBitmap(), false, &image_data);
+  gfx::PNGCodec::EncodeBGRASkBitmap(
+      favicon.image.AsBitmap(), false, &image_data);
   service->SetFavicon(
       entry->GetURL(), favicon.url, image_data, history::FAVICON);
 }

chrome/browser/favicon/favicon_tab_helper.h
@@ -15,6 +15,10 @@
 #include "content/public/browser/web_contents_observer.h"
 #include "googleurl/src/gurl.h"
 
+namespace gfx {
+class Image;
+}
+
 class FaviconHandler;
 class SkBitmap;
 
@@ -38,9 +42,9 @@ class FaviconTabHelper : public content::WebContentsObserver,
 
   // Returns the favicon for this tab, or IDR_DEFAULT_FAVICON if the tab does
   // not have a favicon. The default implementation uses the current navigation
-  // entry. This will return an isNull bitmap if there are no navigation
+  // entry. This will return an empty bitmap if there are no navigation
   // entries, which should rarely happen.
-  SkBitmap GetFavicon() const;
+  gfx::Image GetFavicon() const;
 
   // Returns true if we have the favicon for the page.
   bool FaviconIsValid() const;

chrome/browser/instant/instant_loader.cc
@@ -379,8 +379,8 @@ void InstantLoader::WebContentsDelegateImpl::CommitHistory(
       !active_entry->GetFavicon().image.IsEmpty()) {
     std::vector<unsigned char> image_data;
     // TODO: Add all variants once the history service supports it.
-    gfx::PNGCodec::EncodeBGRASkBitmap(active_entry->GetFavicon().AsBitmap(),
-                                      false, &image_data);
+    gfx::PNGCodec::EncodeBGRASkBitmap(
+        active_entry->GetFavicon().image.AsBitmap(), false, &image_data);
     favicon_service->SetFavicon(active_entry->GetURL(),
                                 active_entry->GetFavicon().url,
                                 image_data,

chrome/browser/task_manager/task_manager_resource_providers.cc
@@ -334,7 +334,7 @@ string16 TaskManagerTabContentsResource::GetProfileName() const {
 gfx::ImageSkia TaskManagerTabContentsResource::GetIcon() const {
   if (IsPrerendering())
     return *prerender_icon_;
-  return tab_contents_->favicon_tab_helper()->GetFavicon();
+  return tab_contents_->favicon_tab_helper()->GetFavicon().AsImageSkia();
 }
 
 WebContents* TaskManagerTabContentsResource::GetWebContents() const {

chrome/browser/ui/browser.cc
@@ -534,7 +534,9 @@ SkBitmap Browser::GetCurrentPageIcon() const {
   TabContents* contents = chrome::GetActiveTabContents(this);
   // |contents| can be NULL since GetCurrentPageIcon() is called by the window
   // during the window's creation (before tabs have been added).
-  return contents ? contents->favicon_tab_helper()->GetFavicon() : SkBitmap();
+  // TODO: Let this return a gfx::Image.
+  return contents ?
+      contents->favicon_tab_helper()->GetFavicon().AsBitmap() : SkBitmap();
 }
 
 string16 Browser::GetWindowTitleForCurrentTab() const {

nwjs/chromium.src

commit 3cba1d554a21093e216de73fb7c1ff90b84b802a

Merge 150092 - Fall back to the default favicon on invalid favicon files.
I regressed this in r149891.

BUG=140689

Review URL: https://chromiumcodereview.appspot.com/10853018

TBR=thakis@chromium.org
Review URL: https://chromiumcodereview.appspot.com/10836113

git-svn-id: svn://svn.chromium.org/chrome/branches/1228/src@150099 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/cocoa/tab_contents/favicon_util_mac.mm
@@ -13,7 +13,7 @@ namespace mac {
 
 NSImage* FaviconForTabContents(TabContents* contents) {
   if (contents && contents->favicon_tab_helper()->FaviconIsValid()) {
-    NSImage* image = contents->favicon_tab_helper()->GetFavicon().ToNSImage();
+    NSImage* image = contents->favicon_tab_helper()->GetFavicon().AsNSImage();
     // The |image| could be nil if the bitmap is null. In that case, fallback
     // to the default image.
     if (image) {

ui/gfx/image/image.cc
@@ -342,8 +342,14 @@ SkBitmap Image::AsBitmap() const {
 }
 
 ImageSkia Image::AsImageSkia() const {
-  return IsEmpty() ? ImageSkia(SkBitmap()) : *ToImageSkia();
+  return IsEmpty() ? ImageSkia() : *ToImageSkia();
 }
+
+#if defined(OS_MACOSX)
+NSImage* Image::AsNSImage() const {
+  return IsEmpty() ? nil : ToNSImage();
+}
+#endif
 
 ImageSkia* Image::CopyImageSkia() const {
   return new ImageSkia(*ToImageSkia());

ui/gfx/image/image.h
@@ -97,7 +97,7 @@ class UI_EXPORT Image {
 
   // Converts the Image to the desired representation and stores it internally.
   // The returned result is a weak pointer owned by and scoped to the life of
-  // the Image.
+  // the Image. Must only be called if IsEmpty() is false.
   const SkBitmap* ToSkBitmap() const;
   const ImageSkia* ToImageSkia() const;
 #if defined(TOOLKIT_GTK)
@@ -110,9 +110,14 @@ class UI_EXPORT Image {
   // Same as ToSkBitmap(), but returns a null SkBitmap if this image is empty.
   SkBitmap AsBitmap() const;
 
-  // Same as ToSkBitmap(), but returns a ImageSkia with a null SkBitmap if this
+  // Same as ToImageSkia(), but returns an empty ImageSkia if this
   // image is empty.
   ImageSkia AsImageSkia() const;
+
+#if defined(OS_MACOSX)
+  // Same as ToSkBitmap(), but returns nil if this image is empty.
+  NSImage* AsNSImage() const;
+#endif
 
   // Performs a conversion, like above, but returns a copy of the result rather
   // than a weak pointer. The caller is responsible for deleting the result.