95bc604b9b54258e3ea6d0bed280a4a0a1545d35
Zhao Cheng <zcbenz@gmail.com>
2012-08-16 10:35:11 +0800

    Put destructor to private according to chromium style

--- base/message_pump_uv.h
+++ base/message_pump_uv.h
@@ -16,7 +16,6 @@ namespace base {
 class BASE_EXPORT MessagePumpUV : public MessagePump {
  public:
   MessagePumpUV();
-  virtual ~MessagePumpUV();
 
   // MessagePump methods:
   virtual void Run(Delegate* delegate) OVERRIDE;
@@ -25,6 +24,8 @@ class BASE_EXPORT MessagePumpUV : public MessagePump {
   virtual void ScheduleDelayedWork(const TimeTicks& delayed_work_time) OVERRIDE;
 
  private:
+  virtual ~MessagePumpUV();
+
   // This flag is set to false when Run should return.
   bool keep_running_;
 
