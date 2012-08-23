180847316b60c03e64c6a457f0b3dee5cc720085
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-23 09:44:23 +0000

    Manual merge of extensions docserver2 data to 1180 branch.
    Created like so: 
    - rm -rf * 
    - cp -R <trunk>/static . 
    - cp -R <trunk>/templates/private . 
    - cp <trunk>/[converter.py and dependencies] . 
    - fix converter.py to work in older environment (basically 
    just change image regex) 
    - run converter.py 
    - rm -rf templates/public/apps 
    - manual copy <trunk>/[api_index.html and samples.html] 
    - use <trunk>/preview.py to test manually
    
    BUG=141997
    Review URL: https://chromiumcodereview.appspot.com/10878022
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1180/src@152972 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/common/extensions/docs/server2/static/js/branch.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(function() {
+  function redirectToBranch() {
+    var value = event.target.value;
+    if (!value)
+      return;
+    var current_branch = window.bootstrap.branchInfo.current;
+    var path = window.location.pathname.split('/');
+    if (path[0] == '')
+      path = path.slice(1);
+    var index = path.indexOf(current_branch);
+    if (index != -1)
+      path[index] = value;
+    else
+      path.splice(0, 0, value);
+    window.location = '/' + path.join('/');
+  }
+
+  document.getElementById('branchChooser').addEventListener(
+      'change',
+      redirectToBranch);
+})()
