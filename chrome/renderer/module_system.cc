75019c263361bfb3be7f02f5fe0b4015d0a7d1c3
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-28 23:21:46 +0000

    Merge 153283 - Make ModuleSystem::DumpException() handle empty handles.
    BUG=144436
    
    
    Review URL: https://chromiumcodereview.appspot.com/10874052
    
    TBR=koz@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10890019
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1180/src@153778 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/module_system.cc
+++ chrome/renderer/module_system.cc
@@ -59,11 +59,23 @@ bool ModuleSystem::IsPresentInCurrentContext() {
 
 // static
 void ModuleSystem::DumpException(v8::Handle<v8::Message> message) {
-  LOG(ERROR) << "["
-             << *v8::String::Utf8Value(
-                 message->GetScriptResourceName()->ToString())
-             << "(" << message->GetLineNumber() << ")] "
-             << *v8::String::Utf8Value(message->Get());
+  std::string resource_name = "<unknown resource>";
+  if (!message->GetScriptResourceName().IsEmpty()) {
+    resource_name = *v8::String::Utf8Value(
+                 message->GetScriptResourceName()->ToString());
+  }
+
+  std::string error_message = "<no error message>";
+  if (!message->Get().IsEmpty())
+    error_message = *v8::String::Utf8Value(message->Get());
+
+  std::string stack_trace = "<stack trace unavailable>";
+  if (!try_catch.StackTrace().IsEmpty())
+    stack_trace = *v8::String::Utf8Value(try_catch.StackTrace());
+
+  LOG(ERROR) << "[" << resource_name << "(" << message->GetLineNumber() << ")] "
+             << error_message
+             << "{" << stack_trace << "}";
 }
 
 void ModuleSystem::Require(const std::string& module_name) {
