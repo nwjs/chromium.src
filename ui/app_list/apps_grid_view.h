a81ba1e8d8116d5d1aff3b0c031e88a790221021
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-31 01:09:17 +0000

    Merge 153863 - app_list: Fix crash after uninstalling last app.
    BUG=145274
    TEST=There should be no crash after uninstalling the last app and then selecting an app.
    
    R=sky@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10892017
    
    TBR=xiyuan@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10915027
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1229/src@154337 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/apps_grid_view.h
+++ ui/app_list/apps_grid_view.h
@@ -52,6 +52,9 @@ class APP_LIST_EXPORT AppsGridView : public views::View,
   virtual bool OnKeyPressed(const views::KeyEvent& event) OVERRIDE;
   virtual bool OnKeyReleased(const views::KeyEvent& event) OVERRIDE;
   virtual void OnPaintFocusBorder(gfx::Canvas* canvas) OVERRIDE;
+  virtual void ViewHierarchyChanged(bool is_add,
+                                    views::View* parent,
+                                    views::View* child) OVERRIDE;
 
  private:
   // Updates from model.
