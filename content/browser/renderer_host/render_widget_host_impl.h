fcf705b873cb6ecd758c209259b2fb6a0ccbc3a2
yzshen@chromium.org <yzshen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-28 00:45:00 +0000

    Merge 158408 - Fix the issue that any tab/window closing hotkey will crash the Pepper Flash Fullscreen.
    BUG=149821
    TEST=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/10968037
    
    TBR=yzshen@chromium.org
    Review URL: https://codereview.chromium.org/10977071
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1229/src@159164 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_impl.h
+++ content/browser/renderer_host/render_widget_host_impl.h
@@ -58,6 +58,8 @@ class CONTENT_EXPORT RenderWidgetHostImpl : virtual public RenderWidgetHost,
  public:
   // routing_id can be MSG_ROUTING_NONE, in which case the next available
   // routing id is taken from the RenderProcessHost.
+  // If this object outlives |delegate|, DetachDelegate() must be called when
+  // |delegate| goes away.
   RenderWidgetHostImpl(RenderWidgetHostDelegate* delegate,
                        RenderProcessHost* process,
                        int routing_id);
@@ -390,6 +392,8 @@ class CONTENT_EXPORT RenderWidgetHostImpl : virtual public RenderWidgetHost,
     allow_privileged_mouse_lock_ = allow;
   }
 
+  void DetachDelegate();
+
  protected:
   virtual RenderWidgetHostImpl* AsRenderWidgetHostImpl() OVERRIDE;
 
@@ -410,10 +414,9 @@ class CONTENT_EXPORT RenderWidgetHostImpl : virtual public RenderWidgetHost,
   // when accelerated compositing is enabled.
   gfx::GLSurfaceHandle GetCompositingSurface();
 
-  // "RenderWidgetHostDelegate" ------------------------------------------------
-  // There is no RenderWidgetHostDelegate but the following methods serve the
-  // same purpose. They are overridden by RenderViewHost to send upwards to its
-  // delegate.
+  // ---------------------------------------------------------------------------
+  // The following methods are overridden by RenderViewHost to send upwards to
+  // its delegate.
 
   // Called when a mousewheel event was not processed by the renderer.
   virtual void UnhandledWheelEvent(const WebKit::WebMouseWheelEvent& event) {}
@@ -603,6 +606,7 @@ class CONTENT_EXPORT RenderWidgetHostImpl : virtual public RenderWidgetHost,
   void TickActiveSmoothScrollGesture();
 
   // Our delegate, which wants to know mainly about keyboard events.
+  // It will remain non-NULL until DetachDelegate() is called.
   RenderWidgetHostDelegate* delegate_;
 
   // Created during construction but initialized during Init*(). Therefore, it
