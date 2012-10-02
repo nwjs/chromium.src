a6c9671b3893c08d95769d8dae5d72d316c9afa9
dharani@google.com <dharani@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-02 15:23:28 +0000

    Revert 159632 - Improve error handling in ApplyProcessMitigationsToCurrentProcess
    BUG=153399
    Review URL: https://codereview.chromium.org/11036009
    
    TBR=jschuh@chromium.org
    Review URL: https://codereview.chromium.org/11031027
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1284/src@159686 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/win/src/process_mitigations.cc
+++ sandbox/win/src/process_mitigations.cc
@@ -34,39 +34,30 @@ bool ApplyProcessMitigationsToCurrentProcess(MitigationFlags flags) {
   if (!IsXPSP2OrLater())
     return true;
 
-  base::win::Version version = base::win::GetVersion();
   HMODULE module = ::GetModuleHandleA("kernel32.dll");
 
-  if (version >= base::win::VERSION_VISTA &&
-      (flags & MITIGATION_DLL_SEARCH_ORDER)) {
+  if (flags & MITIGATION_DLL_SEARCH_ORDER) {
     SetDefaultDllDirectoriesFunction set_default_dll_directories =
         reinterpret_cast<SetDefaultDllDirectoriesFunction>(
             ::GetProcAddress(module, "SetDefaultDllDirectories"));
 
     // Check for SetDefaultDllDirectories since it requires KB2533623.
     if (set_default_dll_directories) {
-      if (!set_default_dll_directories(LOAD_LIBRARY_SEARCH_DEFAULT_DIRS) &&
-          ERROR_ACCESS_DENIED != ::GetLastError()) {
+      if (!set_default_dll_directories(LOAD_LIBRARY_SEARCH_DEFAULT_DIRS))
         return false;
-      }
     }
   }
 
   // Set the heap to terminate on corruption
-  if (version >= base::win::VERSION_VISTA &&
-      (flags & MITIGATION_HEAP_TERMINATE)) {
+  if (flags & MITIGATION_HEAP_TERMINATE) {
     if (!::HeapSetInformation(NULL, HeapEnableTerminationOnCorruption,
-                              NULL, 0) &&
-        ERROR_ACCESS_DENIED != ::GetLastError()) {
+                              NULL, 0))
       return false;
-    }
   }
 
 #if !defined(_WIN64)  // DEP is always enabled on 64-bit.
   if (flags & MITIGATION_DEP) {
     DWORD dep_flags = PROCESS_DEP_ENABLE;
-    // DEP support is quirky on XP, so don't force a failure in that case.
-    const bool return_on_fail = version >= base::win::VERSION_VISTA;
 
     if (flags & MITIGATION_DEP_NO_ATL_THUNK)
       dep_flags |= PROCESS_DEP_DISABLE_ATL_THUNK_EMULATION;
@@ -76,7 +67,7 @@ bool ApplyProcessMitigationsToCurrentProcess(MitigationFlags flags) {
             ::GetProcAddress(module, "SetProcessDEPPolicy"));
     if (set_process_dep_policy) {
       if (!set_process_dep_policy(dep_flags) &&
-          ERROR_ACCESS_DENIED != ::GetLastError() && return_on_fail) {
+          ERROR_ACCESS_DENIED != ::GetLastError()) {
         return false;
       }
     } else {
@@ -98,7 +89,7 @@ bool ApplyProcessMitigationsToCurrentProcess(MitigationFlags flags) {
       if (!SUCCEEDED(set_information_process(GetCurrentProcess(),
                                              ProcessExecuteFlags,
                                              &dep, sizeof(dep))) &&
-          ERROR_ACCESS_DENIED != ::GetLastError() && return_on_fail) {
+          ERROR_ACCESS_DENIED != ::GetLastError()) {
         return false;
       }
     }
@@ -106,6 +97,7 @@ bool ApplyProcessMitigationsToCurrentProcess(MitigationFlags flags) {
 #endif
 
   // This is all we can do in Win7 and below.
+  base::win::Version version = base::win::GetVersion();
   if (version < base::win::VERSION_WIN8)
     return true;
 
