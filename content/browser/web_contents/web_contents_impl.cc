12428de732adb5bd95ae192e536975751ba65f32
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-15 22:23:32 +0000

    Merge 161459 - Use blocking worker pool for sync file operations in Pepper.
    Switch from posting file operations on the file thread to posting them
    on the blocking worker pool.  Previous instrumentation indicated that
    this reduces hung-plugin infobars by around 20%.
    
    BUG=153383
    
    
    Review URL: https://chromiumcodereview.appspot.com/11093059
    
    TBR=shess@chromium.org
    Review URL: https://codereview.chromium.org/11150030
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1271/src@161982 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -2297,6 +2297,8 @@ void WebContentsImpl::OnSetSelectedColorInColorChooser(int color_chooser_id,
 void WebContentsImpl::OnPepperPluginHung(int plugin_child_id,
                                          const FilePath& path,
                                          bool is_hung) {
+  UMA_HISTOGRAM_COUNTS("Pepper.PluginHung", 1);
+
   FOR_EACH_OBSERVER(WebContentsObserver, observers_,
                     PluginHungStatusChanged(plugin_child_id, path, is_hung));
 }
