8131507a5a756774ec3ee53aaae43e8e53365238
Zhao Cheng <zcbenz@gmail.com>
2012-10-19 13:55:11 +0800

    Make menu item respond to change of enabled.

--- ui/views/controls/menu/menu_item_view_win.cc
+++ ui/views/controls/menu/menu_item_view_win.cc
@@ -20,6 +20,9 @@ namespace views {
 void MenuItemView::PaintButton(gfx::Canvas* canvas, PaintButtonMode mode) {
   const MenuConfig& config = MenuConfig::instance();
 
+  // Hook to make sure the enabled state gets update.
+  SetEnabled(GetDelegate()->IsCommandEnabled(GetCommand()));
+
   bool render_selection =
       (mode == PB_NORMAL && IsSelected() &&
        parent_menu_item_->GetSubmenu()->GetShowSelection(this) &&
