92d504e5d07908cb42652dccb24b9d8205cef6ef
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-25 21:34:15 +0000

    Merge 163884 - Limit extension install cancellation on quit to Chrome OS
    We want to backport the extension install cancellation code to Chrome OS for M23, but it's late in the game for Windows/Mac/Linux M23.  Limit the code to Chrome OS, then backport the original change and this change to M23.  Then we can make it cross-platform in trunk for M24.
    
    BUG=155994,157609
    TEST=compiles
    
    Review URL: https://codereview.chromium.org/11277003
    
    TBR=jamescook@chromium.org
    Review URL: https://codereview.chromium.org/11266059
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1271/src@164182 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/crx_installer.cc
+++ chrome/browser/extensions/crx_installer.cc
@@ -400,8 +400,13 @@ void CrxInstaller::OnUnpackSuccess(const FilePath& temp_dir,
 
 void CrxInstaller::CheckRequirements() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  if (!frontend_weak_.get() || frontend_weak_->browser_terminating())
+  if (!frontend_weak_.get())
     return;
+#if defined(OS_CHROMEOS)
+  // TODO(jamescook): Remove ifdef after M23 backport, crbug.com/155994
+  if (frontend_weak_->browser_terminating())
+    return;
+#endif
   AddRef();  // Balanced in OnRequirementsChecked().
   requirements_checker_->Check(extension_,
                                base::Bind(&CrxInstaller::OnRequirementsChecked,
@@ -426,8 +431,13 @@ void CrxInstaller::OnRequirementsChecked(
 
 void CrxInstaller::ConfirmInstall() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  if (!frontend_weak_.get() || frontend_weak_->browser_terminating())
+  if (!frontend_weak_.get())
     return;
+#if defined(OS_CHROMEOS)
+  // TODO(jamescook): Remove ifdef after M23 backport, crbug.com/155994
+  if (frontend_weak_->browser_terminating())
+    return;
+#endif
 
   string16 error;
   if (!ExtensionSystem::Get(profile_)->management_policy()->
@@ -613,8 +623,13 @@ void CrxInstaller::ReportSuccessFromFileThread() {
 void CrxInstaller::ReportSuccessFromUIThread() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 
-  if (!frontend_weak_.get() || frontend_weak_->browser_terminating())
+  if (!frontend_weak_.get())
     return;
+#if defined(OS_CHROMEOS)
+  // TODO(jamescook): Remove ifdef after M23 backport, crbug.com/155994
+  if (frontend_weak_->browser_terminating())
+    return;
+#endif
 
   // If there is a client, tell the client about installation.
   if (client_) {
--- chrome/browser/extensions/extension_service_unittest.cc
+++ chrome/browser/extensions/extension_service_unittest.cc
@@ -1489,6 +1489,8 @@ TEST_F(ExtensionServiceTest, InstallUserScript) {
   ExtensionErrorReporter::GetInstance()->ClearErrors();
 }
 
+#if defined(OS_CHROMEOS)
+// TODO(jamescook): Remove ifdef after M23 backport, crbug.com/155994
 // Extensions don't install during shutdown.
 TEST_F(ExtensionServiceTest, InstallExtensionDuringShutdown) {
   InitializeEmptyExtensionService();
@@ -1505,6 +1507,7 @@ TEST_F(ExtensionServiceTest, InstallExtensionDuringShutdown) {
   EXPECT_FALSE(installed_) << "Extension installed during shutdown.";
   ASSERT_EQ(0u, loaded_.size()) << "Extension loaded during shutdown.";
 }
+#endif  // OS_CHROMEOS
 
 // This tests that the granted permissions preferences are correctly set when
 // installing an extension.
@@ -2373,6 +2376,8 @@ TEST_F(ExtensionServiceTest, UpdateExtension) {
             version()->GetString());
 }
 
+#if defined(OS_CHROMEOS)
+// TODO(jamescook): Remove ifdef after M23 backport, crbug.com/155994
 // Extensions should not be updated during browser shutdown.
 TEST_F(ExtensionServiceTest, UpdateExtensionDuringShutdown) {
   InitializeEmptyExtensionService();
@@ -2393,6 +2398,7 @@ TEST_F(ExtensionServiceTest, UpdateExtensionDuringShutdown) {
             service_->GetExtensionById(good_crx, false)->
                 version()->GetString());
 }
+#endif
 
 // Test updating a not-already-installed extension - this should fail
 TEST_F(ExtensionServiceTest, UpdateNotInstalledExtension) {
