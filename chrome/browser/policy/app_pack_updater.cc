dfea28c2174cd54deceed496b3d9ae0b1619e199
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-05 22:21:47 +0000

    Merge 166801 > Recover from damaged screensaver in the AppPack cache. > > BUG=159826 > > > Review URL: https://chromiumcodereview.appspot.com/11312150
    TBR=joaodasilva@chromium.org
    Review URL: https://codereview.chromium.org/11448026
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1271/src@171332 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/app_pack_updater.cc
+++ chrome/browser/policy/app_pack_updater.cc
@@ -162,10 +162,12 @@ void AppPackUpdater::Observe(int type,
       LoadPolicy();
       break;
 
-    case chrome::NOTIFICATION_EXTENSION_INSTALL_ERROR:
-      OnCrxInstallFailed(
-          content::Source<extensions::CrxInstaller>(source).ptr());
+    case chrome::NOTIFICATION_EXTENSION_INSTALL_ERROR: {
+      extensions::CrxInstaller* installer =
+          content::Source<extensions::CrxInstaller>(source).ptr();
+      OnDamagedFileDetected(installer->source_file());
       break;
+    }
 
     default:
       NOTREACHED();
@@ -558,9 +560,7 @@ void AppPackUpdater::OnCacheEntryInstalled(const std::string& id,
   }
 }
 
-void AppPackUpdater::OnCrxInstallFailed(extensions::CrxInstaller* installer) {
-  FilePath path = installer->source_file();
-
+void AppPackUpdater::OnDamagedFileDetected(const FilePath& path) {
   // Search for |path| in |cached_extensions_|, and delete it if found.
   for (CacheEntryMap::iterator it = cached_extensions_.begin();
        it != cached_extensions_.end(); ++it) {
