00d839b91fe041ca01d94d8fb4ebb54165002793
pastarmovj@google.com <pastarmovj@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-17 10:33:39 +0000

    Merge 176908 - Fix a problem where OpenDevice would open the wrong device if multiple are avaiable.
    The issue comes from the fact the OpenDevice should not select the device
    based on the user prefence but on the client request passed in the
    requested_device_id. Only if this is not possible will it use the
    default device.
    
    BUG=166925
    TEST=Manual: Plug multiple capture devices and try to select each one in flash settings.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11820050
    
    TBR=pastarmovj@chromium.org
    Review URL: https://codereview.chromium.org/11975042
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1364/src@177381 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/media/media_stream_manager.cc
+++ content/browser/renderer_host/media/media_stream_manager.cc
@@ -581,7 +581,8 @@ void MediaStreamManager::PostRequestToUI(const std::string& label) {
                                 request->render_view_id,
                                 request->options,
                                 request->security_origin,
-                                request->type);
+                                request->type,
+                                request->requested_device_id);
 }
 
 void MediaStreamManager::HandleRequest(const std::string& label) {
