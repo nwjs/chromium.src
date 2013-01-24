1fdd16b55208831c604b3f5ae3c57bc9e96ee9c6
tanyarad@chromium.org <tanyarad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-24 21:44:16 +0000

    Revert 178256 > Make RenderWidget responsible for the composited view's lifetime > > Chromium side of https://bugs.webkit.org/show_bug.cgi?id=105071. > content::RenderWidget constructs a WebLayerTreeView when WebKit > asks it and then retains ownership. This has to be destroyed > before the WebWidget is closed since the WLTVClient is (currently) > a WebKit object. > > BUG=156175,171950 > > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=175303 > > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=175827 > > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=176271 > > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=176707 > > Review URL: https://codereview.chromium.org/11575049
    TBR=jamesr@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1393/src@178653 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -5742,8 +5742,6 @@ void RenderViewImpl::OnClearFocusedNode() {
 void RenderViewImpl::OnSetBackground(const SkBitmap& background) {
   if (webview())
     webview()->setIsTransparent(!background.empty());
-  if (web_layer_tree_view_)
-    web_layer_tree_view_->setHasTransparentBackground(!background.empty());
 
   SetBackground(background);
 }
