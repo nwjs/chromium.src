031a43dd86819f56cf58486fb5ae014917dedfde
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-30 20:15:04 +0000

    Merge 179515 > Having URLFetcher handle SSL client auth by proceeding anonymously (eg: NULL > certificate) causes issues with both the SSL session cache and the SPDY session > pool when dealing with SSL proxies that require client certificate > authentication, but do so by allowing the handshake to continue and returning > 403 error codes. > > BUG=172349 > R=rch@chromium.org > > Review URL: https://codereview.chromium.org/12087056
    TBR=rsleevi@chromium.org
    Review URL: https://codereview.chromium.org/12092073
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1364/src@179688 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_fetcher_core.cc
+++ net/url_request/url_fetcher_core.cc
@@ -613,12 +613,6 @@ void URLFetcherCore::OnReadCompleted(URLRequest* request,
   }
 }
 
-void URLFetcherCore::OnCertificateRequested(
-    URLRequest* request,
-    SSLCertRequestInfo* cert_request_info) {
-  request->ContinueWithCertificate(NULL);
-}
-
 void URLFetcherCore::CancelAll() {
   g_registry.Get().CancelAll();
 }
