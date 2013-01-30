c6f2e6249e9552c6471983e7cb276a4ab33898c7
xians@chromium.org <xians@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-30 17:29:06 +0000

    Merge 178699 > Allowing multiple MediaPlayers to connect to WebRtcAudioDeviceImpl by sharing one WebRtcAudioRenderer. > > The audio is gone when new peerconnection is connecting to a media stream. What is happening is that the stream will pause the existing MediaPlayer and create new MediaPlayers to associated to it. But since we only allow one WebRtcAudioRenderer to connect to WebRtcAudioDeviceImpl, the new MediaPlayers audio won't be able to associate to stream. > > This patch fixes the problem by allowing multiple MediaPlayers to connect to WebRtcAudioDeviceImpl by sharing one WebRtcAudioRenderer > > BUG=173092 > TEST= > Use http://www.corp.google.com/~xians/rtc_peerconnection_multi.html to allow the page to create two mediastreams. > 2. Click on the add button once to create a PeerConnection. Notice that you can here audio. > 3. Click on the add button again. Notice that you can see another video stream but the audio playout stops. > > Review URL: https://chromiumcodereview.appspot.com/11896089
    TBR=xians@chromium.org
    Review URL: https://codereview.chromium.org/12088073
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1364/src@179665 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/webrtc_audio_renderer.cc
+++ content/renderer/media/webrtc_audio_renderer.cc
@@ -84,7 +84,8 @@ void AddHistogramFramesPerBuffer(int param) {
 WebRtcAudioRenderer::WebRtcAudioRenderer(int source_render_view_id)
     : state_(UNINITIALIZED),
       source_render_view_id_(source_render_view_id),
-      source_(NULL) {
+      source_(NULL),
+      play_ref_count_(0) {
 }
 
 WebRtcAudioRenderer::~WebRtcAudioRenderer() {
@@ -217,6 +218,8 @@ void WebRtcAudioRenderer::Play() {
   if (state_ == UNINITIALIZED)
     return;
 
+  DCHECK(play_ref_count_ == 0 || state_ == PLAYING);
+  ++play_ref_count_;
   state_ = PLAYING;
 }
 
@@ -225,7 +228,10 @@ void WebRtcAudioRenderer::Pause() {
   if (state_ == UNINITIALIZED)
     return;
 
-  state_ = PAUSED;
+  DCHECK_EQ(state_, PLAYING);
+  DCHECK_GT(play_ref_count_, 0);
+  if (!--play_ref_count_)
+    state_ = PAUSED;
 }
 
 void WebRtcAudioRenderer::Stop() {
