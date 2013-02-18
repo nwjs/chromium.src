CyanogenMod/android_external_chromium_org

commit 0e1a6c28769586170a0d42abe27b9e72aa4e4d41

Make ServerBoundCertStore interface async, move SQLiteServerBoundCertStore load onto DB thread.
Fix chromeos::ProfileAuthData::Transfer to only transfer server bound certs when cookies are being transferred.

BUG=89665,166919

Review URL: https://chromiumcodereview.appspot.com/11742037

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178742 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 646a620d85405183eb7de2613ff3ed64223a8dc0

chrome/browser/chromeos/login/profile_auth_data.cc
@@ -22,127 +22,189 @@ namespace chromeos {
 
 namespace {
 
-// Callback for transferring |cookies_to_transfer| into |cookie_monster| if
-// its jar is completely empty.
-void OnTransferCookiesIfEmptyJar(
-    net::CookieMonster* cookie_monster,
-    const net::CookieList& cookies_to_transfer,
-    const base::Callback<void()>& cookies_transfered_callback,
-    const net::CookieList& cookies_in_jar) {
-  std::string sid;
-  std::string lsid;
-  // Transfer only if the existing cookie jar is empty.
-  if (!cookies_in_jar.size())
-    cookie_monster->InitializeFrom(cookies_to_transfer);
+class ProfileAuthDataTransferer {
+ public:
+  ProfileAuthDataTransferer(
+      Profile* from_profile,
+      Profile* to_profile,
+      bool transfer_cookies,
+      const base::Closure& completion_callback);
 
+  void BeginTransfer();
+
+ private:
+  void BeginTransferOnIOThread();
+  void MaybeDoCookieAndCertTransfer();
+  void Finish();
+
+  void OnTransferCookiesIfEmptyJar(const net::CookieList& cookies_in_jar);
+  void OnGetCookiesToTransfer(const net::CookieList& cookies_to_transfer);
+  void RetrieveDefaultCookies();
+  void OnGetServerBoundCertsToTransfer(
+      const net::ServerBoundCertStore::ServerBoundCertList& certs);
+  void RetrieveDefaultServerBoundCerts();
+  void TransferDefaultAuthCache();
+
+  scoped_refptr<net::URLRequestContextGetter> from_context_;
+  scoped_refptr<net::URLRequestContextGetter> to_context_;
+  bool transfer_cookies_;
+  base::Closure completion_callback_;
+
+  net::CookieList cookies_to_transfer_;
+  net::ServerBoundCertStore::ServerBoundCertList certs_to_transfer_;
+
+  bool got_cookies_;
+  bool got_server_bound_certs_;
+};
+
+ProfileAuthDataTransferer::ProfileAuthDataTransferer(
+    Profile* from_profile,
+    Profile* to_profile,
+    bool transfer_cookies,
+    const base::Closure& completion_callback)
+    : from_context_(from_profile->GetRequestContext()),
+      to_context_(to_profile->GetRequestContext()),
+      transfer_cookies_(transfer_cookies),
+      completion_callback_(completion_callback),
+      got_cookies_(false),
+      got_server_bound_certs_(false) {
+}
+
+void ProfileAuthDataTransferer::BeginTransfer() {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  // If we aren't transferring cookies, post the completion callback
+  // immediately.  Otherwise, it will be called when both cookies and channel
+  // ids are finished transferring.
+  if (!transfer_cookies_) {
+    BrowserThread::PostTask(BrowserThread::UI, FROM_HERE, completion_callback_);
+    // Null the callback so that when Finish is called the callback won't be
+    // called again.
+    completion_callback_.Reset();
+  }
   BrowserThread::PostTask(
-      BrowserThread::UI, FROM_HERE, cookies_transfered_callback);
-  return;
+      BrowserThread::IO, FROM_HERE,
+      base::Bind(&ProfileAuthDataTransferer::BeginTransferOnIOThread,
+                 base::Unretained(this)));
+}
+
+void ProfileAuthDataTransferer::BeginTransferOnIOThread() {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  TransferDefaultAuthCache();
+
+  if (transfer_cookies_) {
+    RetrieveDefaultCookies();
+    RetrieveDefaultServerBoundCerts();
+  } else {
+    Finish();
+  }
+}
+
+// If both cookies and server bound certs have been retrieved, see if we need to
+// do the actual transfer.
+void ProfileAuthDataTransferer::MaybeDoCookieAndCertTransfer() {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  if (!(got_cookies_ && got_server_bound_certs_))
+    return;
+
+  // Nothing to transfer over?
+  if (!cookies_to_transfer_.size()) {
+    Finish();
+    return;
+  }
+
+  // Now let's see if the target cookie monster's jar is even empty.
+  net::CookieStore* to_store =
+      to_context_->GetURLRequestContext()->cookie_store();
+  net::CookieMonster* to_monster = to_store->GetCookieMonster();
+  to_monster->GetAllCookiesAsync(
+      base::Bind(&ProfileAuthDataTransferer::OnTransferCookiesIfEmptyJar,
+                 base::Unretained(this)));
+}
+
+// Post the |completion_callback_| and delete ourself.
+void ProfileAuthDataTransferer::Finish() {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  if (!completion_callback_.is_null())
+    BrowserThread::PostTask(BrowserThread::UI, FROM_HERE, completion_callback_);
+  delete this;
+}
+
+// Callback for transferring |cookies_to_transfer_| into |to_context_|'s
+// CookieMonster if its jar is completely empty.  If authentication was
+// performed by an extension, then the set of cookies that was acquired through
+// such that process will be automatically transfered into the profile.
+void ProfileAuthDataTransferer::OnTransferCookiesIfEmptyJar(
+    const net::CookieList& cookies_in_jar) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  // Transfer only if the existing cookie jar is empty.
+  if (!cookies_in_jar.size()) {
+    net::CookieStore* to_store =
+        to_context_->GetURLRequestContext()->cookie_store();
+    net::CookieMonster* to_monster = to_store->GetCookieMonster();
+    to_monster->InitializeFrom(cookies_to_transfer_);
+
+    net::ServerBoundCertService* to_cert_service =
+        to_context_->GetURLRequestContext()->server_bound_cert_service();
+    to_cert_service->GetCertStore()->InitializeFrom(certs_to_transfer_);
+  }
+
+  Finish();
 }
 
 // Callback for receiving |cookies_to_transfer| from the authentication profile
 // cookie jar.
-void OnGetCookiesToTransfer(
-    net::CookieMonster* cookie_monster,
-    const base::Callback<void()>& cookies_transfered_callback,
+void ProfileAuthDataTransferer::OnGetCookiesToTransfer(
     const net::CookieList& cookies_to_transfer) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
 
-  // Nothing to transfer over?
-  if (!cookies_to_transfer.size()) {
-    BrowserThread::PostTask(
-        BrowserThread::UI, FROM_HERE, cookies_transfered_callback);
-    return;
-  }
-  // Now let's see if the target cookie monster's jar is even empty.
-  cookie_monster->GetAllCookiesAsync(
-      base::Bind(&OnTransferCookiesIfEmptyJar,
-                 make_scoped_refptr(cookie_monster),
-                 cookies_to_transfer,
-                 cookies_transfered_callback));
+  got_cookies_ = true;
+  MaybeDoCookieAndCertTransfer();
 }
 
-// Transfers initial set of Profile cookies from the |from_context| to cookie
-// jar of |to_context|.
-void TransferDefaultCookiesOnIOThread(
-    net::URLRequestContextGetter* from_context,
-    net::URLRequestContextGetter* to_context,
-    const base::Callback<void()>& cookies_transfered_callback) {
+// Retrieves initial set of Profile cookies from the |from_context_|.
+void ProfileAuthDataTransferer::RetrieveDefaultCookies() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
-  net::CookieStore* to_store =
-      to_context->GetURLRequestContext()->cookie_store();
-  net::CookieMonster* to_monster = to_store->GetCookieMonster();
 
   net::CookieStore* from_store =
-      from_context->GetURLRequestContext()->cookie_store();
+      from_context_->GetURLRequestContext()->cookie_store();
   net::CookieMonster* from_monster = from_store->GetCookieMonster();
   from_monster->SetKeepExpiredCookies();
-  from_monster->GetAllCookiesAsync(base::Bind(&OnGetCookiesToTransfer,
-                                              make_scoped_refptr(to_monster),
-                                              cookies_transfered_callback));
+  from_monster->GetAllCookiesAsync(
+      base::Bind(&ProfileAuthDataTransferer::OnGetCookiesToTransfer,
+                 base::Unretained(this)));
 }
 
-// Transfers default server bound certs of |from_context| to server bound certs
-// storage of |to_context|.
-void TransferDefaultServerBoundCertsIOThread(
-    net::URLRequestContextGetter* from_context,
-    net::URLRequestContextGetter* to_context) {
+// Callback for receiving |cookies_to_transfer| from the authentication profile
+// cookie jar.
+void ProfileAuthDataTransferer::OnGetServerBoundCertsToTransfer(
+    const net::ServerBoundCertStore::ServerBoundCertList& certs) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
-  net::ServerBoundCertService* default_service =
-      from_context->GetURLRequestContext()->server_bound_cert_service();
-
-  net::ServerBoundCertStore::ServerBoundCertList server_bound_certs;
-  default_service->GetCertStore()->GetAllServerBoundCerts(&server_bound_certs);
-
-  net::ServerBoundCertService* new_service =
-      to_context->GetURLRequestContext()->server_bound_cert_service();
-  new_service->GetCertStore()->InitializeFrom(server_bound_certs);
+  certs_to_transfer_ = certs;
+  got_server_bound_certs_ = true;
+  MaybeDoCookieAndCertTransfer();
 }
 
-// Transfers default auth cache of |from_context| to auth cache storage of
-// |to_context|.
-void TransferDefaultAuthCacheOnIOThread(
-    net::URLRequestContextGetter* from_context,
-    net::URLRequestContextGetter* to_context) {
+// Retrieves server bound certs of |from_context_|.
+void ProfileAuthDataTransferer::RetrieveDefaultServerBoundCerts() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
-  net::HttpAuthCache* new_cache = to_context->GetURLRequestContext()->
-      http_transaction_factory()->GetSession()->http_auth_cache();
-  new_cache->UpdateAllFrom(*from_context->GetURLRequestContext()->
-      http_transaction_factory()->GetSession()->http_auth_cache());
+  net::ServerBoundCertService* from_service =
+      from_context_->GetURLRequestContext()->server_bound_cert_service();
+
+  from_service->GetCertStore()->GetAllServerBoundCerts(
+      base::Bind(&ProfileAuthDataTransferer::OnGetServerBoundCertsToTransfer,
+                 base::Unretained(this)));
 }
 
-// Transfers cookies and server bound certs from the |from_profile| into
-// the |to_profile|.  If authentication was performed by an extension, then
-// the set of cookies that was acquired through such that process will be
-// automatically transfered into the profile.
-void TransferDefaultCookiesAndServerBoundCerts(
-    Profile* from_profile,
-    Profile* to_profile,
-    const base::Callback<void()>& cookies_transfered_callback) {
-  BrowserThread::PostTask(
-      BrowserThread::IO, FROM_HERE,
-      base::Bind(&TransferDefaultCookiesOnIOThread,
-                 make_scoped_refptr(from_profile->GetRequestContext()),
-                 make_scoped_refptr(to_profile->GetRequestContext()),
-                 cookies_transfered_callback));
-  BrowserThread::PostTask(
-      BrowserThread::IO, FROM_HERE,
-      base::Bind(&TransferDefaultServerBoundCertsIOThread,
-                 make_scoped_refptr(from_profile->GetRequestContext()),
-                 make_scoped_refptr(to_profile->GetRequestContext())));
-}
-
-// Transfers HTTP authentication cache from the |from_profile|
-// into the |to_profile|. If user was required to authenticate with a proxy
+// Transfers HTTP authentication cache from the |from_context_|
+// into the |to_context_|. If user was required to authenticate with a proxy
 // during the login, this authentication information will be transferred
 // into the new session.
-void TransferDefaultAuthCache(Profile* from_profile,
-                              Profile* to_profile) {
-  BrowserThread::PostTask(
-      BrowserThread::IO, FROM_HERE,
-      base::Bind(&TransferDefaultAuthCacheOnIOThread,
-                 make_scoped_refptr(from_profile->GetRequestContext()),
-                 make_scoped_refptr(to_profile->GetRequestContext())));
+void ProfileAuthDataTransferer::TransferDefaultAuthCache() {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  net::HttpAuthCache* new_cache = to_context_->GetURLRequestContext()->
+      http_transaction_factory()->GetSession()->http_auth_cache();
+  new_cache->UpdateAllFrom(*from_context_->GetURLRequestContext()->
+      http_transaction_factory()->GetSession()->http_auth_cache());
 }
 
 }  // namespace
@@ -151,18 +213,10 @@ void ProfileAuthData::Transfer(
     Profile* from_profile,
     Profile* to_profile,
     bool transfer_cookies,
-    const base::Callback<void()>& cookies_transfered_callback) {
+    const base::Closure& completion_callback) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  if (transfer_cookies) {
-    TransferDefaultCookiesAndServerBoundCerts(from_profile,
-                                              to_profile,
-                                              cookies_transfered_callback);
-  } else {
-    BrowserThread::PostTask(
-        BrowserThread::UI, FROM_HERE, cookies_transfered_callback);
-  }
-
-  TransferDefaultAuthCache(from_profile, to_profile);
+  (new ProfileAuthDataTransferer(from_profile, to_profile, transfer_cookies,
+                                 completion_callback))->BeginTransfer();
 }
 
 }  // namespace chromeos

chrome/browser/browsing_data/browsing_data_remover.cc
@@ -991,7 +991,13 @@ void BrowsingDataRemover::ClearServerBoundCertsOnIOThread(
   net::ServerBoundCertService* server_bound_cert_service =
       rq_context->GetURLRequestContext()->server_bound_cert_service();
   server_bound_cert_service->GetCertStore()->DeleteAllCreatedBetween(
-      delete_begin_, delete_end_);
+      delete_begin_, delete_end_,
+      base::Bind(&BrowsingDataRemover::OnClearedServerBoundCertsOnIOThread,
+                 base::Unretained(this), base::Unretained(rq_context)));
+}
+
+void BrowsingDataRemover::OnClearedServerBoundCertsOnIOThread(
+    net::URLRequestContextGetter* rq_context) {
   // Need to close open SSL connections which may be using the channel ids we
   // are deleting.
   // TODO(mattm): http://crbug.com/166069 Make the server bound cert

chrome/browser/browsing_data/browsing_data_remover.h
@@ -321,6 +321,11 @@ class BrowsingDataRemover : public content::NotificationObserver,
   void ClearServerBoundCertsOnIOThread(
       net::URLRequestContextGetter* rq_context);
 
+  // Callback on IO Thread when server bound certs have been deleted. Clears SSL
+  // connection pool and posts to UI thread to run OnClearedServerBoundCerts.
+  void OnClearedServerBoundCertsOnIOThread(
+      net::URLRequestContextGetter* rq_context);
+
   // Callback when server bound certs have been deleted. Invokes
   // NotifyAndDeleteIfDone.
   void OnClearedServerBoundCerts();

chrome/browser/browsing_data/browsing_data_remover_unittest.cc
@@ -297,6 +297,11 @@ class RemoveServerBoundCertTester : public net::SSLConfigService::Observer {
                                 now + base::TimeDelta::FromDays(1));
   }
 
+  void GetCertList(net::ServerBoundCertStore::ServerBoundCertList* certs) {
+    GetCertStore()->GetAllServerBoundCerts(
+        base::Bind(&RemoveServerBoundCertTester::GetAllCertsCallback, certs));
+  }
+
   net::ServerBoundCertStore* GetCertStore() {
     return server_bound_cert_service_->GetCertStore();
   }
@@ -311,6 +316,12 @@ class RemoveServerBoundCertTester : public net::SSLConfigService::Observer {
   }
 
  private:
+  static void GetAllCertsCallback(
+      net::ServerBoundCertStore::ServerBoundCertList* dest,
+      const net::ServerBoundCertStore::ServerBoundCertList& result) {
+    *dest = result;
+  }
+
   net::ServerBoundCertService* server_bound_cert_service_;
   scoped_refptr<net::SSLConfigService> ssl_config_service_;
   int ssl_config_changed_count_;
@@ -711,7 +722,8 @@ TEST_F(BrowsingDataRemoverTest, RemoveServerBoundCertLastHour) {
   EXPECT_EQ(1, tester.ssl_config_changed_count());
   ASSERT_EQ(1, tester.ServerBoundCertCount());
   net::ServerBoundCertStore::ServerBoundCertList certs;
-  tester.GetCertStore()->GetAllServerBoundCerts(&certs);
+  tester.GetCertList(&certs);
+  ASSERT_EQ(1U, certs.size());
   EXPECT_EQ(kTestOrigin2, certs.front().server_identifier());
 }
 

chrome/browser/browsing_data/browsing_data_server_bound_cert_helper.cc
@@ -31,17 +31,18 @@ class BrowsingDataServerBoundCertHelperImpl
   // Fetch the certs. This must be called in the IO thread.
   void FetchOnIOThread();
 
+  void OnFetchComplete(
+      const net::ServerBoundCertStore::ServerBoundCertList& cert_list);
+
   // Notifies the completion callback. This must be called in the UI thread.
-  void NotifyInUIThread();
+  void NotifyInUIThread(
+      const net::ServerBoundCertStore::ServerBoundCertList& cert_list);
 
   // Delete a single cert. This must be called in IO thread.
   void DeleteOnIOThread(const std::string& server_id);
 
-  // Access to |server_bound_cert_list_| is triggered indirectly via the UI
-  // thread and guarded by |is_fetching_|. This means |server_bound_cert_list_|
-  // is only accessed while |is_fetching_| is true. The flag |is_fetching_| is
-  // only accessed on the UI thread.
-  net::ServerBoundCertStore::ServerBoundCertList server_bound_cert_list_;
+  // Called when deletion is done.
+  void DeleteCallback();
 
   // Indicates whether or not we're currently fetching information:
   // it's true when StartFetching() is called in the UI thread, and it's reset
@@ -97,20 +98,28 @@ void BrowsingDataServerBoundCertHelperImpl::FetchOnIOThread() {
       request_context_getter_->GetURLRequestContext()->
       server_bound_cert_service()->GetCertStore();
   if (cert_store) {
-    server_bound_cert_list_.clear();
-    cert_store->GetAllServerBoundCerts(&server_bound_cert_list_);
-    content::BrowserThread::PostTask(
-        content::BrowserThread::UI, FROM_HERE,
-        base::Bind(&BrowsingDataServerBoundCertHelperImpl::NotifyInUIThread,
-                   this));
+    cert_store->GetAllServerBoundCerts(base::Bind(
+        &BrowsingDataServerBoundCertHelperImpl::OnFetchComplete, this));
+  } else {
+    OnFetchComplete(net::ServerBoundCertStore::ServerBoundCertList());
   }
 }
 
-void BrowsingDataServerBoundCertHelperImpl::NotifyInUIThread() {
+void BrowsingDataServerBoundCertHelperImpl::OnFetchComplete(
+    const net::ServerBoundCertStore::ServerBoundCertList& cert_list) {
+  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::IO));
+  content::BrowserThread::PostTask(
+      content::BrowserThread::UI, FROM_HERE,
+      base::Bind(&BrowsingDataServerBoundCertHelperImpl::NotifyInUIThread,
+                 this, cert_list));
+}
+
+void BrowsingDataServerBoundCertHelperImpl::NotifyInUIThread(
+    const net::ServerBoundCertStore::ServerBoundCertList& cert_list) {
   DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
   DCHECK(is_fetching_);
   is_fetching_ = false;
-  completion_callback_.Run(server_bound_cert_list_);
+  completion_callback_.Run(cert_list);
   completion_callback_.Reset();
 }
 
@@ -121,14 +130,21 @@ void BrowsingDataServerBoundCertHelperImpl::DeleteOnIOThread(
       request_context_getter_->GetURLRequestContext()->
       server_bound_cert_service()->GetCertStore();
   if (cert_store) {
-    cert_store->DeleteServerBoundCert(server_id);
-    // Need to close open SSL connections which may be using the channel ids we
-    // are deleting.
-    // TODO(mattm): http://crbug.com/166069 Make the server bound cert
-    // service/store have observers that can notify relevant things directly.
-    request_context_getter_->GetURLRequestContext()->ssl_config_service()->
-        NotifySSLConfigChange();
+    cert_store->DeleteServerBoundCert(
+        server_id,
+        base::Bind(&BrowsingDataServerBoundCertHelperImpl::DeleteCallback,
+                   this));
   }
+}
+
+void BrowsingDataServerBoundCertHelperImpl::DeleteCallback() {
+  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::IO));
+  // Need to close open SSL connections which may be using the channel ids we
+  // are deleting.
+  // TODO(mattm): http://crbug.com/166069 Make the server bound cert
+  // service/store have observers that can notify relevant things directly.
+  request_context_getter_->GetURLRequestContext()->ssl_config_service()->
+      NotifySSLConfigChange();
 }
 
 }  // namespace

chrome/browser/chromeos/login/profile_auth_data.h
@@ -18,12 +18,12 @@ class ProfileAuthData {
  public:
   // Transfers proxy authentication cache and optionally |transfer_cookies| and
   // server bound certs from the profile that was used for authentication.
-  // |cookies_transfered_callback| will be called on UI thread after cookie
-  // transfer part of this operation is completed.
+  // |completion_callback| will be called on UI thread after the operation is
+  // completed.
   static void Transfer(Profile* from_profile,
                        Profile* to_profile,
                        bool transfer_cookies,
-                       const base::Closure& cookies_transfered_callback);
+                       const base::Closure& completion_callback);
 
  private:
   DISALLOW_IMPLICIT_CONSTRUCTORS(ProfileAuthData);

chrome/browser/net/sqlite_server_bound_cert_store.cc
@@ -41,9 +41,8 @@ class SQLiteServerBoundCertStore::Backend
         clear_on_exit_policy_(clear_on_exit_policy) {
   }
 
-  // Creates or load the SQLite database.
-  bool Load(
-      std::vector<net::DefaultServerBoundCertStore::ServerBoundCert*>* certs);
+  // Creates or loads the SQLite database.
+  void Load(const LoadedCallback& loaded_callback);
 
   // Batch a server bound cert addition.
   void AddServerBoundCert(
@@ -63,6 +62,10 @@ class SQLiteServerBoundCertStore::Backend
   void SetForceKeepSessionState();
 
  private:
+  void LoadOnDBThreadAndNotify(const LoadedCallback& loaded_callback);
+  void LoadOnDBThread(
+      std::vector<net::DefaultServerBoundCertStore::ServerBoundCert*>* certs);
+
   friend class base::RefCountedThreadSafe<SQLiteServerBoundCertStore::Backend>;
 
   // You should call Close() before destructing this object.
@@ -155,15 +158,36 @@ bool InitTable(sql::Connection* db) {
 
 }  // namespace
 
-bool SQLiteServerBoundCertStore::Backend::Load(
-    std::vector<net::DefaultServerBoundCertStore::ServerBoundCert*>* certs) {
+void SQLiteServerBoundCertStore::Backend::Load(
+    const LoadedCallback& loaded_callback) {
   // This function should be called only once per instance.
   DCHECK(!db_.get());
 
-  // TODO(paivanof@gmail.com): We do a lot of disk access in this function,
-  // thus we do an exception to allow IO on the UI thread. This code will be
-  // moved to the DB thread as part of http://crbug.com/89665.
-  base::ThreadRestrictions::ScopedAllowIO allow_io;
+  BrowserThread::PostTask(
+      BrowserThread::DB, FROM_HERE,
+      base::Bind(&Backend::LoadOnDBThreadAndNotify, this, loaded_callback));
+}
+
+void SQLiteServerBoundCertStore::Backend::LoadOnDBThreadAndNotify(
+    const LoadedCallback& loaded_callback) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::DB));
+  scoped_ptr<ScopedVector<net::DefaultServerBoundCertStore::ServerBoundCert> >
+      certs(new ScopedVector<net::DefaultServerBoundCertStore::ServerBoundCert>(
+          ));
+
+  LoadOnDBThread(&certs->get());
+
+  BrowserThread::PostTask(
+      BrowserThread::IO, FROM_HERE,
+      base::Bind(loaded_callback, base::Passed(&certs)));
+}
+
+void SQLiteServerBoundCertStore::Backend::LoadOnDBThread(
+    std::vector<net::DefaultServerBoundCertStore::ServerBoundCert*>* certs) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::DB));
+
+  // This method should be called only once per instance.
+  DCHECK(!db_.get());
 
   base::TimeTicks start = base::TimeTicks::Now();
 
@@ -171,7 +195,7 @@ bool SQLiteServerBoundCertStore::Backend::Load(
   // from it.
   const FilePath dir = path_.DirName();
   if (!file_util::PathExists(dir) && !file_util::CreateDirectory(dir))
-    return false;
+    return;
 
   int64 db_size = 0;
   if (file_util::GetFileSize(path_, &db_size))
@@ -181,13 +205,13 @@ bool SQLiteServerBoundCertStore::Backend::Load(
   if (!db_->Open(path_)) {
     NOTREACHED() << "Unable to open cert DB.";
     db_.reset();
-    return false;
+    return;
   }
 
   if (!EnsureDatabaseVersion() || !InitTable(db_.get())) {
     NOTREACHED() << "Unable to open cert DB.";
     db_.reset();
-    return false;
+    return;
   }
 
   db_->Preload();
@@ -198,7 +222,7 @@ bool SQLiteServerBoundCertStore::Backend::Load(
       "creation_time FROM origin_bound_certs"));
   if (!smt.is_valid()) {
     db_.reset();
-    return false;
+    return;
   }
 
   while (smt.Step()) {
@@ -218,12 +242,14 @@ bool SQLiteServerBoundCertStore::Backend::Load(
   }
 
   UMA_HISTOGRAM_COUNTS_10000("DomainBoundCerts.DBLoadedCount", certs->size());
+  base::TimeDelta load_time = base::TimeTicks::Now() - start;
   UMA_HISTOGRAM_CUSTOM_TIMES("DomainBoundCerts.DBLoadTime",
-                             base::TimeTicks::Now() - start,
+                             load_time,
                              base::TimeDelta::FromMilliseconds(1),
                              base::TimeDelta::FromMinutes(1),
                              50);
-  return true;
+  DVLOG(1) << "loaded " << certs->size() << " in " << load_time.InMilliseconds()
+           << " ms";
 }
 
 bool SQLiteServerBoundCertStore::Backend::EnsureDatabaseVersion() {
@@ -544,9 +570,9 @@ SQLiteServerBoundCertStore::SQLiteServerBoundCertStore(
     : backend_(new Backend(path, clear_on_exit_policy)) {
 }
 
-bool SQLiteServerBoundCertStore::Load(
-    std::vector<net::DefaultServerBoundCertStore::ServerBoundCert*>* certs) {
-  return backend_->Load(certs);
+void SQLiteServerBoundCertStore::Load(
+    const LoadedCallback& loaded_callback) {
+  backend_->Load(loaded_callback);
 }
 
 void SQLiteServerBoundCertStore::AddServerBoundCert(

chrome/browser/net/sqlite_server_bound_cert_store.h
@@ -28,9 +28,7 @@ class SQLiteServerBoundCertStore
                              ClearOnExitPolicy* clear_on_exit_policy);
 
   // net::DefaultServerBoundCertStore::PersistentStore:
-  virtual bool Load(
-      std::vector<net::DefaultServerBoundCertStore::ServerBoundCert*>* certs)
-          OVERRIDE;
+  virtual void Load(const LoadedCallback& loaded_callback) OVERRIDE;
   virtual void AddServerBoundCert(
       const net::DefaultServerBoundCertStore::ServerBoundCert& cert) OVERRIDE;
   virtual void DeleteServerBoundCert(

nwjs/chromium.src

commit 17f748c00bb59ba87e3a319ae1906673357f743b

Merge 182833 > Fixes cookie jar transfer regression. > > This regression introduced with r178742. > > BUG=176513 > TEST=make sure cookie jar transfer works even if connection is lost during login > > Review URL: https://codereview.chromium.org/12287002
TBR=zelidrag@chromium.org
Review URL: https://codereview.chromium.org/12301011

git-svn-id: svn://svn.chromium.org/chrome/branches/1410/src@183132 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/chromeos/login/profile_auth_data.cc
@@ -158,6 +158,7 @@ void ProfileAuthDataTransferer::OnGetCookiesToTransfer(
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
 
   got_cookies_ = true;
+  cookies_to_transfer_ = cookies_to_transfer;
   MaybeDoCookieAndCertTransfer();
 }
 