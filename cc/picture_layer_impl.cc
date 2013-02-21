07847b5ba1d8834789fbbc130ea5c7c7858ce734
sievers@chromium.org <sievers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-21 23:24:06 +0000

    Merge 182932 > cc: Avoid unnecessary blending with impl-side painting. > > Prevent blending from being use when drawing all picture layers by > setting Tile::opaque_rect_ to contents rect when layer contents is > opaque. > > BUG=176446 > NOTRY=True > > > Review URL: https://chromiumcodereview.appspot.com/12278011
    TBR=reveman@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1410/src@183894 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/picture_layer_impl.cc
+++ cc/picture_layer_impl.cc
@@ -333,6 +333,7 @@ scoped_refptr<Tile> PictureLayerImpl::CreateTile(PictureLayerTiling* tiling,
       content_rect.size(),
       GL_RGBA,
       content_rect,
+      contentsOpaque() ? content_rect : gfx::Rect(),
       tiling->contents_scale()));
 }
 
