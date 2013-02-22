468cb167f14a89ab8d02272ca62989b12437308e
kerz@chromium.org <kerz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-22 02:23:08 +0000

    Revert 183647 > Merge 182894 > > Added interstitial page for merge session. This page will let us delay loading of Google properties during ChromeOS cookie session restore at login (aka /MergeSession). > > > > The interstitial page will show 3 seconds of no UI (white tab) followed with up to 7 seconds of UI with only "Loading..." message showing in tabs. For huge majority of cases, that secondary UI won't show since merge session will probably complete by then. > > > > BUG=171980 > > TEST=added merge_session_load_page_unittest.cc > > TBR=sky > > > > Review URL: https://codereview.chromium.org/12256046 > > TBR=zelidrag@chromium.org > Review URL: https://codereview.chromium.org/12319034
    TBR=zelidrag@chromium.org
    Review URL: https://codereview.chromium.org/12340017
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1364/src@183979 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/oauth_login_manager.h
+++ chrome/browser/chromeos/login/oauth_login_manager.h
@@ -5,6 +5,8 @@
 #ifndef CHROME_BROWSER_CHROMEOS_LOGIN_OAUTH_LOGIN_MANAGER_H_
 #define CHROME_BROWSER_CHROMEOS_LOGIN_OAUTH_LOGIN_MANAGER_H_
 
+#include <string>
+
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "chrome/browser/chromeos/cros/cert_library.h"
@@ -42,9 +44,6 @@ class OAuthLoginManager {
   class Delegate {
    public:
     virtual ~Delegate() {}
-
-    // Raised when merge session is completed.
-    virtual void OnCompletedMergeSession() = 0;
 
     // Raised when cookie jar authentication is successfully completed.
     virtual void OnCompletedAuthentication(Profile* user_profile) = 0;
