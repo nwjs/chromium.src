e7411bb33b4fcab8d4cb64d3f2a836d6fbb7d353
jln@chromium.org <jln@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-28 01:15:39 +0000

    Merge 184810 > Linux sandbox : allow part of advanced scheduling API > > In PPAPI processes we allow part of the advanced scheduling > API in the seccomp-bpf sandbox. > > BUG=178357 > NOTRY=true > > Review URL: https://chromiumcodereview.appspot.com/12314121
    TBR=jln@chromium.org
    Review URL: https://codereview.chromium.org/12381007
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1410/src@185091 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/sandbox_seccomp_bpf_linux.cc
+++ content/common/sandbox_seccomp_bpf_linux.cc
@@ -1403,7 +1403,11 @@ ErrorCode RendererOrWorkerProcessPolicy(int sysno, void *) {
 
 ErrorCode FlashProcessPolicy(int sysno, void *) {
   switch (sysno) {
+    case __NR_sched_get_priority_max:
+    case __NR_sched_get_priority_min:
     case __NR_sched_getaffinity:
+    case __NR_sched_getparam:
+    case __NR_sched_getscheduler:
     case __NR_sched_setscheduler:
     case __NR_times:
       return ErrorCode(ErrorCode::ERR_ALLOWED);
