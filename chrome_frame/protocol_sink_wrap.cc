31b26dc4d608dd82710294123b49626559c64380
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-04 17:37:57 +0000

    Revert 184747 > Merge 184718 - ChromeFrame sites in IE would appear truncated at times. > > This was a regression caused by the change to invalidate the cached protocol data when IE (urlmon) > terminates the protocol. While this works well for non CF requests, for CF requests when we report > the changed mime type to be that of chrome, the protocol is terminated with cached data. When Chrome > attempts to read it we don't have anything to report which leads to the problem. > > Fix is to not invalidate the protocol data for Chrome requests in our Terminate hook. We should invalidate > it for Abort because that seems like an error condition. > > BUG=178421 > R=robertshield. > Review URL: https://chromiumcodereview.appspot.com/12310145 > > TBR=ananta@chromium.org > Review URL: https://chromiumcodereview.appspot.com/12314131
    TBR=ananta@chromium.org
    Review URL: https://codereview.chromium.org/12391064
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1364/src@185915 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/protocol_sink_wrap.cc
+++ chrome_frame/protocol_sink_wrap.cc
@@ -871,9 +871,7 @@ STDMETHODIMP Hook_Terminate(InternetProtocol_Terminate_Fn orig_req,
                             IInternetProtocol* protocol,
                             DWORD options) {
   scoped_refptr<ProtData> prot_data = ProtData::DataFromProtocol(protocol);
-  // TODO(ananta/robertshield)
-  // Write a test for this bug http://crbug.com/178421
-  if (prot_data && !IsChrome(prot_data->renderer_type()))
+  if (prot_data)
     prot_data->Invalidate();
 
   // We are just pass through at this point, avoid false positive crash
