b22c2fb50d0b876a52aa6809b1e865f0717a7387
apatrick@google.com <apatrick@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-07 19:50:31 +0000

    Merge 186345 > Windows: Disable GPU sandbox if DisplayLink version 7.1 or earlier is installed. > > DisplayLink crashes the GPU process while the Direct3D device is being created if the sandbox is enabled. > > Before landing this, I will revert r185146 and rebase so that it merges without conflict. > > BUG=177611 > > Review URL: https://chromiumcodereview.appspot.com/12381085
    TBR=apatrick@chromium.org
    Review URL: https://codereview.chromium.org/12632009
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1410/src@186767 0039d316-1c4b-4281-b951-d872f2087c98

--- content/gpu/gpu_info_collector_win.cc
+++ content/gpu/gpu_info_collector_win.cc
@@ -25,6 +25,7 @@
 #include "base/string_number_conversions.h"
 #include "base/string_util.h"
 #include "base/threading/worker_pool.h"
+#include "base/win/registry.h"
 #include "base/win/scoped_com_initializer.h"
 #include "base/win/scoped_comptr.h"
 #include "base/win/windows_version.h"
@@ -33,6 +34,14 @@
 #include "ui/gl/gl_surface_egl.h"
 
 namespace {
+
+// This must be kept in sync with histograms.xml.
+enum DisplayLinkInstallationStatus {
+  DISPLAY_LINK_NOT_INSTALLED,
+  DISPLAY_LINK_7_1_OR_EARLIER,
+  DISPLAY_LINK_7_2_OR_LATER,
+  DISPLAY_LINK_INSTALLATION_STATUS_MAX
+};
 
 float ReadXMLFloatValue(XmlReader* reader) {
   std::string score_string;
@@ -153,6 +162,28 @@ content::GpuPerformanceStats RetrieveGpuPerformanceStatsWithHistograms() {
   return stats;
 }
 
+// Returns the display link driver version or an invalid version if it is
+// not installed.
+Version DisplayLinkVersion() {
+  base::win::RegKey key;
+
+  if (FAILED(key.Open(
+      HKEY_LOCAL_MACHINE, L"SOFTWARE", KEY_READ | KEY_WOW64_64KEY))) {
+    return Version();
+  }
+
+  if (FAILED(key.OpenKey(L"DisplayLink", KEY_READ | KEY_WOW64_64KEY)))
+    return Version();
+
+  if (FAILED(key.OpenKey(L"Core", KEY_READ | KEY_WOW64_64KEY)))
+    return Version();
+
+  string16 version;
+  if (FAILED(key.ReadValue(L"Version", &version)))
+    return Version();
+
+  return Version(WideToASCII(version));
+}
 }  // namespace anonymous
 
 namespace gpu_info_collector {
@@ -476,6 +507,22 @@ bool CollectBasicGraphicsInfo(content::GPUInfo* gpu_info) {
   // nvd3d9wrap.dll is loaded into all processes when Optimus is enabled.
   HMODULE nvd3d9wrap = GetModuleHandleW(L"nvd3d9wrap.dll");
   gpu_info->optimus = nvd3d9wrap != NULL;
+
+  gpu_info->display_link_version = DisplayLinkVersion();
+
+  if (!gpu_info->display_link_version .IsValid()) {
+    UMA_HISTOGRAM_ENUMERATION("GPU.DisplayLinkInstallationStatus",
+                              DISPLAY_LINK_NOT_INSTALLED,
+                              DISPLAY_LINK_INSTALLATION_STATUS_MAX);
+  } else if (gpu_info->display_link_version.IsOlderThan("7.2")) {
+    UMA_HISTOGRAM_ENUMERATION("GPU.DisplayLinkInstallationStatus",
+                              DISPLAY_LINK_7_1_OR_EARLIER,
+                              DISPLAY_LINK_INSTALLATION_STATUS_MAX);
+  } else {
+    UMA_HISTOGRAM_ENUMERATION("GPU.DisplayLinkInstallationStatus",
+                              DISPLAY_LINK_7_2_OR_LATER,
+                              DISPLAY_LINK_INSTALLATION_STATUS_MAX);
+  }
 
   // Taken from http://developer.nvidia.com/object/device_ids.html
   DISPLAY_DEVICE dd;
