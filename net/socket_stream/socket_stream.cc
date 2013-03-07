2064b43c7f2f36654673a298e024fa89c39f1711
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-07 06:42:22 +0000

    Revert 186637 > Merge 184840 > > Attempt to connect to wss:// which requested client cert even if it doesn't have any corresponding client cert. > > > > > > BUG=177606 > > TEST=none > > > > > > Review URL: https://chromiumcodereview.appspot.com/12335027 > > TBR=tyoshino@chromium.org > Review URL: https://codereview.chromium.org/12585002
    TBR=toyoshim@chromium.org
    Review URL: https://codereview.chromium.org/12589002
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1364/src@186646 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket_stream/socket_stream.cc
+++ net/socket_stream/socket_stream.cc
@@ -1143,10 +1143,9 @@ GURL SocketStream::ProxyAuthOrigin() const {
 }
 
 int SocketStream::HandleCertificateRequest(int result, SSLConfig* ssl_config) {
-  if (ssl_config->send_client_cert) {
+  if (ssl_config->send_client_cert)
     // We already have performed SSL client authentication once and failed.
     return result;
-  }
 
   DCHECK(socket_.get());
   scoped_refptr<SSLCertRequestInfo> cert_request_info = new SSLCertRequestInfo;
@@ -1161,22 +1160,14 @@ int SocketStream::HandleCertificateRequest(int result, SSLConfig* ssl_config) {
   if (!session.get())
     return result;
 
-  // If the user selected one of the certificates in client_certs or declined
-  // to provide one for this server before, use the past decision
-  // automatically.
   scoped_refptr<X509Certificate> client_cert;
-  if (!session->ssl_client_auth_cache()->Lookup(
-          cert_request_info->host_and_port, &client_cert)) {
+  bool found_cached_cert = session->ssl_client_auth_cache()->Lookup(
+      cert_request_info->host_and_port, &client_cert);
+  if (!found_cached_cert)
     return result;
-  }
+  if (!client_cert)
+    return result;
 
-  // Note: |client_cert| may be NULL, indicating that the caller
-  // wishes to proceed anonymously (eg: continue the handshake
-  // without sending a client cert)
-  //
-  // Check that the certificate selected is still a certificate the server
-  // is likely to accept, based on the criteria supplied in the
-  // CertificateRequest message.
   const std::vector<scoped_refptr<X509Certificate> >& client_certs =
       cert_request_info->client_certs;
   bool cert_still_valid = false;
@@ -1186,7 +1177,7 @@ int SocketStream::HandleCertificateRequest(int result, SSLConfig* ssl_config) {
       break;
     }
   }
-  if (client_cert && !cert_still_valid)
+  if (!cert_still_valid)
     return result;
 
   ssl_config->send_client_cert = true;
