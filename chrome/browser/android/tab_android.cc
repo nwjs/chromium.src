2ff4f4fff8e2195c669fc44a76d7f4957d5ed5f1
newt@chromium.org <newt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-07 00:52:34 +0000

    Merge 186242 > Show alternate error page for non-existent URLs. > > Navigating to iwoeaslkdfja.com will now show the alternate error/suggestion > page, instead of the net::ERR_NAME_NOT_RESOLVED error page. > > BUG=179399 > > > Review URL: https://chromiumcodereview.appspot.com/12391082
    TBR=newt@chromium.org
    Review URL: https://codereview.chromium.org/12472021
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1410/src@186557 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/tab_android.cc
+++ chrome/browser/android/tab_android.cc
@@ -15,6 +15,7 @@
 #include "chrome/browser/prerender/prerender_tab_helper.h"
 #include "chrome/browser/sessions/session_tab_helper.h"
 #include "chrome/browser/ssl/ssl_tab_helper.h"
+#include "chrome/browser/ui/alternate_error_tab_observer.h"
 #include "chrome/browser/ui/android/window_android_helper.h"
 #include "chrome/browser/ui/autofill/tab_autofill_manager_delegate.h"
 #include "chrome/browser/ui/blocked_content/blocked_content_tab_helper.h"
@@ -56,6 +57,7 @@ void BrowserTabContents::AttachTabHelpers(WebContents* contents) {
   SessionTabHelper::CreateForWebContents(contents);
 
   TabAutofillManagerDelegate::CreateForWebContents(contents);
+  AlternateErrorPageTabObserver::CreateForWebContents(contents);
   AutofillManager::CreateForWebContentsAndDelegate(
       contents, TabAutofillManagerDelegate::FromWebContents(contents));
   AutofillExternalDelegate::CreateForWebContentsAndManager(
