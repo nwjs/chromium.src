0bbaa1a506049c255183c2de56a181c28a2fac5a
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-12 01:19:43 +0000

    Merge 186830 "Fix crash in ExtensionUninstallDialogGtk::Show."
    > Fix crash in ExtensionUninstallDialogGtk::Show.
    > 
    > BUG=179634
    > 
    > Review URL: https://chromiumcodereview.appspot.com/12432009
    
    TBR=meacer@chromium.org
    Review URL: https://codereview.chromium.org/12440032
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1410/src@187442 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_uninstall_dialog.cc
+++ chrome/browser/extensions/extension_uninstall_dialog.cc
@@ -55,7 +55,7 @@ ExtensionUninstallDialog::ExtensionUninstallDialog(
       ui_loop_(MessageLoop::current()) {
   if (browser) {
     registrar_.Add(this,
-                   chrome::NOTIFICATION_BROWSER_CLOSING,
+                   chrome::NOTIFICATION_BROWSER_CLOSED,
                    content::Source<Browser>(browser));
   }
 }
@@ -116,7 +116,7 @@ void ExtensionUninstallDialog::Observe(
     int type,
     const content::NotificationSource& source,
     const content::NotificationDetails& details) {
-  DCHECK(type == chrome::NOTIFICATION_BROWSER_CLOSING);
+  DCHECK(type == chrome::NOTIFICATION_BROWSER_CLOSED);
 
   browser_ = NULL;
   // If the browser is closed while waiting for the image, we need to send a
