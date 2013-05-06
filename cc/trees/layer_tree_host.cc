cbc2b8a76882896e5c9e093419c91ea5912f7ae5
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-06 17:28:08 +0000

    Merge 196324 "If we give up on compositing, never try to reiniti..."
    > If we give up on compositing, never try to reinitialize the renderer.
    > 
    > If the layer tree host calls DidRecreateOutputSurface(false), it means that
    > we've given up. We can't use compositing mode. Normally, the embedder takes this
    > as a cue to stop using the compositor. It looks like in this bug, we're forcing
    > the compositor back into action (possibly by trying to take a screenshot). This
    > patch adds a safeguard to prevent us from incorrectly attempting reinitialize
    > the renderer in this case.
    > 
    > 
    > BUG=174138
    > 
    > Review URL: https://chromiumcodereview.appspot.com/14238011
    
    TBR=vollick@chromium.org
    
    Review URL: https://codereview.chromium.org/14741006
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1453/src@198475 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/trees/layer_tree_host.cc
+++ cc/trees/layer_tree_host.cc
@@ -79,6 +79,7 @@ LayerTreeHost::LayerTreeHost(LayerTreeHostClient* client,
       client_(client),
       commit_number_(0),
       rendering_stats_instrumentation_(RenderingStatsInstrumentation::Create()),
+      renderer_can_be_initialized_(true),
       renderer_initialized_(false),
       output_surface_lost_(false),
       num_failed_recreate_attempts_(0),
@@ -150,6 +151,7 @@ void LayerTreeHost::InitializeRenderer() {
   if (!proxy_->InitializeRenderer()) {
     // Uh oh, better tell the client that we can't do anything with this output
     // surface.
+    renderer_can_be_initialized_ = false;
     client_->DidRecreateOutputSurface(false);
     return;
   }
@@ -209,6 +211,7 @@ LayerTreeHost::RecreateResult LayerTreeHost::RecreateOutputSurface() {
 
   // We have tried too many times to recreate the output surface. Tell the
   // host to fall back to software rendering.
+  renderer_can_be_initialized_ = false;
   client_->DidRecreateOutputSurface(false);
   return RecreateFailedAndGaveUp;
 }
@@ -653,6 +656,9 @@ void LayerTreeHost::ScheduleComposite() {
 }
 
 bool LayerTreeHost::InitializeRendererIfNeeded() {
+  if (!renderer_can_be_initialized_)
+    return false;
+
   if (!renderer_initialized_) {
     InitializeRenderer();
     // If we couldn't initialize, then bail since we're returning to software
