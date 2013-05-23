ChromiumWebApps/chromium

commit 0248d9208d15bb5d7ae5bb482740e90ee62c8b2a

sync: Finish the SyncScheduler refactor
This change removes SyncSessionJob entirely.  The first step towards
this goal was to refactor all functions that depended on SyncSessionJob.
All these functions have either been inlined or modified to take
different parameters instead.  DoSyncSessionJob was split into
two separate functions, one for configure jobs and one for nudge jobs,
which removes the need for SyncSessionJob's "purpose" member.

The SyncScheduler's pending_configure_job_ has been replaced with
a ConfigParams member, since that was the only part of the job still
being referenced.  The pending_nudge_job_ has been replaced with
scheduled_nudge_time_ (which is similar to the old scheduled_start_
member of SyncSessionJob) and a new object called a NudgeTracker.

The NudgeTracker inherits the SyncSessionJob's responsibilities with
respect to coalescing nudge sources.  The plan is to extend this class
to support more sophisticated nudge coalescing in the future.  For now
it tries to emulate the old SyncSessionJob behaviour as closely as
possible.

Some of the refactoring does change behaviour.  In particular, the
decision-making logic has been updated to fix issues 179515 and 155296.

BUG=175024,179515,155296

Review URL: https://chromiumcodereview.appspot.com/13743003

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194766 0039d316-1c4b-4281-b951-d872f2087c98

sync/engine/sync_scheduler_impl.h
@@ -22,17 +22,21 @@
 #include "sync/engine/net/server_connection_manager.h"
 #include "sync/engine/nudge_source.h"
 #include "sync/engine/sync_scheduler.h"
-#include "sync/engine/sync_session_job.h"
 #include "sync/engine/syncer.h"
 #include "sync/internal_api/public/base/model_type_invalidation_map.h"
 #include "sync/internal_api/public/engine/polling_constants.h"
 #include "sync/internal_api/public/util/weak_handle.h"
+#include "sync/sessions/nudge_tracker.h"
 #include "sync/sessions/sync_session.h"
 #include "sync/sessions/sync_session_context.h"
 
 namespace syncer {
 
 class BackoffDelayProvider;
+
+namespace sessions {
+struct ModelNeutralState;
+}
 
 class SYNC_EXPORT_PRIVATE SyncSchedulerImpl
     : public SyncScheduler,
@@ -83,15 +87,6 @@ class SYNC_EXPORT_PRIVATE SyncSchedulerImpl
       const sessions::SyncSessionSnapshot& snapshot) OVERRIDE;
 
  private:
-  enum JobProcessDecision {
-    // Indicates we should continue with the current job.
-    CONTINUE,
-    // Indicates that we should save it to be processed later.
-    SAVE,
-    // Indicates we should drop this job.
-    DROP,
-  };
-
   enum JobPriority {
     // Non-canary jobs respect exponential backoff.
     NORMAL_PRIORITY,
@@ -99,10 +94,18 @@ class SYNC_EXPORT_PRIVATE SyncSchedulerImpl
     CANARY_PRIORITY
   };
 
+  enum PollAdjustType {
+    // Restart the poll interval.
+    FORCE_RESET,
+    // Restart the poll interval only if its length has changed.
+    UPDATE_INTERVAL,
+  };
+
   friend class SyncSchedulerTest;
   friend class SyncSchedulerWhiteboxTest;
   friend class SyncerTest;
 
+  FRIEND_TEST_ALL_PREFIXES(SyncSchedulerWhiteboxTest, NoNudgesInConfigureMode);
   FRIEND_TEST_ALL_PREFIXES(SyncSchedulerWhiteboxTest,
       DropNudgeWhileExponentialBackOff);
   FRIEND_TEST_ALL_PREFIXES(SyncSchedulerWhiteboxTest, SaveNudge);
@@ -145,57 +148,37 @@ class SYNC_EXPORT_PRIVATE SyncSchedulerImpl
 
   static const char* GetModeString(Mode mode);
 
-  static const char* GetDecisionString(JobProcessDecision decision);
-
-  // Invoke the syncer to perform a non-POLL job.
-  bool DoSyncSessionJobImpl(scoped_ptr<SyncSessionJob> job,
-                            JobPriority priority);
-
   // Invoke the syncer to perform a nudge job.
   void DoNudgeSyncSessionJob(JobPriority priority);
 
   // Invoke the syncer to perform a configuration job.
   bool DoConfigurationSyncSessionJob(JobPriority priority);
 
-  // Returns whether or not it's safe to run a poll job at this time.
-  bool ShouldPoll();
+  // Helper function for Do{Nudge,Configuration}SyncSessionJob.
+  void HandleFailure(
+      const sessions::ModelNeutralState& model_neutral_state);
 
   // Invoke the Syncer to perform a poll job.
   void DoPollSyncSessionJob();
 
-  // Called after the Syncer has performed the sync represented by |job|, to
-  // reset our state.  |exited_prematurely| is true if the Syncer did not
-  // cycle from job.start_step() to job.end_step(), likely because the
-  // scheduler was forced to quit the job mid-way through.
-  bool FinishSyncSessionJob(SyncSessionJob* job,
-                            bool exited_prematurely,
-                            sessions::SyncSession* session);
-
-  // Helper to schedule retries of a failed configure or nudge job.
-  void ScheduleNextSync(scoped_ptr<SyncSessionJob> finished_job,
-                        sessions::SyncSession* session);
-
-  // Helper to configure polling intervals. Used by Start and ScheduleNextSync.
-  void AdjustPolling(const SyncSessionJob* old_job);
+  // Adjusts the poll timer to account for new poll interval, and possibly
+  // resets the poll interval, depedning on the flag's value.
+  void AdjustPolling(PollAdjustType type);
 
   // Helper to restart waiting with |wait_interval_|'s timer.
   void RestartWaiting();
 
-  // Helper to ScheduleNextSync in case of consecutive sync errors.
-  void HandleContinuationError(scoped_ptr<SyncSessionJob> old_job,
-                               sessions::SyncSession* session);
+  // Helper to adjust our wait interval when we expereince a transient failure.
+  void UpdateExponentialBackoff(
+      const sessions::ModelNeutralState& model_neutral_state);
 
-  // Decide whether we should CONTINUE, SAVE or DROP the job.
-  JobProcessDecision DecideOnJob(const SyncSessionJob& job,
-                                 JobPriority priority);
+  // Determines if we're allowed to contact the server right now.
+  bool CanRunJobNow(JobPriority priority);
 
-  // Decide on whether to CONTINUE, SAVE or DROP the job when we are in
-  // backoff mode.
-  JobProcessDecision DecideWhileInWaitInterval(const SyncSessionJob& job,
-                                               JobPriority priority);
+  // Determines if we're allowed to contact the server right now.
+  bool CanRunNudgeJobNow(JobPriority priority);
 
-  // 'Impl' here refers to real implementation of public functions, running on
-  // |thread_|.
+  // 'Impl' here refers to real implementation of public functions.
   void StopImpl(const base::Closure& callback);
 
   // If the scheduler's current state supports it, this will create a job based
@@ -230,9 +213,9 @@ class SYNC_EXPORT_PRIVATE SyncSchedulerImpl
   // Creates a session for a poll and performs the sync.
   void PollTimerCallback();
 
-  // Called once the first time thread_ is started to broadcast an initial
-  // session snapshot containing data like initial_sync_ended.  Important when
-  // the client starts up and does not need to perform an initial sync.
+  // Called as we are started to broadcast an initial session snapshot
+  // containing data like initial_sync_ended.  Important when the client starts
+  // up and does not need to perform an initial sync.
   void SendInitialSnapshot();
 
   // This is used for histogramming and analysis of ScheduleNudge* APIs.
@@ -280,13 +263,15 @@ class SYNC_EXPORT_PRIVATE SyncSchedulerImpl
   // The event that will wake us up.
   base::OneShotTimer<SyncSchedulerImpl> pending_wakeup_timer_;
 
-  // Pending configure job storage.  Note that
-  // (mode_ != CONFIGURATION_MODE) \implies !pending_configure_job_.
-  scoped_ptr<SyncSessionJob> pending_configure_job_;
+  // Storage for variables related to an in-progress configure request.  Note
+  // that (mode_ != CONFIGURATION_MODE) \implies !pending_configure_params_.
+  scoped_ptr<ConfigurationParams> pending_configure_params_;
 
-  // Pending nudge job storage.  These jobs can exist in CONFIGURATION_MODE, but
-  // they will be run only in NORMAL_MODE.
-  scoped_ptr<SyncSessionJob> pending_nudge_job_;
+  // If we have a nudge pending to run soon, it will be listed here.
+  base::TimeTicks scheduled_nudge_time_;
+
+  // Keeps track of work that the syncer needs to handle.
+  sessions::NudgeTracker nudge_tracker_;
 
   // Invoked to run through the sync cycle.
   scoped_ptr<Syncer> syncer_;

chrome/browser/sync/profile_sync_service_harness.h
@@ -195,6 +195,10 @@ class ProfileSyncServiceHarness
   // Gets the |auto_start_enabled_| variable from the |service_|.
   bool AutoStartEnabled();
 
+  // Returns true if a status change took place, false on timeout.
+  bool AwaitStatusChangeWithTimeout(int timeout_milliseconds,
+                                    const std::string& reason);
+
  private:
   friend class StateChangeTimeoutEvent;
 
@@ -285,10 +289,6 @@ class ProfileSyncServiceHarness
   // Finite state machine for controlling state.  Returns true only if a state
   // change has taken place.
   bool RunStateChangeMachine();
-
-  // Returns true if a status change took place, false on timeout.
-  bool AwaitStatusChangeWithTimeout(int timeout_milliseconds,
-                                    const std::string& reason);
 
   // A helper for implementing IsDataSynced() and IsFullySynced().
   bool IsDataSyncedImpl(

chrome/browser/sync/test/integration/sync_errors_test.cc
@@ -140,20 +140,13 @@ IN_PROC_BROWSER_TEST_F(SyncErrorTest,
       protocol_error.error_description);
 }
 
-// Trigger an auth error and make sure the sync client detects it when
-// trying to commit.
+// Trigger an auth error and make sure the sync client displays a warning in the
+// UI.
 IN_PROC_BROWSER_TEST_F(SyncErrorTest, AuthErrorTest) {
-  ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
-
-  const BookmarkNode* node1 = AddFolder(0, 0, L"title1");
-  SetTitle(0, node1, L"new_title1");
-  ASSERT_TRUE(GetClient(0)->AwaitFullSyncCompletion("Sync."));
-
+  ASSERT_TRUE(SetupClients());
   TriggerAuthError();
 
-  const BookmarkNode* node2 = AddFolder(0, 0, L"title2");
-  SetTitle(0, node2, L"new_title2");
-  ASSERT_TRUE(GetClient(0)->AwaitExponentialBackoffVerification());
+  ASSERT_FALSE(GetClient(0)->SetupSync());
   ASSERT_EQ(GoogleServiceAuthError::INVALID_GAIA_CREDENTIALS,
             GetClient(0)->service()->GetAuthError().state());
 }

sync/engine/sync_scheduler_unittest.cc
@@ -724,7 +724,7 @@ TEST_F(SyncSchedulerTest, ThrottlingDoesThrottle) {
   StartSyncScheduler(SyncScheduler::NORMAL_MODE);
 
   scheduler()->ScheduleNudgeAsync(
-      zero(), NUDGE_SOURCE_LOCAL, types, FROM_HERE);
+      TimeDelta::FromMicroseconds(1), NUDGE_SOURCE_LOCAL, types, FROM_HERE);
   PumpLoop();
 
   StartSyncScheduler(SyncScheduler::CONFIGURATION_MODE);

sync/engine/sync_scheduler_whitebox_unittest.cc
@@ -1,258 +1,0 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "base/message_loop.h"
-#include "base/time.h"
-#include "sync/engine/backoff_delay_provider.h"
-#include "sync/engine/sync_scheduler_impl.h"
-#include "sync/engine/throttled_data_type_tracker.h"
-#include "sync/internal_api/public/engine/polling_constants.h"
-#include "sync/sessions/sync_session_context.h"
-#include "sync/sessions/test_util.h"
-#include "sync/test/engine/fake_model_worker.h"
-#include "sync/test/engine/mock_connection_manager.h"
-#include "sync/test/engine/test_directory_setter_upper.h"
-#include "sync/test/fake_extensions_activity_monitor.h"
-#include "testing/gmock/include/gmock/gmock.h"
-#include "testing/gtest/include/gtest/gtest.h"
-
-using base::TimeDelta;
-using base::TimeTicks;
-
-namespace syncer {
-using sessions::SyncSession;
-using sessions::SyncSessionContext;
-using sessions::SyncSourceInfo;
-using sync_pb::GetUpdatesCallerInfo;
-
-class SyncSchedulerWhiteboxTest : public testing::Test {
- public:
-  virtual void SetUp() {
-    dir_maker_.SetUp();
-    Syncer* syncer = new Syncer();
-
-    ModelSafeRoutingInfo routes;
-    routes[BOOKMARKS] = GROUP_UI;
-    routes[NIGORI] = GROUP_PASSIVE;
-
-    workers_.push_back(make_scoped_refptr(new FakeModelWorker(GROUP_UI)));
-    workers_.push_back(make_scoped_refptr(new FakeModelWorker(GROUP_PASSIVE)));
-
-    std::vector<ModelSafeWorker*> workers;
-    for (std::vector<scoped_refptr<FakeModelWorker> >::iterator it =
-         workers_.begin(); it != workers_.end(); ++it) {
-      workers.push_back(it->get());
-    }
-
-    connection_.reset(new MockConnectionManager(NULL));
-    throttled_data_type_tracker_.reset(new ThrottledDataTypeTracker(NULL));
-    context_.reset(
-        new SyncSessionContext(
-            connection_.get(), dir_maker_.directory(),
-            workers, &extensions_activity_monitor_,
-            throttled_data_type_tracker_.get(),
-            std::vector<SyncEngineEventListener*>(), NULL, NULL,
-            true,  // enable keystore encryption
-            "fake_invalidator_client_id"));
-    context_->set_notifications_enabled(true);
-    context_->set_account_name("Test");
-    scheduler_.reset(
-        new SyncSchedulerImpl("TestSyncSchedulerWhitebox",
-            BackoffDelayProvider::FromDefaults(),
-            context(),
-            syncer));
-  }
-
-  virtual void TearDown() {
-    scheduler_.reset();
-  }
-
-  void SetMode(SyncScheduler::Mode mode) {
-    scheduler_->mode_ = mode;
-  }
-
-  void ResetWaitInterval() {
-    scheduler_->wait_interval_.reset();
-  }
-
-  void SetWaitIntervalToThrottled() {
-    scheduler_->wait_interval_.reset(new SyncSchedulerImpl::WaitInterval(
-        SyncSchedulerImpl::WaitInterval::THROTTLED, TimeDelta::FromSeconds(1)));
-  }
-
-  void SetWaitIntervalToExponentialBackoff() {
-    scheduler_->wait_interval_.reset(
-       new SyncSchedulerImpl::WaitInterval(
-       SyncSchedulerImpl::WaitInterval::EXPONENTIAL_BACKOFF,
-       TimeDelta::FromSeconds(1)));
-  }
-
-  SyncSchedulerImpl::JobProcessDecision DecideOnJob(
-      const SyncSessionJob& job,
-      SyncSchedulerImpl::JobPriority priority) {
-    return scheduler_->DecideOnJob(job, priority);
-  }
-
-  void InitializeSyncerOnNormalMode() {
-    SetMode(SyncScheduler::NORMAL_MODE);
-    ResetWaitInterval();
-  }
-
-  SyncSchedulerImpl::JobProcessDecision CreateAndDecideJob(
-      SyncSessionJob::Purpose purpose) {
-    SyncSessionJob job(purpose, TimeTicks::Now(),
-                       SyncSourceInfo(), ConfigurationParams());
-    return DecideOnJob(job, SyncSchedulerImpl::NORMAL_PRIORITY);
-  }
-
-  bool ShouldPoll() {
-    return scheduler_->ShouldPoll();
-  }
-
-  SyncSessionContext* context() { return context_.get(); }
-
- private:
-  MessageLoop message_loop_;
-  scoped_ptr<MockConnectionManager> connection_;
-  scoped_ptr<SyncSessionContext> context_;
-  std::vector<scoped_refptr<FakeModelWorker> > workers_;
-  FakeExtensionsActivityMonitor extensions_activity_monitor_;
-  scoped_ptr<ThrottledDataTypeTracker> throttled_data_type_tracker_;
-  TestDirectorySetterUpper dir_maker_;
-
- protected:
-  // Declared here to ensure it is destructed before the objects it references.
-  scoped_ptr<SyncSchedulerImpl> scheduler_;
-};
-
-TEST_F(SyncSchedulerWhiteboxTest, SaveNudge) {
-  InitializeSyncerOnNormalMode();
-
-  // Now set the mode to configure.
-  SetMode(SyncScheduler::CONFIGURATION_MODE);
-
-  SyncSchedulerImpl::JobProcessDecision decision =
-      CreateAndDecideJob(SyncSessionJob::NUDGE);
-
-  EXPECT_EQ(decision, SyncSchedulerImpl::SAVE);
-}
-
-TEST_F(SyncSchedulerWhiteboxTest, SaveNudgeWhileTypeThrottled) {
-  InitializeSyncerOnNormalMode();
-
-  const ModelTypeSet types(BOOKMARKS);
-
-  // Mark bookmarks as throttled.
-  context()->throttled_data_type_tracker()->SetUnthrottleTime(
-      types, base::TimeTicks::Now() + base::TimeDelta::FromHours(2));
-
-  const ModelTypeInvalidationMap& invalidation_map =
-      ModelTypeSetToInvalidationMap(types, std::string());
-
-  SyncSourceInfo info(GetUpdatesCallerInfo::LOCAL, invalidation_map);
-
-  // Now schedule a nudge with just bookmarks and the change is local.
-  SyncSessionJob job(SyncSessionJob::NUDGE,
-                     TimeTicks::Now(),
-                     info,
-                     ConfigurationParams());
-  SyncSchedulerImpl::JobProcessDecision decision =
-      DecideOnJob(job, SyncSchedulerImpl::NORMAL_PRIORITY);
-  // TODO(tim): This shouldn't drop. Bug 177659.
-  EXPECT_EQ(decision, SyncSchedulerImpl::DROP);
-}
-
-TEST_F(SyncSchedulerWhiteboxTest, ContinueNudge) {
-  InitializeSyncerOnNormalMode();
-
-  SyncSchedulerImpl::JobProcessDecision decision = CreateAndDecideJob(
-      SyncSessionJob::NUDGE);
-
-  EXPECT_EQ(decision, SyncSchedulerImpl::CONTINUE);
-}
-
-TEST_F(SyncSchedulerWhiteboxTest, ContinuePoll) {
-  InitializeSyncerOnNormalMode();
-  EXPECT_TRUE(ShouldPoll());
-}
-
-TEST_F(SyncSchedulerWhiteboxTest, DropPollInConfigureMode) {
-  InitializeSyncerOnNormalMode();
-  SetMode(SyncScheduler::CONFIGURATION_MODE);
-  EXPECT_FALSE(ShouldPoll());
-}
-
-TEST_F(SyncSchedulerWhiteboxTest, DropPollWhenThrottled) {
-  InitializeSyncerOnNormalMode();
-  SetWaitIntervalToThrottled();
-  EXPECT_FALSE(ShouldPoll());
-}
-
-TEST_F(SyncSchedulerWhiteboxTest, DropPollInBackoff) {
-  InitializeSyncerOnNormalMode();
-  SetWaitIntervalToExponentialBackoff();
-  EXPECT_FALSE(ShouldPoll());
-}
-
-TEST_F(SyncSchedulerWhiteboxTest, ContinueConfiguration) {
-  InitializeSyncerOnNormalMode();
-  SetMode(SyncScheduler::CONFIGURATION_MODE);
-
-  SyncSchedulerImpl::JobProcessDecision decision = CreateAndDecideJob(
-      SyncSessionJob::CONFIGURATION);
-
-  EXPECT_EQ(decision, SyncSchedulerImpl::CONTINUE);
-}
-
-TEST_F(SyncSchedulerWhiteboxTest, SaveConfigurationWhileThrottled) {
-  InitializeSyncerOnNormalMode();
-  SetMode(SyncScheduler::CONFIGURATION_MODE);
-
-  SetWaitIntervalToThrottled();
-
-  SyncSchedulerImpl::JobProcessDecision decision = CreateAndDecideJob(
-      SyncSessionJob::CONFIGURATION);
-
-  EXPECT_EQ(decision, SyncSchedulerImpl::SAVE);
-}
-
-TEST_F(SyncSchedulerWhiteboxTest, SaveNudgeWhileThrottled) {
-  InitializeSyncerOnNormalMode();
-  SetMode(SyncScheduler::CONFIGURATION_MODE);
-
-  SetWaitIntervalToThrottled();
-
-  SyncSchedulerImpl::JobProcessDecision decision = CreateAndDecideJob(
-      SyncSessionJob::NUDGE);
-
-  EXPECT_EQ(decision, SyncSchedulerImpl::SAVE);
-}
-
-TEST_F(SyncSchedulerWhiteboxTest, DropNudgeWhileExponentialBackOff) {
-  InitializeSyncerOnNormalMode();
-  SetMode(SyncScheduler::NORMAL_MODE);
-  SetWaitIntervalToExponentialBackoff();
-
-  SyncSchedulerImpl::JobProcessDecision decision = CreateAndDecideJob(
-      SyncSessionJob::NUDGE);
-
-  EXPECT_EQ(decision, SyncSchedulerImpl::DROP);
-}
-
-TEST_F(SyncSchedulerWhiteboxTest, ContinueCanaryJobConfig) {
-  InitializeSyncerOnNormalMode();
-  SetMode(SyncScheduler::CONFIGURATION_MODE);
-  SetWaitIntervalToExponentialBackoff();
-
-  SyncSessionJob job(SyncSessionJob::CONFIGURATION,
-                     TimeTicks::Now(), SyncSourceInfo(),
-                     ConfigurationParams());
-
-  SyncSchedulerImpl::JobProcessDecision decision =
-      DecideOnJob(job, SyncSchedulerImpl::CANARY_PRIORITY);
-
-  EXPECT_EQ(decision, SyncSchedulerImpl::CONTINUE);
-}
-
-}  // namespace syncer

sync/engine/sync_session_job.cc
@@ -1,130 +1,0 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "sync/engine/sync_session_job.h"
-#include "sync/internal_api/public/sessions/model_neutral_state.h"
-
-namespace syncer {
-
-SyncSessionJob::~SyncSessionJob() {
-}
-
-SyncSessionJob::SyncSessionJob(
-    Purpose purpose,
-    base::TimeTicks start,
-    const sessions::SyncSourceInfo& source_info,
-    const ConfigurationParams& config_params)
-    : purpose_(purpose),
-      source_info_(source_info),
-      scheduled_start_(start),
-      config_params_(config_params) {
-}
-
-#define ENUM_CASE(x) case x: return #x; break;
-const char* SyncSessionJob::GetPurposeString(SyncSessionJob::Purpose purpose) {
-  switch (purpose) {
-    ENUM_CASE(UNKNOWN);
-    ENUM_CASE(POLL);
-    ENUM_CASE(NUDGE);
-    ENUM_CASE(CONFIGURATION);
-  }
-  NOTREACHED();
-  return "";
-}
-#undef ENUM_CASE
-
-bool SyncSessionJob::Finish(bool early_exit, sessions::SyncSession* session) {
-  // Did we quit part-way through due to premature exit condition, like
-  // shutdown?  Note that this branch will not be hit for other kinds
-  // of early return scenarios, like certain kinds of transient errors.
-  if (early_exit)
-    return false;
-
-  // Did we hit any errors along the way?
-  if (sessions::HasSyncerError(
-      session->status_controller().model_neutral_state())) {
-    return false;
-  }
-
-  const sessions::ModelNeutralState& state(
-      session->status_controller().model_neutral_state());
-  switch (purpose_) {
-    case POLL:
-    case NUDGE:
-      DCHECK_NE(state.last_download_updates_result, UNSET);
-      DCHECK_NE(state.commit_result, UNSET);
-      break;
-    case CONFIGURATION:
-      DCHECK_NE(state.last_download_updates_result, UNSET);
-      break;
-    case UNKNOWN:
-    default:
-      NOTREACHED();
-  }
-
-  if (!config_params_.ready_task.is_null())
-    config_params_.ready_task.Run();
-  return true;
-}
-
-void SyncSessionJob::CoalesceSources(const sessions::SyncSourceInfo& source) {
-  CoalesceStates(source.types, &source_info_.types);
-  source_info_.updates_source = source.updates_source;
-}
-
-SyncSessionJob::Purpose SyncSessionJob::purpose() const {
-  return purpose_;
-}
-
-const sessions::SyncSourceInfo& SyncSessionJob::source_info() const {
-  return source_info_;
-}
-
-base::TimeTicks SyncSessionJob::scheduled_start() const {
-  return scheduled_start_;
-}
-
-void SyncSessionJob::set_scheduled_start(base::TimeTicks start) {
-  scheduled_start_ = start;
-};
-
-ConfigurationParams SyncSessionJob::config_params() const {
-  return config_params_;
-}
-
-SyncerStep SyncSessionJob::start_step() const {
-  SyncerStep start, end;
-  GetSyncerStepsForPurpose(purpose_, &start, &end);
-  return start;
-}
-
-SyncerStep SyncSessionJob::end_step() const {
-  SyncerStep start, end;
-  GetSyncerStepsForPurpose(purpose_, &start, &end);
-  return end;
-}
-
-// static
-void SyncSessionJob::GetSyncerStepsForPurpose(Purpose purpose,
-                                              SyncerStep* start,
-                                              SyncerStep* end) {
-  switch (purpose) {
-    case SyncSessionJob::CONFIGURATION:
-      *start = DOWNLOAD_UPDATES;
-      *end = APPLY_UPDATES;
-      return;
-    case SyncSessionJob::NUDGE:
-    case SyncSessionJob::POLL:
-      *start = SYNCER_BEGIN;
-      *end = SYNCER_END;
-      return;
-    default:
-      NOTREACHED();
-      *start = SYNCER_END;
-      *end = SYNCER_END;
-      return;
-  }
-}
-
-}  // namespace syncer

nwjs/chromium.src

commit 16ba5b95c0a861812d4d0e0f4e45d5f4179f8380

Merge part of r200655: Fix handling of data type throttling
This branch is susceptible to only the second of the two issues that
this patch tries to fix.  This merge contains only the changes required
to fix that issue.

Original commit message for r200655 follows.

=============================================

This change attempts to fix two semi-related issues in data type
throttling that were introduced in r194766. Data type throttling has
always had a few rough edge cases, but that commit introduced some
issues that must be fixed.

1:
Prior to r194766, data type throttling would block sync cycles only if
the source was LOCAL and all types were throttled. This meant that a
refresh request or notification could override data type throttling, if
it arrived at the right time.

That change made this behaviour much more deterministic. It would fail
to perform any sync cycles as long as the only local nudges that were
currently tracked were for throttled types. Even notificiations and
local refresh requests for these types will not be sufficient to force a
sync cycle.

This change tries to find a better balance. It will allow sync cycles
in the presence of unthrottled data types if one of the following is
true:
- There is an unserviced invalidation for any type.
- There is an unservice refresh request for any type.
- There is a local nudge for any non-throttled type.

2:
The other problem has to do with exponential backoff in the presence of
per-type throttling. In r194766, we made the assumption that we can
early-exit from attempted sync cycle only if there was some other reason
to perform a sync cycle later. Common causes of early-exit include
throttling, exponential backoff, and network connectivity issues, all of
which should be able to get back into a normal state on their own.

However, one of those early-exit causes is data type throttling. In
that case, there is no timer or notification that will restart sync once

sync/engine/sync_scheduler_impl.cc
@@ -541,8 +541,12 @@ void SyncSchedulerImpl::HandleFailure(
   if (IsSyncingCurrentlySilenced()) {
     SDVLOG(2) << "Was throttled during previous sync cycle.";
     RestartWaiting();
-  } else {
-    UpdateExponentialBackoff(model_neutral_state);
+  } else if (!IsBackingOff()) {
+    // Setup our backoff if this is our first such failure.
+    TimeDelta length = delay_provider_->GetDelay(
+        delay_provider_->GetInitialDelay(model_neutral_state));
+    wait_interval_.reset(
+        new WaitInterval(WaitInterval::EXPONENTIAL_BACKOFF, length));
     SDVLOG(2) << "Sync cycle failed.  Will back off for "
         << wait_interval_->length.InMilliseconds() << "ms.";
     RestartWaiting();
@@ -643,20 +647,9 @@ void SyncSchedulerImpl::RestartWaiting() {
     pending_wakeup_timer_.Start(
         FROM_HERE,
         wait_interval_->length,
-        base::Bind(&SyncSchedulerImpl::TryCanaryJob,
+        base::Bind(&SyncSchedulerImpl::ExponentialBackoffRetry,
                    weak_ptr_factory_.GetWeakPtr()));
   }
-}
-
-void SyncSchedulerImpl::UpdateExponentialBackoff(
-    const sessions::ModelNeutralState& model_neutral_state) {
-  DCHECK(CalledOnValidThread());
-
-  TimeDelta length = delay_provider_->GetDelay(
-      IsBackingOff() ? wait_interval_->length :
-          delay_provider_->GetInitialDelay(model_neutral_state));
-  wait_interval_.reset(new WaitInterval(WaitInterval::EXPONENTIAL_BACKOFF,
-                                        length));
 }
 
 void SyncSchedulerImpl::RequestStop(const base::Closure& callback) {
@@ -731,6 +724,22 @@ void SyncSchedulerImpl::Unthrottle() {
   // that we're careful to update routing info (etc) with such potentially
   // stale canary jobs.
   TryCanaryJob();
+}
+
+void SyncSchedulerImpl::ExponentialBackoffRetry() {
+  TryCanaryJob();
+
+  if (IsBackingOff()) {
+    // If we succeeded, our wait interval would have been cleared.  If it hasn't
+    // been cleared, then we should increase our backoff interval and schedule
+    // another retry.
+    TimeDelta length = delay_provider_->GetDelay(wait_interval_->length);
+    wait_interval_.reset(
+      new WaitInterval(WaitInterval::EXPONENTIAL_BACKOFF, length));
+    SDVLOG(2) << "Sync cycle failed.  Will back off for "
+        << wait_interval_->length.InMilliseconds() << "ms.";
+    RestartWaiting();
+  }
 }
 
 void SyncSchedulerImpl::Notify(SyncEngineEvent::EventCause cause) {

sync/engine/sync_scheduler_impl.h
@@ -168,10 +168,6 @@ class SYNC_EXPORT_PRIVATE SyncSchedulerImpl
   // Helper to restart waiting with |wait_interval_|'s timer.
   void RestartWaiting();
 
-  // Helper to adjust our wait interval when we expereince a transient failure.
-  void UpdateExponentialBackoff(
-      const sessions::ModelNeutralState& model_neutral_state);
-
   // Determines if we're allowed to contact the server right now.
   bool CanRunJobNow(JobPriority priority);
 
@@ -206,6 +202,9 @@ class SYNC_EXPORT_PRIVATE SyncSchedulerImpl
 
   // Transitions out of the THROTTLED WaitInterval then calls TryCanaryJob().
   void Unthrottle();
+
+  // Attempts to exit EXPONENTIAL_BACKOFF by calling TryCanaryJob().
+  void ExponentialBackoffRetry();
 
   // Called when the root cause of the current connection error is fixed.
   void OnServerConnectionErrorFixed();