4f8372b0083c1893bb643b0783aa3b9831ebd289
aelias@chromium.org <aelias@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-21 22:59:31 +0000

    Merge 218391 "Call ManageTiles() in SetVisible(false)."
    > Call ManageTiles() in SetVisible(false).
    > 
    > When a tab goes invisible, the memory policy is changed, but we may
    > never have any opportunity to apply it, since tile management is tied to
    > draws and timer ticks.  Fix this with an explicit ManageTiles call.
    > 
    > NOTRY=true
    > BUG=273378
    > 
    > Review URL: https://chromiumcodereview.appspot.com/23064013
    
    TBR=aelias@chromium.org
    
    Review URL: https://codereview.chromium.org/22870026
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1599/src@218853 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -1527,6 +1527,11 @@ void LayerTreeHostImpl::SetVisible(bool visible) {
   DidVisibilityChange(this, visible_);
   EnforceManagedMemoryPolicy(ActualManagedMemoryPolicy());
 
+  // Evict tiles immediately if invisible since this tab may never get another
+  // draw or timer tick.
+  if (!visible_)
+    ManageTiles();
+
   if (!renderer_)
     return;
 
