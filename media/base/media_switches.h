46935dfe354433cd4d4e5071a332fee9014060de
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-03 20:12:19 +0000

    Merge 217670 "Always use the WASAPI hardware channel layout unle..."
    > Always use the WASAPI hardware channel layout unless a flag is set.
    > 
    > Causes Chrome to always uses the hardware channel layout unless the
    > user specifies --try-supported-channel-layouts, in which case we'll ask
    > the OS if the source layout is supported and if so use it instead.
    > 
    > This is a speculative fix for garbled playback and system hangs.  The
    > feature is in demand by generally tech savvy users, so keeping it behind
    > a flag should be fine.
    > 
    > The speculated reason is that some drivers are lying to the OS about the
    > layouts they actually support or have broken implementations.
    > 
    > BUG=177872, 259165, 261487,
    > TEST=Run Chrome w/ and w/o switch, watch VLOG.
    > R=scherkus@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/21671005
    
    TBR=dalecurtis@google.com
    
    Review URL: https://codereview.chromium.org/23815004
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1547/src@221008 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/media_switches.h
+++ media/base/media_switches.h
@@ -42,6 +42,7 @@ MEDIA_EXPORT extern const char kDisableMainThreadAudio[];
 #if defined(OS_WIN)
 MEDIA_EXPORT extern const char kEnableExclusiveAudio[];
 MEDIA_EXPORT extern const char kForceWaveAudio[];
+MEDIA_EXPORT extern const char kTrySupportedChannelLayouts[];
 MEDIA_EXPORT extern const char kWaveOutBuffers[];
 #endif
 
