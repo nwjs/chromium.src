17c6b79c881f7bfab015d858c611598b49a595f7
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-07 19:49:42 +0000

    Merge 224408 "Verify the integrity of the pref file."
    > Verify the integrity of the pref file.
    > 
    > R=sail@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/23531072
    
    TBR=gab@chromium.org
    
    Review URL: https://codereview.chromium.org/26260003
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1599/src@227321 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_impl.cc
+++ chrome/browser/profiles/profile_impl.cc
@@ -98,6 +98,7 @@
 #include "ui/base/l10n/l10n_util.h"
 
 #if defined(OS_WIN)
+#include "chrome/browser/profiles/file_path_verifier_win.h"
 #include "chrome/installer/util/install_util.h"
 #endif
 
@@ -236,6 +237,17 @@ std::string ExitTypeToSessionTypePrefValue(Profile::ExitType type) {
   }
   NOTREACHED();
   return std::string();
+}
+
+void SchedulePrefsFileVerification(const base::FilePath& prefs_file) {
+#if defined(OS_WIN)
+  // Only do prefs file verification on Windows.
+  const int kVerifyPrefsFileDelaySeconds = 60;
+  BrowserThread::GetBlockingPool()->PostDelayedTask(
+        FROM_HERE,
+        base::Bind(&VerifyFileAtPath, prefs_file, "Preferences"),
+        base::TimeDelta::FromSeconds(kVerifyPrefsFileDelaySeconds));
+#endif
 }
 
 }  // namespace
@@ -767,6 +779,8 @@ void ProfileImpl::OnPrefsLoaded(bool success) {
         prerender::PrerenderManagerFactory::GetForProfile(this),
         predictor_));
   }
+
+  SchedulePrefsFileVerification(GetPrefFilePath());
 
   ChromeVersionService::OnProfileLoaded(prefs_.get(), IsNewProfile());
   DoFinalInit();
