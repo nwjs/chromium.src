94863e95afe52f8a8a0b3685d28cdcda31633e53
laforge@chromium.org <laforge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-15 15:52:38 +0000

    Revert 225514 "Merge 225142 "Fix activation problems seen in non..."
    > Merge 225142 "Fix activation problems seen in non AURA chrome Wi..."
    > 
    > > Fix activation problems seen in non AURA chrome Windows which regressed in revision r224304
    > > 
    > > We introduced a check for whether the lParam is our HWND. That is not correct in the deactivation
    > > case where WM_ACTIVATE is sent to the window being deactivated with lParam being the handle to the
    > > window being activated and vice versa. In the minimize case lParam is 0 with wParam containing the
    > > value 0x200001 which indicates that the window is minimized but is active for tab preview purposes.
    > > The window check basically meant that we never saved the view which had focus when we lost activation
    > > and thus were unable to restore it when we got activated.
    > > 
    > > Reverted the check for the window introduced in r224304 and added a check for whether the window is
    > > being minimized in the HWNDMessageHandler::PostProcessActivateMessage function. We treat it as inactive
    > > and save away the last focused view and restore it when the window is restored./
    > > 
    > > BUG=297525
    > > R=sky@chromium.org, sky
    > > 
    > > Review URL: https://codereview.chromium.org/24468003
    > 
    > TBR=ananta@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/24786002
    
    TBR=maruel@chromium.org
    
    Review URL: https://codereview.chromium.org/27348002
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1650_5/src@228689 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/win/hwnd_message_handler.cc
+++ ui/views/win/hwnd_message_handler.cc
@@ -905,9 +905,10 @@ LRESULT HWNDMessageHandler::OnWndProc(UINT message,
   }
 
   // Only top level widget should store/restore focus.
-  if (message == WM_ACTIVATE && delegate_->CanSaveFocus())
-    PostProcessActivateMessage(LOWORD(w_param), !!HIWORD(w_param));
-
+  if (message == WM_ACTIVATE && delegate_->CanSaveFocus() &&
+      reinterpret_cast<HWND>(l_param) == window) {
+    PostProcessActivateMessage(LOWORD(w_param));
+  }
   if (message == WM_ENABLE && restore_focus_when_enabled_) {
     // This path should be executed only for top level as
     // restore_focus_when_enabled_ is set in PostProcessActivateMessage.
@@ -958,10 +959,9 @@ void HWNDMessageHandler::SetInitialFocus() {
   }
 }
 
-void HWNDMessageHandler::PostProcessActivateMessage(int activation_state,
-                                                    bool minimized) {
+void HWNDMessageHandler::PostProcessActivateMessage(int activation_state) {
   DCHECK(delegate_->CanSaveFocus());
-  if (WA_INACTIVE == activation_state || minimized) {
+  if (WA_INACTIVE == activation_state) {
     // We might get activated/inactivated without being enabled, so we need to
     // clear restore_focus_when_enabled_.
     restore_focus_when_enabled_ = false;
