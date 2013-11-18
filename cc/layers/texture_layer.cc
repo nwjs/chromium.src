e01a8c1f6a1be1f16a64a71a13367e0cc373da7f
junov@chromium.org <junov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-18 15:18:10 +0000

    Merge 235241 "Prevent cc:TextureLayer from calling AcquireLayerT..."
    > Prevent cc:TextureLayer from calling AcquireLayerTexture when using mailboxes
    > 
    > BUG=315170
    > R=danakj@chromium.org, enne@chromium.org, piman@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/69933014
    
    TBR=junov@chromium.org
    
    Review URL: https://codereview.chromium.org/61603007
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1650/src@235730 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layers/texture_layer.cc
+++ cc/layers/texture_layer.cc
@@ -153,7 +153,8 @@ void TextureLayer::SetTextureMailbox(
 }
 
 void TextureLayer::WillModifyTexture() {
-  if (layer_tree_host() && (DrawsContent() || content_committed_)) {
+  if (!uses_mailbox_ && layer_tree_host() && (DrawsContent() ||
+      content_committed_)) {
     layer_tree_host()->AcquireLayerTextures();
     content_committed_ = false;
   }
@@ -254,8 +255,8 @@ void TextureLayer::PushPropertiesTo(LayerImpl* layer) {
     needs_set_mailbox_ = false;
   } else {
     texture_layer->set_texture_id(texture_id_);
+    content_committed_ = DrawsContent();
   }
-  content_committed_ = DrawsContent();
 }
 
 Region TextureLayer::VisibleContentOpaqueRegion() const {
