770c3fc0f87db46cbf54e970a450a2182dbf61a7
sievers@google.com <sievers@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-21 19:01:08 +0000

    Merge 245660 "Android: Shallow instead of deep flush after textu..."
    > Android: Shallow instead of deep flush after texture consume
    > 
    > The deep flush is not necessary anymore since we don't modify texture
    > state anymore and it looks like it might avoid a driver bug with
    >  incomplete draws.
    > 
    > BUG=328761
    > R=piman@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/142003006
    
    TBR=sievers@chromium.org
    
    Review URL: https://codereview.chromium.org/141033004
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1750/src@246083 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/image_transport_factory_android.cc
+++ content/browser/renderer_host/image_transport_factory_android.cc
@@ -139,7 +139,7 @@ void CmdBufferImageTransportFactory::AcquireTexture(
   }
   context_->bindTexture(GL_TEXTURE_2D, texture_id);
   context_->consumeTextureCHROMIUM(GL_TEXTURE_2D, mailbox_name);
-  context_->flush();
+  context_->shallowFlushCHROMIUM();
 }
 
 GLHelper* CmdBufferImageTransportFactory::GetGLHelper() {
