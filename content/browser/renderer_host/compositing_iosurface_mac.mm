cba19e1bbfafd24bb4c3cf7242239bab9b971d1a
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-22 23:54:36 +0000

    Merge 246077 "Fix bug where GL fence objects were shared between..."
    > Fix bug where GL fence objects were shared between contexts.
    > 
    > These objects were allocated during DrawIOSurface when copying
    > the IOSurface to a video frame.
    > 
    > This pulls the copy to the video frame up to CompositorSwapBuffers,
    > and performs the copy in a separate context. Using the separate
    > context makes a call to glFlush before the copy necessary (to be sure
    > that the copy gets the correct contents), and after, to kick off the async
    > copy.
    > 
    > Moving the copy up to the swap make it so that we won't send more
    > frames just because of window damage.
    > 
    > BUG=334742
    > 
    > Review URL: https://codereview.chromium.org/137893022
    
    
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1750/src@246451 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/compositing_iosurface_mac.mm
+++ content/browser/renderer_host/compositing_iosurface_mac.mm
@@ -328,7 +328,8 @@ CompositingIOSurfaceMac::~CompositingIOSurfaceMac() {
   offscreen_context_ = NULL;
 }
 
-bool CompositingIOSurfaceMac::SetIOSurface(
+bool CompositingIOSurfaceMac::SetIOSurfaceWithContextCurrent(
+    scoped_refptr<CompositingIOSurfaceContext> current_context,
     uint64 io_surface_handle,
     const gfx::Size& size,
     float scale_factor,
@@ -337,14 +338,8 @@ bool CompositingIOSurfaceMac::SetIOSurface(
   scale_factor_ = scale_factor;
   dip_io_surface_size_ = gfx::ToFlooredSize(
       gfx::ScaleSize(pixel_io_surface_size_, 1.0 / scale_factor_));
-
-  CGLError cgl_error = CGLSetCurrentContext(offscreen_context_->cgl_context());
-  if (cgl_error != kCGLNoError) {
-    LOG(ERROR) << "CGLSetCurrentContext error in SetIOSurface: " << cgl_error;
-    return false;
-  }
-  bool result = MapIOSurfaceToTexture(io_surface_handle);
-  CGLSetCurrentContext(0);
+  bool result = MapIOSurfaceToTextureWithContextCurrent(
+      current_context, io_surface_handle);
   latency_info_.MergeWith(latency_info);
   return result;
 }
@@ -362,7 +357,6 @@ bool CompositingIOSurfaceMac::DrawIOSurface(
     scoped_refptr<CompositingIOSurfaceContext> drawing_context,
     const gfx::Rect& window_rect,
     float window_scale_factor,
-    RenderWidgetHostViewFrameSubscriber* frame_subscriber,
     bool flush_drawable) {
   DCHECK_EQ(CGLGetCurrentContext(), drawing_context->cgl_context());
 
@@ -467,18 +461,6 @@ bool CompositingIOSurfaceMac::DrawIOSurface(
     glFinish();
   }
 
-  base::Closure copy_done_callback;
-  if (frame_subscriber) {
-    const base::Time present_time = base::Time::Now();
-    scoped_refptr<media::VideoFrame> frame;
-    RenderWidgetHostViewFrameSubscriber::DeliverFrameCallback callback;
-    if (frame_subscriber->ShouldCaptureFrame(present_time, &frame, &callback)) {
-      copy_done_callback = CopyToVideoFrameWithinContext(
-          gfx::Rect(pixel_io_surface_size_), true, frame,
-          base::Bind(callback, present_time));
-    }
-  }
-
   bool result = true;
   if (flush_drawable) {
     CGLError cgl_error =  CGLFlushDrawable(drawing_context->cgl_context());
@@ -571,7 +553,8 @@ base::Closure CompositingIOSurfaceMac::CopyToVideoFrameWithinContext(
       NULL, target, callback);
 }
 
-bool CompositingIOSurfaceMac::MapIOSurfaceToTexture(
+bool CompositingIOSurfaceMac::MapIOSurfaceToTextureWithContextCurrent(
+    const scoped_refptr<CompositingIOSurfaceContext>& current_context,
     uint64 io_surface_handle) {
   if (io_surface_.get() && io_surface_handle == io_surface_handle_)
     return true;
@@ -603,7 +586,7 @@ bool CompositingIOSurfaceMac::MapIOSurfaceToTexture(
   CHECK_AND_SAVE_GL_ERROR();
   GLuint plane = 0;
   CGLError cgl_error = io_surface_support_->CGLTexImageIOSurface2D(
-      offscreen_context_->cgl_context(),
+      current_context->cgl_context(),
       GL_TEXTURE_RECTANGLE_ARB,
       GL_RGBA,
       rounded_size.width(),
