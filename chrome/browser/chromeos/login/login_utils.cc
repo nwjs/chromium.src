3aa28927e31f361df119d575786a76e0de1e8d6d
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-05 13:54:26 +0000

    Merge r247829 - Make an online wildcard login check for enterprise devices.
    This checks with GAIA to reconfirm whether a given user is permitted
    to log in to a device. The check complements the existing login
    whitelist check based on device policy.
    
    BUG=chromium:336284
    TEST=Manual
    TBR=mnissler@chromium.org
    
    Review URL: https://codereview.chromium.org/155833002
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1750/src@248985 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -855,7 +855,8 @@ void LoginUtils::Set(LoginUtils* mock) {
 }
 
 // static
-bool LoginUtils::IsWhitelisted(const std::string& username) {
+bool LoginUtils::IsWhitelisted(const std::string& username,
+                               bool* wildcard_match) {
   // Skip whitelist check for tests.
   if (CommandLine::ForCurrentProcess()->HasSwitch(
       chromeos::switches::kOobeSkipPostLogin)) {
@@ -867,7 +868,8 @@ bool LoginUtils::IsWhitelisted(const std::string& username) {
   cros_settings->GetBoolean(kAccountsPrefAllowNewUser, &allow_new_user);
   if (allow_new_user)
     return true;
-  return cros_settings->FindEmailInList(kAccountsPrefUsers, username);
+  return cros_settings->FindEmailInList(
+      kAccountsPrefUsers, username, wildcard_match);
 }
 
 }  // namespace chromeos
