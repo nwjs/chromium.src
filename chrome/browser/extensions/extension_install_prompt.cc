14fdd871811096bff06ddbdd18497cd802327507
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-13 19:14:25 +0000

    Merge 250657 "Fix crash when running chrome --install-from-webstore"
    > Fix crash when running chrome --install-from-webstore
    > 
    > We were using an invisible dummy WebContents to decide where to parent the
    > confirmation dialog. This crashes under Aura.
    > 
    > BUG=334524
    > TEST=manual, "chrome --install-from-webstore=gplegfbjlmmehdoakndmohflojccocli" should not crash
    > 
    > Review URL: https://codereview.chromium.org/160093002
    
    TBR=jamescook@chromium.org
    
    Review URL: https://codereview.chromium.org/164283003
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1750/src@251096 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_install_prompt.cc
+++ chrome/browser/extensions/extension_install_prompt.cc
@@ -522,11 +522,11 @@ scoped_refptr<Extension>
       error);
 }
 
-ExtensionInstallPrompt::ExtensionInstallPrompt(
-    content::WebContents* contents)
+ExtensionInstallPrompt::ExtensionInstallPrompt(content::WebContents* contents)
     : record_oauth2_grant_(false),
       ui_loop_(base::MessageLoop::current()),
       extension_(NULL),
+      bundle_(NULL),
       install_ui_(ExtensionInstallUI::Create(ProfileForWebContents(contents))),
       show_params_(contents),
       delegate_(NULL),
@@ -541,6 +541,7 @@ ExtensionInstallPrompt::ExtensionInstallPrompt(
     : record_oauth2_grant_(false),
       ui_loop_(base::MessageLoop::current()),
       extension_(NULL),
+      bundle_(NULL),
       install_ui_(ExtensionInstallUI::Create(profile)),
       show_params_(native_window, navigator),
       delegate_(NULL),
