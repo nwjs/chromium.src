3b0a23d3f52700389ea6773157ee91c9ec15193a
dharani@google.com <dharani@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 22:39:14 +0000

    Revert 257162 "Remove unused --disable-oobe-animation flag"
    Reverting this in 1897 branch to verify bug 353469
    
    > Remove unused --disable-oobe-animation flag
    > 
    > BUG=352344
    > TEST=browser_tests
    > R=nkostylev@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/199683002
    
    TBR=dpolukhin@chromium.org
    
    Review URL: https://codereview.chromium.org/202743004
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1897/src@257791 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/background/ash_user_wallpaper_delegate.cc
+++ chrome/browser/chromeos/background/ash_user_wallpaper_delegate.cc
@@ -67,11 +67,15 @@ class UserWallpaperDelegate : public ash::UserWallpaperDelegate {
 
     // It is a first boot case now. If kDisableBootAnimation flag
     // is passed, it only disables any transition after OOBE.
+    // |kDisableOobeAnimation| disables OOBE animation for slow hardware.
     bool is_registered = StartupUtils::IsDeviceRegistered();
     const CommandLine* command_line = CommandLine::ForCurrentProcess();
     bool disable_boot_animation = command_line->
         HasSwitch(switches::kDisableBootAnimation);
-    if (is_registered && disable_boot_animation)
+    bool disable_oobe_animation = command_line->
+        HasSwitch(switches::kDisableOobeAnimation);
+    if ((!is_registered && disable_oobe_animation) ||
+        (is_registered && disable_boot_animation))
       return false;
 
     return true;
