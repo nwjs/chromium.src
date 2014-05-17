dc34cde2ad332c62f9a4017632e6789fa0cceee2
amineer@chromium.org <amineer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-17 04:15:25 +0000

    Revert 266654 "Re-land: cc: Enable impl-side painting by default..."
    > Re-land: cc: Enable impl-side painting by default on all platforms where threaded compositing is used.
    > 
    > BUG=169282
    > 
    > Review URL: https://codereview.chromium.org/242803007
    
    TBR=reveman@chromium.org
    
    Review URL: https://codereview.chromium.org/290613007
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1997/src@271154 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/compositor_util.cc
+++ content/browser/gpu/compositor_util.cc
@@ -204,10 +204,11 @@ bool IsImplSidePaintingEnabled() {
       switches::kEnableBleedingEdgeRenderingFastPaths))
     return true;
 
-#if defined(OS_MACOSX)
-  return false;
+#if defined(OS_ANDROID)
+  return true;
 #else
-  return IsThreadedCompositingEnabled();
+  return false;
+#endif
 #endif
 }
 
