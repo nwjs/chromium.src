b9b5bfcfbfa5a32e6c7acc03a62bf4caf78b2ca3
Roger <wenrui@gmail.com>
2014-06-08 21:52:08 +0800

    [nw10] fix autofill

--- components/autofill/core/browser/autofill_manager.cc
+++ components/autofill/core/browser/autofill_manager.cc
@@ -706,7 +706,7 @@ void AutofillManager::OnDidEndTextFieldEditing() {
 }
 
 bool AutofillManager::IsAutofillEnabled() const {
-  return manager_delegate_->GetPrefs()->GetBoolean(prefs::kAutofillEnabled);
+  return true;
 }
 
 void AutofillManager::ImportFormData(const FormStructure& submitted_form) {
@@ -860,10 +860,12 @@ bool AutofillManager::RefreshDataModels() const {
     return false;
 
   // No autofill data to return if the profiles are empty.
+#if 0
   if (personal_data_->GetProfiles().empty() &&
       personal_data_->GetCreditCards().empty()) {
     return false;
   }
+#endif
 
   return true;
 }
