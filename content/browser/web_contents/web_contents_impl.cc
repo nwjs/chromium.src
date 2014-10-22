9483bd74dcd5ada293c1c5f45a10d156d5307b34
Roger <wenrui@gmail.com>
2014-10-22 12:32:34 +0800

    [linux] fix fullscreen

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -2549,9 +2549,9 @@ void WebContentsImpl::DidNavigateMainFramePreCommit(
     // No page change?  Then, the renderer and browser can remain in fullscreen.
     return;
   }
-  if (IsFullscreenForCurrentTab())
-    GetRenderViewHost()->ExitFullscreen();
-  DCHECK(!IsFullscreenForCurrentTab());
+  //  if (IsFullscreenForCurrentTab())
+  //  GetRenderViewHost()->ExitFullscreen();
+  //DCHECK(!IsFullscreenForCurrentTab());
 }
 
 void WebContentsImpl::DidNavigateMainFramePostCommit(
