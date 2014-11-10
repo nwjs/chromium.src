6080089f27e08330b467725f75fa7277ee30e9f8
Roger <wenrui@gmail.com>
2014-11-10 13:44:32 +0800

    [Linux] set initial size hints for non-resizable window
    Part of rogerwang/node-webkit#2319

--- ui/views/widget/desktop_aura/desktop_window_tree_host_x11.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_x11.cc
@@ -53,6 +53,7 @@
 #include "ui/wm/core/compound_event_filter.h"
 #include "ui/wm/core/window_util.h"
 
+#include "ui/views/widget/widget_delegate.h"
 namespace views {
 
 DesktopWindowTreeHostX11* DesktopWindowTreeHostX11::g_current_capture =
@@ -1563,6 +1564,13 @@ void DesktopWindowTreeHostX11::MapWindow(ui::WindowShowState show_state) {
   size_hints.flags = PPosition;
   size_hints.x = bounds_.x();
   size_hints.y = bounds_.y();
+  if (!native_widget_delegate_->AsWidget()->widget_delegate()->CanResize()) {
+    size_hints.flags |= PMinSize | PMaxSize;
+    size_hints.min_width = bounds_.width();
+    size_hints.min_height = bounds_.height();
+    size_hints.max_width = bounds_.width();
+    size_hints.max_height = bounds_.height();
+  }
   XSetWMNormalHints(xdisplay_, xwindow_, &size_hints);
 
   // If SHOW_STATE_INACTIVE, tell the window manager not to focus the window
