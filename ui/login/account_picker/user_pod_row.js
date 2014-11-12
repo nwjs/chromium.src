16dd778b91d78347d7b6142ec538b4e85750af88
Xiyuan Xia <xiyuan@chromium.org>
2014-11-12 11:10:10 -0800

    Merge "easy-unlock: Add signing-in transition."
    > - Change 'Click to enter' color to grey;
    > - Show 'Signing in' with animated ellipsis during signing in;
    >
    > BUG=428374
    >
    > Review URL: https://codereview.chromium.org/709813002
    >
    > Cr-Commit-Position: refs/heads/master@{#303504}
    > (cherry picked from commit 11ca5425a2b159e58562761a2f632811f821b63d)
    
    TBR=xiyuan@chromium.org
    
    Review URL: https://codereview.chromium.org/722803002
    
    Cr-Commit-Position: refs/branch-heads/2171@{#417}
    Cr-Branched-From: 267aeeb8d85c8503a7fd12bd14654b8ea78d3974-refs/heads/master@{#297060}

--- ui/login/account_picker/user_pod_row.js
+++ ui/login/account_picker/user_pod_row.js
@@ -1089,6 +1089,7 @@ cr.define('login', function() {
         this.showSigninUI();
       } else if (this.isAuthTypeUserClick) {
         Oobe.disableSigninUI();
+        this.classList.toggle('signing-in', true);
         chrome.send('attemptUnlock', [this.user.username]);
       } else if (this.isAuthTypePassword) {
         if (!this.passwordElement.value)
@@ -1157,6 +1158,7 @@ cr.define('login', function() {
      */
     reset: function(takeFocus) {
       this.passwordElement.value = '';
+      this.classList.toggle('signing-in', false);
       if (takeFocus) {
         if (!this.multiProfilesPolicyApplied)
           this.focusInput();  // This will set a custom tab order.
