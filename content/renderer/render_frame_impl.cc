60d10f46b3fc8ac2e37f28cb2a70e89a120d604c
Roger <wenrui@gmail.com>
2014-12-05 22:13:40 +0800

    add willHandleNavigationPolicy
    From: Roger <wenrui@gmail.com>
    
    Conflicts:
    	content/public/renderer/content_renderer_client.h
    	content/renderer/render_frame_impl.cc
    	content/renderer/render_frame_impl.h

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -4249,4 +4249,12 @@ RendererCdmManager* RenderFrameImpl::GetCdmManager() {
 }
 #endif  // defined(ENABLE_BROWSER_CDMS)
 
+void RenderFrameImpl::willHandleNavigationPolicy(
+                                                blink::WebFrame* frame,
+                                                const blink::WebURLRequest& request,
+                                                blink::WebNavigationPolicy* policy) {
+  GetContentClient()->renderer()
+    ->willHandleNavigationPolicy(render_view_.get(), frame, request, policy);
+}
+
 }  // namespace content
