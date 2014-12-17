174db5e2bbcaf5a72e87d7b21f3e7bf92420fbc4
erikchen <erikchen@chromium.org>
2014-12-16 17:17:23 -0800

    Merge "mac: Fixed bug where infobar did not show in undocked DevTools window." to 2171.
    > The layout logic for BrowserWindowController assumed that all windows would
    > either have a toolbar or a location bar. The DevTools window has neither.
    >
    > BUG=418491
    >
    > Review URL: https://codereview.chromium.org/622183003
    >
    > Cr-Commit-Position: refs/heads/master@{#298363}
    >
    > Conflicts:
    >   chrome/browser/ui/cocoa/browser_window_layout_unittest.mm
    
    BUG=418491
    TBR=rsesek@chromium.org
    
    Review URL: https://codereview.chromium.org/810913002
    
    Cr-Commit-Position: refs/branch-heads/2171@{#466}
    Cr-Branched-From: 267aeeb8d85c8503a7fd12bd14654b8ea78d3974-refs/heads/master@{#297060}

--- chrome/browser/ui/cocoa/browser_window_layout_unittest.mm
+++ chrome/browser/ui/cocoa/browser_window_layout_unittest.mm
@@ -112,8 +112,20 @@ TEST_F(BrowserWindowLayoutTest, TestPopupWindow) {
   EXPECT_TRUE(NSEqualRects(NSZeroRect, output.bookmarkFrame));
   EXPECT_TRUE(NSEqualRects(NSZeroRect, output.fullscreenBackingBarFrame));
   EXPECT_EQ(567, output.findBarMaxY);
-  EXPECT_TRUE(NSEqualRects(NSMakeRect(0, 495, 600, 86), output.infoBarFrame));
+  EXPECT_TRUE(NSEqualRects(NSMakeRect(0, 495, 600, 72), output.infoBarFrame));
   EXPECT_TRUE(NSEqualRects(NSZeroRect, output.downloadShelfFrame));
   EXPECT_TRUE(
       NSEqualRects(NSMakeRect(0, 0, 600, 495), output.contentAreaFrame));
+}
+
+TEST_F(BrowserWindowLayoutTest, TestInfobarLayoutWithoutToolbarOrLocationBar) {
+  [layout setHasTabStrip:NO];
+  [layout setHasToolbar:NO];
+  [layout setHasLocationBar:NO];
+  [layout setBookmarkBarHidden:YES];
+  [layout setHasDownloadShelf:NO];
+
+  chrome::LayoutOutput output = [layout computeLayout];
+
+  EXPECT_TRUE(NSEqualRects(NSMakeRect(0, 528, 600, 72), output.infoBarFrame));
 }
