d6ab949d6b0b36193e66f8f75d0d5b3392750eb7
Cong Liu <cong.liu@intel.com>
2015-04-28 22:12:13 +0800

    Remove force lowercase commandline parameters for Windows

--- base/command_line.cc
+++ base/command_line.cc
@@ -98,12 +98,12 @@ void AppendSwitchesAndArguments(CommandLine& command_line,
 }
 
 // Lowercase switches for backwards compatiblity *on Windows*.
-std::string LowerASCIIOnWindows(const std::string& string) {
-#if defined(OS_WIN)
-  return base::StringToLowerASCII(string);
-#elif defined(OS_POSIX)
+const std::string& LowerASCIIOnWindows(const std::string& string) {
+// #if defined(OS_WIN)
+//   return base::StringToLowerASCII(string);
+// #elif defined(OS_POSIX)
   return string;
-#endif
+// #endif
 }
 
 
