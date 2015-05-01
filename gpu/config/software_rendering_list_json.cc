4adc54082e4da88d78984e78cba0cfde499d8dca
Bo Liu <boliu@chromium.org>
2015-05-01 14:45:45 -0700

    branch 2311_135: Expand gpu raster blacklist to all PowerVR Rogue
    Do not limit to android version < 5.0. We are seeing issues on Android
    5.0 and up as well.
    
    BUG=483574
    
    Review URL: https://codereview.chromium.org/1120763003
    
    Cr-Commit-Position: refs/heads/master@{#327974}
    (cherry picked from commit e0f51802e33cbcd9c5d21cebb172d6bdecf95dc6)
    TBR=boliu@chromium.org
    
    Review URL: https://codereview.chromium.org/1118773003
    
    Cr-Commit-Position: refs/branch-heads/2311_135@{#12}
    Cr-Branched-From: 4e7063b6f4e912e3cb334f7feadf29ea49a29cab-refs/branch-heads/2311@{#506}
    Cr-Branched-From: 09b7de5dd7254947cd4306de907274fa63373d48-refs/heads/master@{#317474}

--- gpu/config/software_rendering_list_json.cc
+++ gpu/config/software_rendering_list_json.cc
@@ -18,7 +18,7 @@ const char kSoftwareRenderingListJson[] = LONG_STRING_CONST(
 {
   "name": "software rendering list",
   // Please update the version number whenever you change this file.
-  "version": "10.1",
+  "version": "10.7",
   "entries": [
     {
       "id": 1,
@@ -1155,13 +1155,9 @@ LONG_STRING_CONST(
     {
       "id": 104,
       "description": "GPU raster broken on PowerVR Rogue",
-      "cr_bugs": [436331],
+      "cr_bugs": [436331, 483574],
       "os": {
-        "type": "android",
-        "version": {
-          "op": "<",
-          "value": "5.0"
-        }
+        "type": "android"
       },
       "gl_renderer": "PowerVR Rogue.*",
       "features": [
