338f3632bb617e81db2e08a4df1dc410420685b9
Torne (Richard Coles) <torne@chromium.org>
2015-05-01 18:12:53 +0100

    Revert "Fix problem with accents caused by race condition."
    This reverts commit b9ef84152bce335b4814175204744ea2d5b148b4 to fix an
    issue with the Samsung IME where composition inserts previously deleted
    characters. This revert is only for WebView (minibranch 2311_135). We'll
    find a real fix in later releases.
    
    BUG=483514
    R=amineer@chromium.org
    
    Review URL: https://codereview.chromium.org/1116003006
    
    Cr-Commit-Position: refs/branch-heads/2311_135@{#7}
    Cr-Branched-From: 4e7063b6f4e912e3cb334f7feadf29ea49a29cab-refs/branch-heads/2311@{#506}
    Cr-Branched-From: 09b7de5dd7254947cd4306de907274fa63373d48-refs/heads/master@{#317474}

--- content/public/android/java/src/org/chromium/content/browser/input/AdapterInputConnection.java
+++ content/public/android/java/src/org/chromium/content/browser/input/AdapterInputConnection.java
@@ -393,14 +393,6 @@ public class AdapterInputConnection extends BaseInputConnection {
             return mImeAdapter.translateAndSendNativeEvents(event, NO_ACCENT);
         }
 
-        // Some keys we just want to pass events straight through.  This allows
-        // proper "repeating key" behavior with physical keyboards.
-        int eventKeyCode = event.getKeyCode();
-        if (eventKeyCode == KeyEvent.KEYCODE_DEL || eventKeyCode == KeyEvent.KEYCODE_FORWARD_DEL) {
-            mPendingAccent = 0;
-            return mImeAdapter.translateAndSendNativeEvents(event, NO_ACCENT);
-        }
-
         int unicodeChar = event.getUnicodeChar();
 
         // If this is a key-up, and backspace/del or if the key has a character representation,
@@ -439,6 +431,7 @@ public class AdapterInputConnection extends BaseInputConnection {
                 return true;
             }
         }
+        mImeAdapter.translateAndSendNativeEvents(event, mPendingAccent);
 
         // Physical keyboards also have their events come through here though not
         // by BaseInputConnection.  In order to support "accent" key sequences
@@ -449,11 +442,9 @@ public class AdapterInputConnection extends BaseInputConnection {
         // Copy class variable to local because class version may get indirectly
         // cleared by the deleteSurroundingText() call below.
         int pendingAccent = mPendingAccent;
-        int nextAccent = mPendingAccent;
 
         if ((unicodeChar & KeyCharacterMap.COMBINING_ACCENT) != 0) {
-            pendingAccent = NO_ACCENT;
-            nextAccent = unicodeChar & KeyCharacterMap.COMBINING_ACCENT_MASK;
+            pendingAccent = unicodeChar & KeyCharacterMap.COMBINING_ACCENT_MASK;
         } else if (pendingAccent != NO_ACCENT) {
             if (event.getAction() == KeyEvent.ACTION_DOWN) {
                 int combined = KeyEvent.getDeadChar(pendingAccent, unicodeChar);
@@ -467,18 +458,16 @@ public class AdapterInputConnection extends BaseInputConnection {
                     // Previous accent doesn't combine with this character
                     // so assume both are completely independent.
                     pendingAccent = NO_ACCENT;
-                    nextAccent = NO_ACCENT;
                 }
             }
 
             if (event.getAction() == KeyEvent.ACTION_UP) {
                 // Forget accent after release of key being accented.
-                nextAccent = NO_ACCENT;
+                pendingAccent = NO_ACCENT;
             }
         }
 
-        mImeAdapter.translateAndSendNativeEvents(event, pendingAccent);
-        mPendingAccent = nextAccent;
+        mPendingAccent = pendingAccent;
         return true;
     }
 
