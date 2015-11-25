d5d3b65b9c43a97f35dabe32fa6f399ab5e3d106
rdevlin.cronin <rdevlin.cronin@chromium.org>
2015-11-25 13:08:14 -0800

    [Extensions] Make handler_function a hidden property
    Fix the glitch and add a test.
    
    BUG=548273
    TBR=jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/1422383003
    
    Cr-Commit-Position: refs/heads/master@{#356985}
    (cherry picked from commit a5ecbc86a598e29ce3a2424c9c5386bfba4e9b74)
    
    NOTRY=true
    NOPRESUBMIT=true
    
    Review URL: https://codereview.chromium.org/1465223004
    
    Cr-Commit-Position: refs/branch-heads/2526@{#473}
    Cr-Branched-From: cb947c0153db0ec02a8abbcb3ca086d88bf6006f-refs/heads/master@{#352221}

--- chrome/browser/extensions/extension_bindings_apitest.cc
+++ chrome/browser/extensions/extension_bindings_apitest.cc
@@ -8,12 +8,15 @@
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/net/url_request_mock_util.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/test/browser_test_utils.h"
 #include "extensions/browser/extension_host.h"
 #include "extensions/browser/process_manager.h"
 #include "extensions/test/extension_test_message_listener.h"
 #include "extensions/test/result_catcher.h"
+#include "net/test/embedded_test_server/embedded_test_server.h"
 
 namespace extensions {
 namespace {
@@ -109,6 +112,25 @@ IN_PROC_BROWSER_TEST_F(ExtensionBindingsApiTest, ApiEnums) {
 // getModuleSystem().
 IN_PROC_BROWSER_TEST_F(ExtensionBindingsApiTest, ModuleSystem) {
   ASSERT_TRUE(RunExtensionTest("bindings/module_system")) << message_;
+}
+
+IN_PROC_BROWSER_TEST_F(ExtensionBindingsApiTest, HandlerFunctionTypeChecking) {
+  ASSERT_TRUE(embedded_test_server()->InitializeAndWaitUntilReady());
+  ui_test_utils::NavigateToURL(
+      browser(),
+      embedded_test_server()->GetURL(
+          "/extensions/api_test/bindings/handler_function_type_checking.html"));
+  content::WebContents* web_contents =
+      browser()->tab_strip_model()->GetActiveWebContents();
+  EXPECT_FALSE(web_contents->IsCrashed());
+  // See handler_function_type_checking.html.
+  std::string result;
+  EXPECT_TRUE(content::ExecuteScriptAndExtractString(
+      web_contents,
+      "window.domAutomationController.send("
+          "document.getElementById('status').textContent.trim());",
+      &result));
+  EXPECT_EQ("success", result);
 }
 
 }  // namespace
