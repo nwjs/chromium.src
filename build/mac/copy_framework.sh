75a3dac3112b1e319eaad8d828f99aa1fa7bcba0
Cong Liu <cong.liu@intel.com>
2016-04-28 14:01:16 +0800

    Fix for MAS submission requirements
    * Fix for "Anatomy of Framework Bundles" compatible
      * Apple requires frameworks to be "versioned". Files in top folder
    should be symblic links to it's current version folder (`Versions/Current`),
    which is also a symbolic link to a real version in the same folder
    (like `Versions/A`).
    * Fixed property list file for mac
      * Removed `CFBundleDocumentTypes` and `CFBundleURLTypes`
      * Declared `*.nw` files being opened by NW.js
    * Removed app_mode_app
    * Removed crashpad for MAS
    * Not to symbolic link to Helpers
      * Helpers folder will not be created if not copying crashpad_handler.
    Symbolic link to Helpers folder will be invalid for uploading to
    App Store.
    * Not to symbolic link to 'Internet Plug-Ins'
      * 'Internet Plug-Ins' folder will not be created if nacl is not copied.
    Symbolic link to this folder will be invalid for uploading to App
    Store.
    * Updated default icons with all required scales
    
    According to [OS X Human Interface Guidelines][1], app icons should
    provide scales of following resolutions:
    
    * icon_512x512@2x : 1024 x 1024
    * icon_512x512    : 512 x 512
    * icon_256x256@2x : 512 x 512
    * icon_256x256    : 256 x 256
    * icon_128x128@2x : 256 x 256
    * icon_128x128    : 128 x 128
    * icon_32x32@2x   : 64 x 64
    * icon_32x32      : 32 x 32
    * icon_16x16@2x   : 32 x 32
    * icon_16x16      : 16 x 16
    
    The new icons are generated from original icon files by using `iconutil`
    tool
    
    ```
    iconutil -c iconset -o <file>.iconset <file>.icns
    iconutil -c icns -o <file>.icns <file>.iconset
    ```
    
    [1]: https://developer.apple.com/library/mac/documentation/UserExperience/Conceptual/OSXHIGuidelines/Designing.html

--- build/mac/copy_framework.sh
+++ build/mac/copy_framework.sh
@@ -4,47 +4,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Copies a framework to its new home, "unversioning" it.
-#
-# Normally, frameworks are versioned bundles.  The contents of a framework are
-# stored in a versioned directory within the bundle, and symbolic links
-# provide access to the actual code and resources.  See
-# http://developer.apple.com/mac/library/documentation/MacOSX/Conceptual/BPFrameworks/Concepts/FrameworkAnatomy.html
-#
-# The symbolic links usually found in frameworks create problems.  Symbolic
-# links are excluded from code signatures.  That means that it's possible to
-# remove or retarget a symbolic link within a framework without affecting the
-# seal.  In Chrome's case, the outer .app bundle contains a framework where
-# all application code and resources live.  In order for the signature on the
-# .app to be meaningful, it encompasses the framework.  Because framework
-# resources are accessed through the framework's symbolic links, this
-# arrangement results in a case where the resources can be altered without
-# affecting the .app signature's validity.
-#
-# Indirection through symbolic links also carries a runtime performance
-# penalty on open() operations, although open() typically completes so quickly
-# that this is not considered a major performance problem.
-#
-# To resolve these problems, the frameworks that ship within Chrome's .app
-# bundle are unversioned.  Unversioning is simple: instead of using the
-# original outer .framework directory as the framework that ships within the
-# .app, the inner versioned directory is used.  Instead of accessing bundled
-# resources through symbolic links, they are accessed directly.  In normal
-# situations, the only hard-coded use of the versioned directory is by dyld,
-# when loading the framework's code, but this is handled through a normal
-# Mach-O load command, and it is easy to adjust the load command to point to
-# the unversioned framework code rather than the versioned counterpart.
-#
-# The resulting framework bundles aren't strictly conforming, but they work
-# as well as normal versioned framework bundles.
-#
-# An option to skip running install_name_tool is available. By passing -I as
-# the first argument to this script, install_name_tool will be skipped. This
-# is only suitable for copied frameworks that will not be linked against, or
-# when install_name_tool will be run on any linker output when something is
-# linked against the copied framework. This option exists to allow signed
-# frameworks to pass through without subjecting them to any modifications that
-# would break their signatures.
+# Copies a framework to its new home and keep its version for MAS
 
 set -e
 
@@ -87,11 +47,10 @@ fi
 
 DESTINATION="${DESTINATION_DIR}/${FRAMEWORK_NAME}"
 
-# Copy the versioned directory within the versioned framework to its
-# destination location.
+# Copy the entire framework to its destination location.
 mkdir -p "${DESTINATION_DIR}"
 rsync -acC --delete --exclude Headers --exclude PrivateHeaders \
-    --include '*.so' "${CURRENT_VERSION}/" "${DESTINATION}"
+    --include '*.so' "${FRAMEWORK}/" "${DESTINATION}"
 
 if [[ -n "${RUN_INSTALL_NAME_TOOL}" ]]; then
   # Adjust the Mach-O LC_ID_DYLIB load command in the framework.  This does not
