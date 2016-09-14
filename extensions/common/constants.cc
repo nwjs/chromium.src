8e30c84d76b424a5e883ebb928154a287b3e2988
Roger Wang <roger.wang@intel.com>
2016-09-14 14:28:15 +0800

    nwjs master

--- extensions/common/constants.cc
+++ extensions/common/constants.cc
@@ -34,6 +34,8 @@ const char kDecodedMessageCatalogsFilename[] = "DECODED_MESSAGE_CATALOGS";
 
 const char kGeneratedBackgroundPageFilename[] =
     "_generated_background_page.html";
+const char kNWJSDefaultAppJS[] =
+    "nwjs/default.js";
 
 const char kModulesDir[] = "_modules";
 
@@ -57,6 +59,8 @@ const char kWebStoreAppId[] = "ahfgeienlihckogmohjhadlkjgocpleb";
 const char kMimeTypeJpeg[] = "image/jpeg";
 const char kMimeTypePng[] = "image/png";
 
+const base::FilePath::CharType kNWJSManifestFilename[] =
+    FILE_PATH_LITERAL("package.json");
 }  // namespace extensions
 
 namespace extension_misc {
