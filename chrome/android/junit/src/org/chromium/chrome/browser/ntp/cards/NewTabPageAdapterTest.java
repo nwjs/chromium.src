d66cbbe3418cfef4486862bd09af0b5519720375
Ian Wen <ianwen@google.com>
2016-10-07 15:58:27 -0700

    Revert "Do not allow sign in if Google play service is absent"
    This reverts commit 7aa9383118fa65b813a5f57900cece8b66028a83.
    
    BUG=653832
    TBR=dfalcantara@chromium.org
    
    Review URL: https://codereview.chromium.org/2402093002 .
    
    Cr-Commit-Position: refs/branch-heads/2883@{#7}
    Cr-Branched-From: 614d31daee2f61b0180df403a8ad43f20b9f6dd7-refs/heads/master@{#423768}

--- chrome/android/junit/src/org/chromium/chrome/browser/ntp/cards/NewTabPageAdapterTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/ntp/cards/NewTabPageAdapterTest.java
@@ -794,7 +794,6 @@ public class NewTabPageAdapterTest {
     @Test
     @Feature({"Ntp"})
     public void testSigninPromo() {
-        when(mMockSigninManager.isSignInAllowed()).thenReturn(true);
         when(mMockSigninManager.isSignedInOnNative()).thenReturn(false);
         MockNewTabPageManager ntpManager = new MockNewTabPageManager(mSource);
         NewTabPageAdapter adapter = NewTabPageAdapter.create(ntpManager, null, null);
@@ -826,7 +825,6 @@ public class NewTabPageAdapterTest {
     @Test
     @Feature({"Ntp"})
     public void testSigninPromoDismissal() {
-        when(mMockSigninManager.isSignInAllowed()).thenReturn(true);
         when(mMockSigninManager.isSignedInOnNative()).thenReturn(false);
         ChromePreferenceManager.getInstance(RuntimeEnvironment.application)
                 .setNewTabPageSigninPromoDismissed(false);
