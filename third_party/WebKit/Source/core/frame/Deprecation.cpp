ae7cb4eda8449b71c3fb1c9860dcaebf8d707964
Rick Byers <rbyers@chromium.org>
2016-10-14 12:43:54 -0400

    Punt touch UGI and doppler deprecations to M56
    Cherry pick and also update the doppler warning to M56 (which was removed from trunk already so can't be merged).
    
    BUG=654978,656030
    
    Review-Url: https://codereview.chromium.org/2408203004
    Cr-Commit-Position: refs/heads/master@{#424739}
    (cherry picked from commit 5228ba9c95482de8e3615f189bcf822806e99010)
    
    R=rtoy@chromium.org
    
    Review URL: https://codereview.chromium.org/2422673002 .
    
    Cr-Commit-Position: refs/branch-heads/2883@{#109}
    Cr-Branched-From: 614d31daee2f61b0180df403a8ad43f20b9f6dd7-refs/heads/master@{#423768}

--- third_party/WebKit/Source/core/frame/Deprecation.cpp
+++ third_party/WebKit/Source/core/frame/Deprecation.cpp
@@ -15,7 +15,6 @@
 namespace {
 
 enum Milestone {
-  M55,
   M56,
   M57,
   M58,
@@ -27,8 +26,6 @@ const char* milestoneString(Milestone milestone) {
   // https://www.chromium.org/developers/calendar
 
   switch (milestone) {
-    case M55:
-      return "M55, around December 2016";
     case M56:
       return "M56, around January 2017";
     case M57:
@@ -294,19 +291,19 @@ String Deprecation::deprecationMessage(UseCounter::Feature feature) {
              "https://code.google.com/p/chromium/issues/detail?id=437569#c2";
 
     case UseCounter::AudioListenerDopplerFactor:
-      return dopplerWillBeRemoved("'AudioListener.dopplerFactor'", M55,
+      return dopplerWillBeRemoved("'AudioListener.dopplerFactor'", M56,
                                   "5238926818148352");
 
     case UseCounter::AudioListenerSpeedOfSound:
-      return dopplerWillBeRemoved("'AudioListener.speedOfSound'", M55,
+      return dopplerWillBeRemoved("'AudioListener.speedOfSound'", M56,
                                   "5238926818148352");
 
     case UseCounter::AudioListenerSetVelocity:
-      return dopplerWillBeRemoved("'AudioListener.setVelocity()'", M55,
+      return dopplerWillBeRemoved("'AudioListener.setVelocity()'", M56,
                                   "5238926818148352");
 
     case UseCounter::PannerNodeSetVelocity:
-      return dopplerWillBeRemoved("'PannerNode.setVelocity()'", M55,
+      return dopplerWillBeRemoved("'PannerNode.setVelocity()'", M56,
                                   "5238926818148352");
 
     case UseCounter::PrefixedWindowURL:
@@ -443,19 +440,19 @@ String Deprecation::deprecationMessage(UseCounter::Feature feature) {
       return willBeRemoved(
           "Performing operations that require explicit user interaction on "
           "touchstart events",
-          M55, "5649871251963904");
+          M56, "5649871251963904");
 
     case UseCounter::TouchMoveUserGestureUtilized:
       return willBeRemoved(
           "Performing operations that require explicit user interaction on "
           "touchmove events",
-          M55, "5649871251963904");
+          M56, "5649871251963904");
 
     case UseCounter::TouchEndDuringScrollUserGestureUtilized:
       return willBeRemoved(
           "Performing operations that require explicit user interaction on "
           "touchend events that occur as part of a scroll",
-          M55, "5649871251963904");
+          M56, "5649871251963904");
 
     case UseCounter::MIDIMessageEventReceivedTime:
       return willBeRemoved("MIDIMessageEvent.receivedTime", M56,
