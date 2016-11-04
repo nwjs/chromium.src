273ba7eecd4b8654aae31b6980d8094a39b9a392
Chris Cunningham <chcunningham@chromium.org>
2016-11-04 16:21:24 -0700

    [TO 54] AudioRendererImpl: Don't advance time when rendering stops.
    This is a quick work-around for the bug below. AudioRendererImpl was
    recently improved to estimate time's advancing along the last rendered
    audio buffer between Render() calls. This means the clock is capable of
    still advancing a bit after OnBufferingStateChange indicates we've run
    out of data. While tehcnically correct, the clocks advancing causes
    blink to emit 'timeupdate' events with time advancing after emitting
    'waiting' indicating that no data is buffered. This change stops the
    clock once the render has underflowed.
    
    Longterm, blink should be improved to gaurd against emitting timeupdate
    after waiting. This larger change will be done separately to give it
    time to bake. https://codereview.chromium.org/2425463002/
    
    BUG=648710
    
    Review-Url: https://codereview.chromium.org/2423903003
    Cr-Commit-Position: refs/heads/master@{#425871}
    (cherry picked from commit 9409db28cd7402d4bad8022afbd2b3719de72d08)
    
    --ALSO--
    
    [TO 54] AudioRendererImpl: Fix data race.
    
    Previous CL introduced multithreaded access to |rendering_|.
    https://codereview.chromium.org/2423903003/
    
    BUG=657001,648710
    
    Review-Url: https://chromiumcodereview.appspot.com/2434003002
    Cr-Commit-Position: refs/heads/master@{#426312}
    (cherry picked from commit e2f429e24bdbcd8e8379b909879e2c729490e1c6)
    
    TBR=sandersd@chromium.org
    
    Review URL: https://codereview.chromium.org/2472183004 .
    
    Cr-Commit-Position: refs/branch-heads/2840@{#821}
    Cr-Branched-From: 1ae106dbab4bddd85132d5b75c670794311f4c57-refs/heads/master@{#414607}

--- media/renderers/audio_renderer_impl.cc
+++ media/renderers/audio_renderer_impl.cc
@@ -102,10 +102,12 @@ AudioRendererImpl::~AudioRendererImpl() {
 void AudioRendererImpl::StartTicking() {
   DVLOG(1) << __func__;
   DCHECK(task_runner_->BelongsToCurrentThread());
+
+  base::AutoLock auto_lock(lock_);
+
   DCHECK(!rendering_);
   rendering_ = true;
 
-  base::AutoLock auto_lock(lock_);
   // Wait for an eventual call to SetPlaybackRate() to start rendering.
   if (playback_rate_ == 0) {
     DCHECK(!sink_playing_);
@@ -132,10 +134,12 @@ void AudioRendererImpl::StartRendering_Locked() {
 void AudioRendererImpl::StopTicking() {
   DVLOG(1) << __func__;
   DCHECK(task_runner_->BelongsToCurrentThread());
+
+  base::AutoLock auto_lock(lock_);
+
   DCHECK(rendering_);
   rendering_ = false;
 
-  base::AutoLock auto_lock(lock_);
   // Rendering should have already been stopped with a zero playback rate.
   if (playback_rate_ == 0) {
     DCHECK(!sink_playing_);
@@ -172,10 +176,26 @@ void AudioRendererImpl::SetMediaTime(base::TimeDelta time) {
   first_packet_timestamp_ = kNoTimestamp;
   last_media_timestamp_ = base::TimeDelta();
   audio_clock_.reset(new AudioClock(time, audio_parameters_.sample_rate()));
+  last_reported_media_time_ = kNoTimestamp;
 }
 
 base::TimeDelta AudioRendererImpl::CurrentMediaTime() {
   base::AutoLock auto_lock(lock_);
+
+  // Re-use last reported time if rendering has stopped to avoid confusing blink
+  // layer (avoid currentTime advancing after signaling buffer underflow).
+  // TODO(chcunningham): Do this in blink instead. Patching it here for now
+  // because HTMLMediaElement's currentTime is a mess.
+  if (!rendering_ && last_reported_media_time_ != kNoTimestamp) {
+    // If rendering stops, be sure to at least report the front time of the most
+    // recently rendered audio buffer.
+    if (last_reported_media_time_ < audio_clock_->front_timestamp())
+      last_reported_media_time_ = audio_clock_->front_timestamp();
+
+    DVLOG(3) << __func__ << " Returning cached time while rendering stopped:"
+             << last_reported_media_time_.InMicroseconds();
+    return last_reported_media_time_;
+  }
 
   // Return the current time based on the known extents of the rendered audio
   // data plus an estimate based on the last time those values were calculated.
@@ -201,6 +221,7 @@ base::TimeDelta AudioRendererImpl::CurrentMediaTime() {
 
   DVLOG(2) << __func__ << ": " << current_media_time;
   last_media_timestamp_ = current_media_time;
+  last_reported_media_time_ = current_media_time;
   return current_media_time;
 }
 
@@ -445,6 +466,7 @@ void AudioRendererImpl::Initialize(DemuxerStream* stream,
 
   audio_clock_.reset(
       new AudioClock(base::TimeDelta(), audio_parameters_.sample_rate()));
+  last_reported_media_time_ = kNoTimestamp;
 
   audio_buffer_stream_->Initialize(
       stream, base::Bind(&AudioRendererImpl::OnAudioBufferStreamInitialized,
