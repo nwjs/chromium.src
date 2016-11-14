d0772abb3a3ca461bcd6832e8b435a68799f7184
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2016-11-14 13:52:35 -0500

    [Merge] Add accessibility label to MD User Menu current profile section.
    BUG=619725
    
    Review-Url: https://codereview.chromium.org/2492893002
    Cr-Commit-Position: refs/heads/master@{#431557}
    (cherry picked from commit 789e3665e9506489a4cd2f6184c07b43c0674588)
    
    Review URL: https://codereview.chromium.org/2497353002 .
    
    Cr-Commit-Position: refs/branch-heads/2883@{#561}
    Cr-Branched-From: 614d31daee2f61b0180df403a8ad43f20b9f6dd7-refs/heads/master@{#423768}

--- chrome/browser/ui/views/profiles/profile_chooser_view.cc
+++ chrome/browser/ui/views/profiles/profile_chooser_view.cc
@@ -1729,6 +1729,9 @@ views::View* ProfileChooserView::CreateMaterialDesignCurrentProfileView(
     return view;
   }
 
+  const base::string16 profile_name =
+      profiles::GetAvatarNameForProfile(browser_->profile()->GetPath());
+
   // The available links depend on the type of profile that is active.
   if (avatar_item.signed_in) {
     if (switches::IsEnableAccountConsistency()) {
@@ -1755,6 +1758,12 @@ views::View* ProfileChooserView::CreateMaterialDesignCurrentProfileView(
           gfx::Insets(name_container_v_spacing, 0));
       profile_name_container->AddChildView(email_label);
     }
+
+    current_profile_card_->SetAccessibleName(
+        l10n_util::GetStringFUTF16(
+            IDS_PROFILES_EDIT_SIGNED_IN_PROFILE_ACCESSIBLE_NAME,
+            profile_name,
+            avatar_item.username));
     return view;
   }
 
@@ -1787,6 +1796,9 @@ views::View* ProfileChooserView::CreateMaterialDesignCurrentProfileView(
     view->AddChildView(extra_links_view);
   }
 
+  current_profile_card_->SetAccessibleName(
+      l10n_util::GetStringFUTF16(
+          IDS_PROFILES_EDIT_PROFILE_ACCESSIBLE_NAME, profile_name));
   return view;
 }
 
