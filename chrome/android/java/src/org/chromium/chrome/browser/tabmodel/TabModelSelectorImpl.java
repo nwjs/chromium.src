b27a7d7a86cfdc06c04ab3728e08ff10f786b975
Alexandre Elias <aelias@chromium.org>
2016-11-16 18:02:30 -0800

    Fix three causes of blank thumbnails [2nd land]
    All three of these fixes are required to address the devil case
    http://crbug.com/659459, and some of them also help with some of the
    other flickers.
    
    1. In onPageLoadStarted, the URL can be the same as before, in
    particular because history-item scroll offset restores count as load
    starts.  These history restores often fire late in page load, *after*
    the thumbnail was taken, leading to immediately throwing out the
    thumbnail we just took earlier in page load.  Switch to the "invalidate"
    path which only ejects the thumbnail if the URL changed.
    
    2. In CacheTab, which is often called just to refresh the thumbnail with
    the latest state, if readback is currently impossible, we used to remove
    the old thumbnail.  Keep it instead.
    
    3. Switch to the more reliable Surfaces-based mechanism for readback.
    The layer-based mechanism often outputs black screenshots in difficult
    racy cases like onStop readbacks.  The Surface-based mechanism had been
    reverted because it tickles a driver bug on Nexus 7 2013 that itself causes
    some flickers, but I verified that the repro scenario for that is quite
    niche, and even on Nexus 7 it's probably less severe than the black
    screenshot problem this fixes.
    
    TBR=tedchoc@chromium.org
    BUG=636630,640561,659459,646336
    
    Review URL: https://codereview.chromium.org/2503363003 .
    
    Review-Url: https://codereview.chromium.org/2503183003
    Cr-Original-Commit-Position: refs/heads/master@{#432411}
    Cr-Commit-Position: refs/branch-heads/2883@{#597}
    Cr-Branched-From: 614d31daee2f61b0180df403a8ad43f20b9f6dd7-refs/heads/master@{#423768}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorImpl.java
@@ -12,7 +12,6 @@ import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.compositor.layouts.OverviewModeBehavior;
 import org.chromium.chrome.browser.compositor.layouts.content.TabContentManager;
-import org.chromium.chrome.browser.ntp.NativePageFactory;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabModel.TabLaunchType;
 import org.chromium.chrome.browser.tabmodel.TabModel.TabSelectionType;
@@ -195,11 +194,7 @@ public class TabModelSelectorImpl extends TabModelSelectorBase implements TabMod
             @Override
             public void onPageLoadStarted(Tab tab, String url) {
                 String previousUrl = tab.getUrl();
-                if (NativePageFactory.isNativePageUrl(previousUrl, tab.isIncognito())) {
-                    mTabContentManager.invalidateTabThumbnail(tab.getId(), previousUrl);
-                } else {
-                    mTabContentManager.removeTabThumbnail(tab.getId());
-                }
+                mTabContentManager.invalidateTabThumbnail(tab.getId(), previousUrl);
             }
 
             @Override
