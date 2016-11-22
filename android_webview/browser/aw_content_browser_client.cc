b0152a63b6a341a79949f97b8d77ea876d73727c
Han Leon <leon.han@intel.com>
2016-11-22 13:39:12 +0800

    [Autofill]Enable mojo AutofillDriver for android_webview.
    BUG=662040
    TBR=sgurun@chromium.org
    
    Review-Url: https://codereview.chromium.org/2491103003
    Cr-Commit-Position: refs/heads/master@{#433418}
    (cherry picked from commit e404109db1d011479ea0fee59c5d415f44d73b22)
    
    Review URL: https://codereview.chromium.org/2522813002 .
    
    Cr-Commit-Position: refs/branch-heads/2883@{#645}
    Cr-Branched-From: 614d31daee2f61b0180df403a8ad43f20b9f6dd7-refs/heads/master@{#423768}

--- android_webview/browser/aw_content_browser_client.cc
+++ android_webview/browser/aw_content_browser_client.cc
@@ -31,6 +31,7 @@
 #include "base/files/scoped_file.h"
 #include "base/memory/ptr_util.h"
 #include "base/path_service.h"
+#include "components/autofill/content/browser/content_autofill_driver_factory.h"
 #include "components/cdm/browser/cdm_message_filter_android.h"
 #include "components/crash/content/browser/crash_micro_dump_manager_android.h"
 #include "components/navigation_interception/intercept_navigation_delegate.h"
@@ -52,6 +53,7 @@
 #include "net/android/network_library.h"
 #include "net/ssl/ssl_cert_request_info.h"
 #include "net/ssl/ssl_info.h"
+#include "services/shell/public/cpp/interface_registry.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/resource/resource_bundle_android.h"
 #include "ui/resources/grit/ui_resources.h"
@@ -529,6 +531,14 @@ AwContentBrowserClient::CreateThrottlesForNavigation(
 content::DevToolsManagerDelegate*
 AwContentBrowserClient::GetDevToolsManagerDelegate() {
   return new AwDevToolsManagerDelegate();
+}
+
+void AwContentBrowserClient::RegisterRenderFrameMojoInterfaces(
+    shell::InterfaceRegistry* registry,
+    content::RenderFrameHost* render_frame_host) {
+  registry->AddInterface(
+      base::Bind(&autofill::ContentAutofillDriverFactory::BindAutofillDriver,
+                 render_frame_host));
 }
 
 }  // namespace android_webview
