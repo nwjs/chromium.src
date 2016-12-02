1070e6a7387b6481179626e497594fb9e50f523e
tmartino <tmartino@chromium.org>
2016-12-02 09:44:15 -0800

    Moving new First Run to use per-profile value, rather than simply checking IsChromeFirstRun.
    This solves several edge cases described in the bug.
    
    BUG=660159
    NOTRY=TRUE
    NOPRESUBMIT=TRUE
    
    Review-Url: https://codereview.chromium.org/2487553002
    Cr-Commit-Position: refs/heads/master@{#433466}
    (cherry picked from commit 6254f4715b9a55b9dac805d432ce9f33376d65d6)
    
    Review-Url: https://codereview.chromium.org/2537773005
    Cr-Commit-Position: refs/branch-heads/2924@{#288}
    Cr-Branched-From: 3a87aecc31cd1ffe751dd72c04e5a96a1fc8108a-refs/heads/master@{#433059}

--- chrome/common/pref_names.cc
+++ chrome/common/pref_names.cc
@@ -1306,6 +1306,11 @@ const char kWebRTCIPHandlingPolicy[] = "webrtc.ip_handling_policy";
 const char kWebRTCUDPPortRange[] = "webrtc.udp_port_range";
 #endif
 
+#if !defined(OS_ANDROID)
+// Whether or not this profile has been shown the Welcome page.
+const char kHasSeenWelcomePage[] = "browser.has_seen_welcome_page";
+#endif
+
 // *************** LOCAL STATE ***************
 // These are attached to the machine/installation
 
