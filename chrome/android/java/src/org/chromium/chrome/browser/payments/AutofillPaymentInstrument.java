4ab21a02a321164016d9e666219cee0f62af5da7
gogerald <gogerald@google.com>
2016-12-14 14:16:47 -0500

    Set card and address editor title when editing complete cards and addresses
    BUG=674161
    
    Review-Url: https://codereview.chromium.org/2569233006
    Cr-Commit-Position: refs/heads/master@{#438552}
    (cherry picked from commit a7bb5795f41d2465fdd2dd5855fab304e166ea1b)
    
    Review-Url: https://codereview.chromium.org/2576763002 .
    Cr-Commit-Position: refs/branch-heads/2924@{#495}
    Cr-Branched-From: 3a87aecc31cd1ffe751dd72c04e5a96a1fc8108a-refs/heads/master@{#433059}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/AutofillPaymentInstrument.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/AutofillPaymentInstrument.java
@@ -250,7 +250,7 @@ public class AutofillPaymentInstrument extends PaymentInstrument
      */
     private void checkAndUpateCardCompleteness() {
         int editMessageResId = 0; // Zero is the invalid resource Id.
-        int editTitleResId = 0;
+        int editTitleResId = R.string.payments_edit_card;
         int invalidFieldsCount = 0;
 
         if (mBillingAddress == null) {
@@ -281,7 +281,7 @@ public class AutofillPaymentInstrument extends PaymentInstrument
         }
 
         mEditMessage = editMessageResId == 0 ? null : mContext.getString(editMessageResId);
-        mEditTitle = editTitleResId == 0 ? null : mContext.getString(editTitleResId);
+        mEditTitle = mContext.getString(editTitleResId);
         mIsComplete = mEditMessage == null;
     }
 
