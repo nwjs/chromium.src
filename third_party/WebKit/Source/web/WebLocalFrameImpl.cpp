4af576a4562fb7b664732f174f1a112715f31f60
Rouslan Solomakhin <rouslan@chromium.org>
2016-12-15 10:28:26 -0500

    [Merge M-56] Support allowpaymentrequest with out-of-process iframes
    BUG=669585
    
    Review-Url: https://codereview.chromium.org/2545693002
    Cr-Commit-Position: refs/heads/master@{#438724}
    (cherry picked from commit a1f17e817c65d8f3e8ba10beee7b43e625f21c04)
    
    Review-Url: https://codereview.chromium.org/2578773003 .
    Cr-Commit-Position: refs/branch-heads/2924@{#510}
    Cr-Branched-From: 3a87aecc31cd1ffe751dd72c04e5a96a1fc8108a-refs/heads/master@{#433059}

--- third_party/WebKit/Source/web/WebLocalFrameImpl.cpp
+++ third_party/WebKit/Source/web/WebLocalFrameImpl.cpp
@@ -1615,7 +1615,8 @@ LocalFrame* WebLocalFrameImpl::createChildFrame(
   WebFrameOwnerProperties ownerProperties(
       ownerElement->scrollingMode(), ownerElement->marginWidth(),
       ownerElement->marginHeight(), ownerElement->allowFullscreen(),
-      ownerElement->csp(), ownerElement->delegatedPermissions());
+      ownerElement->allowPaymentRequest(), ownerElement->csp(),
+      ownerElement->delegatedPermissions());
   // FIXME: Using subResourceAttributeName as fallback is not a perfect
   // solution. subResourceAttributeName returns just one attribute name. The
   // element might not have the attribute, and there might be other attributes
