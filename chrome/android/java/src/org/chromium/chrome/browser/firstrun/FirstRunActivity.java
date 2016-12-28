9c8cfe469b2a0136611eab784e17ba01c75fcd5f
Khushal <khushalsagar@google.com>
2016-12-28 08:11:37 -0800

    Remove unused context params from FirstRunStatus.
    Split out from:
    https://codereview.chromium.org/2559573002/
    
    There is a downstream caller for one method, so will
    land this first to allow a quicker patch time.
    
    TBR=yusufo@chromium.org
    BUG=671149, 677235
    
    Review-Url: https://codereview.chromium.org/2565183005
    Cr-Commit-Position: refs/heads/master@{#437937}
    (cherry picked from commit f95b8c27f742ee59f328e9d05838a163c9c177e2)
    
    Review-Url: https://codereview.chromium.org/2605053002 .
    Cr-Commit-Position: refs/branch-heads/2924@{#631}
    Cr-Branched-From: 3a87aecc31cd1ffe751dd72c04e5a96a1fc8108a-refs/heads/master@{#433059}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivity.java
@@ -228,7 +228,7 @@ public class FirstRunActivity extends AppCompatActivity implements FirstRunPageD
         super.onStart();
         stopProgressionIfNotAcceptedTermsOfService();
         if (!mFreProperties.getBoolean(EXTRA_USE_FRE_FLOW_SEQUENCER)) {
-            if (FirstRunStatus.getFirstRunFlowComplete(this)) {
+            if (FirstRunStatus.getFirstRunFlowComplete()) {
                 // This is a parallel flow that needs to be refreshed/re-fired.
                 // Signal the FRE flow completion and re-launch the original intent.
                 completeFirstRunExperience();
@@ -363,7 +363,7 @@ public class FirstRunActivity extends AppCompatActivity implements FirstRunPageD
         // If default is true then it corresponds to opt-out and false corresponds to opt-in.
         UmaUtils.recordMetricsReportingDefaultOptIn(!DEFAULT_METRICS_AND_CRASH_REPORTING);
         sGlue.acceptTermsOfService(allowCrashUpload);
-        FirstRunStatus.setSkipWelcomePage(FirstRunActivity.this, true);
+        FirstRunStatus.setSkipWelcomePage(true);
         flushPersistentData();
         stopProgressionIfNotAcceptedTermsOfService();
         jumpToPage(mPager.getCurrentItem() + 1);
