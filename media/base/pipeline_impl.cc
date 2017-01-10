f683e0741087dbe0ddf4ed5d878bc72b61246ff8
Chris Cunningham <chcunningham@chromium.org>
2017-01-10 14:03:48 -0800

    [TO 56] Fix race in media Pipeline::GetMediaTime() after seeking.
    Seeking posts a task from main thread to media thread to seek the
    renderer. Calling GetMediaTime() should be safe even when the
    renderer has not yet received the message to performed the seek.
    
    This change gaurds against using the renderer's media time while
    a seek is pending. This prevents polluting |last_media_time_|
    with bad values from the renderer that doesn't yet know about
    the seek.
    
    BUG=675556
    TEST=New unit test && manually verfied bug fix.
    TBR=sandersd@chromium.org
    
    Review-Url: https://codereview.chromium.org/2616703002
    Cr-Commit-Position: refs/heads/master@{#441526}
    (cherry picked from commit 454da202ef172f3ecbfb48c154ff97c8c10011b0)
    
    Review-Url: https://codereview.chromium.org/2621183002 .
    Cr-Commit-Position: refs/branch-heads/2924@{#722}
    Cr-Branched-From: 3a87aecc31cd1ffe751dd72c04e5a96a1fc8108a-refs/heads/master@{#433059}

--- media/base/pipeline_impl.cc
+++ media/base/pipeline_impl.cc
@@ -938,6 +938,7 @@ void PipelineImpl::Start(Demuxer* demuxer,
   client_ = client;
   seek_cb_ = seek_cb;
   last_media_time_ = base::TimeDelta();
+  seek_time_ = kNoTimestamp;
 
   std::unique_ptr<TextRenderer> text_renderer;
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
@@ -1019,6 +1020,7 @@ void PipelineImpl::Seek(base::TimeDelta time, const PipelineStatusCB& seek_cb) {
 
   DCHECK(seek_cb_.is_null());
   seek_cb_ = seek_cb;
+  seek_time_ = time;
   last_media_time_ = base::TimeDelta();
   media_task_runner_->PostTask(
       FROM_HERE, base::Bind(&RendererWrapper::Seek,
@@ -1049,6 +1051,7 @@ void PipelineImpl::Resume(std::unique_ptr<Renderer> renderer,
   DCHECK(IsRunning());
   DCHECK(seek_cb_.is_null());
   seek_cb_ = seek_cb;
+  seek_time_ = time;
   last_media_time_ = base::TimeDelta();
 
   media_task_runner_->PostTask(
@@ -1102,6 +1105,14 @@ void PipelineImpl::SetVolume(float volume) {
 
 base::TimeDelta PipelineImpl::GetMediaTime() const {
   DCHECK(thread_checker_.CalledOnValidThread());
+
+  // Don't trust renderer time during a pending seek. Renderer may return
+  // pre-seek time which may corrupt |last_media_time_| used for clamping.
+  if (seek_time_ != kNoTimestamp) {
+    DVLOG(3) << __func__ << ": (seeking) " << seek_time_.InMilliseconds()
+             << " ms";
+    return seek_time_;
+  }
 
   base::TimeDelta media_time = renderer_wrapper_->GetMediaTime();
 
@@ -1280,6 +1291,8 @@ void PipelineImpl::OnSeekDone() {
   DVLOG(3) << __func__;
   DCHECK(thread_checker_.CalledOnValidThread());
   DCHECK(IsRunning());
+
+  seek_time_ = kNoTimestamp;
 
   DCHECK(!seek_cb_.is_null());
   base::ResetAndReturn(&seek_cb_).Run(PIPELINE_OK);
