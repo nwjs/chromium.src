7c9ed478a023c8de301121afadf6bf172ff8ea9d
Min Qin <qinmin@chromium.org>
2017-01-18 16:35:50 -0800

    Catch exception from DownloadManager.getUriForDownloadedFile() for Xiaomi phones
    This is crashing on Xiaomi phones
    Android API doc never deprecates the call, so it is their ROM issue
    Catch the exception so Chrome will not crash.
    But click on download completion notification will not have any effect.
    
    TBR=dfalcantara@chromium.org
    BUG=678938
    
    Review-Url: https://codereview.chromium.org/2616893005
    Cr-Commit-Position: refs/heads/master@{#442750}
    (cherry picked from commit f8bee1d41368c1a6f66901fdf65dbdb49e002127)
    
    Fix an issue that we are getting incognito profile on startup
    
    Calling GetDownloadManager() will automatically call UpdateNotifier()
    So there is no need to call it while we are calling the former
    Also we shouldn't do it for incognito if it is not requested.
    
    BUG=679400
    
    Review-Url: https://codereview.chromium.org/2624493002
    Cr-Commit-Position: refs/heads/master@{#442324}
    (cherry picked from commit b63b4eb306b837c5e31c7e743f2ff1fcd499b87c)
    
    Review-Url: https://codereview.chromium.org/2643773004 .
    Cr-Commit-Position: refs/branch-heads/2924@{#797}
    Cr-Branched-From: 3a87aecc31cd1ffe751dd72c04e5a96a1fc8108a-refs/heads/master@{#433059}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadManagerService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadManagerService.java
@@ -986,11 +986,12 @@ public class DownloadManagerService extends BroadcastReceiver implements
             Context context, @Nullable String filePath, long downloadId,
             boolean isSupportedMimeType) {
         assert !ThreadUtils.runningOnUiThread();
-        DownloadManager manager =
-                (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
-        Uri contentUri = manager.getUriForDownloadedFile(downloadId);
+        Uri contentUri = DownloadManagerDelegate.getContentUriFromDownloadManager(
+                context, downloadId);
         if (contentUri == null) return null;
 
+        DownloadManager manager =
+                (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
         String mimeType = manager.getMimeTypeForDownloadedFile(downloadId);
         if (isSupportedMimeType) {
             // Redirect the user to an internal media viewer.  The file path is necessary to show
@@ -999,7 +1000,6 @@ public class DownloadManagerService extends BroadcastReceiver implements
             if (filePath != null) fileUri = Uri.fromFile(new File(filePath));
             return DownloadUtils.getMediaViewerIntentForDownloadItem(fileUri, contentUri, mimeType);
         }
-
         return DownloadUtils.createViewIntentForDownloadItem(contentUri, mimeType);
     }
 
