431a5e87e4eb425d0c65cf12070b4bd3a3d0ea22
mmenke <mmenke@chromium.org>
2017-01-24 13:06:25 -0800

    Merge 2617c4a136497244376e1bcf11aa356afb2a1e06 Enable HTTP/0.9 on non-standard ports by default
    This is a temporary patch to enable HTTP/0.9 on non-standard ports by
    default.  After merging it with Chrome 56, it will be reverted, in favor
    of specifically adding detection of Shoutcast servers on HTTP ports
    other than port 80.
    
    TBR=eroman@chromium.org
    NOTRY=TRUE
    NOPRESUBMIT=TRUE
    BUG=669800
    
    Review-Url: https://codereview.chromium.org/2651753002
    Cr-Original-Commit-Position: refs/heads/master@{#445743}
    Review-Url: https://codereview.chromium.org/2656693003
    Cr-Commit-Position: refs/branch-heads/2924@{#856}
    Cr-Branched-From: 3a87aecc31cd1ffe751dd72c04e5a96a1fc8108a-refs/heads/master@{#433059}

--- net/http/http_network_session.cc
+++ net/http/http_network_session.cc
@@ -127,7 +127,7 @@ HttpNetworkSession::Params::Params()
       quic_do_not_fragment(false),
       proxy_delegate(NULL),
       enable_token_binding(false),
-      http_09_on_non_default_ports_enabled(false) {
+      http_09_on_non_default_ports_enabled(true) {
   quic_supported_versions.push_back(QUIC_VERSION_35);
 }
 
