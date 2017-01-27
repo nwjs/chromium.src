61ef8268f9904aecb561105246c92d753ca53dda
amineer <amineer@chromium.org>
2017-01-27 08:33:20 -0800

    Revert of Destroy incognito profile from close all incognito notification. (patchset #1 id:1 of https://codereview.chromium.org/2653753008/ )
    Reason for revert:
    Breaks build:
    
    ../../chrome/android/java/src/org/chromium/chrome/browser/incognito/IncognitoNotificationService.java:84: error: method get in class BrowserStartupController cannot be applied to given types;
                    if (BrowserStartupController.get(LibraryProcessType.PROCESS_BROWSER)
                                                ^
      required: Context,int
      found: int
      reason: actual and formal argument lists differ in length
    
    https://uberchromegw.corp.google.com/i/official.android.continuous/builders/beta-arm/builds/6052
    
    Original issue's description:
    > Destroy incognito profile from close all incognito notification.
    >
    > Also destroy the incognito profile when starting up tabbed mode
    > and there are no discoverable incognito tabs around.
    >
    > BUG=673489
    >
    > Review-Url: https://codereview.chromium.org/2649773007
    > Cr-Original-Commit-Position: refs/heads/master@{#446140}
    > Review-Url: https://codereview.chromium.org/2653753008 .
    > Cr-Commit-Position: refs/branch-heads/2987@{#115}
    > Cr-Branched-From: ad51088c0e8776e8dcd963dbe752c4035ba6dab6-refs/heads/master@{#444943}
    > Committed: https://chromium.googlesource.com/chromium/src/+/29a4956a7ff9985a5e2b951d41b435d1b3d38b94
    
    TBR=tedchoc@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=673489
    
    Review-Url: https://codereview.chromium.org/2655383003
    Cr-Commit-Position: refs/branch-heads/2987@{#146}
    Cr-Branched-From: ad51088c0e8776e8dcd963dbe752c4035ba6dab6-refs/heads/master@{#444943}

--- chrome/android/java/src/org/chromium/chrome/browser/incognito/IncognitoNotificationService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/incognito/IncognitoNotificationService.java
@@ -23,15 +23,12 @@ import org.chromium.base.ApplicationStatus;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.VisibleForTesting;
-import org.chromium.base.library_loader.LibraryProcessType;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.TabState;
 import org.chromium.chrome.browser.document.ChromeLauncherActivity;
 import org.chromium.chrome.browser.document.DocumentUtils;
-import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tabmodel.TabWindowManager;
 import org.chromium.chrome.browser.tabmodel.TabbedModeTabPersistencePolicy;
-import org.chromium.content.browser.BrowserStartupController;
 
 import java.io.File;
 import java.lang.ref.WeakReference;
@@ -75,18 +72,10 @@ public class IncognitoNotificationService extends IntentService {
             @Override
             public void run() {
                 int incognitoCount = TabWindowManager.getInstance().getIncognitoTabCount();
-                if (incognitoCount != 0) {
-                    assert false : "Not all incognito tabs closed as expected";
-                    return;
-                }
-                IncognitoNotificationManager.dismissIncognitoNotification();
+                assert incognitoCount == 0;
 
-                if (BrowserStartupController.get(LibraryProcessType.PROCESS_BROWSER)
-                        .isStartupSuccessfullyCompleted()) {
-                    if (Profile.getLastUsedProfile().hasOffTheRecordProfile()) {
-                        Profile.getLastUsedProfile().getOffTheRecordProfile()
-                                .destroyWhenAppropriate();
-                    }
+                if (incognitoCount == 0) {
+                    IncognitoNotificationManager.dismissIncognitoNotification();
                 }
             }
         });
