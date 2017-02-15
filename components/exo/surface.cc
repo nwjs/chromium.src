04f58dfe787645e9eebb68d86ec5d3449a3c5965
Fady Samuel <fsamuel@chromium.org>
2017-02-15 13:34:02 -0500

    Fix SurfaceManager lifetime issues in exo
    CustomSurfaceReferenceFactory is buggy because it can outlive the
    SurfaceManager. This CL makes exo use DirectSurfaceReferenceFactory
    for now until we find an alternative.
    
    BUG=683395
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2660913003
    Cr-Commit-Position: refs/heads/master@{#448431}
    (cherry picked from commit c3a0a945cefa972daf56a0cc49f0c495527fbd15)
    
    Review-Url: https://codereview.chromium.org/2696223002 .
    Cr-Commit-Position: refs/branch-heads/2987@{#523}
    Cr-Branched-From: ad51088c0e8776e8dcd963dbe752c4035ba6dab6-refs/heads/master@{#444943}

--- components/exo/surface.cc
+++ components/exo/surface.cc
@@ -140,30 +140,6 @@ class CustomWindowTargeter : public aura::WindowTargeter {
   DISALLOW_COPY_AND_ASSIGN(CustomWindowTargeter);
 };
 
-class CustomSurfaceReferenceFactory
-    : public cc::SequenceSurfaceReferenceFactory {
- public:
-  explicit CustomSurfaceReferenceFactory(CompositorFrameSinkHolder* sink_holder)
-      : sink_holder_(sink_holder) {}
-
- private:
-  ~CustomSurfaceReferenceFactory() override = default;
-
-  // Overridden from cc::SequenceSurfaceReferenceFactory:
-  void SatisfySequence(const cc::SurfaceSequence& sequence) const override {
-    sink_holder_->Satisfy(sequence);
-  }
-
-  void RequireSequence(const cc::SurfaceId& surface_id,
-                       const cc::SurfaceSequence& sequence) const override {
-    sink_holder_->Require(surface_id, sequence);
-  }
-
-  scoped_refptr<CompositorFrameSinkHolder> sink_holder_;
-
-  DISALLOW_COPY_AND_ASSIGN(CustomSurfaceReferenceFactory);
-};
-
 }  // namespace
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -181,8 +157,12 @@ Surface::Surface()
   compositor_frame_sink_holder_ = new CompositorFrameSinkHolder(
       this, frame_sink_id_,
       aura::Env::GetInstance()->context_factory_private()->GetSurfaceManager());
-  surface_reference_factory_ =
-      new CustomSurfaceReferenceFactory(compositor_frame_sink_holder_.get());
+  // TODO(samans): exo::Surface should not be using
+  // DirectSurfaceReferenceFactory (crbug.com/688573).
+  surface_reference_factory_ = aura::Env::GetInstance()
+                                   ->context_factory_private()
+                                   ->GetSurfaceManager()
+                                   ->reference_factory();
   window_->SetType(ui::wm::WINDOW_TYPE_CONTROL);
   window_->SetName("ExoSurface");
   window_->SetProperty(kSurfaceKey, this);
