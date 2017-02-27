nwjs/chromium.src

commit bd716dcb8575e046af147dd1b69f56090c77b4e4

[Android] Allow get/setsockopt SOL_SOCKET SO_SNDTIMEO.
BUG=682488
R=jorgelo@chromium.org

Review-Url: https://codereview.chromium.org/2653923005
Cr-Commit-Position: refs/heads/master@{#447507}

content/common/sandbox_linux/android/sandbox_bpf_base_policy_android.cc
@@ -38,6 +38,8 @@ namespace content {
 #define SOCK_NONBLOCK O_NONBLOCK
 #endif
 
+#define CASES SANDBOX_BPF_DSL_CASES
+
 namespace {
 
 #if !defined(__i386__)
@@ -177,16 +179,26 @@ ResultExpr SandboxBPFBasePolicyAndroid::EvaluateSyscall(int sysno) const {
     // documented to be a valid errno, but we will use it anyways.
     return Error(EPERM);
   }
+
+  // https://crbug.com/682488
+  if (sysno == __NR_getsockopt || sysno == __NR_setsockopt) {
+    // The baseline policy applies other restrictions to these syscalls.
+    const Arg<int> level(1);
+    const Arg<int> option(2);
+    return If(AllOf(level == SOL_SOCKET, option == SO_SNDTIMEO), Allow())
+           .Else(SandboxBPFBasePolicy::EvaluateSyscall(sysno));
+  }
 #elif defined(__i386__)
   if (sysno == __NR_socketcall) {
+    // The baseline policy allows other socketcall sub-calls.
     const Arg<int> socketcall(0);
-    const Arg<int> domain(1);
-    const Arg<int> type(2);
-    const Arg<int> protocol(3);
-    return If(socketcall == SYS_CONNECT, Allow())
-           .ElseIf(socketcall == SYS_SOCKET, Allow())
-           .ElseIf(socketcall == SYS_GETSOCKOPT, Allow())
-           .Else(Error(EPERM));
+    return Switch(socketcall)
+        .CASES((SYS_CONNECT,
+                SYS_SOCKET,
+                SYS_SETSOCKOPT,
+                SYS_GETSOCKOPT),
+               Allow())
+        .Default(SandboxBPFBasePolicy::EvaluateSyscall(sysno));
   }
 #endif
 

commit 403dfed9b16ca8da37e3786921e3aea45c47bee4

[M57 Merge] Fix socket restrictions in the Android seccomp policy.
BUG=682488,691118
TBR=jorgelo@chromium.org

---

  [Android] Do not restrict the arguments to getsockopt.

  They were previously un-restricted, and bd716dcb8575e046af147dd1b69f56090c77b4e4
  regressed that.

  BUG=682488
  R=jorgelo@chromium.org

  Review-Url: https://codereview.chromium.org/2669293003
  Cr-Commit-Position: refs/heads/master@{#447995}
  (cherry picked from commit 2d367b561d91f5f0c46724d66ea64852eadbb15a)

---

  [Android] Allow get/setsockopt SOL_SOCKET SO_SNDTIMEO.

  BUG=682488
  R=jorgelo@chromium.org

  Review-Url: https://codereview.chromium.org/2653923005
  Cr-Commit-Position: refs/heads/master@{#447507}
  (cherry picked from commit bd716dcb8575e046af147dd1b69f56090c77b4e4)

Review-Url: https://codereview.chromium.org/2722593003 .
Cr-Commit-Position: refs/branch-heads/2987@{#705}
Cr-Branched-From: ad51088c0e8776e8dcd963dbe752c4035ba6dab6-refs/heads/master@{#444943}

content/common/sandbox_linux/android/sandbox_bpf_base_policy_android.cc
@@ -38,6 +38,8 @@ namespace content {
 #define SOCK_NONBLOCK O_NONBLOCK
 #endif
 
+#define CASES SANDBOX_BPF_DSL_CASES
+
 namespace {
 
 #if !defined(__i386__)
@@ -177,16 +179,26 @@ ResultExpr SandboxBPFBasePolicyAndroid::EvaluateSyscall(int sysno) const {
     // documented to be a valid errno, but we will use it anyways.
     return Error(EPERM);
   }
+
+  // https://crbug.com/682488
+  if (sysno == __NR_setsockopt) {
+    // The baseline policy applies other restrictions to setsockopt.
+    const Arg<int> level(1);
+    const Arg<int> option(2);
+    return If(AllOf(level == SOL_SOCKET, option == SO_SNDTIMEO), Allow())
+           .Else(SandboxBPFBasePolicy::EvaluateSyscall(sysno));
+  }
 #elif defined(__i386__)
   if (sysno == __NR_socketcall) {
+    // The baseline policy allows other socketcall sub-calls.
     const Arg<int> socketcall(0);
-    const Arg<int> domain(1);
-    const Arg<int> type(2);
-    const Arg<int> protocol(3);
-    return If(socketcall == SYS_CONNECT, Allow())
-           .ElseIf(socketcall == SYS_SOCKET, Allow())
-           .ElseIf(socketcall == SYS_GETSOCKOPT, Allow())
-           .Else(Error(EPERM));
+    return Switch(socketcall)
+        .CASES((SYS_CONNECT,
+                SYS_SOCKET,
+                SYS_SETSOCKOPT,
+                SYS_GETSOCKOPT),
+               Allow())
+        .Default(SandboxBPFBasePolicy::EvaluateSyscall(sysno));
   }
 #endif
 