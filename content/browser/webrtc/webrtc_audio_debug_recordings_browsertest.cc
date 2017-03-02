nwjs/chromium.src

commit 086a4113b499025bdd7db2c19a10bd8cd34a5997

Audio output debug recording.
Class diagram: https://docs.google.com/a/google.com/drawings/d/1qD-gv02z1vFxNDfB_exAbMA2ugDGGX2IFCEX5H_Ujao/edit?usp=sharing

Adds two new classes in media/:
* AudioDebugRecordingHelper - wraps an AudioDebugFileWriter and handles data copying and thread hop.
* AudioDebugRecordingManager owned by AudioManagerBase and responsible for keeping track of AudioDebugRecordingHelpers and enable/disable on them.

Three new functions on AudioManager to initialize, enable and disable. Recordings are enabled/disabled from WebRTCInternals (chrome://webrtc-internals) and AudioDebugRecordingsHandler (from WebRTC logging extension API).

BUG=531883

CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel

Review-Url: https://codereview.chromium.org/2582703003
Cr-Commit-Position: refs/heads/master@{#454218}

content/browser/webrtc/webrtc_audio_debug_recordings_browsertest.cc
@@ -26,7 +26,14 @@
 namespace {
 
 const int kExpectedConsumerId = 1;
-const int kExpectedStreamId = 1;
+const int kExpectedInputStreamId = 1;
+const int kExpectedFirstOutputStreamId = 1;
+
+const int kWaveHeaderSizeBytes = 44;
+
+const base::FilePath::CharType kBaseFilename[] =
+    FILE_PATH_LITERAL("audio_debug");
+const base::FilePath::CharType kWaveExtension[] = FILE_PATH_LITERAL("wav");
 
 // Get the ID for the render process host when there should only be one.
 bool GetRenderProcessHostId(base::ProcessId* id) {
@@ -44,22 +51,34 @@ bool GetRenderProcessHostId(base::ProcessId* id) {
 // Get the expected AEC dump file name. The name will be
 // <temporary path>.<render process id>.aec_dump.<consumer id>, for example
 // "/tmp/.com.google.Chrome.Z6UC3P.12345.aec_dump.1".
-base::FilePath GetExpectedAecDumpFileName(const base::FilePath& base_file,
+base::FilePath GetExpectedAecDumpFileName(const base::FilePath& base_file_path,
                                           int render_process_id) {
-  return base_file.AddExtension(IntToStringType(render_process_id))
-                  .AddExtension(FILE_PATH_LITERAL("aec_dump"))
-                  .AddExtension(IntToStringType(kExpectedConsumerId));
+  return base_file_path.AddExtension(IntToStringType(render_process_id))
+      .AddExtension(FILE_PATH_LITERAL("aec_dump"))
+      .AddExtension(IntToStringType(kExpectedConsumerId));
 }
 
 // Get the expected input audio file name. The name will be
-// <temporary path>.<render process id>.source_input.<stream id>.pcm, for
-// example "/tmp/.com.google.Chrome.Z6UC3P.12345.source_input.1.pcm".
-base::FilePath GetExpectedInputAudioFileName(const base::FilePath& base_file,
-                                             int render_process_id) {
-  return base_file.AddExtension(IntToStringType(render_process_id))
-                  .AddExtension(FILE_PATH_LITERAL("source_input"))
-                  .AddExtension(IntToStringType(kExpectedStreamId))
-                  .AddExtension(FILE_PATH_LITERAL("wav"));
+// <temporary path>.<render process id>.source_input.<stream id>.wav, for
+// example "/tmp/.com.google.Chrome.Z6UC3P.12345.source_input.1.wav".
+base::FilePath GetExpectedInputAudioFileName(
+    const base::FilePath& base_file_path,
+    int render_process_id) {
+  return base_file_path.AddExtension(IntToStringType(render_process_id))
+      .AddExtension(FILE_PATH_LITERAL("source_input"))
+      .AddExtension(IntToStringType(kExpectedInputStreamId))
+      .AddExtension(kWaveExtension);
+}
+
+// Get the expected output audio file name. The name will be
+// <temporary path>.output.<running stream id>.wav, for example
+// "/tmp/.com.google.Chrome.Z6UC3P.output.1.wav".
+base::FilePath GetExpectedOutputAudioFileName(
+    const base::FilePath& base_file_path,
+    int id) {
+  return base_file_path.AddExtension(FILE_PATH_LITERAL("output"))
+      .AddExtension(IntToStringType(id))
+      .AddExtension(kWaveExtension);
 }
 
 }  // namespace
@@ -78,6 +97,7 @@ class WebRtcAudioDebugRecordingsBrowserTest
 
 #if defined(OS_LINUX) && !defined(OS_CHROMEOS) && defined(ARCH_CPU_ARM_FAMILY)
 // Timing out on ARM linux bot: http://crbug.com/238490
+// TODO(grunell): Re-enable for ARM Linux. Bug is closed as fixed.
 #define MAYBE_CallWithAudioDebugRecordings DISABLED_CallWithAudioDebugRecordings
 #elif defined(OS_ANDROID) && defined(ADDRESS_SANITIZER)
 // Renderer crashes under Android ASAN: https://crbug.com/408496.
@@ -90,62 +110,76 @@ class WebRtcAudioDebugRecordingsBrowserTest
 #endif
 
 // This tests will make a complete PeerConnection-based call, verify that
-// video is playing for the call, and verify that a non-empty AEC dump file
-// exists. The AEC dump is enabled through webrtc-internals. The HTML and
-// Javascript is bypassed since it would trigger a file picker dialog. Instead,
-// the dialog callback FileSelected() is invoked directly. In fact, there's
-// never a webrtc-internals page opened at all since that's not needed.
+// video is playing for the call, and verify that non-empty audio debug
+// recording files exist. The recording is enabled through webrtc-internals. The
+// HTML and Javascript is bypassed since it would trigger a file picker dialog.
+// Instead, the dialog callback FileSelected() is invoked directly. In fact,
+// there's never a webrtc-internals page opened at all since that's not needed.
 IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
                        MAYBE_CallWithAudioDebugRecordings) {
-  bool prev = base::ThreadRestrictions::SetIOAllowed(true);
   if (!media::AudioManager::Get()->HasAudioOutputDevices()) {
     LOG(INFO) << "Missing output devices: skipping test...";
     return;
   }
+
+  bool prev_io_allowed = base::ThreadRestrictions::SetIOAllowed(true);
 
   ASSERT_TRUE(embedded_test_server()->Start());
 
   // We must navigate somewhere first so that the render process is created.
   NavigateToURL(shell(), GURL(""));
 
-  base::FilePath base_file;
-  ASSERT_TRUE(CreateTemporaryFile(&base_file));
-  base::DeleteFile(base_file, false);
+  // Create a temp directory and setup base file path.
+  base::FilePath temp_dir_path;
+  ASSERT_TRUE(
+      CreateNewTempDirectory(base::FilePath::StringType(), &temp_dir_path));
+  base::FilePath base_file_path = temp_dir_path.Append(kBaseFilename);
 
   // This fakes the behavior of another open tab with webrtc-internals, and
-  // enabling AEC dump in that tab.
-  WebRTCInternals::GetInstance()->FileSelected(base_file, -1, NULL);
+  // enabling audio debug recordings in that tab.
+  WebRTCInternals::GetInstance()->FileSelected(base_file_path, -1, NULL);
 
+  // Make a call.
   GURL url(embedded_test_server()->GetURL("/media/peerconnection-call.html"));
   NavigateToURL(shell(), url);
   ExecuteJavascriptAndWaitForOk("call({video: true, audio: true});");
 
-  EXPECT_FALSE(base::PathExists(base_file));
-
   // Verify that the expected AEC dump file exists and contains some data.
   base::ProcessId render_process_id = base::kNullProcessId;
   EXPECT_TRUE(GetRenderProcessHostId(&render_process_id));
-  base::FilePath aec_dump_file = GetExpectedAecDumpFileName(base_file,
-                                                            render_process_id);
-
-  EXPECT_TRUE(base::PathExists(aec_dump_file));
+  base::FilePath file_path =
+      GetExpectedAecDumpFileName(base_file_path, render_process_id);
+  EXPECT_TRUE(base::PathExists(file_path));
   int64_t file_size = 0;
-  EXPECT_TRUE(base::GetFileSize(aec_dump_file, &file_size));
+  EXPECT_TRUE(base::GetFileSize(file_path, &file_size));
   EXPECT_GT(file_size, 0);
-
-  base::DeleteFile(aec_dump_file, false);
+  EXPECT_TRUE(base::DeleteFile(file_path, false));
 
   // Verify that the expected input audio file exists and contains some data.
-  base::FilePath input_audio_file =
-      GetExpectedInputAudioFileName(base_file, render_process_id);
-
-  EXPECT_TRUE(base::PathExists(input_audio_file));
+  file_path = GetExpectedInputAudioFileName(base_file_path, render_process_id);
+  EXPECT_TRUE(base::PathExists(file_path));
   file_size = 0;
-  EXPECT_TRUE(base::GetFileSize(input_audio_file, &file_size));
-  EXPECT_GT(file_size, 0);
+  EXPECT_TRUE(base::GetFileSize(file_path, &file_size));
+  EXPECT_GT(file_size, kWaveHeaderSizeBytes);
+  EXPECT_TRUE(base::DeleteFile(file_path, false));
 
-  base::DeleteFile(input_audio_file, false);
-  base::ThreadRestrictions::SetIOAllowed(prev);
+  // Verify that the expected output audio files exists and contains some data.
+  // Two files are expected, one for each peer in the call.
+  for (int i = 0; i < 2; ++i) {
+    file_path = GetExpectedOutputAudioFileName(
+        base_file_path, kExpectedFirstOutputStreamId + i);
+    EXPECT_TRUE(base::PathExists(file_path));
+    file_size = 0;
+    EXPECT_TRUE(base::GetFileSize(file_path, &file_size));
+    EXPECT_GT(file_size, kWaveHeaderSizeBytes);
+    EXPECT_TRUE(base::DeleteFile(file_path, false));
+  }
+
+  // Verify that no other files exist and remove temp dir.
+  EXPECT_TRUE(base::IsDirectoryEmpty(temp_dir_path));
+  EXPECT_TRUE(base::DeleteFile(temp_dir_path, false));
+
+  base::ThreadRestrictions::SetIOAllowed(prev_io_allowed);
 }
 
 // TODO(grunell): Add test for multiple dumps when re-use of
@@ -153,6 +187,7 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
 
 #if defined(OS_LINUX) && !defined(OS_CHROMEOS) && defined(ARCH_CPU_ARM_FAMILY)
 // Timing out on ARM linux bot: http://crbug.com/238490
+// TODO(grunell): Re-enable for ARM Linux. Bug is closed as fixed.
 #define MAYBE_CallWithAudioDebugRecordingsEnabledThenDisabled DISABLED_CallWithAudioDebugRecordingsEnabledThenDisabled
 #elif defined(OS_ANDROID) && defined(ADDRESS_SANITIZER)
 // Renderer crashes under Android ASAN: https://crbug.com/408496.
@@ -161,59 +196,57 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
 #define MAYBE_CallWithAudioDebugRecordingsEnabledThenDisabled CallWithAudioDebugRecordingsEnabledThenDisabled
 #endif
 
-// As above, but enable and disable dump before starting a call. The file should
-// be created, but should be empty.
+// As above, but enable and disable recordings before starting a call. No files
+// should be created.
 IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
                        MAYBE_CallWithAudioDebugRecordingsEnabledThenDisabled) {
-  bool prev = base::ThreadRestrictions::SetIOAllowed(true);
   if (!media::AudioManager::Get()->HasAudioOutputDevices()) {
     LOG(INFO) << "Missing output devices: skipping test...";
     return;
   }
+
+  bool prev_io_allowed = base::ThreadRestrictions::SetIOAllowed(true);
 
   ASSERT_TRUE(embedded_test_server()->Start());
 
   // We must navigate somewhere first so that the render process is created.
   NavigateToURL(shell(), GURL(""));
 
-  base::FilePath base_file;
-  ASSERT_TRUE(CreateTemporaryFile(&base_file));
-  base::DeleteFile(base_file, false);
+  // Create a temp directory and setup base file path.
+  base::FilePath temp_dir_path;
+  ASSERT_TRUE(
+      CreateNewTempDirectory(base::FilePath::StringType(), &temp_dir_path));
+  base::FilePath base_file_path = temp_dir_path.Append(kBaseFilename);
 
   // This fakes the behavior of another open tab with webrtc-internals, and
-  // enabling AEC dump in that tab, then disabling it.
-  WebRTCInternals::GetInstance()->FileSelected(base_file, -1, NULL);
+  // enabling audio debug recordings in that tab, then disabling it.
+  WebRTCInternals::GetInstance()->FileSelected(base_file_path, -1, NULL);
   WebRTCInternals::GetInstance()->DisableAudioDebugRecordings();
 
+  // Make a call.
   GURL url(embedded_test_server()->GetURL("/media/peerconnection-call.html"));
   NavigateToURL(shell(), url);
   ExecuteJavascriptAndWaitForOk("call({video: true, audio: true});");
 
-  // Verify that the expected AEC dump file doesn't exist.
-  base::ProcessId render_process_id = base::kNullProcessId;
-  EXPECT_TRUE(GetRenderProcessHostId(&render_process_id));
-  base::FilePath aec_dump_file = GetExpectedAecDumpFileName(base_file,
-                                                            render_process_id);
-  EXPECT_FALSE(base::PathExists(aec_dump_file));
-  base::DeleteFile(aec_dump_file, false);
+  // Verify that no files exist and remove temp dir.
+  EXPECT_TRUE(base::IsDirectoryEmpty(temp_dir_path));
+  EXPECT_TRUE(base::DeleteFile(temp_dir_path, false));
 
-  // Verify that the expected input audio file doesn't exist.
-  base::FilePath input_audio_file =
-      GetExpectedInputAudioFileName(base_file, render_process_id);
-  EXPECT_FALSE(base::PathExists(input_audio_file));
-  base::DeleteFile(input_audio_file, false);
-  base::ThreadRestrictions::SetIOAllowed(prev);
+  base::ThreadRestrictions::SetIOAllowed(prev_io_allowed);
 }
 
 // Timing out on ARM linux bot: http://crbug.com/238490
 // Renderer crashes under Android ASAN: https://crbug.com/408496.
+// TODO(grunell): Re-enable on all but Android ASAN. ARM Linux bug is closed
+// as fixed. See conditions for the above two tests.
 IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
                        DISABLED_TwoCallsWithAudioDebugRecordings) {
-  bool prev = base::ThreadRestrictions::SetIOAllowed(true);
   if (!media::AudioManager::Get()->HasAudioOutputDevices()) {
     LOG(INFO) << "Missing output devices: skipping test...";
     return;
   }
+
+  bool prev_io_allowed = base::ThreadRestrictions::SetIOAllowed(true);
 
   ASSERT_TRUE(embedded_test_server()->Start());
 
@@ -224,16 +257,18 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
   Shell* shell2 = CreateBrowser();
   NavigateToURL(shell2, GURL(""));
 
-  base::FilePath base_file;
-  ASSERT_TRUE(CreateTemporaryFile(&base_file));
-  base::DeleteFile(base_file, false);
+  // Create a temp directory and setup base file path.
+  base::FilePath temp_dir_path;
+  ASSERT_TRUE(
+      CreateNewTempDirectory(base::FilePath::StringType(), &temp_dir_path));
+  base::FilePath base_file_path = temp_dir_path.Append(kBaseFilename);
 
   // This fakes the behavior of another open tab with webrtc-internals, and
-  // enabling AEC dump in that tab.
-  WebRTCInternals::GetInstance()->FileSelected(base_file, -1, NULL);
+  // enabling audio debug recordings in that tab.
+  WebRTCInternals::GetInstance()->FileSelected(base_file_path, -1, NULL);
 
+  // Make the calls.
   GURL url(embedded_test_server()->GetURL("/media/peerconnection-call.html"));
-
   NavigateToURL(shell(), url);
   NavigateToURL(shell2, url);
   ExecuteJavascriptAndWaitForOk("call({video: true, audio: true});");
@@ -242,10 +277,10 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
       shell2, "call({video: true, audio: true});", &result));
   ASSERT_STREQ("OK", result.c_str());
 
-  EXPECT_FALSE(base::PathExists(base_file));
-
   RenderProcessHost::iterator it =
       content::RenderProcessHost::AllHostsIterator();
+  base::FilePath file_path;
+  int64_t file_size = 0;
 
   for (; !it.IsAtEnd(); it.Advance()) {
     base::ProcessId render_process_id =
@@ -253,28 +288,41 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
     EXPECT_NE(base::kNullProcessId, render_process_id);
 
     // Verify that the expected AEC dump file exists and contains some data.
-    base::FilePath aec_dump_file =
-        GetExpectedAecDumpFileName(base_file, render_process_id);
-
-    EXPECT_TRUE(base::PathExists(aec_dump_file));
-    int64_t file_size = 0;
-    EXPECT_TRUE(base::GetFileSize(aec_dump_file, &file_size));
+    file_path = GetExpectedAecDumpFileName(base_file_path, render_process_id);
+    EXPECT_TRUE(base::PathExists(file_path));
+    file_size = 0;
+    EXPECT_TRUE(base::GetFileSize(file_path, &file_size));
     EXPECT_GT(file_size, 0);
-
-    base::DeleteFile(aec_dump_file, false);
+    EXPECT_TRUE(base::DeleteFile(file_path, false));
 
     // Verify that the expected input audio file exists and contains some data.
-    base::FilePath input_audio_file =
-        GetExpectedInputAudioFileName(base_file, render_process_id);
-
-    EXPECT_TRUE(base::PathExists(input_audio_file));
+    file_path =
+        GetExpectedInputAudioFileName(base_file_path, render_process_id);
+    EXPECT_TRUE(base::PathExists(file_path));
     file_size = 0;
-    EXPECT_TRUE(base::GetFileSize(input_audio_file, &file_size));
-    EXPECT_GT(file_size, 0);
-
-    base::DeleteFile(input_audio_file, false);
+    EXPECT_TRUE(base::GetFileSize(file_path, &file_size));
+    EXPECT_GT(file_size, kWaveHeaderSizeBytes);
+    EXPECT_TRUE(base::DeleteFile(file_path, false));
   }
-  base::ThreadRestrictions::SetIOAllowed(prev);
+
+  // Verify that the expected output audio files exists and contains some data.
+  // Four files are expected, one for each peer in each call. (Two calls * two
+  // peers.)
+  for (int i = 0; i < 4; ++i) {
+    file_path = GetExpectedOutputAudioFileName(
+        base_file_path, kExpectedFirstOutputStreamId + i);
+    EXPECT_TRUE(base::PathExists(file_path));
+    file_size = 0;
+    EXPECT_TRUE(base::GetFileSize(file_path, &file_size));
+    EXPECT_GT(file_size, kWaveHeaderSizeBytes);
+    EXPECT_TRUE(base::DeleteFile(file_path, false));
+  }
+
+  // Verify that no other files exist and remove temp dir.
+  EXPECT_TRUE(base::IsDirectoryEmpty(temp_dir_path));
+  EXPECT_TRUE(base::DeleteFile(temp_dir_path, false));
+
+  base::ThreadRestrictions::SetIOAllowed(prev_io_allowed);
 }
 
 }  // namespace content

chrome/browser/chrome_content_browser_client.cc
@@ -185,6 +185,7 @@
 #include "device/usb/public/interfaces/device_manager.mojom.h"
 #include "extensions/features/features.h"
 #include "gpu/config/gpu_switches.h"
+#include "media/audio/audio_manager.h"
 #include "media/media_features.h"
 #include "net/base/mime_util.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
@@ -1183,8 +1184,10 @@ void ChromeContentBrowserClient::RenderProcessWillLaunch(
                     new base::UserDataAdapter<WebRtcLoggingHandlerHost>(
                         webrtc_logging_handler_host));
 
+  // The audio manager outlives the host, so it's safe to hand a raw pointer to
+  // it to the AudioDebugRecordingsHandler, which is owned by the host.
   AudioDebugRecordingsHandler* audio_debug_recordings_handler =
-      new AudioDebugRecordingsHandler(profile);
+      new AudioDebugRecordingsHandler(profile, media::AudioManager::Get());
   host->SetUserData(
       AudioDebugRecordingsHandler::kAudioDebugRecordingsHandlerKey,
       new base::UserDataAdapter<AudioDebugRecordingsHandler>(

chrome/browser/media/audio_debug_recordings_handler.h
@@ -18,12 +18,16 @@
 
 namespace content {
 class RenderProcessHost;
-}  // namespace content
+}
+
+namespace media {
+class AudioManager;
+}
 
 class Profile;
 
 // AudioDebugRecordingsHandler provides an interface to start and stop
-// AudioDebugRecordings, including WebRTC AEC dumps.
+// AudioDebugRecordings, including WebRTC AEC dumps. Lives on the UI thread.
 class AudioDebugRecordingsHandler
     : public base::RefCountedThreadSafe<AudioDebugRecordingsHandler> {
  public:
@@ -35,7 +39,8 @@ class AudioDebugRecordingsHandler
   // Key used to attach the handler to the RenderProcessHost
   static const char kAudioDebugRecordingsHandlerKey[];
 
-  explicit AudioDebugRecordingsHandler(Profile* profile);
+  AudioDebugRecordingsHandler(Profile* profile,
+                              media::AudioManager* audio_manager);
 
   // Starts an audio debug recording. The recording lasts the given |delay|,
   // unless |delay| is zero, in which case recording will continue until
@@ -81,13 +86,15 @@ class AudioDebugRecordingsHandler
   // The profile associated with our renderer process.
   Profile* const profile_;
 
-  // Must be accessed on the UI thread.
+  // Set if recordings are in progress.
   bool is_audio_debug_recordings_in_progress_;
 
   // This counter allows saving each debug recording in separate files.
   uint64_t current_audio_debug_recordings_id_;
 
-  base::ThreadChecker thread_checker_;
+  // Audio manager, used for enabling output recordings.
+  media::AudioManager* const audio_manager_;
+
   DISALLOW_COPY_AND_ASSIGN(AudioDebugRecordingsHandler);
 };
 

chrome/browser/media/webrtc/audio_debug_recordings_handler.cc
@@ -17,6 +17,7 @@
 #include "chrome/common/chrome_switches.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_process_host.h"
+#include "media/audio/audio_manager.h"
 
 using content::BrowserThread;
 
@@ -37,11 +38,16 @@ base::FilePath GetAudioDebugRecordingsPrefixPath(
 
 }  // namespace
 
-AudioDebugRecordingsHandler::AudioDebugRecordingsHandler(Profile* profile)
+AudioDebugRecordingsHandler::AudioDebugRecordingsHandler(
+    Profile* profile,
+    media::AudioManager* audio_manager)
     : profile_(profile),
       is_audio_debug_recordings_in_progress_(false),
-      current_audio_debug_recordings_id_(0) {
+      current_audio_debug_recordings_id_(0),
+      audio_manager_(audio_manager) {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
   DCHECK(profile_);
+  DCHECK(audio_manager_);
 }
 
 AudioDebugRecordingsHandler::~AudioDebugRecordingsHandler() {}
@@ -51,7 +57,7 @@ void AudioDebugRecordingsHandler::StartAudioDebugRecordings(
     base::TimeDelta delay,
     const RecordingDoneCallback& callback,
     const RecordingErrorCallback& error_callback) {
-  DCHECK(thread_checker_.CalledOnValidThread());
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
   BrowserThread::PostTaskAndReplyWithResult(
       BrowserThread::FILE, FROM_HERE,
@@ -65,7 +71,7 @@ void AudioDebugRecordingsHandler::StopAudioDebugRecordings(
     content::RenderProcessHost* host,
     const RecordingDoneCallback& callback,
     const RecordingErrorCallback& error_callback) {
-  DCHECK(thread_checker_.CalledOnValidThread());
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
   const bool is_manual_stop = true;
   BrowserThread::PostTaskAndReplyWithResult(
       BrowserThread::FILE, FROM_HERE,
@@ -94,7 +100,7 @@ void AudioDebugRecordingsHandler::DoStartAudioDebugRecordings(
     const RecordingDoneCallback& callback,
     const RecordingErrorCallback& error_callback,
     const base::FilePath& log_directory) {
-  DCHECK(thread_checker_.CalledOnValidThread());
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
   if (is_audio_debug_recordings_in_progress_) {
     error_callback.Run("Audio debug recordings already in progress");
@@ -105,6 +111,13 @@ void AudioDebugRecordingsHandler::DoStartAudioDebugRecordings(
   base::FilePath prefix_path = GetAudioDebugRecordingsPrefixPath(
       log_directory, ++current_audio_debug_recordings_id_);
   host->EnableAudioDebugRecordings(prefix_path);
+
+  // AudioManager is deleted on the audio thread, and the AudioManager outlives
+  // this object, which is owned by content::RenderProcessHost, so it's safe to
+  // post unretained.
+  audio_manager_->GetTaskRunner()->PostTask(
+      FROM_HERE, base::Bind(&media::AudioManager::EnableOutputDebugRecording,
+                            base::Unretained(audio_manager_), prefix_path));
 
   if (delay.is_zero()) {
     const bool is_stopped = false, is_manual_stop = false;
@@ -128,7 +141,7 @@ void AudioDebugRecordingsHandler::DoStopAudioDebugRecordings(
     const RecordingDoneCallback& callback,
     const RecordingErrorCallback& error_callback,
     const base::FilePath& log_directory) {
-  DCHECK(thread_checker_.CalledOnValidThread());
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
   DCHECK_LE(audio_debug_recordings_id, current_audio_debug_recordings_id_);
 
   base::FilePath prefix_path = GetAudioDebugRecordingsPrefixPath(
@@ -149,7 +162,15 @@ void AudioDebugRecordingsHandler::DoStopAudioDebugRecordings(
     return;
   }
 
+  // AudioManager is deleted on the audio thread, and the AudioManager outlives
+  // this object, which is owned by content::RenderProcessHost, so it's safe to
+  // post unretained.
+  audio_manager_->GetTaskRunner()->PostTask(
+      FROM_HERE, base::Bind(&media::AudioManager::DisableOutputDebugRecording,
+                            base::Unretained(audio_manager_)));
+
   host->DisableAudioDebugRecordings();
+
   is_audio_debug_recordings_in_progress_ = false;
   const bool is_stopped = true;
   callback.Run(prefix_path.AsUTF8Unsafe(), is_stopped, is_manual_stop);

content/browser/browser_main_loop.cc
@@ -1778,6 +1778,7 @@ void BrowserMainLoop::CreateAudioManager() {
     audio_thread_ = base::MakeUnique<AudioManagerThread>();
     audio_manager_ = media::AudioManager::Create(
         audio_thread_->task_runner(), audio_thread_->worker_task_runner(),
+        BrowserThread::GetTaskRunnerForThread(BrowserThread::FILE),
         MediaInternals::GetInstance());
   }
   CHECK(audio_manager_);

content/browser/webrtc/webrtc_internals.cc
@@ -19,6 +19,7 @@
 #include "content/public/browser/web_contents.h"
 #include "device/power_save_blocker/power_save_blocker.h"
 #include "ipc/ipc_platform_file.h"
+#include "media/audio/audio_manager.h"
 #include "media/media_features.h"
 
 #if defined(OS_WIN)
@@ -323,13 +324,22 @@ void WebRTCInternals::DisableAudioDebugRecordings() {
 
   // Tear down the dialog since the user has unchecked the audio debug
   // recordings box.
-  select_file_dialog_ = NULL;
+  select_file_dialog_ = nullptr;
 
   for (RenderProcessHost::iterator i(
            content::RenderProcessHost::AllHostsIterator());
        !i.IsAtEnd(); i.Advance()) {
     i.GetCurrentValue()->DisableAudioDebugRecordings();
   }
+
+  // It's safe to get the AudioManager pointer here. That pointer is invalidated
+  // on the UI thread, which we're on.
+  // AudioManager is deleted on the audio thread, and the AudioManager outlives
+  // this object, so it's safe to post unretained to the audio thread.
+  media::AudioManager* audio_manager = media::AudioManager::Get();
+  audio_manager->GetTaskRunner()->PostTask(
+      FROM_HERE, base::Bind(&media::AudioManager::DisableOutputDebugRecording,
+                            base::Unretained(audio_manager)));
 #endif
 }
 
@@ -493,12 +503,23 @@ void WebRTCInternals::EnableAudioDebugRecordingsOnAllRenderProcessHosts() {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
   audio_debug_recordings_ = true;
+
   for (RenderProcessHost::iterator i(
            content::RenderProcessHost::AllHostsIterator());
        !i.IsAtEnd(); i.Advance()) {
     i.GetCurrentValue()->EnableAudioDebugRecordings(
         audio_debug_recordings_file_path_);
   }
+
+  // It's safe to get the AudioManager pointer here. That pointer is invalidated
+  // on the UI thread, which we're on.
+  // AudioManager is deleted on the audio thread, and the AudioManager outlives
+  // this object, so it's safe to post unretained to the audio thread.
+  media::AudioManager* audio_manager = media::AudioManager::Get();
+  audio_manager->GetTaskRunner()->PostTask(
+      FROM_HERE, base::Bind(&media::AudioManager::EnableOutputDebugRecording,
+                            base::Unretained(audio_manager),
+                            audio_debug_recordings_file_path_));
 }
 
 void WebRTCInternals::EnableEventLogRecordingsOnAllRenderProcessHosts() {

commit 6a964e37a2eea4ac09dabde38018d675fae85b1b

Fix WebRtcAudioDebugRecordingsBrowserTest.
https://chromium.googlesource.com/chromium/src/+/086a4113b499025bdd7db2c19a10bd8cd34a5997 broke https://build.chromium.org/p/chromium.webrtc/builders/Win10%20Tester/

(Only that particular Win 10 bot.)

The short-term fix to get the bot green is to revert to not check expectations of return value when deleting files.

Long-term it should be investigated why this happens. Perhaps explicitly disabling would avoid the issue.

BUG=697845
TBR=olka@chromium.org,guidou@chromium.org

Review-Url: https://codereview.chromium.org/2730713002
Cr-Commit-Position: refs/heads/master@{#454247}

content/browser/webrtc/webrtc_audio_debug_recordings_browsertest.cc
@@ -153,7 +153,7 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
   int64_t file_size = 0;
   EXPECT_TRUE(base::GetFileSize(file_path, &file_size));
   EXPECT_GT(file_size, 0);
-  EXPECT_TRUE(base::DeleteFile(file_path, false));
+  base::DeleteFile(file_path, false);
 
   // Verify that the expected input audio file exists and contains some data.
   file_path = GetExpectedInputAudioFileName(base_file_path, render_process_id);
@@ -161,7 +161,7 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
   file_size = 0;
   EXPECT_TRUE(base::GetFileSize(file_path, &file_size));
   EXPECT_GT(file_size, kWaveHeaderSizeBytes);
-  EXPECT_TRUE(base::DeleteFile(file_path, false));
+  base::DeleteFile(file_path, false);
 
   // Verify that the expected output audio files exists and contains some data.
   // Two files are expected, one for each peer in the call.
@@ -172,12 +172,12 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
     file_size = 0;
     EXPECT_TRUE(base::GetFileSize(file_path, &file_size));
     EXPECT_GT(file_size, kWaveHeaderSizeBytes);
-    EXPECT_TRUE(base::DeleteFile(file_path, false));
+    base::DeleteFile(file_path, false);
   }
 
   // Verify that no other files exist and remove temp dir.
   EXPECT_TRUE(base::IsDirectoryEmpty(temp_dir_path));
-  EXPECT_TRUE(base::DeleteFile(temp_dir_path, false));
+  base::DeleteFile(temp_dir_path, false);
 
   base::ThreadRestrictions::SetIOAllowed(prev_io_allowed);
 }