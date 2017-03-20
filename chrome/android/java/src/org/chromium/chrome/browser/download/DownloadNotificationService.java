10612b02312f40aab60239076a7755e512fa549b
David Trainor <dtrainor@chromium.org>
2017-03-20 14:21:45 -0700

    Add download summary notification subtext
    The summary notification supports subtext that provides more context for
    what the notification is.  This allows us to say something like
    "Downloads" which makes it clear what this group of items is.
    
    BUG=701989
    
    Review-Url: https://codereview.chromium.org/2749953006
    Cr-Commit-Position: refs/heads/master@{#457720}
    (cherry picked from commit 6017c10d14b38656bb6993ec2ef43d994886fe5d)
    
    Review-Url: https://codereview.chromium.org/2761073002 .
    Cr-Commit-Position: refs/branch-heads/3029@{#311}
    Cr-Branched-From: 939b32ee5ba05c396eef3fd992822fcca9a2e262-refs/heads/master@{#454471}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadNotificationService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadNotificationService.java
@@ -315,9 +315,6 @@ public class DownloadNotificationService extends Service {
      * @return        a {@link Notification} that represents the summary icon for all downloads.
      */
     private static Notification buildSummaryNotificationWithIcon(Context context, int iconId) {
-        String title =
-                context.getResources().getString(R.string.download_notification_summary_title);
-
         ChromeNotificationBuilder builder =
                 AppHooks.get()
                         .createChromeNotificationBuilder(true /* preferCompat */,
@@ -325,7 +322,9 @@ public class DownloadNotificationService extends Service {
                                 context.getString(R.string.notification_category_browser),
                                 NotificationConstants.CATEGORY_GROUP_ID_GENERAL,
                                 context.getString(R.string.notification_category_group_general))
-                        .setContentTitle(title)
+                        .setContentTitle(
+                                context.getString(R.string.download_notification_summary_title))
+                        .setSubText(context.getString(R.string.menu_downloads))
                         .setSmallIcon(iconId)
                         .setLocalOnly(true)
                         .setGroup(NotificationConstants.GROUP_DOWNLOADS)
