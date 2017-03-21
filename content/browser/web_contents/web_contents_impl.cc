c28ea1434e6f9a9c31b35bf9bc760a605fc1a356
lfg <lfg@chromium.org>
2017-03-21 12:05:52 -0700

    Only release the pointer lock when the RenderWidgetHost being destroyed is the one holding the pointer lock.
    This fixes a crash where the WebContents would stop tracking which
    RenderWidgetHost was holding the pointer lock.
    
    BUG=703284
    
    Review-Url: https://codereview.chromium.org/2760343002
    Cr-Commit-Position: refs/heads/master@{#458446}
    (cherry picked from commit bf846a94da5c2b4f8ff93a7942d3d50799381675)
    
    TBR=alexmos@chromium.org,creis@chromium.org
    NOTRY=true
    NOPRESUBMIT=true
    
    Review-Url: https://codereview.chromium.org/2768593002
    Cr-Commit-Position: refs/branch-heads/3029@{#338}
    Cr-Branched-From: 939b32ee5ba05c396eef3fd992822fcca9a2e262-refs/heads/master@{#454471}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -1734,7 +1734,7 @@ void WebContentsImpl::RenderWidgetDeleted(
       view_->RestoreFocus();
   }
 
-  if (mouse_lock_widget_)
+  if (render_widget_host == mouse_lock_widget_)
     LostMouseLock(mouse_lock_widget_);
 }
 
