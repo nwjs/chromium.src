d901147176890fb8becc27a696448e59f92b74e6
Qiang Xu <warx@chromium.org>
2017-03-21 14:32:03 -0700

    [Merge to M58] cros: Do not schedule attempt when behind captive portal with response 200 is detected
    Changes:
    (1) Do not schedule attempt if we are already behind captive portal with response 200, which can be either the result of NetworkPortalDetector or shill's detection. In test, this can reduce the call times of CaptivePortalDetector::DetectCaptivePortal from 6+ to 2~3 within 10s.
    (2) modify tests to adapt this change
    
    TBR=stevenjb@chromium.org
    BUG=702273
    TEST=If we log in CaptivePortalDetector::DetectCaptivePortal, we can see the attempts reduced to 2~3 for connecting to captive portal wifi.
    
    Review-Url: https://codereview.chromium.org/2756643002
    Cr-Commit-Position: refs/heads/master@{#457829}
    (cherry picked from commit db14329b13d1109723acf5528a0253d6ee8c1651)
    
    Review-Url: https://codereview.chromium.org/2764813006 .
    Cr-Commit-Position: refs/branch-heads/3029@{#344}
    Cr-Branched-From: 939b32ee5ba05c396eef3fd992822fcca9a2e262-refs/heads/master@{#454471}

--- chrome/browser/chromeos/net/network_portal_detector_impl_unittest.cc
+++ chrome/browser/chromeos/net/network_portal_detector_impl_unittest.cc
@@ -336,7 +336,7 @@ TEST_F(NetworkPortalDetectorImplTest, Portal) {
 
   CompleteURLFetch(net::OK, 200, NULL);
 
-  ASSERT_FALSE(is_state_idle());
+  ASSERT_TRUE(is_state_idle());
   CheckPortalState(
       NetworkPortalDetector::CAPTIVE_PORTAL_STATUS_PORTAL, 200, kStubWireless1);
 
@@ -427,7 +427,7 @@ TEST_F(NetworkPortalDetectorImplTest, TwoNetworks) {
 
   // WiFi is in portal state.
   CompleteURLFetch(net::OK, 200, NULL);
-  ASSERT_FALSE(is_state_idle());
+  ASSERT_TRUE(is_state_idle());
 
   SetConnected(kStubEthernet);
   ASSERT_TRUE(is_state_checking_for_portal());
@@ -510,7 +510,7 @@ TEST_F(NetworkPortalDetectorImplTest, NetworkStateChanged) {
 
   CompleteURLFetch(net::OK, 200, NULL);
 
-  ASSERT_FALSE(is_state_idle());
+  ASSERT_TRUE(is_state_idle());
   CheckPortalState(
       NetworkPortalDetector::CAPTIVE_PORTAL_STATUS_PORTAL, 200, kStubWireless1);
 
@@ -528,7 +528,7 @@ TEST_F(NetworkPortalDetectorImplTest, NetworkStateChanged) {
 
   CompleteURLFetch(net::OK, 200, NULL);
 
-  ASSERT_FALSE(is_state_idle());
+  ASSERT_TRUE(is_state_idle());
   CheckPortalState(
       NetworkPortalDetector::CAPTIVE_PORTAL_STATUS_PORTAL, 200, kStubWireless1);
 
@@ -827,7 +827,7 @@ TEST_F(NetworkPortalDetectorImplTest, ErrorScreenStrategyForPortalNetwork) {
 
   CompleteURLFetch(net::OK, 200, NULL);
   ASSERT_EQ(0, no_response_result_count());
-  ASSERT_TRUE(is_state_portal_detection_pending());
+  ASSERT_TRUE(is_state_idle());
   CheckPortalState(
       NetworkPortalDetector::CAPTIVE_PORTAL_STATUS_PORTAL, 200, kStubWireless1);
 
@@ -836,6 +836,7 @@ TEST_F(NetworkPortalDetectorImplTest, ErrorScreenStrategyForPortalNetwork) {
 
   disable_error_screen_strategy();
 
+  // Set a different strategy will Start detection if idle.
   ASSERT_TRUE(is_state_portal_detection_pending());
   CheckPortalState(
       NetworkPortalDetector::CAPTIVE_PORTAL_STATUS_PORTAL, 200, kStubWireless1);
@@ -889,7 +890,7 @@ TEST_F(NetworkPortalDetectorImplTest, TestDetectionRestart) {
 
   CheckPortalState(
       NetworkPortalDetector::CAPTIVE_PORTAL_STATUS_PORTAL, 200, kStubWireless1);
-  ASSERT_FALSE(is_state_idle());
+  ASSERT_TRUE(is_state_idle());
 
   ASSERT_TRUE(
       MakeResultHistogramChecker()
