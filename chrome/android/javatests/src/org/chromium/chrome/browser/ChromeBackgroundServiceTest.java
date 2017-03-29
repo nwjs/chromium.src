1e3f6951e12f953fb21ea932e7df0db8040457d8
Pete Williamson <petewil@chromium.org>
2017-03-29 11:09:18 -0700

    Fix ChromeBackgroundServiceWaiter timeout, and restart offline pages
    We had been using a 4 minute max timeout in ChromeBackgroundServcieWatier.
    However, the GCM NM docs state that we are only guaranteed 3 minutes.
    So, I'm backing off the 4 minute timeout to just under 3 minutes.
    
    Also, since I wrote the file, others have added a mechanism to restore
    tasks if Google Play Services (GPS) upgrades, I added a similar mechanism
    for OfflinePages.
    
    BUG=705584
    
    Review-Url: https://codereview.chromium.org/2780643002
    Cr-Commit-Position: refs/heads/master@{#460432}
    (cherry picked from commit bb5b80f0f2882522fb2ead659374aa05ac218c9f)
    
    Review-Url: https://codereview.chromium.org/2776243006 .
    Cr-Commit-Position: refs/branch-heads/3029@{#477}
    Cr-Branched-From: 939b32ee5ba05c396eef3fd992822fcca9a2e262-refs/heads/master@{#454471}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ChromeBackgroundServiceTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ChromeBackgroundServiceTest.java
@@ -69,6 +69,9 @@ public class ChromeBackgroundServiceTest extends InstrumentationTestCase {
         @Override
         protected void reschedulePrecacheTasksOnUpgrade() {}
 
+        @Override
+        protected void rescheduleOfflinePagesTasksOnUpgrade() {}
+
         // Posts an assertion task to the UI thread. Since this is only called after the call
         // to onRunTask, it will be enqueued after any possible call to launchBrowser, and we
         // can reliably check whether launchBrowser was called.
