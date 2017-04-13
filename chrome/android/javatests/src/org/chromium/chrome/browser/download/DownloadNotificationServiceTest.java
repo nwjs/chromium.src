0c40252c938ca1a82587ec0001aa7c6f8a6e9c76
Jérôme Lebel <jlebel@chromium.org>
2017-04-13 14:55:24 +0200

    Fix DownloadNotificationService crash caused by early getLastUsedProfile() call
    This CL fixes crashes caused by calls to Profile.getLastUsedProfile()
    before browser initialization is completed.
    In such a case ProfileManager will try to create new Profile
    and that will eventually crash Chrome, because Prefs are not registered yet
    (Profile-related Prefs are registered during browser initialization by
    ChromeBrowserMainExtraPartsProfiles::EnsureBrowserContextKeyedServiceFactoriesBuilt).
    
    BUG=704025,710968
    
    Review-Url: https://codereview.chromium.org/2809223003
    Cr-Commit-Position: refs/heads/master@{#464377}
    (cherry picked from commit 16904d20070be165ac263fa38155d6174c8839ca)
    
    Review-Url: https://codereview.chromium.org/2810943006 .
    Cr-Commit-Position: refs/branch-heads/3029@{#691}
    Cr-Branched-From: 939b32ee5ba05c396eef3fd992822fcca9a2e262-refs/heads/master@{#454471}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadNotificationServiceTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadNotificationServiceTest.java
@@ -76,6 +76,16 @@ public class DownloadNotificationServiceTest extends
     }
 
     @Override
+    protected void shutdownService() {
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                DownloadNotificationServiceTest.super.shutdownService();
+            }
+        });
+    }
+
+    @Override
     protected void tearDown() throws Exception {
         SharedPreferences sharedPrefs = ContextUtils.getAppSharedPreferences();
         SharedPreferences.Editor editor = sharedPrefs.edit();
@@ -400,7 +410,14 @@ public class DownloadNotificationServiceTest extends
                 DownloadSharedPreferenceHelper.KEY_PENDING_DOWNLOAD_NOTIFICATIONS, notifications);
         editor.apply();
         startNotificationService();
-        getService().onTaskRemoved(new Intent());
+
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                getService().onTaskRemoved(new Intent());
+            }
+        });
+
         assertTrue(getService().isPaused());
         assertFalse(sharedPrefs.contains(
                 DownloadSharedPreferenceHelper.KEY_PENDING_DOWNLOAD_NOTIFICATIONS));
