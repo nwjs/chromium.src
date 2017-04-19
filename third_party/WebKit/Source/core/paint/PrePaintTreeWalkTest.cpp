083dce500b259c3b8ff3c9d3bf641f42bf37fa37
wangxianzhu <wangxianzhu@chromium.org>
2017-04-19 08:51:26 -0700

    Don't update descendant visual rects on border radius change
    Border radius doesn't affect descendants' visual rects.
    This fixes the "Check failed: parent_context.tree_builder_context->
    is_actually_needed" failure when running
    tools/perf/run_benchmark run rasterize_and_record_micro.top_25 --browser=debug --story-filter=plus
    The failure was because the subtree was marked to update visual rects but
    ancestors didn't update paint properties.
    
    BUG=710665
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2821183002
    Cr-Commit-Position: refs/heads/master@{#465093}
    
    TBR=wangxianzhu@chromium.org
    NOTRY=true
    NOPRESUBMIT=true
    
    Review-Url: https://codereview.chromium.org/2829673002
    Cr-Commit-Position: refs/branch-heads/3071@{#50}
    Cr-Branched-From: a106f0abbf69dad349d4aaf4bcc4f5d376dd2377-refs/heads/master@{#464641}

--- third_party/WebKit/Source/core/paint/PrePaintTreeWalkTest.cpp
+++ third_party/WebKit/Source/core/paint/PrePaintTreeWalkTest.cpp
@@ -261,4 +261,26 @@ TEST_P(PrePaintTreeWalkTest, ClearSubsequenceCachingClipChangePosFixed) {
   EXPECT_TRUE(child_paint_layer->NeedsRepaint());
 }
 
+TEST_P(PrePaintTreeWalkTest, ClipChangeHasRadius) {
+  SetBodyInnerHTML(
+      "<style>"
+      "  #target {"
+      "    position: absolute;"
+      "    z-index: 0;"
+      "    overflow: hidden;"
+      "    width: 50px;"
+      "    height: 50px;"
+      "  }"
+      "</style>"
+      "<div id='target'></div>");
+
+  auto* target = GetDocument().GetElementById("target");
+  auto* target_object = ToLayoutBoxModelObject(target->GetLayoutObject());
+  target->setAttribute(HTMLNames::styleAttr, "border-radius: 5px");
+  GetDocument().View()->UpdateAllLifecyclePhasesExceptPaint();
+  EXPECT_TRUE(target_object->Layer()->NeedsRepaint());
+  // And should not trigger any assert failure.
+  GetDocument().View()->UpdateAllLifecyclePhases();
+}
+
 }  // namespace blink
