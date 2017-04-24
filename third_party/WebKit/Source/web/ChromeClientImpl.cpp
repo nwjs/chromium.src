31455b249cf2737bf96ab751252b84e8c85b3804
Kent Tamura <tkent@chromium.org>
2017-04-24 09:58:58 +0900

    Merge "window.print() should close form validation bubble." to M59
    Usually, window.open() deactivates the origin window and validation bubble on the
    origin window is closed. However, if window.print() is executed, it suspends message
    loop of the window, and deactivation isn't noticed until print dialog is closed.
    So, we need to close validation popup explicitly for window.print().
    
    BUG=713686
    
    Review-Url: https://codereview.chromium.org/2834783002
    Cr-Commit-Position: refs/heads/master@{#466273}
    (cherry picked from commit 9dbd356b0cc52911caef089b09de63572cd9e39f)
    
    Review-Url: https://codereview.chromium.org/2833303002 .
    Cr-Commit-Position: refs/branch-heads/3071@{#151}
    Cr-Branched-From: a106f0abbf69dad349d4aaf4bcc4f5d376dd2377-refs/heads/master@{#464641}

--- third_party/WebKit/Source/web/ChromeClientImpl.cpp
+++ third_party/WebKit/Source/web/ChromeClientImpl.cpp
@@ -687,6 +687,7 @@ void ChromeClientImpl::DispatchViewportPropertiesDidChange(
 }
 
 void ChromeClientImpl::PrintDelegate(LocalFrame* frame) {
+  NotifyPopupOpeningObservers();
   if (web_view_->Client())
     web_view_->Client()->PrintPage(WebLocalFrameImpl::FromFrame(frame));
 }
