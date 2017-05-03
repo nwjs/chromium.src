842888acfba2c94e3164a848ef0359ac5c2e3fc0
Ben Smith <binji@chromium.org>
2017-05-02 17:45:25 -0700

    Make DOMArrayBuffer::Transfer neuter v8::ArrayBuffers
    This behavior is currently done by ScriptValueSerializer, but it's more
    convenient if the necessary logic is moved to DOMArrayBuffer::Transfer
    directly; you'll never want to transfer without also calling Neuter on the V8
    ArrayBuffer objects.
    
    BUG=711824
    
    Review-Url: https://codereview.chromium.org/2826263002
    Cr-Commit-Position: refs/heads/master@{#467441}
    (cherry picked from commit ca0862600acc57baec238c114be2af70e889708f)
    
    Review-Url: https://codereview.chromium.org/2858643006 .
    Cr-Commit-Position: refs/branch-heads/3071@{#367}
    Cr-Branched-From: a106f0abbf69dad349d4aaf4bcc4f5d376dd2377-refs/heads/master@{#464641}

--- third_party/WebKit/Source/modules/webaudio/BaseAudioContext.cpp
+++ third_party/WebKit/Source/modules/webaudio/BaseAudioContext.cpp
@@ -285,8 +285,19 @@ ScriptPromise BaseAudioContext::decodeAudioData(
 
   DCHECK_GT(rate, 0);
 
-  if (audio_data->IsNeutered()) {
-    // If audioData is detached (neutered) we need to reject the
+  v8::Isolate* isolate = script_state->GetIsolate();
+  WTF::ArrayBufferContents buffer_contents;
+  // Detach the audio array buffer from the main thread and start
+  // async decoding of the data.
+  if (audio_data->IsNeuterable(isolate) &&
+      audio_data->Transfer(isolate, buffer_contents)) {
+    DOMArrayBuffer* audio = DOMArrayBuffer::Create(buffer_contents);
+
+    decode_audio_resolvers_.insert(resolver);
+    audio_decoder_.DecodeAsync(audio, rate, success_callback, error_callback,
+                               resolver, this);
+  } else {
+    // If audioData is already detached (neutered) we need to reject the
     // promise with an error.
     DOMException* error = DOMException::Create(
         kDataCloneError, "Cannot decode detached ArrayBuffer");
@@ -294,16 +305,6 @@ ScriptPromise BaseAudioContext::decodeAudioData(
     if (error_callback) {
       error_callback->handleEvent(error);
     }
-  } else {
-    // Detach the audio array buffer from the main thread and start
-    // async decoding of the data.
-    WTF::ArrayBufferContents buffer_contents;
-    audio_data->Transfer(buffer_contents);
-    DOMArrayBuffer* audio = DOMArrayBuffer::Create(buffer_contents);
-
-    decode_audio_resolvers_.insert(resolver);
-    audio_decoder_.DecodeAsync(audio, rate, success_callback, error_callback,
-                               resolver, this);
   }
 
   return promise;
