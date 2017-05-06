0788f08aa43bc035dbccdbdc1a016b137b818fd4
emircan <emircan@chromium.org>
2017-05-05 17:47:09 -0700

    Merge 59: [Cherrypick] Change TaskQueueImpl to use a Deque with an inline capacity of 8
    A speculative change that might help mitigate problems with
    WTF::Deque::expandCapacity which sometimes takes over a second
    due to heavy load on PartitionAlloc.
    
    I'm hoping in normal operation the default_tq doesn't get more
    than a handful of tasks (a quick trace of handouts on a high end
    pc suggests the queue depth is <= 3) which would let us avoid
    dynamic allocations entirely.
    
    BUG=710245
    
    Review-Url: https://codereview.chromium.org/2860813004
    Cr-Commit-Position: refs/heads/master@{#469302}
    (cherry picked from commit 05864d5cb320715302b6d39f88728ff0a8776378)
    
    Review-Url: https://codereview.chromium.org/2862023002 .
    Cr-Commit-Position: refs/branch-heads/3086@{#3}
    Cr-Branched-From: 4159f36e93c398b9816b734a14e97220e83fc912-refs/heads/master@{#468266}
    (cherry picked from commit 2d81885f86384317c7337f9d63f7e0baff09a784)
    NOTRY=true
    NOPRESUBMIT=true
    TBR=haraken@chromium.org,skyostil@chromium.org,alexclarke@chromium.org
    
    Review-Url: https://codereview.chromium.org/2865803002
    Cr-Commit-Position: refs/branch-heads/3071@{#431}
    Cr-Branched-From: a106f0abbf69dad349d4aaf4bcc4f5d376dd2377-refs/heads/master@{#464641}

--- third_party/WebKit/Source/platform/scheduler/base/task_queue_impl.cc
+++ third_party/WebKit/Source/platform/scheduler/base/task_queue_impl.cc
@@ -380,9 +380,9 @@ void TaskQueueImpl::ReloadImmediateWorkQueueIfEmpty() {
   main_thread_only().immediate_work_queue->ReloadEmptyImmediateQueue();
 }
 
-WTF::Deque<TaskQueueImpl::Task> TaskQueueImpl::TakeImmediateIncomingQueue() {
+TaskQueueImpl::TaskDeque TaskQueueImpl::TakeImmediateIncomingQueue() {
   base::AutoLock immediate_incoming_queue_lock(immediate_incoming_queue_lock_);
-  WTF::Deque<TaskQueueImpl::Task> queue;
+  TaskQueueImpl::TaskDeque queue;
   queue.Swap(immediate_incoming_queue());
   return queue;
 }
@@ -711,7 +711,7 @@ EnqueueOrder TaskQueueImpl::GetFenceForTest() const {
 }
 
 // static
-void TaskQueueImpl::QueueAsValueInto(const WTF::Deque<Task>& queue,
+void TaskQueueImpl::QueueAsValueInto(const TaskDeque& queue,
                                      base::trace_event::TracedValue* state) {
   for (const Task& task : queue) {
     TaskAsValueInto(task, state);
