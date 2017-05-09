08c8d2e52ebf3e0334723516cc8bda4394ebc759
John Bauman <jbauman@chromium.org>
2017-05-09 16:13:54 -0700

    Don't crash when retrieving D3D11 device from ANGLE fails.
    ANGLE won't return the D3D11 device if it's currently lost.
    
    BUG=718791
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2862263003
    Cr-Commit-Position: refs/heads/master@{#469778}
    (cherry picked from commit 4c0ccf7a91083913925c4bb31731fcd98bbd64ea)
    
    Review-Url: https://codereview.chromium.org/2874573002 .
    Cr-Commit-Position: refs/branch-heads/3071@{#491}
    Cr-Branched-From: a106f0abbf69dad349d4aaf4bcc4f5d376dd2377-refs/heads/master@{#464641}

--- gpu/ipc/service/direct_composition_surface_win.cc
+++ gpu/ipc/service/direct_composition_surface_win.cc
@@ -817,7 +817,11 @@ bool DirectCompositionSurfaceWin::AreOverlaysSupported() {
 
   base::win::ScopedComPtr<ID3D11Device> d3d11_device =
       gl::QueryD3D11DeviceObjectFromANGLE();
-  DCHECK(d3d11_device);
+  if (!d3d11_device) {
+    DLOG(ERROR) << "Failing to create overlay swapchain because couldn't "
+                   "retrieve D3D11 device from ANGLE.";
+    return false;
+  }
 
   base::win::ScopedComPtr<IDXGIDevice> dxgi_device;
   d3d11_device.QueryInterface(dxgi_device.Receive());
