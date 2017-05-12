dee8c0c05b78d9802408cdf7494b18570c3a8001
cylee <cylee@chromium.org>
2017-05-12 12:13:22 -0700

    Revert of TabManager: Do not overkill when target memory to free is negative. (patchset #1 id:1 of https://codereview.chromium.org/2876033004/ )
    Reason for revert:
    Didn't handle a merge conflict well.
    
    Original issue's description:
    > TabManager: Do not overkill when target memory to free is negative.
    >
    > BUG=chrome:713944
    > TEST=samus
    >
    > Review-Url: https://codereview.chromium.org/2843633002
    > Cr-Original-Commit-Position: refs/heads/master@{#467044}
    > Review-Url: https://codereview.chromium.org/2876033004 .
    > Cr-Commit-Position: refs/branch-heads/3071@{#530}
    > Cr-Branched-From: a106f0abbf69dad349d4aaf4bcc4f5d376dd2377-refs/heads/master@{#464641}
    > Committed: https://chromium.googlesource.com/chromium/src/+/fca840d27869f026f8b31657c4853c8b9ee2fbd7
    
    TBR=
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=chrome:713944
    
    Review-Url: https://codereview.chromium.org/2876243003
    Cr-Commit-Position: refs/branch-heads/3071@{#533}
    Cr-Branched-From: a106f0abbf69dad349d4aaf4bcc4f5d376dd2377-refs/heads/master@{#464641}

--- chrome/browser/memory/tab_manager_delegate_chromeos.cc
+++ chrome/browser/memory/tab_manager_delegate_chromeos.cc
@@ -607,17 +607,14 @@ void TabManagerDelegate::LowMemoryKillImpl(
   // The list is sorted by descending importance, so we go through the list
   // backwards.
   for (auto it = candidates.rbegin(); it != candidates.rend(); ++it) {
-    MEMORY_LOG(ERROR) << "Target memory to free: " << target_memory_to_free_kb
-                      << " KB";
-    if (target_memory_to_free_kb <= 0)
-      break;
+    VLOG(3) << "Target memory to free: " << target_memory_to_free_kb << " KB";
     // Never kill selected tab or Android foreground app, regardless whether
     // they're in the active window. Since the user experience would be bad.
     ProcessType process_type = it->process_type();
     if (process_type == ProcessType::VISIBLE_APP ||
         process_type == ProcessType::FOCUSED_APP ||
         process_type == ProcessType::FOCUSED_TAB) {
-      MEMORY_LOG(ERROR) << "Skipped killing " << *it;
+      VLOG(2) << "Skipped killing " << *it;
       continue;
     }
     if (it->app()) {
@@ -633,8 +630,7 @@ void TabManagerDelegate::LowMemoryKillImpl(
         MemoryKillsMonitor::LogLowMemoryKill("APP", estimated_memory_freed_kb);
         MEMORY_LOG(ERROR) << "Killed " << *it << ", estimated "
                           << estimated_memory_freed_kb << " KB freed";
-      } else {
-        MEMORY_LOG(ERROR) << "Failed to kill " << *it;
+        killed_candidate = true;
       }
     } else {
       int64_t tab_id = it->tab()->tab_contents_id;
@@ -648,14 +644,16 @@ void TabManagerDelegate::LowMemoryKillImpl(
         MemoryKillsMonitor::LogLowMemoryKill("TAB", estimated_memory_freed_kb);
         MEMORY_LOG(ERROR) << "Killed " << *it << ", estimated "
                           << estimated_memory_freed_kb << " KB freed";
-      } else {
-        MEMORY_LOG(ERROR) << "Failed to kill " << *it;
+        killed_candidate = true;
       }
     }
+    if (target_memory_to_free_kb < 0)
+      break;
   }
-  if (target_memory_to_free_kb > 0) {
-    MEMORY_LOG(ERROR)
-        << "Unable to kill enough candidates to meet target_memory_to_free_kb ";
+  if (!killed_candidate) {
+    MEMORY_LOG(ERROR) << "Low memory: Unable to kill any candidates. "
+                      << "Attempted to free " << target_memory_to_free_kb
+                      << " KB";
   }
 }
 
