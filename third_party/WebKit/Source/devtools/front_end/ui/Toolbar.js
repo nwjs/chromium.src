c0d1592d03d65b6cf06887617a64417618cbc80d
luoe <luoe@chromium.org>
2017-06-05 16:35:26 -0700

    DevTools: the sources debug toolbar buttons should not be drag targets
    BUG=728789
    NOTRY=true
    NOPRESUBMIT=true
    
    Review-Url: https://codereview.chromium.org/2915013003
    Cr-Original-Commit-Position: refs/heads/master@{#476500}
    Review-Url: https://codereview.chromium.org/2922183002
    Cr-Commit-Position: refs/branch-heads/3112@{#171}
    Cr-Branched-From: b6460e24cf59f429d69de255538d0fc7a425ccf9-refs/heads/master@{#474897}

--- third_party/WebKit/Source/devtools/front_end/ui/Toolbar.js
+++ third_party/WebKit/Source/devtools/front_end/ui/Toolbar.js
@@ -193,6 +193,13 @@ UI.Toolbar = class {
   }
 
   /**
+   * @return {!Element}
+   */
+  gripElementForResize() {
+    return this._contentElement;
+  }
+
+  /**
    * @param {boolean=} reverse
    * @param {boolean=} growVertically
    */
