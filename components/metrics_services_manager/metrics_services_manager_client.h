a265a036fa4f54c791897c65597a9b8e6dacc999
Steven Holte <holte@google.com>
2017-07-10 18:14:40 +0000

    Disable UKM in incognito mode.
    Add explicit checks to make sure UKM is disabled in incognito mode,
    and add browser tests to check that behavior.
    
    (cherry picked from commit 30f9a63797e1403c229b6e54a773339593f9c017)
    
    Additionally merges:
    Test fixes from commit c5aeb440d84ce37b5c8a1d8f5bc185479b325665
    Disable UKM on Android from commit 094e496b0751a1efa5463cba8662318499d1fec8
    
    Bug: 734091
    Change-Id: I8202b19795b2fe1b82d0c20bc567560f680e3c1c
    Reviewed-on: https://chromium-review.googlesource.com/564087
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3112@{#561}
    Cr-Branched-From: b6460e24cf59f429d69de255538d0fc7a425ccf9-refs/heads/master@{#474897}

--- components/metrics_services_manager/metrics_services_manager_client.h
+++ components/metrics_services_manager/metrics_services_manager_client.h
@@ -54,6 +54,9 @@ class MetricsServicesManagerClient {
   // Whether the metrics services should record but not report metrics.
   virtual bool OnlyDoMetricsRecording() = 0;
 
+  // Returns whether there are any Incognito browsers/tabs open.
+  virtual bool IsIncognitoSessionActive() = 0;
+
   // Update the running state of metrics services managed by the embedder, for
   // example, crash reporting.
   virtual void UpdateRunningServices(bool may_record, bool may_upload) {}
