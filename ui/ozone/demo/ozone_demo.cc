b2195f84d0f6d9fb0a3d069d516eca434c6dd19f
Alexandre Courbot <acourbot@chromium.org>
2017-07-13 03:18:14 +0000

    Ozone: simplify and fix GPU test helper
    The task runner passed by FakeGpuProcessHost to GpuPlatformSupportHost was
    misnamed as being the GPU task runner while GpuPlatformSupportHost
    expects the UI runner. As a result, the VDA decode test passed the wrong
    runner, triggering DCHECKs in debug build.
    
    This patch renames the task runner properly and fixes the VDA decode
    test accordingly. The fix allows us to simplify OzoneGpuTestHelper a
    bit, so take that extra step.
    
    BUG=717963
    TEST=Confirmed VDA decode test runs successfully in debug mode with
    DCHECK enabled on x86 and aarch64 devices.
    
    TBR=acourbot@chromium.org
    
    (cherry picked from commit 97445b4f994675054cf5ae3363f18d8a6a52fa54)
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I43379702b4cb4b793fb176125e87a3560c5212e5
    Reviewed-on: https://chromium-review.googlesource.com/547382
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Pawel Osciak <posciak@chromium.org>
    Commit-Queue: Alexandre Courbot <acourbot@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#484036}
    Reviewed-on: https://chromium-review.googlesource.com/569200
    Cr-Commit-Position: refs/branch-heads/3112@{#603}
    Cr-Branched-From: b6460e24cf59f429d69de255538d0fc7a425ccf9-refs/heads/master@{#474897}

--- ui/ozone/demo/ozone_demo.cc
+++ ui/ozone/demo/ozone_demo.cc
@@ -201,8 +201,7 @@ RendererFactory::~RendererFactory() {
 bool RendererFactory::Initialize() {
   base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
   if (!command_line->HasSwitch(kDisableGpu) && gl::init::InitializeGLOneOff() &&
-      gpu_helper_.Initialize(base::ThreadTaskRunnerHandle::Get(),
-                             base::ThreadTaskRunnerHandle::Get())) {
+      gpu_helper_.Initialize(base::ThreadTaskRunnerHandle::Get())) {
     type_ = GL;
   } else {
     type_ = SOFTWARE;
