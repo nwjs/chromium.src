0ce66c72793d1654aa3a30fcd739673ca50b535a
Tim Song <tengs@chromium.org>
2017-08-09 18:23:54 +0000

    [EasyUnlock] Add easy_unlock.enabled pref and sync it to local state.
    Previously, the login screen looks at the presence of the hardlock local pref to
    determine if the user has set up EasyUnlock. This was a very unreliable and
    unintuitive thing to rely on, so we now explicitly have an easy_unlock.enabled
    pref.
    
    For easy of merging, this CL also fixes a notification bug when setting up
    EasyUnlock.
    
    BUG=746945,751808
    TBR=tengs@chromium.org
    
    (cherry picked from commit a3e8ece8271886809b1f2f3eece110aecd154b82)
    
    Change-Id: I437bcd3dc5f9d5e113b22a9861467f35ec1e7d62
    Reviewed-on: https://chromium-review.googlesource.com/599349
    Commit-Queue: Tim Song <tengs@chromium.org>
    Reviewed-by: Gustavo Sacomoto <sacomoto@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#492850}
    Reviewed-on: https://chromium-review.googlesource.com/609120
    Reviewed-by: Tim Song <tengs@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3163@{#406}
    Cr-Branched-From: ff259bab28b35d242e10186cd63af7ed404fae0d-refs/heads/master@{#488528}

--- components/proximity_auth/proximity_auth_profile_pref_manager.cc
+++ components/proximity_auth/proximity_auth_profile_pref_manager.cc
@@ -30,6 +30,7 @@ ProximityAuthProfilePrefManager::~ProximityAuthProfilePrefManager() {
 void ProximityAuthProfilePrefManager::RegisterPrefs(
     user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterBooleanPref(prefs::kEasyUnlockAllowed, true);
+  registry->RegisterBooleanPref(prefs::kEasyUnlockEnabled, false);
   registry->RegisterInt64Pref(prefs::kProximityAuthLastPasswordEntryTimestampMs,
                               0L);
   registry->RegisterInt64Pref(
@@ -67,6 +68,7 @@ void ProximityAuthProfilePrefManager::StartSyncingToLocalState(
 
   registrar_.Init(pref_service_);
   registrar_.Add(prefs::kEasyUnlockAllowed, on_pref_changed_callback);
+  registrar_.Add(prefs::kEasyUnlockEnabled, on_pref_changed_callback);
   registrar_.Add(proximity_auth::prefs::kEasyUnlockProximityThreshold,
                  on_pref_changed_callback);
   registrar_.Add(proximity_auth::prefs::kProximityAuthIsChromeOSLoginEnabled,
@@ -79,12 +81,14 @@ void ProximityAuthProfilePrefManager::SyncPrefsToLocalState() {
   std::unique_ptr<base::DictionaryValue> user_prefs_dict(
       new base::DictionaryValue());
 
-  user_prefs_dict->SetBooleanWithoutPathExpansion(prefs::kEasyUnlockAllowed,
-                                                  IsEasyUnlockAllowed());
-  user_prefs_dict->SetIntegerWithoutPathExpansion(
-      prefs::kEasyUnlockProximityThreshold, GetProximityThreshold());
-  user_prefs_dict->SetBooleanWithoutPathExpansion(
-      prefs::kProximityAuthIsChromeOSLoginEnabled, IsChromeOSLoginEnabled());
+  user_prefs_dict->SetKey(prefs::kEasyUnlockAllowed,
+                          base::Value(IsEasyUnlockAllowed()));
+  user_prefs_dict->SetKey(prefs::kEasyUnlockEnabled,
+                          base::Value(IsEasyUnlockEnabled()));
+  user_prefs_dict->SetKey(prefs::kEasyUnlockProximityThreshold,
+                          base::Value(GetProximityThreshold()));
+  user_prefs_dict->SetKey(prefs::kProximityAuthIsChromeOSLoginEnabled,
+                          base::Value(IsChromeOSLoginEnabled()));
 
   DictionaryPrefUpdate update(local_state_,
                               prefs::kEasyUnlockLocalStateUserPrefs);
@@ -94,6 +98,15 @@ void ProximityAuthProfilePrefManager::SyncPrefsToLocalState() {
 
 bool ProximityAuthProfilePrefManager::IsEasyUnlockAllowed() const {
   return pref_service_->GetBoolean(prefs::kEasyUnlockAllowed);
+}
+
+void ProximityAuthProfilePrefManager::SetIsEasyUnlockEnabled(
+    bool is_easy_unlock_enabled) const {
+  pref_service_->SetBoolean(prefs::kEasyUnlockEnabled, is_easy_unlock_enabled);
+}
+
+bool ProximityAuthProfilePrefManager::IsEasyUnlockEnabled() const {
+  return pref_service_->GetBoolean(prefs::kEasyUnlockEnabled);
 }
 
 void ProximityAuthProfilePrefManager::SetLastPasswordEntryTimestampMs(
