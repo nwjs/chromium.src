c00f27d3521ccd7eeb1d0a68a6c1d8097baba9fb
Kyle Horimoto <khorimoto@google.com>
2017-08-10 18:13:45 +0000

    Force Layout every time a HoverHightlightView is updated.
    When an item in the system tray network list goes from connecting to disconnected without a Layout
    of the parent view being triggered, it isn't rendered correctly. This is probably due to the fact
    that the "connecting" view has a sub_text_label and the "disconnected" view does not. The problem
    is solved when a Layout of the view is performed every time UpdateViewForNetwork is called.
    
    TBR=lesliewatkins@chromium.org
    
    (cherry picked from commit 82d95b25401b8b33f3f2df41948fb4c63dd7df32)
    
    Bug: 750322, 672263
    Change-Id: I785b2874db583a6d28aaeefed86d813141f6e79a
    Reviewed-on: https://chromium-review.googlesource.com/609216
    Reviewed-by: Terry Anderson <tdanderson@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Leslie Watkins <lesliewatkins@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#493446}
    Reviewed-on: https://chromium-review.googlesource.com/610887
    Cr-Commit-Position: refs/branch-heads/3163@{#440}
    Cr-Branched-From: ff259bab28b35d242e10186cd63af7ed404fae0d-refs/heads/master@{#488528}

--- ash/system/network/network_list.cc
+++ ash/system/network/network_list.cc
@@ -549,6 +549,8 @@ void NetworkListView::UpdateViewForNetwork(HoverHighlightView* view,
     if (controlled_icon)
       view->AddRightView(controlled_icon);
   }
+
+  needs_relayout_ = true;
 }
 
 views::View* NetworkListView::CreatePowerStatusView(const NetworkInfo& info) {
