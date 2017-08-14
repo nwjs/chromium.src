d7accc53acc12310d2c3403760913ddbe2fa8602
Michael Lippautz <mlippautz@chromium.org>
2017-08-14 20:08:30 +0000

    Oilpan: Fix tracing of AccessibleNode
    The dispatch to EventTargetWithInlineData was missing, effectively
    resulting in premature collections of EventTargetData and all its
    belongings.
    
    TBR=mlippautz@chromium.org
    
    (cherry picked from commit 3cdbb8676a2d14c4f1f0da0f06f656e3cc0dca33)
    
    Bug: chromium:753293
    Change-Id: I0e2acf87dad001158bb407fdf4543bf2db9457fd
    Reviewed-on: https://chromium-review.googlesource.com/610144
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#493427}
    Reviewed-on: https://chromium-review.googlesource.com/614400
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3163@{#550}
    Cr-Branched-From: ff259bab28b35d242e10186cd63af7ed404fae0d-refs/heads/master@{#488528}

--- third_party/WebKit/Source/core/dom/AccessibleNode.cpp
+++ third_party/WebKit/Source/core/dom/AccessibleNode.cpp
@@ -1070,6 +1070,7 @@ DEFINE_TRACE(AccessibleNode) {
   visitor->Trace(element_);
   visitor->Trace(relation_properties_);
   visitor->Trace(relation_list_properties_);
+  EventTargetWithInlineData::Trace(visitor);
 }
 
 }  // namespace blink
