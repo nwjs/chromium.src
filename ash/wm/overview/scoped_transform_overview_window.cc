bc7e55d2f5c9a42ef7fdf253b9e0eca77e602d2e
Mitsuru Oshima <oshima@chromium.org>
2017-08-18 20:54:18 +0000

    Restore minimized window's ignored_by_shelf state correctly
    when exiting overview mode
    
    BUG=756313
    TEST=covered by unit test (reused old disabled test)
    
    (cherry picked from commit ea65a745e112a8585a75b15e18c4f7dbc72fa13e)
    
    Change-Id: I93bffc61b12b03a25dba36a18d0ea4596dd24297
    Reviewed-on: https://chromium-review.googlesource.com/620151
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#495637}
    Reviewed-on: https://chromium-review.googlesource.com/621968
    Cr-Commit-Position: refs/branch-heads/3163@{#683}
    Cr-Branched-From: ff259bab28b35d242e10186cd63af7ed404fae0d-refs/heads/master@{#488528}

--- ash/wm/overview/scoped_transform_overview_window.cc
+++ ash/wm/overview/scoped_transform_overview_window.cc
@@ -177,6 +177,7 @@ ScopedTransformOverviewWindow::~ScopedTransformOverviewWindow() {}
 
 void ScopedTransformOverviewWindow::RestoreWindow() {
   ShowHeader();
+  wm::GetWindowState(window_)->set_ignored_by_shelf(ignored_by_shelf_);
   if (minimized_widget_) {
     // TODO(oshima): Use unminimize animation instead of hiding animation.
     minimized_widget_->CloseNow();
@@ -190,7 +191,6 @@ void ScopedTransformOverviewWindow::RestoreWindow() {
   ScopedOverviewAnimationSettings animation_settings(
       OverviewAnimationType::OVERVIEW_ANIMATION_LAY_OUT_SELECTOR_ITEMS,
       window_);
-  wm::GetWindowState(window_)->set_ignored_by_shelf(ignored_by_shelf_);
   SetOpacity(original_opacity_);
 }
 
