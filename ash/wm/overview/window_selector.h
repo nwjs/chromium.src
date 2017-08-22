ca7c0c9efcf8085857577637e45301aca3ac028a
Muyuan Li <muyuanli@chromium.org>
2017-08-22 19:26:44 +0000

    add option for windows to be skipped or hidden in overview.
    BUG=b:63068164
    TEST=ash_unittests --gtest_filter=WindowSelectorTest.SkipOverviewWindow
    
    Change-Id: I2320345e369a5464cda984ae729335b2ff0c90a8
    Reviewed-on: https://chromium-review.googlesource.com/601350
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Commit-Queue: Muyuan Li <muyuanli@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#495407}
    Reviewed-on: https://chromium-review.googlesource.com/624546
    Reviewed-by: Vladislav Kaznacheev <kaznacheev@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3163@{#767}
    Cr-Branched-From: ff259bab28b35d242e10186cd63af7ed404fae0d-refs/heads/master@{#488528}

--- ash/wm/overview/window_selector.h
+++ ash/wm/overview/window_selector.h
@@ -13,6 +13,7 @@
 #include <vector>
 
 #include "ash/ash_export.h"
+#include "ash/wm/overview/scoped_hide_overview_windows.h"
 #include "base/macros.h"
 #include "base/time/time.h"
 #include "ui/aura/window_observer.h"
@@ -50,7 +51,7 @@ class ASH_EXPORT WindowSelector : public display::DisplayObserver,
   ~WindowSelector() override;
 
   // Initialize with the windows that can be selected.
-  void Init(const WindowList& windows);
+  void Init(const WindowList& windows, const WindowList& hide_windows);
 
   // Perform cleanup that cannot be done in the destructor.
   void Shutdown();
@@ -192,6 +193,8 @@ class ASH_EXPORT WindowSelector : public display::DisplayObserver,
   int text_filter_bottom_;
 
   bool is_shut_down_ = false;
+
+  std::unique_ptr<ScopedHideOverviewWindows> hide_overview_windows_;
 
   DISALLOW_COPY_AND_ASSIGN(WindowSelector);
 };
