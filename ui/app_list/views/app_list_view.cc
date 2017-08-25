132abc79bbb2efc2dd458bcc7e1b10c604aa8138
Maajid <maajid@chromium.org>
2017-08-25 22:21:27 +0000

    CrOS: Set launcher default theme color.
    When no dark muted color is available, we use the Shelf default theme,
    which is #000.
    
    Changes:
    1. Refactor code to get background color from prominent_colors.
    2. If no dark muted color is available, then use default theme color.
    
    TBR=wutao@chromium.org
    
    (cherry picked from commit 7b696e42343224f9704207395f58deff1f8f7841)
    
    Bug: 757513
    Test: on device.
    Change-Id: I286ec127257ce4989c4c07e868fc55afdebb1d01
    Reviewed-on: https://chromium-review.googlesource.com/626338
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#496436}
    Reviewed-on: https://chromium-review.googlesource.com/636704
    Reviewed-by: Vadim Tryshev <vadimt@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3163@{#905}
    Cr-Branched-From: ff259bab28b35d242e10186cd63af7ed404fae0d-refs/heads/master@{#488528}

--- ui/app_list/views/app_list_view.cc
+++ ui/app_list/views/app_list_view.cc
@@ -137,26 +137,20 @@ class AppListOverlayView : public views::View {
   DISALLOW_COPY_AND_ASSIGN(AppListOverlayView);
 };
 
-// An event targeter for the search box widget which will ignore events that
-// are on the search box's shadow.
-class SearchBoxWindowTargeter : public wm::MaskedWindowTargeter {
- public:
-  explicit SearchBoxWindowTargeter(views::View* search_box)
-      : wm::MaskedWindowTargeter(search_box->GetWidget()->GetNativeWindow()),
-        search_box_(search_box) {}
-  ~SearchBoxWindowTargeter() override {}
+SkColor GetBackgroundShieldColor(const std::vector<SkColor>& prominent_colors) {
+  if (prominent_colors.empty())
+    return app_list::AppListView::kDefaultBackgroundColor;
 
- private:
-  // wm::MaskedWindowTargeter:
-  bool GetHitTestMask(aura::Window* window, gfx::Path* mask) const override {
-    mask->addRect(gfx::RectToSkRect(search_box_->GetContentsBounds()));
-    return true;
-  }
+  DCHECK_EQ(static_cast<size_t>(ColorProfileType::NUM_OF_COLOR_PROFILES),
+            prominent_colors.size());
 
-  views::View* search_box_;
-
-  DISALLOW_COPY_AND_ASSIGN(SearchBoxWindowTargeter);
-};
+  const SkColor dark_muted =
+      prominent_colors[static_cast<int>(ColorProfileType::DARK_MUTED)];
+  if (SK_ColorTRANSPARENT == dark_muted)
+    return app_list::AppListView::kDefaultBackgroundColor;
+  return color_utils::AlphaBlend(SK_ColorBLACK, dark_muted,
+                                 app_list::AppListView::kDarkMutedBlendAlpha);
+}
 
 }  // namespace
 
@@ -1329,19 +1323,8 @@ void AppListView::SetBackgroundShieldColor() {
 
   std::vector<SkColor> prominent_colors;
   GetWallpaperProminentColors(&prominent_colors);
-
-  if (prominent_colors.empty()) {
-    app_list_background_shield_->layer()->SetColor(kDefaultBackgroundColor);
-  } else {
-    DCHECK_EQ(static_cast<size_t>(ColorProfileType::NUM_OF_COLOR_PROFILES),
-              prominent_colors.size());
-
-    const SkColor dark_muted =
-        prominent_colors[static_cast<int>(ColorProfileType::DARK_MUTED)];
-    const SkColor dark_muted_mixed = color_utils::AlphaBlend(
-        SK_ColorBLACK, dark_muted, kDarkMutedBlendAlpha);
-    app_list_background_shield_->layer()->SetColor(dark_muted_mixed);
-  }
+  app_list_background_shield_->layer()->SetColor(
+      GetBackgroundShieldColor(prominent_colors));
 }
 
 }  // namespace app_list
