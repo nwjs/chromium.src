b769908f060eca5d42107f65db395bcf26171846
Dave Tapuska <dtapuska@chromium.org>
2017-08-28 15:40:35 +0000

    Set pointer id to 0 for mouse event in mac .
    In this patch, we disable support for multi-pen on mac.
    Set WebMouseEvent id to 0 before the return of event with type
    NSMouseExited or NSMouseEntered to make all the event
    have the id set.
    In trybot(mac10.9), NSMouseExited and NSMouseEntered don't have
    [event deviceID]. (Although in my machine 10.12, they have it).
    
    Also, re-enable drag by pen on mac. pen dragging should only disable
    on windows.
    
    TBR=dtapuska@chromium.org
    
    (cherry picked from commit 6038027e89747913716cf298c822f70f67444812)
    
    Bug: 757069
    Change-Id: I44d0021f749151bf8f76e89019bb5b5edf74957c
    Reviewed-on: https://chromium-review.googlesource.com/633356
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#497463}
    Reviewed-on: https://chromium-review.googlesource.com/638531
    Cr-Commit-Position: refs/branch-heads/3163@{#926}
    Cr-Branched-From: ff259bab28b35d242e10186cd63af7ed404fae0d-refs/heads/master@{#488528}

--- third_party/WebKit/Source/core/input/MouseEventManager.cpp
+++ third_party/WebKit/Source/core/input/MouseEventManager.cpp
@@ -782,12 +782,15 @@ WebInputEventResult MouseEventManager::HandleMouseDraggedEvent(
   if (!mouse_pressed_)
     return WebInputEventResult::kNotHandled;
 
-  // We disable the drag and drop actions on pen input.
-  if (event.Event().pointer_type !=
-          blink::WebPointerProperties::PointerType::kPen &&
-      HandleDrag(event, DragInitiator::kMouse)) {
+  // We disable the drag and drop actions on pen input on windows.
+  bool should_handle_drag = true;
+#if defined(OS_WIN)
+  should_handle_drag = event.Event().pointer_type !=
+          blink::WebPointerProperties::PointerType::kPen;
+#endif
+
+  if (should_handle_drag && HandleDrag(event, DragInitiator::kMouse))
     return WebInputEventResult::kHandledSystem;
-  }
 
   Node* target_node = event.InnerNode();
   if (!target_node)
