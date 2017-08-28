7f6100d8795bfa1d8144e857daaf96577fc5a782
Vladislav Kaznacheev <kaznacheev@chromium.org>
2017-08-28 21:20:23 +0000

    Revert "Extensions: use leveldb::DestroyDB to delete database."
    This reverts commit a6fd32aff141cb028b427706cd889b912e1091bc.
    
    Reason for revert: breaks compile
    extensions/browser/value_store/lazy_leveldb.cc:269:64: error: no member named 'Options' in namespace 'leveldb_env'
    
    Original change's description:
    > Extensions: use leveldb::DestroyDB to delete database.
    > 
    > Switched from base::DeleteFile to using leveldb::DestroyDB when
    > deleting the database. DestroyDB acquires the lock to synchronize
    > directory access, whereas DeleteFile does not.
    > 
    > One other difference between these two functions is that DeleteFile
    > will delete all files in a directory - or at least try. DestroyDB
    > will only delete leveldb files (ie *.ldb, CURRENT, manifest, etc.).
    > If the user, or another program, puts non database files in the
    > leveldb folder then they will be left behind.
    > 
    > This is a highly speculative fix for issue 746155.
    > 
    > BUG=746155
    > 
    > (cherry picked from commit 56246d9c3988584fc9019b39ea105d1371737e81)
    > 
    > Change-Id: I2245a5a6bb94fa2dbbec34424d85aa5decb4d0b5
    > Reviewed-on: https://chromium-review.googlesource.com/627601
    > Commit-Queue: Chris Mumford <cmumford@chromium.org>
    > Reviewed-by: Ben Wells <benwells@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/master@{#496945}
    > Reviewed-on: https://chromium-review.googlesource.com/639370
    > Reviewed-by: Chris Mumford <cmumford@chromium.org>
    > Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    > Cr-Commit-Position: refs/branch-heads/3163@{#945}
    > Cr-Branched-From: ff259bab28b35d242e10186cd63af7ed404fae0d-refs/heads/master@{#488528}
    
    TBR=benwells@chromium.org,rdevlin.cronin@chromium.org,cmumford@chromium.org
    
    Change-Id: I92810ad75965ed4607520e64f1e4cbe53d15d26e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 746155
    Reviewed-on: https://chromium-review.googlesource.com/639164
    Reviewed-by: Vladislav Kaznacheev <kaznacheev@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3163@{#947}
    Cr-Branched-From: ff259bab28b35d242e10186cd63af7ed404fae0d-refs/heads/master@{#488528}

--- extensions/browser/value_store/lazy_leveldb.cc
+++ extensions/browser/value_store/lazy_leveldb.cc
@@ -4,6 +4,7 @@
 
 #include "extensions/browser/value_store/lazy_leveldb.h"
 
+#include "base/files/file_util.h"
 #include "base/json/json_reader.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/string_util.h"
@@ -263,13 +264,9 @@ ValueStore::Status LazyLevelDb::ToValueStoreError(
 
 bool LazyLevelDb::DeleteDbFile() {
   base::ThreadRestrictions::AssertIOAllowed();
-  db_.reset();  // Close the database.
-
-  leveldb::Status s =
-      leveldb::DestroyDB(db_path_.AsUTF8Unsafe(), leveldb_env::Options());
-  if (!s.ok()) {
-    LOG(WARNING) << "Failed to destroy leveldb database at "
-                 << db_path_.value();
+  db_.reset();  // release any lock on the directory
+  if (!base::DeleteFile(db_path_, true /* recursive */)) {
+    LOG(WARNING) << "Failed to delete leveldb database at " << db_path_.value();
     return false;
   }
   return true;
