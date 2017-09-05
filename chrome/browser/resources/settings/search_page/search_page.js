351367719aa3f43e29a01aa7e91cf17dcc3205fb
Vladislav Kaznacheev <kaznacheev@google.com>
2017-09-05 18:37:23 +0000

    hide voice interaction settings when arc is disabled.
    BUG=b:65069241
    TEST=local test with arc disabled / enabled.
    TBR=muyuanli@chromium.org
    
    (cherry picked from commit a462deaeb4aa07b59f606d184289c6fdfe2fe17b)
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I459826d08853488fff1a702cc2b72f20645da34f
    Reviewed-on: https://chromium-review.googlesource.com/644321
    Commit-Queue: Muyuan Li <muyuanli@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#499002}
    Reviewed-on: https://chromium-review.googlesource.com/650836
    Reviewed-by: Vladislav Kaznacheev <kaznacheev@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3163@{#1105}
    Cr-Branched-From: ff259bab28b35d242e10186cd63af7ed404fae0d-refs/heads/master@{#488528}

--- chrome/browser/resources/settings/search_page/search_page.js
+++ chrome/browser/resources/settings/search_page/search_page.js
@@ -205,6 +205,16 @@ Polymer({
         toggleValue ? 'searchGoogleAssistantEnabled' :
                       'searchGoogleAssistantDisabled');
   },
+
+  /**
+   * @param {boolean} featureAvailable
+   * @param {boolean} arcEnabled
+   * @return {boolean}
+   * @private
+   */
+  showAssistantSection_: function(featureAvailable, arcEnabled) {
+    return featureAvailable && arcEnabled;
+  },
   // </if>
 
   /**
