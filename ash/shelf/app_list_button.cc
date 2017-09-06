c29d17bc4cbfb5d00ed9b64f50237f03a657a365
Sammie Quon <sammiequon@chromium.org>
2017-09-06 16:55:56 +0000

    Revert "[merge to 61] shelf: Hide back button fully when leaving tablet mode."
    This reverts commit 59ab2b619b149b9a904e6b5eac9f7293f9df36fa.
    
    Reason for revert: Parent cl https://chromium-review.googlesource.com/c/chromium/src/+/652943 needs to be reverted.
    
    Original change's description:
    > [merge to 61] shelf: Hide back button fully when leaving tablet mode.
    > 
    > Back button sometimes is visible when transitioning to clamshell mode, especially when the launcher is open. I believe not all animation ticks are causing a redraw, including the last animation tick. So when the animation ends, call a SchedulePaint.
    > 
    > (cherry picked from commit d59e3e851c456b0372ccafe0e603f711faf6cc52)
    > 
    > TBR: msw@chromium.org
    > Test: manual
    > Bug: 754064
    > Change-Id: I87c4ea2b88365ba7676c39007b20db9264509dd5
    > Reviewed-on: https://chromium-review.googlesource.com/643753
    > Reviewed-by: Michael Wasserman <msw@chromium.org>
    > Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/master@{#498559}
    > Reviewed-on: https://chromium-review.googlesource.com/652944
    > Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    > Cr-Commit-Position: refs/branch-heads/3163@{#1116}
    > Cr-Branched-From: ff259bab28b35d242e10186cd63af7ed404fae0d-refs/heads/master@{#488528}
    
    TBR=sammiequon@chromium.org
    
    Change-Id: I885f4b62e550ad8a2b1ea5f0aab38c71601a6891
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 754064
    Reviewed-on: https://chromium-review.googlesource.com/653437
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3163@{#1119}
    Cr-Branched-From: ff259bab28b35d242e10186cd63af7ed404fae0d-refs/heads/master@{#488528}

--- ash/shelf/app_list_button.cc
+++ ash/shelf/app_list_button.cc
@@ -482,9 +482,6 @@ void AppListButton::OnBoundsAnimationFinished() {
   // finished.
   if (is_showing_app_list_)
     AnimateInkDrop(views::InkDropState::ACTIVATED, nullptr);
-
-  // Redraw to ensure the app list button is drawn at its expected final state.
-  SchedulePaint();
 }
 
 void AppListButton::OnAppListVisibilityChanged(bool shown,
