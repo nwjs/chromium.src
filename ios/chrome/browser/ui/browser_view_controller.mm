9d779b84f9c23e5b8d8887c70b691e7a70695df1
Jean-François Geyelin <jif@chromium.org>
2017-09-15 18:13:09 +0000

    Revert "[Payment Request] Enables Payment Request on iOS by default"
    This reverts commit 033b8b51499ca5b4a000f6ea7a184956c1b391a7.
    
    Reason for revert: Conflicts made some test not compile
    
    Original change's description:
    > [Payment Request] Enables Payment Request on iOS by default
    > 
    > - Sets PaymentRequestManager's active WebState to nullptr in BVC's shutdown
    >   method to make sure the active WebState is clean when BVC shuts down.
    > - Adds a call to update PaymentRequestManager's active WebState in
    >   -tabModel:didReplaceTab:withTab:atIndex: in order to clean the the
    >   previously active WebState and enable PaymentRequest on the new one. This
    >   call happens when a recent tab is opened.
    > - This CL adds a unit test to verify that the BVC shuts down cleanly with
    >   Payment Request enabled.
    > - This CL removes the calls to enable/disable the active WebState in the
    >   [PaymentRequestManager enablePaymentRequest:] and instead changes the
    >   behavior so that the script commands are ignored when the Payment Request
    >   is disabled. The reason for this change is that this method is called
    >   when the BVC is disabled before it shuts down. This could result in a
    >   second attempt to disable the active WebState when the BVC actually shuts
    >   down which is wrong.
    > 
    > TBR=mahmadi@chromium.org, rouslan@
    > 
    > (cherry picked from commit bec07eb12d64b84330e6a582ac7aa38a103a9e7d)
    > 
    > Bug: 761358, 755799, 602666
    > Change-Id: Id984aec77194993b23085448cb802ba9fe120379
    > Reviewed-on: https://chromium-review.googlesource.com/660480
    > Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    > Reviewed-by: Mark Cogan <marq@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/master@{#501363}
    > Reviewed-on: https://chromium-review.googlesource.com/667718
    > Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    > Cr-Commit-Position: refs/branch-heads/3202@{#220}
    > Cr-Branched-From: fa6a5d87adff761bc16afc5498c3f5944c1daa68-refs/heads/master@{#499098}
    
    TBR=marq@chromium.org,mahmadi@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 761358, 755799, 602666
    Change-Id: I016757f312ccbca44f8e5cee8349743aa3f19493
    Reviewed-on: https://chromium-review.googlesource.com/668579
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3202@{#254}
    Cr-Branched-From: fa6a5d87adff761bc16afc5498c3f5944c1daa68-refs/heads/master@{#499098}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -2439,7 +2439,6 @@ bubblePresenterForFeature:(const base::Feature&)feature
     _voiceSearchController->SetDelegate(nil);
   [_rateThisAppDialog setDelegate:nil];
   [_model closeAllTabs];
-  [_paymentRequestManager setActiveWebState:nullptr];
 }
 
 #pragma mark - SnapshotOverlayProvider methods
@@ -4766,9 +4765,6 @@ bubblePresenterForFeature:(const base::Feature&)feature
   // Add |newTab|'s view to the hierarchy if it's the current Tab.
   if (self.active && model.currentTab == newTab)
     [self displayTab:newTab isNewSelection:NO];
-
-  if (newTab)
-    [_paymentRequestManager setActiveWebState:newTab.webState];
 }
 
 // A tab has been removed, remove its views from display if necessary.
--- ios/chrome/browser/ui/payments/payment_request_manager.mm
+++ ios/chrome/browser/ui/payments/payment_request_manager.mm
@@ -327,8 +327,12 @@ struct PendingPaymentResponse {
     return;
 
   _enabled = enabled;
-  if (!enabled)
+  if (!enabled) {
     [self cancelRequest];
+    [self disableActiveWebState];
+  } else if (!_activeWebStateEnabled) {
+    [self enableActiveWebState];
+  }
 }
 
 - (void)cancelRequest {
@@ -421,10 +425,6 @@ struct PendingPaymentResponse {
 }
 
 - (BOOL)handleScriptCommand:(const base::DictionaryValue&)JSONCommand {
-  // Early return if the Payment Request is not enabled.
-  if (!_enabled)
-    return NO;
-
   std::string command;
   if (!JSONCommand.GetString("command", &command)) {
     DLOG(ERROR) << "RECEIVED BAD JSON - NO 'command' field";
