b69dd4e0b15ba9d98ccbc1ca93e280115048cf55
Kyle Horimoto <khorimoto@google.com>
2017-09-18 22:49:08 +0000

    [CrOS Tether] Support Pixel phones. See crbug.com/765732 for details.
    TBR=khorimoto@google.com
    
    (cherry picked from commit 764dc5040a0478988900f3e688e385865b0f44e4)
    
    Bug: 765732, 672263
    Change-Id: I2a0d889d1ee856b8283b26d8017eb26664192334
    Reviewed-on: https://chromium-review.googlesource.com/669316
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#502489}
    Reviewed-on: https://chromium-review.googlesource.com/671197
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3163@{#1227}
    Cr-Branched-From: ff259bab28b35d242e10186cd63af7ed404fae0d-refs/heads/master@{#488528}

--- chrome/browser/chromeos/tether/tether_service.cc
+++ chrome/browser/chromeos/tether/tether_service.cc
@@ -302,7 +302,7 @@ void TetherService::OnPrefsChanged() {
 
 bool TetherService::HasSyncedTetherHosts() const {
   return !cryptauth_service_->GetCryptAuthDeviceManager()
-              ->GetTetherHosts()
+              ->GetPixelTetherHosts()
               .empty();
 }
 
