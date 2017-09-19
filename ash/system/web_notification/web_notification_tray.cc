4ad8fb3a21372f2d8cf6057600436f8b466425e5
MinChen <minch@chromium.org>
2017-09-19 20:26:28 +0000

    [Merge to M61][M61 stable block]: Fixed crash if call AnchorUpdated when tray bubble is during animation to close.
    TBR=oshima@chromium.org
    
    (cherry picked from commit 95543e1ac47ab992f6f75f6a33da5c27c05fd79f)
    
    Bug: 765310,754468
    Change-Id: I236cd517587afb002b10d8d489fd38e0455eeb54
    Reviewed-on: https://chromium-review.googlesource.com/670024
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: min c <minch@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#502777}
    Reviewed-on: https://chromium-review.googlesource.com/673205
    Reviewed-by: min c <minch@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3163@{#1239}
    Cr-Branched-From: ff259bab28b35d242e10186cd63af7ed404fae0d-refs/heads/master@{#488528}

--- ash/system/web_notification/web_notification_tray.cc
+++ ash/system/web_notification/web_notification_tray.cc
@@ -417,7 +417,13 @@ void WebNotificationTray::UpdateAfterShelfAlignmentChange() {
 void WebNotificationTray::AnchorUpdated() {
   if (message_center_bubble()) {
     message_center_bubble()->bubble_view()->UpdateBubble();
-    UpdateBubbleViewArrow(message_center_bubble()->bubble_view());
+    // Should check |message_center_bubble_| again here. Since UpdateBubble
+    // above set the bounds of the bubble which will stop the current
+    // animation. If web notification bubble is during animation to close,
+    // CloseBubbleObserver in TrayBackgroundView will close the bubble if
+    // animation finished.
+    if (message_center_bubble())
+      UpdateBubbleViewArrow(message_center_bubble()->bubble_view());
   }
 }
 
