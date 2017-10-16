0b5055129eb350d725d81793dfa7463ec6a24f09
Kyle Horimoto <khorimoto@google.com>
2017-10-16 22:20:26 +0000

    [CrOS Tether] Add metrics for enabling/disabling Instant Tethering.
    There are two metrics:
    (1) Toggle metric: will be logged each time the setting is toggled from enabled
    to disabled or vice versa. This provides us statistics on how often the
    preference is changed.
    (2) Startup metric: will be logged each time the user logs in. This provides us
    statistics on what percentage of users have the preference enabled.
    
    TBR=khorimoto@google.com
    
    (cherry picked from commit a5837cf4f67606dc5ae3cbfb2139a85af35c7fe1)
    
    Bug: 772474, 672263
    Change-Id: Ie0a405d6d1031d3e70e4d8c8b71c3533e04748fd
    Reviewed-on: https://chromium-review.googlesource.com/713035
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#508700}
    Reviewed-on: https://chromium-review.googlesource.com/722116
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3239@{#21}
    Cr-Branched-From: adb61db19020ed8ecee5e91b1a0ea4c924ae2988-refs/heads/master@{#508578}

--- chrome/browser/chromeos/tether/tether_service_unittest.cc
+++ chrome/browser/chromeos/tether/tether_service_unittest.cc
@@ -814,6 +814,9 @@ TEST_F(TetherServiceTest, TestEnabled) {
   EXPECT_FALSE(
       profile_->GetPrefs()->GetBoolean(prefs::kInstantTetheringEnabled));
   VerifyTetherActiveStatus(false /* expected_active */);
+  histogram_tester_.ExpectBucketCount(
+      "InstantTethering.UserPreference.OnToggle", false,
+      1u /* expected_count */);
 
   SetTetherTechnologyStateEnabled(true);
   EXPECT_EQ(chromeos::NetworkStateHandler::TechnologyState::TECHNOLOGY_ENABLED,
@@ -822,6 +825,9 @@ TEST_F(TetherServiceTest, TestEnabled) {
   EXPECT_TRUE(
       profile_->GetPrefs()->GetBoolean(prefs::kInstantTetheringEnabled));
   VerifyTetherActiveStatus(true /* expected_active */);
+  histogram_tester_.ExpectBucketCount(
+      "InstantTethering.UserPreference.OnToggle", true,
+      1u /* expected_count */);
 
   VerifyTetherFeatureStateRecorded(TetherService::TetherFeatureState::ENABLED,
                                    2 /* expected_count */);
