a1646c6dfb53d4ed35e77f4d5139d9a6a422328c
Kazuhiro Inaba <kinaba@chromium.org>
2017-11-08 02:18:59 +0000

    Add a Mojo call for opening chrome://settings/ main and sub pages.
    The preceding change crrev.com/717336 prohibited ARC++ apps to open
    chrome:// URLs with one exception (chrome://settings/multidevice),
    but this was too restrictive to support some Anrdoid framework APIs.
    
    This CL adds a method dedicated for opening whitelisted (and
    extendable) chrome://settings URLs to allow necessary cases.
    
    BUG=b/68655417
    TEST=adb shell am start -a android.settings.INPUT_METHOD_SETTINGS
    TBR=kinaba@chromium.org
    
    (cherry picked from commit 7d5a92f12d6b1671d4365d2a96d2ac65f18e9a9f)
    
    Change-Id: Iadaa7bf584b6a02656dbd484450a721decd1a7c8
    Reviewed-on: https://chromium-review.googlesource.com/750501
    Reviewed-by: Kazuhiro Inaba <kinaba@chromium.org>
    Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Kazuhiro Inaba <kinaba@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#514081}
    Reviewed-on: https://chromium-review.googlesource.com/758059
    Cr-Commit-Position: refs/branch-heads/3239@{#417}
    Cr-Branched-From: adb61db19020ed8ecee5e91b1a0ea4c924ae2988-refs/heads/master@{#508578}

--- components/arc/intent_helper/arc_intent_helper_bridge_unittest.cc
+++ components/arc/intent_helper/arc_intent_helper_bridge_unittest.cc
@@ -284,11 +284,19 @@ TEST_F(ArcIntentHelperTest, TestOnOpenUrl_ChromeScheme) {
   EXPECT_FALSE(test_open_url_delegate_->last_opened_url().is_valid());
 }
 
-// Tests that OnOpenChromeSettingsMultideviceUrl opens the multidevice settings
-// section in the Chrome browser.
-TEST_F(ArcIntentHelperTest, TestOnOpenChromeSettingsMultideviceUrl) {
-  instance_->OnOpenChromeSettingsMultideviceUrl();
-  EXPECT_EQ(GURL(ArcIntentHelperBridge::kMultideviceSettingsUrl),
+// Tests that OnOpenChromeSettings opens the specified settings section in the
+// Chrome browser.
+TEST_F(ArcIntentHelperTest, TestOnOpenChromeSettings) {
+  instance_->OnOpenChromeSettings(mojom::SettingsPage::MAIN);
+  EXPECT_EQ(GURL("chrome://settings"),
+            test_open_url_delegate_->last_opened_url());
+
+  instance_->OnOpenChromeSettings(mojom::SettingsPage::MULTIDEVICE);
+  EXPECT_EQ(GURL("chrome://settings/multidevice"),
+            test_open_url_delegate_->last_opened_url());
+
+  instance_->OnOpenChromeSettings(mojom::SettingsPage::WIFI);
+  EXPECT_EQ(GURL("chrome://settings/networks/?type=WiFi"),
             test_open_url_delegate_->last_opened_url());
 }
 
