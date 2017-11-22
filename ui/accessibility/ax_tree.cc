4e93a4b19a932671bcd6965b946f7d3c94a92cf7
David Tseng <dtseng@chromium.org>
2017-11-22 01:15:39 +0000

    Fix bad merge
    TBR=dmazzoni@chromium.org
    
    Bug: 
    Change-Id: Icab850b62f85d323041f0e211b1f7074687c3ef1
    Reviewed-on: https://chromium-review.googlesource.com/784112
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3239@{#559}
    Cr-Branched-From: adb61db19020ed8ecee5e91b1a0ea4c924ae2988-refs/heads/master@{#508578}

--- ui/accessibility/ax_tree.cc
+++ ui/accessibility/ax_tree.cc
@@ -231,36 +231,6 @@ gfx::RectF AXTree::RelativeToTreeBounds(const AXNode* node,
       bounds.Offset(-scroll_x, -scroll_y);
     }
 
-    // If this is the root web area, make sure we clip the node to fit.
-    // This is disabled as a bugfix for Chrome 63, see crbug.com/786164
-    if (false) {
-      if (!clipped.IsEmpty()) {
-        // We can simply clip it to the container.
-        bounds = clipped;
-        // No need to update |offscreen| if it is set, because it should be
-        // false by default.
-      } else {
-        // Totally offscreen. Find the nearest edge or corner.
-        // Make the minimum dimension 1 instead of 0.
-        if (bounds.x() >= container_bounds.width()) {
-          bounds.set_x(container_bounds.width() - 1);
-          bounds.set_width(1);
-        } else if (bounds.x() + bounds.width() <= 0) {
-          bounds.set_x(0);
-          bounds.set_width(1);
-        }
-        if (bounds.y() >= container_bounds.height()) {
-          bounds.set_y(container_bounds.height() - 1);
-          bounds.set_height(1);
-        } else if (bounds.y() + bounds.height() <= 0) {
-          bounds.set_y(0);
-          bounds.set_height(1);
-        }
-        if (offscreen != nullptr)
-          *offscreen |= true;
-      }
-    }
-
     node = container;
   }
 
