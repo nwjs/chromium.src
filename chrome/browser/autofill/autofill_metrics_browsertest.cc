c627818c66620fad029b9d4c37236ad1c37d2ff5
sebsg <sebsg@chromium.org>
2017-11-29 16:30:21 +0000

    [Autofill] Fix AutofillManagerTest on the branch
    Bug: 789507
    Change-Id: I9aaa369c338ec92ac7c16207af39460f50413746
    Reviewed-on: https://chromium-review.googlesource.com/796594
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3239@{#609}
    Cr-Branched-From: adb61db19020ed8ecee5e91b1a0ea4c924ae2988-refs/heads/master@{#508578}

--- chrome/browser/autofill/autofill_metrics_browsertest.cc
+++ chrome/browser/autofill/autofill_metrics_browsertest.cc
@@ -47,6 +47,11 @@ class AutofillMetricsMetricsBrowserTest : public InProcessBrowserTest {
   void PreRunTestOnMainThread() override {
     InProcessBrowserTest::PreRunTestOnMainThread();
 
+    // UKM DCHECKs if the active UkmRecorder is changed from one instance
+    // to another, rather than being changed from a nullptr; browser_tests
+    // need to circumvent that to be able to intercept UKM calls with its
+    // own TestUkmRecorder instance rather than the default UkmRecorder.
+    ukm::UkmRecorder::Set(nullptr);
     test_ukm_recorder_ = std::make_unique<ukm::TestAutoSetUkmRecorder>();
   }
 
--- components/autofill/core/browser/autofill_manager_unittest.cc
+++ components/autofill/core/browser/autofill_manager_unittest.cc
@@ -982,9 +982,11 @@ class AutofillManagerTest : public testing::Test {
     if (is_https) {
       form->origin = GURL("https://myform.com/form.html");
       form->action = GURL("https://myform.com/submit.html");
+      form->main_frame_origin = GURL("https://myform_root.com/form.html");
     } else {
       form->origin = GURL("http://myform.com/form.html");
       form->action = GURL("http://myform.com/submit.html");
+      form->main_frame_origin = GURL("http://myform_root.com/form.html");
     }
 
     FormFieldData field;
@@ -5156,6 +5158,8 @@ TEST_F(AutofillManagerTest, UploadCreditCard_MultipleCvcFields) {
   credit_card_form.name = ASCIIToUTF16("MyForm");
   credit_card_form.origin = GURL("https://myform.com/form.html");
   credit_card_form.action = GURL("https://myform.com/submit.html");
+  credit_card_form.main_frame_origin =
+      GURL("https://myform_root.com/form.html");
 
   FormFieldData field;
   test::CreateTestFormField("Card Name", "cardname", "", "text", &field);
@@ -5216,6 +5220,8 @@ TEST_F(AutofillManagerTest, UploadCreditCard_NoCvcFieldOnForm) {
   credit_card_form.name = ASCIIToUTF16("MyForm");
   credit_card_form.origin = GURL("https://myform.com/form.html");
   credit_card_form.action = GURL("https://myform.com/submit.html");
+  credit_card_form.main_frame_origin =
+      GURL("https://myform_root.com/form.html");
 
   FormFieldData field;
   test::CreateTestFormField("Card Name", "cardname", "", "text", &field);
@@ -5271,6 +5277,8 @@ TEST_F(AutofillManagerTest,
   credit_card_form.name = ASCIIToUTF16("MyForm");
   credit_card_form.origin = GURL("https://myform.com/form.html");
   credit_card_form.action = GURL("https://myform.com/submit.html");
+  credit_card_form.main_frame_origin =
+      GURL("https://myform_root.com/form.html");
 
   FormFieldData field;
   test::CreateTestFormField("Card Name", "cardname", "", "text", &field);
@@ -5329,6 +5337,8 @@ TEST_F(AutofillManagerTest,
   credit_card_form.name = ASCIIToUTF16("MyForm");
   credit_card_form.origin = GURL("https://myform.com/form.html");
   credit_card_form.action = GURL("https://myform.com/submit.html");
+  credit_card_form.main_frame_origin =
+      GURL("https://myform_root.com/form.html");
 
   FormFieldData field;
   test::CreateTestFormField("Card Name", "cardname", "", "text", &field);
@@ -5389,6 +5399,8 @@ TEST_F(AutofillManagerTest,
   credit_card_form.name = ASCIIToUTF16("MyForm");
   credit_card_form.origin = GURL("https://myform.com/form.html");
   credit_card_form.action = GURL("https://myform.com/submit.html");
+  credit_card_form.main_frame_origin =
+      GURL("https://myform_root.com/form.html");
 
   FormFieldData field;
   test::CreateTestFormField("Card Name", "cardname", "", "text", &field);
@@ -5456,6 +5468,8 @@ TEST_F(AutofillManagerTest,
   credit_card_form.name = ASCIIToUTF16("MyForm");
   credit_card_form.origin = GURL("https://myform.com/form.html");
   credit_card_form.action = GURL("https://myform.com/submit.html");
+  credit_card_form.main_frame_origin =
+      GURL("https://myform_root.com/form.html");
 
   FormFieldData field;
   test::CreateTestFormField("Card Name", "cardname", "", "text", &field);
@@ -5517,6 +5531,8 @@ TEST_F(AutofillManagerTest, UploadCreditCard_NoCvcFieldOnFormExperimentOff) {
   credit_card_form.name = ASCIIToUTF16("MyForm");
   credit_card_form.origin = GURL("https://myform.com/form.html");
   credit_card_form.action = GURL("https://myform.com/submit.html");
+  credit_card_form.main_frame_origin =
+      GURL("https://myform_root.com/form.html");
 
   FormFieldData field;
   test::CreateTestFormField("Card Name", "cardname", "", "text", &field);
