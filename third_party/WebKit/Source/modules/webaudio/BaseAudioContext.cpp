f11293eaaabc7cd32ce44c91574add89f8f8e3f8
Yuki Shiino <yukishiino@chromium.org>
2017-12-12 07:52:56 +0000

    v8binding: Fixes wrapper-tracing of BaseAudioContext's callbacks.
    BaseAudioContext has no parent object that does wrapper-tracing,
    and author script does not hold the V8 wrapper object in general.
    The only thing that makes the BaseAudioContext alive is a closure
    posted to a task queue, which doesn't support wrapper-tracing.
    
    Thus, this patch uses persistent handles to make the callback
    functions in BaseAudioContext alive.
    
    This is acceptable because it's guaranteed that each callback will
    be removed once their task gets done regardless of whether it's
    successful or not.
    
    TBR=yukishiino@chromium.org
    
    (cherry picked from commit 182dd1a3b8d8a57d9eb796b6027bd6ed2009a2a7)
    
    Bug: 791436
    Change-Id: Ibfd16c6806734a562c3a59b263bc95072fa0eb2c
    Reviewed-on: https://chromium-review.googlesource.com/816642
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#522795}
    Reviewed-on: https://chromium-review.googlesource.com/822018
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3282@{#169}
    Cr-Branched-From: 5fdc0fab22ce7efd32532ee989b223fa12f8171e-refs/heads/master@{#520840}

--- third_party/WebKit/Source/modules/webaudio/BaseAudioContext.cpp
+++ third_party/WebKit/Source/modules/webaudio/BaseAudioContext.cpp
@@ -29,8 +29,6 @@
 #include "bindings/core/v8/ExceptionMessages.h"
 #include "bindings/core/v8/ExceptionState.h"
 #include "bindings/core/v8/ScriptPromiseResolver.h"
-#include "bindings/modules/v8/v8_decode_error_callback.h"
-#include "bindings/modules/v8/v8_decode_success_callback.h"
 #include "core/dom/DOMException.h"
 #include "core/dom/Document.h"
 #include "core/dom/ExceptionCode.h"
@@ -991,8 +989,6 @@ void BaseAudioContext::Trace(blink::Visitor* visitor) {
   visitor->Trace(listener_);
   visitor->Trace(active_source_nodes_);
   visitor->Trace(resume_resolvers_);
-  visitor->Trace(success_callbacks_);
-  visitor->Trace(error_callbacks_);
   visitor->Trace(decode_audio_resolvers_);
   visitor->Trace(periodic_wave_sine_);
   visitor->Trace(periodic_wave_square_);
@@ -1017,7 +1013,7 @@ void BaseAudioContext::TraceWrappers(
   }
 }
 
-SecurityOrigin* BaseAudioContext::GetSecurityOrigin() const {
+const SecurityOrigin* BaseAudioContext::GetSecurityOrigin() const {
   if (GetExecutionContext())
     return GetExecutionContext()->GetSecurityOrigin();
 
