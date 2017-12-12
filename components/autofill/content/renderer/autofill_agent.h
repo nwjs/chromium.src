c88a991f09256993426e821ff7600efd47d7e0aa
Changwan Ryu <changwan@chromium.org>
2017-12-12 22:06:53 +0000

    Fix autofill focus handling for webview
    On Chrome, the autofill is triggered in the following complex steps:
    
    1) User-triggered focus change causes showSoftInput().
    2) showSoftInput() resizes window size and therefore view size.
    3) ContentViewCore#updateAfterSizeChanged()
    4) WebContents#scrollFocusedEditableNodeIntoView()
    5) RenderViewImpl::ScrollFocusedEditableNodeIntoRect()
    6) AutofillAgent::DidCompleteFocusChangeInFrame()
    7) AutofillAgent::FormControlElementClicked()
    
    I think this logic helps trigger autofill *after* resize and
    scrolling is finished.
    
    However, on WebView, step 1) does not occur when the app uses
    softInputMode:adjustPan or webview is small enough.
    
    The approach in this CL is:
    1) User-triggered focus change calls
    AutofillAgent::FormControlElementClicked()
    2) Allow scrolling to adjust the position of the autofill suggestion UI.
    
    Note that because Android framework does not provide a way to scroll the
    autofill suggestion UI, the adjustment in step 2) is somewhat clunky.
    
    There is no O trybot, and this is tested locally.
    
    BUG=730764
    
    (cherry picked from commit eb8a057e1e373652f61585b12313db4fe57ab8fd)
    
    Change-Id: I31e531b6841c48098d49c660468869c54705aa9d
    Reviewed-on: https://chromium-review.googlesource.com/748887
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Reviewed-by: Tao Bai <michaelbai@chromium.org>
    Commit-Queue: Changwan Ryu <changwan@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#522839}
    Reviewed-on: https://chromium-review.googlesource.com/823235
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3282@{#183}
    Cr-Branched-From: 5fdc0fab22ce7efd32532ee989b223fa12f8171e-refs/heads/master@{#520840}

--- components/autofill/content/renderer/autofill_agent.h
+++ components/autofill/content/renderer/autofill_agent.h
@@ -82,6 +82,7 @@ class AutofillAgent : public content::RenderFrameObserver,
       const PasswordFormFillData& form_data) override;
   void SetUserGestureRequired(bool required) override;
   void SetSecureContextRequired(bool required) override;
+  void SetFocusRequiresScroll(bool require) override;
 
   void ShowNotSecureWarning(const blink::WebInputElement& element);
 
@@ -286,6 +287,9 @@ class AutofillAgent : public content::RenderFrameObserver,
   bool was_focused_before_now_ = false;
   blink::WebFormControlElement last_clicked_form_control_element_for_testing_;
   bool last_clicked_form_control_element_was_focused_for_testing_ = false;
+
+  // Whether or not we delay focus handling until scrolling occurs.
+  bool focus_requires_scroll_ = true;
 
   mojo::Binding<mojom::AutofillAgent> binding_;
 
