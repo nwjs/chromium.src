6c66452acabf8411691761987cd5bf493eccd619
Makoto Shimazu <shimazu@chromium.org>
2017-12-14 05:31:50 +0000

    [Merge to M64] ServiceWorker: Clear event timeouts when restarting the worker
    Currently ServiceWorkerVersion clears |pending_requests_| but not
    |request_timeouts_|, and it causes inconsistency after restart. This patch fixes
    it.
    
    TBR=shimazu@chromium.org
    
    (cherry picked from commit 769adb1745ce3f75336b656f33178013c471e3e4)
    
    Bug: 791451
    Change-Id: I50778a81ab31d1094db5776a49137c0196c1fb5f
    Reviewed-on: https://chromium-review.googlesource.com/807835
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#522384}
    Reviewed-on: https://chromium-review.googlesource.com/826667
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3282@{#220}
    Cr-Branched-From: 5fdc0fab22ce7efd32532ee989b223fa12f8171e-refs/heads/master@{#520840}

--- content/browser/service_worker/service_worker_version.cc
+++ content/browser/service_worker/service_worker_version.cc
@@ -1495,6 +1495,8 @@ void ServiceWorkerVersion::DidEnsureLiveRegistrationForStartWorker(
 
 void ServiceWorkerVersion::StartWorkerInternal() {
   DCHECK_EQ(EmbeddedWorkerStatus::STOPPED, running_status());
+  DCHECK(pending_requests_.IsEmpty());
+  DCHECK(request_timeouts_.empty());
   DCHECK(start_worker_first_purpose_);
 
   if (!ServiceWorkerMetrics::ShouldExcludeSiteFromHistogram(site_for_uma_)) {
@@ -1931,6 +1933,7 @@ void ServiceWorkerVersion::OnStoppedInternal(EmbeddedWorkerStatus old_status) {
     iter.Advance();
   }
   pending_requests_.Clear();
+  request_timeouts_.clear();
   external_request_uuid_to_request_id_.clear();
   event_dispatcher_.reset();
   controller_ptr_.reset();
