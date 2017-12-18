843d606a97a222a5ff7556fffb9a1f3eeeab5ca2
stkhapugin@chromium.org <stkhapugin@chromium.org>
2017-12-18 15:07:14 +0000

    Set target location bar view frame to trigger autolayout.
    Set the view frame in addition to animating view's layer bounds because
    just changing layer bounds doesn't allow autolayout inside LBV to
    trigger.
    Note that after [_locationBarView layer].bounds = toBounds; the
    LBV's frame will become LayoutRectGetRect(toLayout) anyway, but the
    since the setter is not called, autolayout seems to be confused.
    Also note that this is not necessary in new toolbar.
    
    Bug: 792503, 789471, 788814
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: If3ccac317bb8d850f71275ee32c7b82d38f6af24
    Reviewed-on: https://chromium-review.googlesource.com/813973
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#522784}(cherry picked from commit f2dfa460b70841647d1b5dcb7700fd0fd614737c)
    Reviewed-on: https://chromium-review.googlesource.com/832646
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3282@{#262}
    Cr-Branched-From: 5fdc0fab22ce7efd32532ee989b223fa12f8171e-refs/heads/master@{#520840}

--- ios/chrome/browser/ui/toolbar/web_toolbar_controller.mm
+++ ios/chrome/browser/ui/toolbar/web_toolbar_controller.mm
@@ -1757,6 +1757,7 @@ using ios::material::TimingFunction;
   // animation_util, and do all of the bounds/position calculations above in
   // terms of frames.
 
+  _locationBarView.frame = LayoutRectGetRect(toLayout);
   CABasicAnimation* resizeAnimation =
       [CABasicAnimation animationWithKeyPath:@"bounds"];
   resizeAnimation.delegate = self;
