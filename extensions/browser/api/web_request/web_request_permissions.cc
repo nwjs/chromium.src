4ac53f6cc73b9d697a286b197228fa2049136afc
Rob Wu <rob@robwu.nl>
2018-01-19 11:43:03 +0000

    Hide DevTools frontend from webRequest API
    Prevent extensions from observing requests for remote DevTools frontends
    and add regression tests.
    
    And update ExtensionTestApi to support initializing the embedded test
    server and port from SetUpCommandLine (before SetUpOnMainThread).
    
    BUG=797497,797500
    TEST=browser_test --gtest_filter=DevToolsFrontendInWebRequestApiTest.HiddenRequests
    TBR=rob@robwu.nl
    
    (cherry picked from commit 2649de11c562aa96d336c06136a1a20c01711be0)
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Ic8f44b5771f2d5796f8c3de128f0a7ab88a77735
    Reviewed-on: https://chromium-review.googlesource.com/844316
    Commit-Queue: Rob Wu <rob@robwu.nl>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#528187}
    Reviewed-on: https://chromium-review.googlesource.com/875984
    Reviewed-by: Rob Wu <rob@robwu.nl>
    Cr-Commit-Position: refs/branch-heads/3282@{#546}
    Cr-Branched-From: 5fdc0fab22ce7efd32532ee989b223fa12f8171e-refs/heads/master@{#520840}

--- extensions/browser/api/web_request/web_request_permissions.cc
+++ extensions/browser/api/web_request/web_request_permissions.cc
@@ -10,6 +10,7 @@
 #include "chromeos/login/login_state.h"
 #include "content/public/browser/child_process_security_policy.h"
 #include "content/public/browser/resource_request_info.h"
+#include "extensions/browser/api/extensions_api_client.h"
 #include "extensions/browser/api/web_request/web_request_api_constants.h"
 #include "extensions/browser/extension_navigation_ui_data.h"
 #include "extensions/browser/guest_view/web_view/web_view_renderer_state.h"
@@ -95,6 +96,14 @@ bool IsSensitiveURL(const GURL& url,
                             base::StartsWith(url.path_piece(), "/webstore",
                                              base::CompareCase::SENSITIVE));
   }
+
+  if (is_request_from_browser_or_webui_renderer) {
+    sensitive_chrome_url =
+        sensitive_chrome_url ||
+        extensions::ExtensionsAPIClient::Get()->ShouldHideBrowserNetworkRequest(
+            url);
+  }
+
   return sensitive_chrome_url || extension_urls::IsWebstoreUpdateUrl(url) ||
          extension_urls::IsBlacklistUpdateUrl(url) ||
          extension_urls::IsSafeBrowsingUrl(origin, url.path_piece());
