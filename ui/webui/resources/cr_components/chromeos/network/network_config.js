563936b6a4d2eff832721265e5958fcaf92f4135
Steven Bennetts <stevenjb@chromium.org>
2018-01-31 17:51:17 +0000

    Revert "Revert "Fix network config UI""
    This reverts commit 8e34de0d47fea18a8ee420b199f538f3be29b2dd.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Revert "Fix network config UI"
    > 
    > BUG=805357
    > 
    > This reverts commit 4a5646ee3ab8887eddef734ef87ed808d9d3793f.
    > 
    > Change-Id: I64bac73cd00352391eaa6dd07b931abc2cf9c0e7
    > Reviewed-on: https://chromium-review.googlesource.com/892560
    > Reviewed-by: Bernie Thompson <bhthompson@chromium.org>
    > Cr-Commit-Position: refs/branch-heads/3325@{#167}
    > Cr-Branched-From: bc084a8b5afa3744a74927344e304c02ae54189f-refs/heads/master@{#530369}
    
    TBR=stevenjb@chromium.org,tbarzic@chromium.org,bhthompson@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 805357
    Change-Id: I8049fc0e5f1c335ce205a0e5102ef91076bfdb79
    Reviewed-on: https://chromium-review.googlesource.com/894833
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3325@{#202}
    Cr-Branched-From: bc084a8b5afa3744a74927344e304c02ae54189f-refs/heads/master@{#530369}

--- ui/webui/resources/cr_components/chromeos/network/network_config.js
+++ ui/webui/resources/cr_components/chromeos/network/network_config.js
@@ -486,7 +486,7 @@ Polymer({
   setNetworkProperties_: function(properties) {
     this.propertiesReceived_ = true;
     this.networkProperties = properties;
-    this.error_ = properties.ErrorState || '';
+    this.setError_(properties.ErrorState);
 
     // Set the current shareNetwork_ value when porperties are received.
     this.setShareNetwork_();
@@ -896,10 +896,7 @@ Polymer({
 
     var requireCerts = (this.showEap_ && this.showEap_.UserCert) ||
         (this.showVpn_ && this.showVpn_.UserCert);
-    if (requireCerts && !this.userCerts_.length)
-      this.error_ = certError;
-    else
-      this.error_ = '';
+    this.setError_(requireCerts && !this.userCerts_.length ? certError : '');
   },
 
   /**
@@ -1024,6 +1021,14 @@ Polymer({
   shareIsVisible_: function() {
     return !this.guid &&
         (this.type == CrOnc.Type.WI_FI || this.type == CrOnc.Type.WI_MAX);
+  },
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  connectingIsVisible_: function() {
+    return this.propertiesSent_ && !this.error_;
   },
 
   /**
@@ -1191,9 +1196,10 @@ Polymer({
 
   /** @private */
   setPropertiesCallback_: function() {
-    this.error_ = this.getRuntimeError_();
+    this.setError_(this.getRuntimeError_());
     if (this.error_) {
       console.error('setProperties error: ' + this.guid + ': ' + this.error_);
+      this.propertiesSent_ = false;
       return;
     }
     var connectState = this.networkProperties.ConnectionState;
@@ -1211,11 +1217,12 @@ Polymer({
    * @private
    */
   createNetworkCallback_: function(guid) {
-    this.error_ = this.getRuntimeError_();
+    this.setError_(this.getRuntimeError_());
     if (this.error_) {
       console.error(
           'createNetworkError, type: ' + this.networkProperties.Type + ': ' +
           'error: ' + this.error_);
+      this.propertiesSent_ = false;
       return;
     }
     this.startConnect_(guid);
@@ -1228,18 +1235,15 @@ Polymer({
   startConnect_: function(guid) {
     this.networkingPrivate.startConnect(guid, () => {
       var error = this.getRuntimeError_();
-      if (!error || error == 'connected' || error == 'connect-canceled') {
-        this.close_();  // Connect completed or canceled, close the dialog.
+      if (!error || error == 'connected' || error == 'connect-canceled' ||
+          error == 'connecting') {
+        // Connect is in progress, completed or canceled, close the dialog.
+        this.close_();
         return;
       }
-      if (error == 'connecting') {
-        // Keep the dialog open while connecting. TODO(stevenjb): Add a listener
-        // for the network properties and close the dialog if connected or show
-        // an error if not.
-        return;
-      }
-      this.error_ = error;
+      this.setError_(error);
       console.error('Error connecting to network: ' + error);
+      this.propertiesSent_ = false;
     });
   },
 
@@ -1279,6 +1283,18 @@ Polymer({
       GUID: properties.GUID || '',
       Type: this.type,
     };
+  },
+
+  /**
+   * @param {string|undefined} error
+   * @private
+   */
+  setError_: function(error) {
+    if (!error) {
+      this.error_ = '';
+      return;
+    }
+    this.error_ = error;
   },
 
   /**
