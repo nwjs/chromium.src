d608af8f049339534ac68adaca1d4253068b486f
Wei Li <weili@chromium.org>
2018-02-15 21:27:49 +0000

    Set up mac v2 sandbox policy for pdf compositor
    Pdf compositor service composites contents printed from different
    renderer process into a complete pdf document. This is needed for
    general printing when site isolation is enabled, because a web page with
    out of process iframes may be rendered in multiple renderers.
    
    Therefore, pdf compositor service needs to access font access.
    This CL set up pdf compositor service's own seatbelt policy.
    
    BUG=809763, 455764
    
    Change-Id: I4a78ef793c1ea5e52b167a74fd02091f197bd61f
    Reviewed-on: https://chromium-review.googlesource.com/912316
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Wei Li <weili@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#536153}(cherry picked from commit a3533592984bdb788d1335a223b37e125a07c99c)
    Reviewed-on: https://chromium-review.googlesource.com/922881
    Reviewed-by: Wei Li <weili@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3325@{#480}
    Cr-Branched-From: bc084a8b5afa3744a74927344e304c02ae54189f-refs/heads/master@{#530369}

--- content/browser/child_process_launcher_helper_mac.cc
+++ content/browser/child_process_launcher_helper_mac.cc
@@ -25,6 +25,7 @@
 #include "services/service_manager/sandbox/mac/common_v2.sb.h"
 #include "services/service_manager/sandbox/mac/gpu_v2.sb.h"
 #include "services/service_manager/sandbox/mac/nacl_loader.sb.h"
+#include "services/service_manager/sandbox/mac/pdf_compositor.sb.h"
 #include "services/service_manager/sandbox/mac/ppapi_v2.sb.h"
 #include "services/service_manager/sandbox/mac/renderer_v2.sb.h"
 #include "services/service_manager/sandbox/mac/utility.sb.h"
@@ -113,8 +114,10 @@ bool ChildProcessLauncherHelper::BeforeLaunchOnLauncherThread(
       case service_manager::SANDBOX_TYPE_RENDERER:
         profile += service_manager::kSeatbeltPolicyString_renderer_v2;
         break;
-      case service_manager::SANDBOX_TYPE_UTILITY:
       case service_manager::SANDBOX_TYPE_PDF_COMPOSITOR:
+        profile += service_manager::kSeatbeltPolicyString_pdf_compositor;
+        break;
+      case service_manager::SANDBOX_TYPE_UTILITY:
       case service_manager::SANDBOX_TYPE_PROFILING:
         profile += service_manager::kSeatbeltPolicyString_utility;
         break;
@@ -137,10 +140,10 @@ bool ChildProcessLauncherHelper::BeforeLaunchOnLauncherThread(
       case service_manager::SANDBOX_TYPE_NACL_LOADER:
       case service_manager::SANDBOX_TYPE_PPAPI:
       case service_manager::SANDBOX_TYPE_RENDERER:
+      case service_manager::SANDBOX_TYPE_PDF_COMPOSITOR:
         SetupCommonSandboxParameters(seatbelt_exec_client_.get());
         break;
       case service_manager::SANDBOX_TYPE_UTILITY:
-      case service_manager::SANDBOX_TYPE_PDF_COMPOSITOR:
       case service_manager::SANDBOX_TYPE_PROFILING:
         SetupUtilitySandboxParameters(seatbelt_exec_client_.get(),
                                       *command_line_.get());
