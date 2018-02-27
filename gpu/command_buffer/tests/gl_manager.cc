020310eaf3e563e53e05235ede32270418eafdaa
Vikas Soni <vikassoni@chromium.org>
2018-02-27 20:00:25 +0000

    Fix tabs sharing TEXTURE_2D_ARRAY/TEXTURE_3D data.
    In linux and android, we are seeing an issue where texture data from one
    tab overwrites the texture data of another tab. This is happening for apps
    which are using webgl2 texture of type TEXTURE_2D_ARRAY/TEXTURE_3D.
    Due to a bug in virtual context save/restore code for above texture formats,
    the texture data is not properly restored while switching tabs. Hence
    texture data from one tab overwrites other.
    
    This CL has fix for that issue, an update for existing test expectations
    and a new unit test for this bug.
    
    
    Bug: 788448
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ie933984cdd2d1381f42eb4638f730c8245207a28
    Reviewed-on: https://chromium-review.googlesource.com/930327
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#539111}(cherry picked from commit d128139d53e9268e87921e82d89b3f2053cb83fd)
    Reviewed-on: https://chromium-review.googlesource.com/939878
    Cr-Commit-Position: refs/branch-heads/3325@{#610}
    Cr-Branched-From: bc084a8b5afa3744a74927344e304c02ae54189f-refs/heads/master@{#530369}

--- gpu/command_buffer/tests/gl_manager.cc
+++ gpu/command_buffer/tests/gl_manager.cc
@@ -268,6 +268,7 @@ void GLManager::InitializeWithWorkaroundsImpl(
   InitializeGpuPreferencesForTestingFromCommandLine(command_line,
                                                     &gpu_preferences_);
 
+  context_type_ = options.context_type;
   if (options.share_mailbox_manager) {
     mailbox_manager_ = options.share_mailbox_manager->mailbox_manager();
   } else if (options.share_group_manager) {
@@ -572,4 +573,7 @@ bool GLManager::CanWaitUnverifiedSyncToken(const gpu::SyncToken& sync_token) {
 
 void GLManager::SetSnapshotRequested() {}
 
+ContextType GLManager::GetContextType() const {
+  return context_type_;
+}
 }  // namespace gpu
