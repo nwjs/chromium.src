cd602920a3c26420bc8090e5310cd1fe07de3f87
Alexander Alekseev <alemate@chromium.org>
2018-03-06 21:20:48 +0000

    Chrome OS OOBE: Change illustration when switching to tablet mode.
    This CL adds setTabletModeState sceen object method and event flow to
    update this status in runtime. Also illustration on the OOBE screen now
    depends on the device status.
    
    TBR=alemate@chromium.org
    
    (cherry picked from commit 5ccf96ac889f2c82e9a91f564abb43339c4fa92b)
    
    Bug: 792719
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I13a5dfb55630fe6b8debb022ca4a04529acdff52
    Reviewed-on: https://chromium-review.googlesource.com/939049
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#539991}
    Reviewed-on: https://chromium-review.googlesource.com/952069
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3325@{#672}
    Cr-Branched-From: bc084a8b5afa3744a74927344e304c02ae54189f-refs/heads/master@{#530369}

--- chrome/browser/ui/webui/chromeos/login/core_oobe_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/core_oobe_handler.cc
@@ -28,6 +28,7 @@
 #include "chrome/browser/chromeos/tpm_firmware_update.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
 #include "chrome/browser/ui/ash/ash_util.h"
+#include "chrome/browser/ui/ash/tablet_mode_client.h"
 #include "chrome/browser/ui/webui/chromeos/login/oobe_ui.h"
 #include "chrome/browser/ui/webui/chromeos/login/signin_screen_handler.h"
 #include "chrome/common/channel_info.h"
@@ -97,9 +98,14 @@ CoreOobeHandler::CoreOobeHandler(OobeUI* oobe_ui,
   } else {
     NOTIMPLEMENTED();
   }
+
+  TabletModeClient* tablet_mode_client = TabletModeClient::Get();
+  tablet_mode_client->AddObserver(this);
 }
 
-CoreOobeHandler::~CoreOobeHandler() {}
+CoreOobeHandler::~CoreOobeHandler() {
+  TabletModeClient::Get()->RemoveObserver(this);
+}
 
 void CoreOobeHandler::DeclareLocalizedValues(
     ::login::LocalizedValuesBuilder* builder) {
@@ -150,6 +156,11 @@ void CoreOobeHandler::Initialize() {
   UpdateDeviceRequisition();
   UpdateKeyboardState();
   UpdateClientAreaSize();
+}
+
+void CoreOobeHandler::GetAdditionalParameters(base::DictionaryValue* dict) {
+  dict->SetKey("isInTabletMode",
+               base::Value(TabletModeClient::Get()->tablet_mode_enabled()));
 }
 
 void CoreOobeHandler::RegisterMessages() {
@@ -495,6 +506,10 @@ void CoreOobeHandler::UpdateKeyboardState() {
     ShowControlBar(!is_keyboard_shown);
     SetVirtualKeyboardShown(is_keyboard_shown);
   }
+}
+
+void CoreOobeHandler::OnTabletModeToggled(bool enabled) {
+  CallJSOrDefer("setTabletModeState", enabled);
 }
 
 void CoreOobeHandler::UpdateClientAreaSize() {
