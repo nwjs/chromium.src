b58c304af12a57843c0b2f667e1055b8331da081
Derek Cheng <imcheng@chromium.org>
2018-03-12 16:59:43 +0000

    [Media Router] Cast sink query part 3.
    Add CastMediaRouteProvider, that interfaces with MediaRouter. It will
    use CastAppDiscoveryService to handle sink queries. (Per profile). The
    CastMediaRouteProvider runs on the IO thread, and thread hopping
    between MediaRouterDesktop and CastMRP is done by Mojo.
    
    Add CastAppDiscoveryService (singleton), which runs on the IO thread.
    It observes sinks from CastMediaSinkService and requests app
    availability via CastMessageHandler.
    
    
    Bug: 698940
    Change-Id: I48c4d1287dcea1ab30e524d71ba7fd947725646a
    Reviewed-on: https://chromium-review.googlesource.com/899954
    Commit-Queue: Derek Cheng <imcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#541271}(cherry picked from commit a5493913a3cc87a361c7e4c84b8d141f42bf53a9)
    Reviewed-on: https://chromium-review.googlesource.com/958013
    Reviewed-by: Derek Cheng <imcheng@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3359@{#164}
    Cr-Branched-From: 66afc5e5d10127546cc4b98b9117aff588b5e66b-refs/heads/master@{#540276}

--- chrome/browser/media/router/test/test_helper.h
+++ chrome/browser/media/router/test/test_helper.h
@@ -15,6 +15,7 @@
 #include "build/build_config.h"
 #include "chrome/browser/media/router/discovery/dial/dial_media_sink_service.h"
 #include "chrome/browser/media/router/discovery/mdns/cast_media_sink_service.h"
+#include "chrome/browser/media/router/discovery/mdns/cast_media_sink_service_impl.h"
 #include "chrome/browser/media/router/issue_manager.h"
 #include "chrome/browser/media/router/issues_observer.h"
 #include "chrome/browser/media/router/media_routes_observer.h"
@@ -127,7 +128,9 @@ class MockCastMediaSinkService : public CastMediaSinkService {
   MockCastMediaSinkService();
   ~MockCastMediaSinkService() override;
 
-  MOCK_METHOD1(Start, void(const OnSinksDiscoveredCallback&));
+  MOCK_METHOD2(Start,
+               void(const OnSinksDiscoveredCallback&,
+                    CastMediaSinkServiceImpl::Observer*));
   MOCK_METHOD0(OnUserGesture, void());
   MOCK_METHOD0(StartMdnsDiscovery, void());
 };
@@ -150,6 +153,9 @@ net::IPEndPoint CreateIPEndPoint(int num);
 //   }
 // }
 MediaSinkInternal CreateDialSink(int num);
+
+// Helper function to create a Cast sink.
+MediaSinkInternal CreateCastSink(int num);
 
 }  // namespace media_router
 
