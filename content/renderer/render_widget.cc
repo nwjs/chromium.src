a5efd980e01501ed7021c74b144c791b796183b8
Christopher Cameron <ccameron@chromium.org>
2018-03-14 00:14:48 +0000

    Surface synchronization: Fix one source of transient invariants violations
    At the most basic level, the invariant we want to maintain for surface
    synchronization is:
    
    Change in device scale factor or Change Compositor Viewport Size =>
    Change in LocalSurfaceId
    
    Browser side we always start with a valid initial local surface ID even
    before we have a valid physical backing size. We shouldn't send a valid
    LocalSurfaceId to the renderer if we don't have a physical backing size.
    
    TBR=fsamuel@chromium.org
    
    (cherry picked from commit a41e9d414caf98f688bb648d4ac2bbd9c1056cbf)
    
    Bug: 814690
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Idc7f391d511615309732f792ff620a9fb7073d07
    Reviewed-on: https://chromium-review.googlesource.com/941736
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Saman Sami <samans@chromium.org>
    Commit-Queue: Fady Samuel <fsamuel@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#540292}
    Reviewed-on: https://chromium-review.googlesource.com/961619
    Reviewed-by: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3359@{#221}
    Cr-Branched-From: 66afc5e5d10127546cc4b98b9117aff588b5e66b-refs/heads/master@{#540276}

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -856,6 +856,8 @@ void RenderWidget::OnEnableDeviceEmulation(
     resize_params.screen_info = screen_info_;
     resize_params.new_size = size_;
     resize_params.physical_backing_size = physical_backing_size_;
+    resize_params.local_surface_id = local_surface_id_;
+    resize_params.content_source_id = current_content_source_id_;
     resize_params.visible_viewport_size = visible_viewport_size_;
     resize_params.is_fullscreen_granted = is_fullscreen_granted_;
     resize_params.display_mode = display_mode_;
@@ -1388,7 +1390,7 @@ void RenderWidget::Resize(const ResizeParams& params) {
   // |current_content_source_id_|, then the given LocalSurfaceId was generated
   // before the navigation. Continue with the resize but don't use the
   // LocalSurfaceId until the right one comes.
-  viz::LocalSurfaceId new_local_surface_id = local_surface_id_;
+  viz::LocalSurfaceId new_local_surface_id;
   if (params.local_surface_id &&
       params.content_source_id == current_content_source_id_) {
     new_local_surface_id = *params.local_surface_id;
@@ -1402,7 +1404,8 @@ void RenderWidget::Resize(const ResizeParams& params) {
     // it receives a valid surface ID. This is a no-op if surface
     // synchronization is disabled.
     DCHECK(!compositor_->IsSurfaceSynchronizationEnabled() ||
-           !params.needs_resize_ack || params.local_surface_id->is_valid());
+           !params.needs_resize_ack || !params.local_surface_id ||
+           params.local_surface_id->is_valid());
     compositor_->SetBrowserControlsHeight(
         params.top_controls_height, params.bottom_controls_height,
         params.browser_controls_shrink_blink_size);
