6f33da3f529d0d5aa62d0938c2f752a6dc486167
rbpotter <rbpotter@chromium.org>
2018-03-19 20:29:42 +0000

    Print Preview: Regenerate PDF for rasterization (M66)
    Since the final PDF from print preview is now sent directly to the
    printer in all cases, the PDF should be regenerated in all cases where
    the user changes a setting that should modify the final PDF, including
    "Print as Image".
    
    Bug: 813267
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I49202c782c35209d319f146b272dd49c2d0cfd07
    Reviewed-on: https://chromium-review.googlesource.com/963282
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#543257}(cherry picked from commit cd1539c5e2f2a50e59b47b1a857224f799559aa5)
    TBR: dpapad@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/969188
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3359@{#325}
    Cr-Branched-From: 66afc5e5d10127546cc4b98b9117aff588b5e66b-refs/heads/master@{#540276}

--- chrome/browser/resources/print_preview/new/preview_area.js
+++ chrome/browser/resources/print_preview/new/preview_area.js
@@ -78,7 +78,7 @@ Polymer({
         'settings.layout.value, settings.margins.value, ' +
         'settings.mediaSize.value, settings.ranges.value,' +
         'settings.selectionOnly.value, settings.scaling.value, ' +
-        'destination.id, destination.capabilities)',
+        'settings.rasterize.value, destination.id, destination.capabilities)',
   ],
 
   /** @private {print_preview.NativeLayer} */
@@ -418,7 +418,7 @@ Polymer({
       printWithCloudPrint: !this.destination.isLocal,
       printWithPrivet: this.destination.isPrivet,
       printWithExtension: this.destination.isExtension,
-      rasterizePDF: false,
+      rasterizePDF: this.getSettingValue('rasterize'),
     };
 
     // Set 'cloudPrintID' only if the this.destination is not local.
