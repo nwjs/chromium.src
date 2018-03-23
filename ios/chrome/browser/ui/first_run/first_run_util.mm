87eece94c86190d1c51df676147a705114ff2cb2
Jérôme Lebel <jlebel@chromium.org>
2018-03-23 16:44:59 +0000

    [iOS] Adding sentinel error histogram
    Adding an histogram to log the error while creating the sentinel file.
    
    Manual cherry-pick from https://chromium-review.googlesource.com/c/chromium/src/+/969261 to branch 3359.
    TBR=pkl@chromium.org,gayane@chromium.org,mark@chromium.org
    
    
    Bug: 822806
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I35606d6a2d497e10e2a0e1d2bc88192f570bb810
    Reviewed-on: https://chromium-review.googlesource.com/978168
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3359@{#398}
    Cr-Branched-From: 66afc5e5d10127546cc4b98b9117aff588b5e66b-refs/heads/master@{#540276}

--- ios/chrome/browser/ui/first_run/first_run_util.mm
+++ ios/chrome/browser/ui/first_run/first_run_util.mm
@@ -6,6 +6,7 @@
 
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/task_scheduler/post_task.h"
@@ -77,28 +78,25 @@ NSString* InsertNewlineBeforeNthToLastWord(NSString* text, int index) {
 }
 
 // Trampoline method for Bind to create the sentinel file.
-bool CreateSentinel() {
-  return FirstRun::CreateSentinel();
+void CreateSentinel() {
+  base::File::Error file_error;
+  FirstRun::SentinelResult sentinel_created =
+      FirstRun::CreateSentinel(&file_error);
+  UMA_HISTOGRAM_ENUMERATION("FirstRun.Sentinel.Created", sentinel_created,
+                            FirstRun::SentinelResult::SENTINEL_RESULT_MAX);
+  if (sentinel_created == FirstRun::SentinelResult::SENTINEL_RESULT_FILE_ERROR)
+    UMA_HISTOGRAM_ENUMERATION("FirstRun.Sentinel.CreatedFileError", -file_error,
+                              -base::File::FILE_ERROR_MAX);
 }
 
-// Helper function for recording first run metrics. Takes an additional
-// |to_record| argument which is the returned value from CreateSentinel().
+// Helper function for recording first run metrics.
 void RecordFirstRunMetricsInternal(ios::ChromeBrowserState* browserState,
                                    bool sign_in_attempted,
-                                   bool has_sso_accounts,
-                                   bool to_record) {
-  // |to_record| is false if the sentinel file was not created which indicates
-  // that the sentinel already exists and metrics were already recorded.
-  // Note: If the user signs in and then signs out during first run, it will be
-  // recorded as a successful sign in.
-  if (!to_record)
-    return;
-
+                                   bool has_sso_accounts) {
+  first_run::SignInStatus sign_in_status;
   bool user_signed_in =
       ios::SigninManagerFactory::GetForBrowserState(browserState)
           ->IsAuthenticated();
-  first_run::SignInStatus sign_in_status;
-
   if (user_signed_in) {
     sign_in_status = has_sso_accounts
                          ? first_run::HAS_SSO_ACCOUNT_SIGNIN_SUCCESSFUL
@@ -148,12 +146,11 @@ void WriteFirstRunSentinelAndRecordMetrics(
     ios::ChromeBrowserState* browserState,
     BOOL sign_in_attempted,
     BOOL has_sso_account) {
-  // Call CreateSentinel() and pass the result into RecordFirstRunMetrics().
-  base::PostTaskWithTraitsAndReplyWithResult(
-      FROM_HERE, {base::MayBlock(), base::TaskPriority::BACKGROUND},
-      base::BindOnce(&CreateSentinel),
-      base::BindOnce(&RecordFirstRunMetricsInternal, browserState,
-                     sign_in_attempted, has_sso_account));
+  base::PostTaskWithTraits(FROM_HERE,
+                           {base::MayBlock(), base::TaskPriority::BACKGROUND},
+                           base::BindOnce(&CreateSentinel));
+  RecordFirstRunMetricsInternal(browserState, sign_in_attempted,
+                                has_sso_account);
 }
 
 void FinishFirstRun(ios::ChromeBrowserState* browserState,
