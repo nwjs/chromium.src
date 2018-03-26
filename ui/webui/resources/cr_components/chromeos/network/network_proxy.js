9fc58b7e579444f970fb018c3d9f77aebcdd4d97
Steven Bennetts <stevenjb@chromium.org>
2018-03-26 17:31:00 +0000

    Proxy config: Correctly set autoconfiguration URL
    This CL:
    * Delays setting Proxy.Type to PAC unless a PAC value is set.
    * Sets proxyModified_ = true otherwise so that the type is not
      overridden on an update.
    * Focus the appropriate input field when the proxy type changes.
    
    TBR=stevenjb@chromium.org
    
    (cherry picked from commit 1986382b3a837039086e2e77effd890062fe0883)
    
    Bug: 822670
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I0e9a68fc8982d2286e144e57dfe14dc61fa84b1b
    Reviewed-on: https://chromium-review.googlesource.com/974119
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#544955}
    Reviewed-on: https://chromium-review.googlesource.com/981078
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3359@{#439}
    Cr-Branched-From: 66afc5e5d10127546cc4b98b9117aff588b5e66b-refs/heads/master@{#540276}

--- ui/webui/resources/cr_components/chromeos/network/network_proxy.js
+++ ui/webui/resources/cr_components/chromeos/network/network_proxy.js
@@ -120,7 +120,7 @@ Polymer({
    * override the edited values.
    * @private {boolean}
    */
-  proxyModified_: false,
+  proxyIsUserModified_: false,
 
   /** @override */
   attached: function() {
@@ -132,14 +132,14 @@ Polymer({
    * is updated correctly.
    */
   reset: function() {
-    this.proxyModified_ = false;
+    this.proxyIsUserModified_ = false;
     this.proxy_ = this.createDefaultProxySettings_();
     this.updateProxy_();
   },
 
   /** @private */
   networkPropertiesChanged_: function() {
-    if (this.proxyModified_)
+    if (this.proxyIsUserModified_)
       return;  // Ignore update.
     this.updateProxy_();
   },
@@ -221,13 +221,13 @@ Polymer({
     // Set this.proxy_ after dom-repeat has been stamped.
     this.async(() => {
       this.proxy_ = proxy;
-      this.proxyModified_ = false;
+      this.proxyIsUserModified_ = false;
     });
   },
 
   /** @private */
   useSameProxyChanged_: function() {
-    this.proxyModified_ = true;
+    this.proxyIsUserModified_ = true;
   },
 
   /**
@@ -283,7 +283,7 @@ Polymer({
         return;
     }
     this.fire('proxy-change', {field: 'ProxySettings', value: proxy});
-    this.proxyModified_ = false;
+    this.proxyIsUserModified_ = false;
   },
 
   /**
@@ -296,10 +296,41 @@ Polymer({
     var type = /** @type {chrome.networkingPrivate.ProxySettingsType} */ (
         target.value);
     this.set('proxy_.Type', type);
-    if (type == CrOnc.ProxySettingsType.MANUAL)
-      this.proxyModified_ = true;
-    else
+    var proxyTypeChangeIsReady;
+    var elementToFocus;
+    switch (type) {
+      case CrOnc.ProxySettingsType.DIRECT:
+      case CrOnc.ProxySettingsType.WPAD:
+        // No addtional values are required, send the type change.
+        proxyTypeChangeIsReady = true;
+        break;
+      case CrOnc.ProxySettingsType.PAC:
+        elementToFocus = this.$$('#pacInput');
+        // If a PAC is already defined, send the type change now, otherwise wait
+        // until the user provides a PAC value.
+        proxyTypeChangeIsReady = !!this.proxy_.PAC;
+        break;
+      case CrOnc.ProxySettingsType.MANUAL:
+        // Manual proxy configuration includes multiple input fields, so wait
+        // until the 'send' button is clicked.
+        proxyTypeChangeIsReady = false;
+        elementToFocus = this.$$('#manualProxy network-proxy-input');
+        break;
+    }
+
+    // If the new proxy type is fully configured, send it, otherwise set
+    // |proxyIsUserModified_| to true so that property updates do not
+    // overwrite user changes.
+    if (proxyTypeChangeIsReady)
       this.sendProxyChange_();
+    else
+      this.proxyIsUserModified_ = true;
+
+    if (elementToFocus) {
+      this.async(() => {
+        elementToFocus.focus();
+      });
+    }
   },
 
   /** @private */
@@ -309,7 +340,7 @@ Polymer({
 
   /** @private */
   onProxyInputChange_: function() {
-    this.proxyModified_ = true;
+    this.proxyIsUserModified_ = true;
   },
 
   /**
@@ -324,7 +355,7 @@ Polymer({
     this.push('proxy_.ExcludeDomains', value);
     // Clear input.
     this.$.proxyExclusion.value = '';
-    this.proxyModified_ = true;
+    this.proxyIsUserModified_ = true;
   },
 
   /**
@@ -333,7 +364,7 @@ Polymer({
    * @private
    */
   onProxyExclusionsChange_: function(event) {
-    this.proxyModified_ = true;
+    this.proxyIsUserModified_ = true;
   },
 
   /** @private */
@@ -406,7 +437,7 @@ Polymer({
    * @private
    */
   isSaveManualProxyEnabled_: function() {
-    if (!this.proxyModified_)
+    if (!this.proxyIsUserModified_)
       return false;
     var manual = this.proxy_.Manual;
     var httpHost = this.get('HTTPProxy.Host', manual);
