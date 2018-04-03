acc0f30fa74a040b656b67450726efb866bbd218
Nick Carter <nick@chromium.org>
2018-04-03 19:47:37 +0000

    Fix test flake in ProcessManagerBrowserTest.NestedURLNavigationsViaProxyBlocked
    Cause of flake was confirmed by adding a slow unload handler to |popup|. This
    fixes it.
    
    Bug: 821596, 822635
    Change-Id: I602c98a0fb37f9951cc85fb9d886616bb22e8b42
    Reviewed-on: https://chromium-review.googlesource.com/967352
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Nick Carter <nick@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#543870}(cherry picked from commit ac31d43d24d2e855e73c588fb9705904276da91e)
    Reviewed-on: https://chromium-review.googlesource.com/990453
    Reviewed-by: Nick Carter <nick@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3359@{#560}
    Cr-Branched-From: 66afc5e5d10127546cc4b98b9117aff588b5e66b-refs/heads/master@{#540276}

--- chrome/browser/extensions/process_manager_browsertest.cc
+++ chrome/browser/extensions/process_manager_browsertest.cc
@@ -1107,6 +1107,8 @@ IN_PROC_BROWSER_TEST_F(ProcessManagerBrowserTest,
     // Setup the test by navigating popup to an extension page. This is allowed
     // because it's web accessible.
     content::WebContents* popup = OpenPopup(main_frame, extension_url);
+    content::RenderFrameDeletedObserver popup_unload_observer(
+        popup->GetMainFrame());
 
     // This frame should now be in an extension process.
     EXPECT_EQ(1u, pm->GetAllFrames().size());
@@ -1120,6 +1122,7 @@ IN_PROC_BROWSER_TEST_F(ProcessManagerBrowserTest,
     EXPECT_TRUE(ExecuteScript(
         tab, "window.popup.location.href = '" + nested_url.spec() + "';"));
     WaitForLoadStop(popup);
+    popup_unload_observer.WaitUntilDeleted();
 
     // Because the navigation was blocked, it should be an empty page.
     EXPECT_NE(nested_url, popup->GetLastCommittedURL());
