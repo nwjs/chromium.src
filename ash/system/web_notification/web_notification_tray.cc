278ee5ec5ef68d11993c955e4e400c41bb6cdd08
Evan Stade <estade@chromium.org>
2018-04-03 19:41:54 +0000

    Message center popups: remove defer timer and related functionality.
    The intended behavior as described in code comments as:
    
    // Denotes a mode when user is clicking the Close button of toasts in a
    // sequence, w/o moving the mouse. We reposition the toasts so the next one
    // happens to be right under the mouse, and the user can just dispose of
    // multipel toasts by clicking. The mode ends when defer_timer_ expires.
    
    In practice, it didn't work if consecutive toasts were not the same
    height, and wasn't necessary if consecutive toasts were the same height.
    Given that it doesn't work, this code adds needless complexity. It is
    also suspected of causing bugs like preventing notification toasts from
    showing.
    
    To test on desktop, you may need to pass
      --disable-features="NativeNotifications"
    
    Bug: 814495,805208
    Change-Id: I197c2a154470068ff6f57e8c91bb374885ca9536
    Reviewed-on: https://chromium-review.googlesource.com/967094
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#545239}(cherry picked from commit 39296e726ce046a4b94719ba131a0714c3ce4e5e)
    Reviewed-on: https://chromium-review.googlesource.com/993614
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3359@{#557}
    Cr-Branched-From: 66afc5e5d10127546cc4b98b9117aff588b5e66b-refs/heads/master@{#540276}

--- ash/system/web_notification/web_notification_tray.cc
+++ ash/system/web_notification/web_notification_tray.cc
@@ -418,7 +418,7 @@ bool WebNotificationTray::ShowPopups() {
   if (IsMessageCenterVisible())
     return false;
 
-  popup_collection_->DoUpdateIfPossible();
+  popup_collection_->DoUpdate();
   return true;
 }
 
