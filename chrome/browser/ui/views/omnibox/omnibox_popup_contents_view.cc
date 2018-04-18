nwjs/chromium.src

commit 1e01ecc2e7298c39cb85c39960c393a9f6cb7347

Omnibox/Views: Remove LocationBarView stroke for touch mode.
This patch removes the 1px outline on LocationBarView and switches to using a
views::FocusRing in touch mode.

See screenshots -
https://drive.google.com/file/d/1g4O_mh4oPb8PJIAb7pjDq0rK32KIUUvP/view?usp=sharing

TBR=patricialor@chromium.org

(cherry picked from commit b167f41ce785881ea141c39ae1cdc39f5145edfc)

Bug: 801583, 829574
Change-Id: I6498ff15d3b1be6a03cab92e9aae8971b54c358e
Reviewed-on: https://chromium-review.googlesource.com/1002716
Commit-Queue: Patti <patricialor@chromium.org>
Reviewed-by: Michael Wasserman <msw@chromium.org>
Reviewed-by: Peter Kasting <pkasting@chromium.org>
Cr-Original-Commit-Position: refs/heads/master@{#550935}
Reviewed-on: https://chromium-review.googlesource.com/1015456
Reviewed-by: Patti <patricialor@chromium.org>
Cr-Commit-Position: refs/branch-heads/3396@{#73}
Cr-Branched-From: 9ef2aa869bc7bc0c089e255d698cca6e47d6b038-refs/heads/master@{#550428}

chrome/browser/ui/views/omnibox/omnibox_popup_contents_view.cc
@@ -56,7 +56,7 @@ base::LazyInstance<gfx::ImageSkia>::DestructorAtExit g_bottom_shadow =
 constexpr int kPopupVerticalPadding = 4;
 
 bool IsNarrow() {
-  return BackgroundWith1PxBorder::IsRounded() ||
+  return LocationBarView::IsRounded() ||
          base::FeatureList::IsEnabled(omnibox::kUIExperimentNarrowDropdown);
 }
 
@@ -176,7 +176,7 @@ class OmniboxPopupContentsView::AutocompletePopupWidget
     params.bounds = bounds;
     params.context = parent_widget->GetNativeWindow();
 
-    if (BackgroundWith1PxBorder::IsRounded())
+    if (LocationBarView::IsRounded())
       RoundedOmniboxResultsFrame::OnBeforeWidgetInit(&params);
     else
       animator_ = std::make_unique<WidgetShrinkAnimation>(this, bounds);
@@ -185,7 +185,7 @@ class OmniboxPopupContentsView::AutocompletePopupWidget
   }
 
   void SetPopupContentsView(OmniboxPopupContentsView* contents) {
-    if (BackgroundWith1PxBorder::IsRounded()) {
+    if (LocationBarView::IsRounded()) {
       SetContentsView(new RoundedOmniboxResultsFrame(
           contents, contents->location_bar_view_));
     } else {
@@ -343,7 +343,7 @@ void OmniboxPopupContentsView::UpdatePopupAppearance() {
     child_at(i)->SetVisible(false);
 
   gfx::Rect new_target_bounds = UpdateMarginsAndGetTargetBounds();
-  if (IsNarrow() && !BackgroundWith1PxBorder::IsRounded()) {
+  if (IsNarrow() && !LocationBarView::IsRounded()) {
     SkColor background_color =
         GetOmniboxColor(OmniboxPart::RESULTS_BACKGROUND, GetTint());
     auto border = std::make_unique<views::BubbleBorder>(
@@ -467,7 +467,7 @@ void OmniboxPopupContentsView::OnGestureEvent(ui::GestureEvent* event) {
 // OmniboxPopupContentsView, private:
 
 gfx::Rect OmniboxPopupContentsView::UpdateMarginsAndGetTargetBounds() {
-  if (BackgroundWith1PxBorder::IsRounded()) {
+  if (LocationBarView::IsRounded()) {
     // The rounded popup is always offset the same amount from the omnibox.
     gfx::Rect content_rect = location_bar_view_->GetBoundsInScreen();
     content_rect.Inset(
@@ -518,9 +518,8 @@ int OmniboxPopupContentsView::CalculatePopupHeight() {
   // amount of space between the text and the popup border as there is in the
   // interior between each row of text.
   int height = popup_height;
-  if (BackgroundWith1PxBorder::IsRounded()) {
-    height += RoundedOmniboxResultsFrame::GetAlignmentInsets(location_bar_view_)
-                  .height();
+  if (LocationBarView::IsRounded()) {
+    height += RoundedOmniboxResultsFrame::GetNonResultSectionHeight();
   } else {
     height += kPopupVerticalPadding * 2 + g_top_shadow.Get().height() +
               g_bottom_shadow.Get().height();
@@ -530,7 +529,7 @@ int OmniboxPopupContentsView::CalculatePopupHeight() {
 
 void OmniboxPopupContentsView::LayoutChildren() {
   gfx::Rect contents_rect = GetContentsBounds();
-  if (!BackgroundWith1PxBorder::IsRounded()) {
+  if (!LocationBarView::IsRounded()) {
     contents_rect.Inset(gfx::Insets(kPopupVerticalPadding, 0));
     contents_rect.Inset(start_margin_, g_top_shadow.Get().height(), end_margin_,
                         0);

chrome/browser/ui/layout_constants.cc
@@ -58,13 +58,7 @@ int GetLayoutConstant(LayoutConstant constant) {
     case LOCATION_BAR_ICON_SIZE:
       return touch_optimized_material ? 20 : 16;
     case LOCATION_BAR_ICON_INTERIOR_PADDING:
-      if (touch_optimized_material) {
-        // TODO(crbug.com/801583): This should actually be 8, but until
-        // LocationBarView is updated to remove its stroke, subtract the dips
-        // reserved for the stroke first.
-        return 7;
-      }
-      return 4;
+      return touch_optimized_material ? 8 : 4;
     case TABSTRIP_NEW_TAB_BUTTON_SPACING: {
       // In non-touch optimized UI, we make the new tab button overlap with the
       // last tab in the tabstrip (i.e negative spacing). However, in

chrome/browser/ui/omnibox/omnibox_theme.cc
@@ -65,6 +65,8 @@ ui::NativeTheme::ColorId GetLegacyColorId(ui::NativeTheme* native_theme,
                  : NativeId::kColorId_LinkEnabled;
     case OmniboxPart::LOCATION_BAR_TEXT_DEFAULT:
       return NativeId::kColorId_TextfieldDefaultColor;
+    case OmniboxPart::LOCATION_BAR_FOCUS_RING:
+      return NativeId::kColorId_FocusedBorderColor;
     case OmniboxPart::RESULTS_BACKGROUND:
       return NormalHoveredSelected(
           state, NativeId::kColorId_ResultsTableNormalBackground,
@@ -229,6 +231,8 @@ SkColor GetOmniboxColor(OmniboxPart part,
       return GetSecurityChipColor(tint, state);
     case OmniboxPart::LOCATION_BAR_SELECTED_KEYWORD:
       return dark ? gfx::kGoogleGrey100 : gfx::kGoogleBlue600;
+    case OmniboxPart::LOCATION_BAR_FOCUS_RING:
+      return dark ? gfx::kGoogleBlueDark600 : gfx::kGoogleBlue600;
     case OmniboxPart::RESULTS_BACKGROUND:
       // The spec calls for transparent black (or white) overlays for hover (6%)
       // and select (8%), which can overlap (for 14%). Pre-blend these with the

chrome/browser/ui/omnibox/omnibox_theme.h
@@ -17,6 +17,7 @@ enum class OmniboxPart {
   LOCATION_BAR_SELECTED_KEYWORD,
   LOCATION_BAR_TEXT_DEFAULT,
   LOCATION_BAR_TEXT_DIMMED,
+  LOCATION_BAR_FOCUS_RING,
 
   RESULTS_BACKGROUND,  // Background of the results dropdown.
   RESULTS_ICON,

chrome/browser/ui/views/find_bar_host.cc
@@ -265,9 +265,8 @@ gfx::Rect FindBarHost::GetDialogPosition(gfx::Rect avoid_overlapping_rect) {
   if (widget_bounds.IsEmpty())
     return gfx::Rect();
 
-  gfx::Insets insets =
-      view()->border()->GetInsets() -
-      gfx::Insets(0, BackgroundWith1PxBorder::kLocationBarBorderThicknessDip);
+  gfx::Insets insets = view()->border()->GetInsets() -
+                       gfx::Insets(0, LocationBarView::GetBorderThicknessDip());
 
   // Ask the view how large an area it needs to draw on.
   gfx::Size prefsize = view()->GetPreferredSize();

chrome/browser/ui/views/harmony/chrome_layout_provider.cc
@@ -122,8 +122,10 @@ bool ChromeLayoutProvider::IsHarmonyMode() const {
 int ChromeLayoutProvider::GetCornerRadiusMetric(
     ChromeEmphasisMetric emphasis_metric,
     const gfx::Rect& bounds) const {
-  // Outside of MD (refresh) mode, just stick to the current fixed value.
-  return emphasis_metric == EMPHASIS_HIGH ? 0 : 4;
+  // Use the current fixed value for non-EMPHASIS_HIGH.
+  return emphasis_metric == EMPHASIS_HIGH
+             ? std::min(bounds.width(), bounds.height()) / 2
+             : 4;
 }
 
 int ChromeLayoutProvider::GetShadowElevationMetric(

chrome/browser/ui/views/location_bar/background_with_1_px_border.cc
@@ -5,6 +5,7 @@
 #include "chrome/browser/ui/views/location_bar/background_with_1_px_border.h"
 
 #include "cc/paint/paint_flags.h"
+#include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "third_party/skia/include/pathops/SkPathOps.h"
 #include "ui/base/material_design/material_design_controller.h"
@@ -17,11 +18,6 @@ BackgroundWith1PxBorder::BackgroundWith1PxBorder(SkColor background,
                                                  SkColor border)
     : border_color_(border) {
   SetNativeControlColor(background);
-}
-
-// static
-bool BackgroundWith1PxBorder::IsRounded() {
-  return ui::MaterialDesignController::IsNewerMaterialUi();
 }
 
 void BackgroundWith1PxBorder::PaintFocusRing(gfx::Canvas* canvas,
@@ -42,7 +38,7 @@ void BackgroundWith1PxBorder::Paint(gfx::Canvas* canvas,
 }
 
 float BackgroundWith1PxBorder::GetBorderRadius(int height_in_px) const {
-  if (IsRounded()) {
+  if (LocationBarView::IsRounded()) {
     // This method returns the inner radius of the border, so subtract 1 pixel
     // off the final border radius since the border thickness is always 1px.
     return height_in_px / 2.f - 1;
@@ -60,13 +56,12 @@ void BackgroundWith1PxBorder::Paint(gfx::Canvas* canvas,
   gfx::RectF border_rect_f(bounds);
   border_rect_f.Scale(scale);
 
-  // Inset by |kLocationBarBorderThicknessDip|, then draw the border along the
-  // outside edge of the result. Make sure the inset amount is a whole number so
-  // the border will still be aligned to the pixel grid. std::floor is chosen
-  // here to ensure the border will be fully contained within the
-  // |kLocationBarBorderThicknessDip| region.
-  border_rect_f.Inset(
-      gfx::InsetsF(std::floor(kLocationBarBorderThicknessDip * scale)));
+  // Inset by |kBorderThicknessDip|, then draw the border along the outside edge
+  // of the result. Make sure the inset amount is a whole number so the border
+  // will still be aligned to the pixel grid. std::floor is chosen here to
+  // ensure the border will be fully contained within the |kBorderThicknessDip|
+  // region.
+  border_rect_f.Inset(gfx::InsetsF(std::floor(kBorderThicknessDip * scale)));
 
   SkRRect inner_rect(SkRRect::MakeRectXY(gfx::RectFToSkRect(border_rect_f),
                                          inner_border_radius,

chrome/browser/ui/views/location_bar/background_with_1_px_border.h
@@ -21,18 +21,16 @@ class View;
 // BackgroundWith1PxBorder renders a solid background color, with a one pixel
 // border with rounded corners. This accounts for the scaling of the canvas, so
 // that the border is one pixel regardless of display scaling.
+// TODO(patricialor): Delete this & replace with CreateRoundRectWith1PxPainter.
 class BackgroundWith1PxBorder : public views::Background {
  public:
-  // The thickness of the location bar's border in DIP.
-  static constexpr int kLocationBarBorderThicknessDip = 1;
+  // The thickness of the border in DIP.
+  static constexpr int kBorderThicknessDip = 1;
 
   // The legacy (non touch/material) border radius.
   static constexpr int kLegacyBorderRadiusPx = 2;
 
   BackgroundWith1PxBorder(SkColor background, SkColor border);
-
-  // Whether the OmniboxBackgroundBorder is a pill shape.
-  static bool IsRounded();
 
   void set_blend_mode(SkBlendMode blend_mode) { blend_mode_ = blend_mode; }
 

commit 0b84ca9df095a29840764a49e45f5f2046c4189e

Omnibox/Views: Fix bad merge caused by 1e01ecc2e7298c39cb85c39960c393a9f6cb7347
TBR=jdonnelly@chromium.org

Change-Id: I93ad93c38cf3618859a9a4366b365dcb692b458d
Reviewed-on: https://chromium-review.googlesource.com/1016213
Reviewed-by: Patti <patricialor@chromium.org>
Cr-Commit-Position: refs/branch-heads/3396@{#75}
Cr-Branched-From: 9ef2aa869bc7bc0c089e255d698cca6e47d6b038-refs/heads/master@{#550428}

chrome/browser/ui/views/omnibox/omnibox_popup_contents_view.cc
@@ -519,7 +519,8 @@ int OmniboxPopupContentsView::CalculatePopupHeight() {
   // interior between each row of text.
   int height = popup_height;
   if (LocationBarView::IsRounded()) {
-    height += RoundedOmniboxResultsFrame::GetNonResultSectionHeight();
+    height += RoundedOmniboxResultsFrame::GetAlignmentInsets(location_bar_view_)
+                  .height();
   } else {
     height += kPopupVerticalPadding * 2 + g_top_shadow.Get().height() +
               g_bottom_shadow.Get().height();