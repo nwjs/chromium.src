33a4d8e116d8cb383fd8599503e3fc7bea734c26
Khushal <khushalsagar@chromium.org>
2018-04-30 17:32:28 +0000

    cc: Cache color conversion results in software raster
    No sense in re-converting the same image over and over.
    Cherry-picked from: https://chromium-review.googlesource.com/c/chromium/src/+/1016063
    
    TBR=ccameron@chromium.org
    
    Bug: 791828
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Idf14cd79607364974301bfe92ec5242db6c79f69
    Reviewed-on: https://chromium-review.googlesource.com/1035523
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3396@{#379}
    Cr-Branched-From: 9ef2aa869bc7bc0c089e255d698cca6e47d6b038-refs/heads/master@{#550428}

--- cc/tiles/software_image_decode_cache.cc
+++ cc/tiles/software_image_decode_cache.cc
@@ -24,6 +24,24 @@ using base::trace_event::MemoryDumpLevelOfDetail;
 namespace cc {
 namespace {
 
+bool UseCacheForDrawImage(const DrawImage& draw_image) {
+  // Lazy generated images are have their decode cached.
+  sk_sp<SkImage> sk_image = draw_image.paint_image().GetSkImage();
+  if (sk_image->isLazyGenerated())
+    return true;
+
+  // Cache images that need to be converted to a non-sRGB color space.
+  // TODO(ccameron): Consider caching when any color conversion is required.
+  // https://crbug.com/791828
+  const gfx::ColorSpace& dst_color_space = draw_image.target_color_space();
+  if (dst_color_space.IsValid() &&
+      dst_color_space != gfx::ColorSpace::CreateSRGB()) {
+    return true;
+  }
+
+  return false;
+}
+
 // The number of entries to keep around in the cache. This limit can be breached
 // if more items are locked. That is, locked items ignore this limit.
 // Depending on the memory state of the system, we limit the amount of items
@@ -206,10 +224,7 @@ SoftwareImageDecodeCache::GetTaskForImageAndRefInternal(
   if (key.target_size().IsEmpty())
     return TaskResult(false);
 
-  // For non-lazy images a decode isn't necessary.
-  // TODO(khushalsagar): If these images require color conversion, we should
-  // still cache that result.
-  if (!image.paint_image().IsLazyGenerated())
+  if (!UseCacheForDrawImage(image))
     return TaskResult(false);
 
   base::AutoLock lock(lock_);
@@ -475,8 +490,8 @@ void SoftwareImageDecodeCache::DecodeImageIfNecessary(
 
 DecodedDrawImage SoftwareImageDecodeCache::GetDecodedImageForDraw(
     const DrawImage& draw_image) {
-  // Non-lazy generated images can be used for raster directly.
-  if (!draw_image.paint_image().GetSkImage()->isLazyGenerated()) {
+  // Non-cached images are be used for raster directly.
+  if (!UseCacheForDrawImage(draw_image)) {
     return DecodedDrawImage(draw_image.paint_image().GetSkImage(),
                             SkSize::Make(0, 0), SkSize::Make(1.f, 1.f),
                             draw_image.filter_quality(),
@@ -523,8 +538,7 @@ DecodedDrawImage SoftwareImageDecodeCache::GetDecodedImageForDrawInternal(
 void SoftwareImageDecodeCache::DrawWithImageFinished(
     const DrawImage& image,
     const DecodedDrawImage& decoded_image) {
-  // We don't cache any data for non-lazy images.
-  if (!image.paint_image().IsLazyGenerated())
+  if (!UseCacheForDrawImage(image))
     return;
 
   TRACE_EVENT1(TRACE_DISABLED_BY_DEFAULT("cc.debug"),
