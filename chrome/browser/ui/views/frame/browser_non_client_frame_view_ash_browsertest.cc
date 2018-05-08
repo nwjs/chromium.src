nwjs/chromium.src

commit 5d480c704994ea82c4f3ca1fd94b1761668c6849

Mash - Support for browser themes in window frame.
1. Move BrowserFrameHeaderAsh to //ash/frame and rename to
   CustomFrameHeader.
2. Use CustomFrameHeader for browser windows (in HeaderView).
   Continue to use DefaultFrameHeader for other windows.
   Possible TODO: share more code between the two FrameHeader classes.
3. Transport frame images over mojo so Mash can apply them
   via CustomFrameHeader.

For now, only the active frame image is propagated (as a POC).
The inactive image, overlay images, and frame colors are TODO.
Ideally, there should be no behavioral changes in classic Ash.

BUG=704942,836992

Change-Id: I15b2d357bd3c8b60b6656ebce910d476ce107b44
Reviewed-on: https://chromium-review.googlesource.com/1043221
Reviewed-by: Tom Sepez <tsepez@chromium.org>
Reviewed-by: Scott Violet <sky@chromium.org>
Commit-Queue: Evan Stade <estade@chromium.org>
Cr-Commit-Position: refs/heads/master@{#556549}

chrome/browser/ui/views/frame/browser_non_client_frame_view_ash_browsertest.cc
@@ -694,8 +694,8 @@ class HostedAppNonClientFrameViewAshTest
 
 // Tests that a web app's theme color is set.
 IN_PROC_BROWSER_TEST_P(HostedAppNonClientFrameViewAshTest, ThemeColor) {
-  EXPECT_EQ(GetThemeColor(), frame_header_->GetActiveFrameColor());
-  EXPECT_EQ(GetThemeColor(), frame_header_->GetInactiveFrameColor());
+  EXPECT_EQ(GetThemeColor(), frame_header_->active_frame_color_for_testing());
+  EXPECT_EQ(GetThemeColor(), frame_header_->inactive_frame_color_for_testing());
   EXPECT_EQ(SK_ColorWHITE, GetActiveIconColor(hosted_app_button_container_));
 }
 

ash/BUILD.gn
@@ -227,6 +227,8 @@ component("ash") {
     "frame/caption_buttons/frame_size_button.cc",
     "frame/caption_buttons/frame_size_button.h",
     "frame/caption_buttons/frame_size_button_delegate.h",
+    "frame/custom_frame_header.cc",
+    "frame/custom_frame_header.h",
     "frame/custom_frame_view_ash.cc",
     "frame/custom_frame_view_ash.h",
     "frame/default_frame_header.cc",

ash/frame/custom_frame_header.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/views/frame/browser_frame_header_ash.h"
+#include "ash/frame/custom_frame_header.h"
 
 #include "ash/ash_layout_constants.h"
 #include "ash/frame/caption_buttons/frame_caption_button.h"
@@ -24,6 +24,8 @@
 #include "ui/views/widget/native_widget_aura.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_delegate.h"
+
+namespace ash {
 
 namespace {
 
@@ -100,47 +102,40 @@ void PaintFrameImagesInRoundRect(gfx::Canvas* canvas,
 }  // namespace
 
 ///////////////////////////////////////////////////////////////////////////////
-// BrowserFrameHeaderAsh, public:
+// CustomFrameHeader, public:
 
-BrowserFrameHeaderAsh::BrowserFrameHeaderAsh() = default;
+CustomFrameHeader::CustomFrameHeader() = default;
 
-BrowserFrameHeaderAsh::~BrowserFrameHeaderAsh() = default;
+CustomFrameHeader::~CustomFrameHeader() = default;
 
-void BrowserFrameHeaderAsh::Init(
+void CustomFrameHeader::Init(
     views::View* view,
     AppearanceProvider* appearance_provider,
     bool incognito,
-    views::View* window_icon,
-    ash::FrameCaptionButtonContainerView* caption_button_container,
-    ash::FrameCaptionButton* back_button) {
+    FrameCaptionButtonContainerView* caption_button_container) {
   DCHECK(view);
   DCHECK(appearance_provider);
-  // window_icon may be null.
   DCHECK(caption_button_container);
-  // back_button may be null.
 
   view_ = view;
   appearance_provider_ = appearance_provider;
   is_incognito_ = incognito;
-  window_icon_ = window_icon;
   caption_button_container_ = caption_button_container;
-  back_button_ = back_button;
 }
 
-int BrowserFrameHeaderAsh::GetMinimumHeaderWidth() const {
+int CustomFrameHeader::GetMinimumHeaderWidth() const {
   // Ensure we have enough space for the window icon and buttons. We allow
   // the title string to collapse to zero width.
   return GetTitleBounds().x() +
          caption_button_container_->GetMinimumSize().width();
 }
 
-void BrowserFrameHeaderAsh::PaintHeader(gfx::Canvas* canvas, Mode mode) {
+void CustomFrameHeader::PaintHeader(gfx::Canvas* canvas, Mode mode) {
   Mode old_mode = mode_;
   mode_ = mode;
 
   if (mode_ != old_mode) {
-    if (!initial_paint_ &&
-        ash::FrameHeaderUtil::CanAnimateActivation(GetWidget())) {
+    if (!initial_paint_ && FrameHeaderUtil::CanAnimateActivation(GetWidget())) {
       activation_animation_.SetSlideDuration(kActivationCrossfadeDurationMs);
       if (mode_ == MODE_ACTIVE)
         activation_animation_.Show();
@@ -164,31 +159,30 @@ void BrowserFrameHeaderAsh::PaintHeader(gfx::Canvas* canvas, Mode mode) {
   }
 }
 
-void BrowserFrameHeaderAsh::LayoutHeader() {
-  // Purposefully set |painted_height_| to an invalid value. We cannot use
-  // |painted_height_| because the computation of |painted_height_| may depend
-  // on having laid out the window controls.
-  painted_height_ = -1;
+void CustomFrameHeader::LayoutHeader() {
   LayoutHeaderInternal();
+  // Default to the header height; owning code may override via
+  // SetHeaderHeightForPainting().
+  painted_height_ = GetHeaderHeight();
 }
 
-int BrowserFrameHeaderAsh::GetHeaderHeight() const {
+int CustomFrameHeader::GetHeaderHeight() const {
   return caption_button_container_->height();
 }
 
-int BrowserFrameHeaderAsh::GetHeaderHeightForPainting() const {
+int CustomFrameHeader::GetHeaderHeightForPainting() const {
   return painted_height_;
 }
 
-void BrowserFrameHeaderAsh::SetHeaderHeightForPainting(int height) {
+void CustomFrameHeader::SetHeaderHeightForPainting(int height) {
   painted_height_ = height;
 }
 
-void BrowserFrameHeaderAsh::SchedulePaintForTitle() {
+void CustomFrameHeader::SchedulePaintForTitle() {
   view_->SchedulePaintInRect(GetTitleBounds());
 }
 
-void BrowserFrameHeaderAsh::SetPaintAsActive(bool paint_as_active) {
+void CustomFrameHeader::SetPaintAsActive(bool paint_as_active) {
   SkColor frame_color =
       appearance_provider_->GetFrameHeaderColor(paint_as_active);
   caption_button_container_->SetPaintAsActive(paint_as_active);
@@ -199,7 +193,7 @@ void BrowserFrameHeaderAsh::SetPaintAsActive(bool paint_as_active) {
   }
 }
 
-void BrowserFrameHeaderAsh::OnShowStateChanged(ui::WindowShowState show_state) {
+void CustomFrameHeader::OnShowStateChanged(ui::WindowShowState show_state) {
   if (show_state == ui::SHOW_STATE_MINIMIZED)
     return;
   // Call LayoutHeaderInternal() instead of LayoutHeader() here because
@@ -207,18 +201,34 @@ void BrowserFrameHeaderAsh::OnShowStateChanged(ui::WindowShowState show_state) {
   LayoutHeaderInternal();
 }
 
-///////////////////////////////////////////////////////////////////////////////
-// gfx::AnimationDelegate overrides:
+void CustomFrameHeader::SetLeftHeaderView(views::View* left_header_view) {
+  window_icon_ = left_header_view;
+}
 
-void BrowserFrameHeaderAsh::AnimationProgressed(
-    const gfx::Animation* animation) {
+void CustomFrameHeader::SetBackButton(FrameCaptionButton* back_button) {
+  back_button_ = back_button;
+}
+
+FrameCaptionButton* CustomFrameHeader::GetBackButton() const {
+  return back_button_;
+}
+
+void CustomFrameHeader::SetFrameColors(SkColor active_frame_color,
+                                       SkColor inactive_frame_color) {
   view_->SchedulePaintInRect(GetPaintedBounds());
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-// BrowserFrameHeaderAsh, private:
+// gfx::AnimationDelegate overrides:
 
-void BrowserFrameHeaderAsh::LayoutHeaderInternal() {
+void CustomFrameHeader::AnimationProgressed(const gfx::Animation* animation) {
+  view_->SchedulePaintInRect(GetPaintedBounds());
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// CustomFrameHeader, private:
+
+void CustomFrameHeader::LayoutHeaderInternal() {
   UpdateCaptionButtons();
   const gfx::Size caption_button_container_size =
       caption_button_container_->GetPreferredSize();
@@ -236,11 +246,11 @@ void BrowserFrameHeaderAsh::LayoutHeaderInternal() {
   // container.
   const gfx::Size icon_size(window_icon_->GetPreferredSize());
   const int icon_offset_y = (GetHeaderHeight() - icon_size.height()) / 2;
-  window_icon_->SetBounds(ash::FrameHeaderUtil::GetLeftViewXInset(),
-                          icon_offset_y, icon_size.width(), icon_size.height());
+  window_icon_->SetBounds(FrameHeaderUtil::GetLeftViewXInset(), icon_offset_y,
+                          icon_size.width(), icon_size.height());
 }
 
-void BrowserFrameHeaderAsh::PaintFrameImages(gfx::Canvas* canvas, bool active) {
+void CustomFrameHeader::PaintFrameImages(gfx::Canvas* canvas, bool active) {
   int alpha = activation_animation_.CurrentValueBetween(0, 0xFF);
   if (!active)
     alpha = 0xFF - alpha;
@@ -257,15 +267,15 @@ void BrowserFrameHeaderAsh::PaintFrameImages(gfx::Canvas* canvas, bool active) {
 
   int corner_radius = 0;
   if (!GetWidget()->IsMaximized() && !GetWidget()->IsFullscreen())
-    corner_radius = ash::FrameHeaderUtil::GetTopCornerRadiusWhenRestored();
+    corner_radius = FrameHeaderUtil::GetTopCornerRadiusWhenRestored();
 
   PaintFrameImagesInRoundRect(canvas, frame_image, frame_overlay_image, alpha,
                               background_color, GetPaintedBounds(),
                               corner_radius,
-                              ash::FrameHeaderUtil::GetThemeBackgroundXInset());
+                              FrameHeaderUtil::GetThemeBackgroundXInset());
 }
 
-void BrowserFrameHeaderAsh::PaintTitleBar(gfx::Canvas* canvas) {
+void CustomFrameHeader::PaintTitleBar(gfx::Canvas* canvas) {
   // The window icon is painted by its own views::View.
   canvas->DrawStringRectWithFlags(
       GetWidget()->widget_delegate()->GetWindowTitle(),
@@ -276,21 +286,19 @@ void BrowserFrameHeaderAsh::PaintTitleBar(gfx::Canvas* canvas) {
       gfx::Canvas::NO_SUBPIXEL_RENDERING);
 }
 
-void BrowserFrameHeaderAsh::UpdateCaptionButtons() {
+void CustomFrameHeader::UpdateCaptionButtons() {
   // When |frame_| minimized, avoid tablet mode toggling to update caption
   // buttons as it would cause mismatch beteen window state and size button.
   if (GetWidget()->IsMinimized())
     return;
-  caption_button_container_->SetButtonImage(ash::CAPTION_BUTTON_ICON_MINIMIZE,
-                                            ash::kWindowControlMinimizeIcon);
-  caption_button_container_->SetButtonImage(ash::CAPTION_BUTTON_ICON_CLOSE,
-                                            ash::kWindowControlCloseIcon);
-  caption_button_container_->SetButtonImage(
-      ash::CAPTION_BUTTON_ICON_LEFT_SNAPPED,
-      ash::kWindowControlLeftSnappedIcon);
-  caption_button_container_->SetButtonImage(
-      ash::CAPTION_BUTTON_ICON_RIGHT_SNAPPED,
-      ash::kWindowControlRightSnappedIcon);
+  caption_button_container_->SetButtonImage(CAPTION_BUTTON_ICON_MINIMIZE,
+                                            kWindowControlMinimizeIcon);
+  caption_button_container_->SetButtonImage(CAPTION_BUTTON_ICON_CLOSE,
+                                            kWindowControlCloseIcon);
+  caption_button_container_->SetButtonImage(CAPTION_BUTTON_ICON_LEFT_SNAPPED,
+                                            kWindowControlLeftSnappedIcon);
+  caption_button_container_->SetButtonImage(CAPTION_BUTTON_ICON_RIGHT_SNAPPED,
+                                            kWindowControlRightSnappedIcon);
 
   const bool is_maximized_or_fullscreen =
       GetWidget()->IsMaximized() || GetWidget()->IsFullscreen();
@@ -298,25 +306,27 @@ void BrowserFrameHeaderAsh::UpdateCaptionButtons() {
       is_maximized_or_fullscreen || appearance_provider_->IsTabletMode()
           ? AshLayoutSize::kBrowserCaptionMaximized
           : AshLayoutSize::kBrowserCaptionRestored;
-  const gfx::VectorIcon* const size_icon =
-      is_maximized_or_fullscreen ? &ash::kWindowControlRestoreIcon
-                                 : &ash::kWindowControlMaximizeIcon;
+  const gfx::VectorIcon* const size_icon = is_maximized_or_fullscreen
+                                               ? &kWindowControlRestoreIcon
+                                               : &kWindowControlMaximizeIcon;
 
   caption_button_container_->SetButtonImage(
-      ash::CAPTION_BUTTON_ICON_MAXIMIZE_RESTORE, *size_icon);
+      CAPTION_BUTTON_ICON_MAXIMIZE_RESTORE, *size_icon);
   caption_button_container_->SetButtonSize(GetAshLayoutSize(button_size_type));
 }
 
-gfx::Rect BrowserFrameHeaderAsh::GetPaintedBounds() const {
+gfx::Rect CustomFrameHeader::GetPaintedBounds() const {
   return gfx::Rect(view_->width(), painted_height_);
 }
 
-gfx::Rect BrowserFrameHeaderAsh::GetTitleBounds() const {
+gfx::Rect CustomFrameHeader::GetTitleBounds() const {
   views::View* left_view = window_icon_ ? window_icon_ : back_button_;
-  return ash::FrameHeaderUtil::GetAvailableTitleBounds(
+  return FrameHeaderUtil::GetAvailableTitleBounds(
       left_view, caption_button_container_, GetHeaderHeight());
 }
 
-views::Widget* BrowserFrameHeaderAsh::GetWidget() {
+views::Widget* CustomFrameHeader::GetWidget() {
   return view_->GetWidget();
 }
+
+}  // namespace ash

ash/frame/custom_frame_header.h
@@ -2,43 +2,47 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_VIEWS_FRAME_BROWSER_FRAME_HEADER_ASH_H_
-#define CHROME_BROWSER_UI_VIEWS_FRAME_BROWSER_FRAME_HEADER_ASH_H_
+#ifndef ASH_FRAME_CUSTOM_FRAME_HEADER_H_
+#define ASH_FRAME_CUSTOM_FRAME_HEADER_H_
 
+#include "ash/ash_export.h"
 #include "ash/frame/frame_header.h"
 #include "base/callback.h"
 #include "base/macros.h"
 #include "ui/gfx/animation/animation_delegate.h"
 #include "ui/gfx/animation/slide_animation.h"
 
-namespace ash {
-class FrameCaptionButton;
-class FrameCaptionButtonContainerView;
-}  // namespace ash
-
 namespace gfx {
 class ImageSkia;
 class Rect;
 }  // namespace gfx
+
 namespace views {
 class View;
 class Widget;
 }  // namespace views
 
-// Helper class for managing the browser window header.
-class BrowserFrameHeaderAsh : public ash::FrameHeader,
-                              public gfx::AnimationDelegate {
+namespace ash {
+
+class FrameCaptionButton;
+class FrameCaptionButtonContainerView;
+
+// Helper class for drawing a custom frame (such as for a themed Chrome Browser
+// frame).
+class ASH_EXPORT CustomFrameHeader : public FrameHeader,
+                                     public gfx::AnimationDelegate {
  public:
   class AppearanceProvider {
    public:
+    virtual ~AppearanceProvider() = default;
     virtual SkColor GetFrameHeaderColor(bool active) = 0;
     virtual gfx::ImageSkia GetFrameHeaderImage(bool active) = 0;
     virtual gfx::ImageSkia GetFrameHeaderOverlayImage(bool active) = 0;
     virtual bool IsTabletMode() = 0;
   };
 
-  BrowserFrameHeaderAsh();
-  ~BrowserFrameHeaderAsh() override;
+  CustomFrameHeader();
+  ~CustomFrameHeader() override;
 
   // BrowserFrameHeaderAsh does not take ownership of any of the parameters.
   // |view| is the view into which |this| will paint. |back_button| can be
@@ -46,11 +50,9 @@ class BrowserFrameHeaderAsh : public ash::FrameHeader,
   void Init(views::View* view,
             AppearanceProvider* appearance_provider,
             bool incognito,
-            views::View* window_icon,
-            ash::FrameCaptionButtonContainerView* caption_button_container,
-            ash::FrameCaptionButton* back_button);
+            FrameCaptionButtonContainerView* caption_button_container);
 
-  // ash::FrameHeader overrides:
+  // FrameHeader overrides:
   int GetMinimumHeaderWidth() const override;
   void PaintHeader(gfx::Canvas* canvas, Mode mode) override;
   void LayoutHeader() override;
@@ -60,6 +62,11 @@ class BrowserFrameHeaderAsh : public ash::FrameHeader,
   void SchedulePaintForTitle() override;
   void SetPaintAsActive(bool paint_as_active) override;
   void OnShowStateChanged(ui::WindowShowState show_state) override;
+  void SetLeftHeaderView(views::View* left_header_view) override;
+  void SetBackButton(FrameCaptionButton* back_button) override;
+  FrameCaptionButton* GetBackButton() const override;
+  void SetFrameColors(SkColor active_frame_color,
+                      SkColor inactive_frame_color) override;
 
  private:
   // gfx::AnimationDelegate override:
@@ -98,8 +105,8 @@ class BrowserFrameHeaderAsh : public ash::FrameHeader,
   bool is_incognito_ = false;
 
   views::View* window_icon_ = nullptr;
-  ash::FrameCaptionButton* back_button_ = nullptr;
-  ash::FrameCaptionButtonContainerView* caption_button_container_ = nullptr;
+  FrameCaptionButton* back_button_ = nullptr;
+  FrameCaptionButtonContainerView* caption_button_container_ = nullptr;
   int painted_height_ = 0;
 
   // Whether the header is painted for the first time.
@@ -110,7 +117,9 @@ class BrowserFrameHeaderAsh : public ash::FrameHeader,
 
   gfx::SlideAnimation activation_animation_{this};
 
-  DISALLOW_COPY_AND_ASSIGN(BrowserFrameHeaderAsh);
+  DISALLOW_COPY_AND_ASSIGN(CustomFrameHeader);
 };
 
-#endif  // CHROME_BROWSER_UI_VIEWS_FRAME_BROWSER_FRAME_HEADER_ASH_H_
+}  // namespace ash
+
+#endif  // ASH_FRAME_CUSTOM_FRAME_HEADER_H_

ash/frame/custom_frame_view_ash.cc
@@ -10,6 +10,7 @@
 
 #include "ash/frame/caption_buttons/frame_caption_button.h"
 #include "ash/frame/caption_buttons/frame_caption_button_container_view.h"
+#include "ash/frame/default_frame_header.h"
 #include "ash/frame/frame_border_hit_test.h"
 #include "ash/frame/header_view.h"
 #include "ash/public/cpp/immersive/immersive_fullscreen_controller.h"
@@ -179,48 +180,6 @@ class CustomFrameViewAshWindowStateDelegate : public wm::WindowStateDelegate,
 bool CustomFrameViewAsh::use_empty_minimum_size_for_test_ = false;
 
 ///////////////////////////////////////////////////////////////////////////////
-// CustomFrameViewAsh::AvatarObserver
-
-// AvatarObserver watches the frame window's avatar icon property and updates
-// HeaderView with it.
-class CustomFrameViewAsh::AvatarObserver : public aura::WindowObserver {
- public:
-  AvatarObserver(views::Widget* frame, HeaderView* header_view)
-      : frame_window_(frame->GetNativeWindow()), header_view_(header_view) {
-    frame_window_->AddObserver(this);
-  }
-
-  ~AvatarObserver() override {
-    if (frame_window_)
-      frame_window_->RemoveObserver(this);
-  }
-
-  // aura::WindowObserver:
-  void OnWindowPropertyChanged(aura::Window* window,
-                               const void* key,
-                               intptr_t old) override {
-    DCHECK_EQ(frame_window_, window);
-    if (key != aura::client::kAvatarIconKey)
-      return;
-
-    gfx::ImageSkia* const avatar_icon =
-        frame_window_->GetProperty(aura::client::kAvatarIconKey);
-    header_view_->SetAvatarIcon(avatar_icon ? *avatar_icon : gfx::ImageSkia());
-  }
-
-  void OnWindowDestroyed(aura::Window* window) override {
-    DCHECK_EQ(frame_window_, window);
-    frame_window_ = nullptr;
-  }
-
- private:
-  aura::Window* frame_window_;
-  HeaderView* const header_view_;
-
-  DISALLOW_COPY_AND_ASSIGN(AvatarObserver);
-};
-
-///////////////////////////////////////////////////////////////////////////////
 // CustomFrameViewAsh::OverlayView
 
 // View which takes up the entire widget and contains the HeaderView. HeaderView
@@ -317,19 +276,18 @@ CustomFrameViewAsh::CustomFrameViewAsh(
       header_view_(new HeaderView(frame, window_style, std::move(model))),
       overlay_view_(new OverlayView(header_view_)),
       immersive_delegate_(immersive_delegate ? immersive_delegate
-                                             : header_view_),
-      avatar_observer_(std::make_unique<AvatarObserver>(frame_, header_view_)) {
+                                             : header_view_) {
   aura::Window* frame_window = frame->GetNativeWindow();
   wm::InstallResizeHandleWindowTargeterForWindow(frame_window, nullptr);
   // |header_view_| is set as the non client view's overlay view so that it can
   // overlay the web contents in immersive fullscreen.
   frame->non_client_view()->SetOverlayView(overlay_view_);
   frame_window->SetProperty(aura::client::kTopViewColor,
-                            header_view_->GetInactiveFrameColor());
+                            DefaultFrameHeader::GetDefaultFrameColor());
   frame_window->SetProperty(ash::kFrameActiveColorKey,
-                            header_view_->GetActiveFrameColor());
+                            DefaultFrameHeader::GetDefaultFrameColor());
   frame_window->SetProperty(ash::kFrameInactiveColorKey,
-                            header_view_->GetInactiveFrameColor());
+                            DefaultFrameHeader::GetDefaultFrameColor());
   frame_window->AddObserver(this);
 
   // A delegate for a more complex way of fullscreening the window may already
@@ -362,7 +320,6 @@ void CustomFrameViewAsh::InitImmersiveFullscreenControllerForView(
 
 void CustomFrameViewAsh::SetFrameColors(SkColor active_frame_color,
                                         SkColor inactive_frame_color) {
-  header_view_->SetFrameColors(active_frame_color, inactive_frame_color);
   aura::Window* frame_window = frame_->GetNativeWindow();
   frame_window->SetProperty(aura::client::kTopViewColor, inactive_frame_color);
   frame_window->SetProperty(ash::kFrameActiveColorKey, active_frame_color);
@@ -428,7 +385,6 @@ void CustomFrameViewAsh::ResetWindowControls() {
 void CustomFrameViewAsh::UpdateWindowIcon() {}
 
 void CustomFrameViewAsh::UpdateWindowTitle() {
-  header_view_->set_title(GetFrameTitle());
   header_view_->SchedulePaintForTitle();
 }
 
@@ -527,19 +483,6 @@ void CustomFrameViewAsh::OnWindowPropertyChanged(aura::Window* window,
   if (key == aura::client::kShowStateKey) {
     header_view_->OnShowStateChanged(
         window->GetProperty(aura::client::kShowStateKey));
-    return;
-  }
-
-  if (key == ash::kFrameActiveColorKey) {
-    header_view_->SetFrameColors(window->GetProperty(ash::kFrameActiveColorKey),
-                                 header_view_->GetInactiveFrameColor());
-    return;
-  }
-
-  if (key == ash::kFrameInactiveColorKey) {
-    header_view_->SetFrameColors(
-        header_view_->GetActiveFrameColor(),
-        window->GetProperty(ash::kFrameInactiveColorKey));
   }
 }
 
@@ -548,11 +491,11 @@ const views::View* CustomFrameViewAsh::GetAvatarIconViewForTest() const {
 }
 
 SkColor CustomFrameViewAsh::GetActiveFrameColorForTest() const {
-  return header_view_->GetActiveFrameColor();
+  return frame_->GetNativeWindow()->GetProperty(ash::kFrameActiveColorKey);
 }
 
 SkColor CustomFrameViewAsh::GetInactiveFrameColorForTest() const {
-  return header_view_->GetInactiveFrameColor();
+  return frame_->GetNativeWindow()->GetProperty(ash::kFrameInactiveColorKey);
 }
 
 void CustomFrameViewAsh::UpdateHeaderView() {

ash/frame/custom_frame_view_ash.h
@@ -144,7 +144,6 @@ class ASH_EXPORT CustomFrameViewAsh : public views::NonClientFrameView,
   void UpdateHeaderView();
 
  private:
-  class AvatarObserver;
   class OverlayView;
   friend class CustomFrameViewAshSizeLock;
   friend class CustomFrameTestWidgetDelegate;
@@ -170,9 +169,6 @@ class ASH_EXPORT CustomFrameViewAsh : public views::NonClientFrameView,
   OverlayView* overlay_view_;
 
   ImmersiveFullscreenControllerDelegate* immersive_delegate_;
-
-  // Observes avatar icon change and updates |header_view_|.
-  std::unique_ptr<AvatarObserver> avatar_observer_;
 
   static bool use_empty_minimum_size_for_test_;
 

commit 22800d9f3d0c979b6edff2727535b180081046c5

Fix BrowserNonClientFrameViewAshTest.ToggleTabletModeOnMinimizedWindow
5d480c704994ea82c4f3ca1fd9 broke this test because it moved the code
that sets the icons from //chrome/ into //ash/. These targets
duplicate //ash/public/vector_icons (when using the component build),
so it's not valid to directly compare icon addresses. Compare by
identifier (name) instead.

Bug: 704942,836992
Change-Id: I47a916a265c976afb61058be569283623d773c10
Reviewed-on: https://chromium-review.googlesource.com/1050379
Commit-Queue: Evan Stade <estade@chromium.org>
Reviewed-by: Scott Violet <sky@chromium.org>
Cr-Commit-Position: refs/heads/master@{#556960}

chrome/browser/ui/views/frame/browser_non_client_frame_view_ash_browsertest.cc
@@ -74,6 +74,7 @@
 #include "ui/events/base_event_utils.h"
 #include "ui/events/event.h"
 #include "ui/gfx/color_palette.h"
+#include "ui/gfx/vector_icon_types.h"
 #include "ui/views/widget/widget.h"
 #include "ui/wm/core/window_util.h"
 
@@ -426,25 +427,30 @@ IN_PROC_BROWSER_TEST_P(BrowserNonClientFrameViewAshTest,
   ash::FrameCaptionButtonContainerView::TestApi test(
       frame_view->caption_button_container_);
   widget->Maximize();
-  // Restore icon for size button in maximized window state.
-  EXPECT_EQ(&ash::kWindowControlRestoreIcon,
-            test.size_button()->icon_definition_for_test());
+
+  // Restore icon for size button in maximized window state. Compare by name
+  // because the address may not be the same for different build targets in the
+  // component build.
+  EXPECT_EQ(*ash::kWindowControlRestoreIcon.name,
+            *test.size_button()->icon_definition_for_test()->name);
   widget->Minimize();
+
   // When entering tablet mode in minimized window state, size button should not
   // get updated.
   TabletModeClient::Get()->OnTabletModeToggled(true);
-  EXPECT_EQ(&ash::kWindowControlRestoreIcon,
-            test.size_button()->icon_definition_for_test());
+  EXPECT_EQ(*ash::kWindowControlRestoreIcon.name,
+            *test.size_button()->icon_definition_for_test()->name);
   // When leaving tablet mode in minimized window state, size button should not
   // get updated.
   TabletModeClient::Get()->OnTabletModeToggled(false);
-  EXPECT_EQ(&ash::kWindowControlRestoreIcon,
-            test.size_button()->icon_definition_for_test());
+  EXPECT_EQ(*ash::kWindowControlRestoreIcon.name,
+            *test.size_button()->icon_definition_for_test()->name);
+
   // When unminimizing in non-tablet mode, size button should match with
   // maximized window state, which is restore icon.
   ::wm::Unminimize(widget->GetNativeWindow());
-  EXPECT_EQ(&ash::kWindowControlRestoreIcon,
-            test.size_button()->icon_definition_for_test());
+  EXPECT_EQ(*ash::kWindowControlRestoreIcon.name,
+            *test.size_button()->icon_definition_for_test()->name);
 }
 
 // This is a regression test that session restore minimized browser should