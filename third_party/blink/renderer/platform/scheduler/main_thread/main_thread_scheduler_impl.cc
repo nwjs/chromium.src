b4ff00451e8f9b887d5d10eb8b5a23764ff1b188
Eric Seckler <eseckler@chromium.org>
2018-05-14 15:57:56 +0000

    scheduler: Collect a task's thread_time into correct UKM field
    thread_time was previously overriding the wall time, but should actually
    be reported in a separate UKM field.
    
    Bug: 842112
    Change-Id: If772b5276c0629a4184006bb5382939d9281d3d7
    Reviewed-on: https://chromium-review.googlesource.com/1054011
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#557860}(cherry picked from commit 2794e1b22391a55780ef2c6d1734ef970fc7acc8)
    Reviewed-on: https://chromium-review.googlesource.com/1057248
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3396@{#586}
    Cr-Branched-From: 9ef2aa869bc7bc0c089e255d698cca6e47d6b038-refs/heads/master@{#550428}

--- third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.cc
@@ -2571,7 +2571,7 @@ void MainThreadSchedulerImpl::RecordTaskUkmImpl(
   builder.SetTaskDuration((end - start).InMicroseconds());
 
   if (thread_time) {
-    builder.SetTaskDuration(thread_time->InMicroseconds());
+    builder.SetTaskCPUDuration(thread_time->InMicroseconds());
   }
 
   builder.Record(ukm_recorder);
