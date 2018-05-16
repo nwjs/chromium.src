souldanger/iridium-browser

net/cert: increase default key length for newly-generated RSA keys
Should we add something for chrome://settings, or just leave
it as hidden as it was before?

net/cert/x509_util.cc
@@ -16,7 +16,7 @@ namespace net {
 namespace x509_util {
 
 // RSA keys created by CreateKeyAndSelfSignedCert will be of this length.
-static const uint16 kRSAKeyLength = 1024;
+static const uint16 kRSAKeyLength = 2048;
 
 // Certificates made by CreateKeyAndSelfSignedCert and
 //  CreateKeyAndChannelIDEC will be signed using this digest algorithm.

nwjs/chromium.src

net/cert/x509_util.cc
@@ -190,7 +190,7 @@ bool GetTLSServerEndPointChannelBinding(const X509Certificate& certificate,
 }
 
 // RSA keys created by CreateKeyAndSelfSignedCert will be of this length.
-static const uint16_t kRSAKeyLength = 1024;
+static const uint16_t kRSAKeyLength = 2048;
 
 // Certificates made by CreateKeyAndSelfSignedCert will be signed using this
 // digest algorithm.