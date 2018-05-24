b17bd28c3ed771d712314f0d6da56ff9a1554719
Nate Fischer <ntfschr@chromium.org>
2018-05-24 23:27:55 +0000

    AW: fix crash from destroying WebView with interstitials
    Previously, destroy()'ing a WebView which had visible interstitials
    would violate a DCHECK (or, crash at a later point if DCHECKs are
    disabled). The interstitial would clean up asynchronously, after the
    corresponding WebContents was destroyed. This violated a DCHECK in
    BrowserViewRenderer's destructor, because it still had a pointer to the
    interstitial's SynchronousCompositorHost.
    
    This modifies RenderWidgetHostViewAndroid to cleanup these references to
    the interstitial compositor synchronously while the WebContents is being
    destroyed. The rest of the interstitial teardown will continue to be
    asynchronous.
    
    Bug: 837829
    Test: run_webview_instrumentation_test_apk -f SafeBrowsingTest#testDestroyWebViewWithInterstitialShowing
    Change-Id: Ie63fcde827aee801b9e7fc1e4b62a43677133a33
    Reviewed-on: https://chromium-review.googlesource.com/1045271
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#560949}(cherry picked from commit cda8254ef3510bc369c64ffb7989d29ab044e987)
    Reviewed-on: https://chromium-review.googlesource.com/1072709
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3396@{#698}
    Cr-Branched-From: 9ef2aa869bc7bc0c089e255d698cca6e47d6b038-refs/heads/master@{#550428}

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -847,6 +847,10 @@ void RenderWidgetHostViewAndroid::ShowDisambiguationPopup(
   tap_disambiguator_->ShowPopup(rect_pixels, zoomed_bitmap);
 }
 
+void RenderWidgetHostViewAndroid::OnInterstitialPageGoingAway() {
+  sync_compositor_.reset();
+}
+
 std::unique_ptr<SyntheticGestureTarget>
 RenderWidgetHostViewAndroid::CreateSyntheticGestureTarget() {
   return std::unique_ptr<SyntheticGestureTarget>(
