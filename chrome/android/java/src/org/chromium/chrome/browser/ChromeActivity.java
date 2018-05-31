a2975779747f7da44959bd21378f6c43995c505b
Ted Choc <tedchoc@google.com>
2018-05-31 22:37:04 +0000

    Fix showing app menu in multi-window when not the focused window.
    In multi-window, the non-focused window will have the state of
    paused even though it is fully visible.  So look for resumed only
    when in non-multi-window, but accept paused when in multi-window.
    
    This differs from MultiWindowUtils.isActivityVisible because that
    only mentions visibility, but I am aiming for visible and not
    occluded.
    
    BUG=845785,844787
    
    Change-Id: Ifcca3784b899a4a19132b3b57efd5227dc2b3e90
    Reviewed-on: https://chromium-review.googlesource.com/1076894
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#562587}(cherry picked from commit cdc84cb76ca57490093fd46c77606bfffc88e32b)
    Reviewed-on: https://chromium-review.googlesource.com/1081076
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3440@{#71}
    Cr-Branched-From: 010ddcfda246975d194964ccf20038ebbdec6084-refs/heads/master@{#561733}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -1438,7 +1438,12 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
     public boolean shouldShowAppMenu() {
         if (isActivityDestroyed()) return false;
 
-        if (ApplicationStatus.getStateForActivity(this) != ActivityState.RESUMED) return false;
+        @ActivityState
+        int state = ApplicationStatus.getStateForActivity(this);
+        boolean inMultiWindow = MultiWindowUtils.getInstance().isInMultiWindowMode(this);
+        if (state != ActivityState.RESUMED && (!inMultiWindow || state != ActivityState.PAUSED)) {
+            return false;
+        }
 
         // Do not show the menu if Contextual Search panel is opened.
         if (mContextualSearchManager != null && mContextualSearchManager.isSearchPanelOpened()) {
