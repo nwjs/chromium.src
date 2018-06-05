7a263ce0a1053cd02e5e18513cf879939aecced6
Roger McFarlane <rogerm@chromium.org>
2018-06-05 13:36:43 +0000

    [autofill] Add UMA counter for times unowned fields are not fillable.
    The CL adds histograms and associated unittests to count the number of
    times autofill is suppressed due to unowned (formless) fields being
    restricted to pages with "checkout" related signals.
    
    Bug: 841784, 806987
    Change-Id: Ied2f55234938793de3c5e7bce248e2cfbe80fb6c
    Reviewed-on: https://chromium-review.googlesource.com/1073568
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Roger McFarlane <rogerm@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#562308}(cherry picked from commit fcbff9947514c47d09446268b43d7f82bedd5378)
    Reviewed-on: https://chromium-review.googlesource.com/1087007
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3440@{#186}
    Cr-Branched-From: 010ddcfda246975d194964ccf20038ebbdec6084-refs/heads/master@{#561733}

--- components/autofill/core/browser/autofill_manager.cc
+++ components/autofill/core/browser/autofill_manager.cc
@@ -1774,8 +1774,9 @@ bool AutofillManager::ParseForm(const FormData& form,
 
   auto form_structure = std::make_unique<FormStructure>(form);
   form_structure->ParseFieldTypesFromAutocompleteAttributes();
-  if (!form_structure->ShouldBeParsed())
+  if (!form_structure->ShouldBeParsed()) {
     return false;
+  }
 
   if (cached_form) {
     // We need to keep the server data if available. We need to use them while
