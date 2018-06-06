19e15eaec660dede1a086c078ce56db6199dd0ad
Xianzhu Wang <wangxianzhu@chromium.org>
2018-06-06 20:59:50 +0000

    [PE] Fix thead under repeating table section
    Now use "const LayoutTableSection* repeating_table_section" instead
    of "bool is_repeating_table_section" so that we can know which table
    section is actually repeating and avoid an inner thead not in repeating
    context (while can repeat) is treated as the repeating section.
    
    We still don't support nested repeating table sections (that is,
    repeating table section under another repeating table section, not
    the cases in fragmentation/nested*.html which are about repeating
    table sections in nested tables under non-repeating table section
    of another table). Filed crbug.com/849459 for it.
    
    TBR=wangxianzhu@chromium.org
    
    (cherry picked from commit 7b3e3e43553aee4495cf47a4867c289966797a2c)
    
    Bug: 849029
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ib0b921a95dc0257cbe11577edef8d1d77071db16
    Reviewed-on: https://chromium-review.googlesource.com/1086285
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#564639}
    Reviewed-on: https://chromium-review.googlesource.com/1089726
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3440@{#219}
    Cr-Branched-From: 010ddcfda246975d194964ccf20038ebbdec6084-refs/heads/master@{#561733}

--- third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
+++ third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
@@ -1400,8 +1400,7 @@ static LayoutRect BoundingBoxInPaginationContainer(
   DCHECK(section.IsRepeatingFooterGroup());
   // Similar to repeating header, expand bounding_box to intersect any
   // fragment containing any row first.
-  const auto* top_section = table.TopNonEmptySection();
-  if (top_section) {
+  if (const auto* top_section = table.TopNonEmptySection()) {
     bounding_box.Unite(MapLocalRectToAncestorLayer(*top_section,
                                                    top_section->BorderBoxRect(),
                                                    enclosing_pagination_layer));
@@ -1410,9 +1409,7 @@ static LayoutRect BoundingBoxInPaginationContainer(
     // total height of the first row and repeating footers from the top of
     // bounding_box to exclude the first fragment without enough space.
     auto top_exclusion = table.RowOffsetFromRepeatingFooter();
-    if (const auto* top_section = table.TopNonEmptySection()) {
-      // Otherwise the footer should not repeating.
-      DCHECK(top_section != section);
+    if (top_section != section) {
       top_exclusion +=
           top_section->FirstRow()->LogicalHeight() + table.VBorderSpacing();
     }
@@ -1817,10 +1814,10 @@ void PaintPropertyTreeBuilder::UpdateCompositedLayerPaginationOffset() {
 
 void PaintPropertyTreeBuilder::
     UpdateRepeatingTableSectionPaintOffsetAdjustment() {
-  if (!context_.is_repeating_table_section)
+  if (!context_.repeating_table_section)
     return;
 
-  if (object_.IsTableSection()) {
+  if (object_ == context_.repeating_table_section) {
     if (ToLayoutTableSection(object_).IsRepeatingHeaderGroup())
       UpdateRepeatingTableHeaderPaintOffsetAdjustment();
     else if (ToLayoutTableSection(object_).IsRepeatingFooterGroup())
@@ -2145,7 +2142,7 @@ void PaintPropertyTreeBuilder::CreateFragmentContextsInFlowThread(
   const auto& flow_thread =
       ToLayoutFlowThread(enclosing_pagination_layer->GetLayoutObject());
   LayoutRect object_bounding_box_in_flow_thread;
-  if (context_.is_repeating_table_section) {
+  if (context_.repeating_table_section) {
     // The object is a descendant of a repeating object. It should use the
     // repeating bounding box to repeat in the same fragments as its
     // repeating ancestor.
@@ -2155,7 +2152,7 @@ void PaintPropertyTreeBuilder::CreateFragmentContextsInFlowThread(
     object_bounding_box_in_flow_thread =
         BoundingBoxInPaginationContainer(object_, *enclosing_pagination_layer);
     if (IsRepeatingTableSection(object_)) {
-      context_.is_repeating_table_section = true;
+      context_.repeating_table_section = &ToLayoutTableSection(object_);
       context_.repeating_table_section_bounding_box =
           object_bounding_box_in_flow_thread;
     }
@@ -2328,7 +2325,7 @@ void PaintPropertyTreeBuilder::
 
 bool PaintPropertyTreeBuilder::IsRepeatingInPagedMedia() const {
   return context_.is_repeating_fixed_position ||
-         (context_.is_repeating_table_section &&
+         (context_.repeating_table_section &&
           !context_.painting_layer->EnclosingPaginationLayer());
 }
 
@@ -2370,7 +2367,7 @@ bool PaintPropertyTreeBuilder::UpdateFragments() {
     context_.is_repeating_fixed_position = true;
     CreateFragmentContextsForRepeatingFixedPosition();
   } else if (ObjectIsRepeatingTableSectionInPagedMedia()) {
-    context_.is_repeating_table_section = true;
+    context_.repeating_table_section = &ToLayoutTableSection(object_);
     CreateFragmentContextsForRepeatingTableSectionInPagedMedia();
   }
 
@@ -2382,7 +2379,7 @@ bool PaintPropertyTreeBuilder::UpdateFragments() {
     InitSingleFragmentFromParent(needs_paint_properties);
     UpdateCompositedLayerPaginationOffset();
     context_.is_repeating_fixed_position = false;
-    context_.is_repeating_table_section = false;
+    context_.repeating_table_section = nullptr;
   }
 
   if (object_.IsSVGHiddenContainer()) {
@@ -2415,7 +2412,7 @@ bool PaintPropertyTreeBuilder::UpdateFragments() {
 bool PaintPropertyTreeBuilder::ObjectTypeMightNeedPaintProperties() const {
   return object_.IsBoxModelObject() || object_.IsSVG() ||
          context_.painting_layer->EnclosingPaginationLayer() ||
-         context_.is_repeating_table_section ||
+         context_.repeating_table_section ||
          context_.is_repeating_fixed_position;
 }
 
