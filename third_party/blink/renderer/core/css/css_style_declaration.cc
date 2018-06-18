73dbd68c995bf6c7e7584768aa1ccb7d1cab68e5
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2018-06-18 21:25:58 +0000

    CSSStyleDeclaration: Stop calling ScriptState::ContextIsValid() in the setter
    When the custom bindings were removed in commit 5b84e88a0 ("Remove custom
    bindings for CSSStyleDeclaration"), the new anonymous setter code added a
    check for ScriptState::ContextIsValid() to ensure it had received a valid
    |script_state|.
    
    It turns out this check is too strict, as in addition to verifying a
    ScriptState has an associated v8::Context it also asserts the ScriptState
    has a valid |per_context_data_|.
    
    This is valid in most cases, but when a node is moved across different
    documents and its previous document gets removed its |per_context_data_| is
    disposed of and ScriptState::ContextIsValid() fails.
    
    Since the anonymous setter is only invoked by the bindings layer, we can
    assume it is passed a ScriptState that is in a minimally usable state, so it
    is possible to relax the ContextIsValid() check and only make sure we can
    get a valid ExecutionContext from the ScriptState.
    
    Bug: 852190
    Change-Id: I307de0e003e5258bf1f670b26f1dc86e4dae9286
    Reviewed-on: https://chromium-review.googlesource.com/1100836
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#567310}(cherry picked from commit b179b0b880afa692b4ed938ca7c232259dbccb0c)
    Reviewed-on: https://chromium-review.googlesource.com/1105257
    Reviewed-by: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/branch-heads/3440@{#419}
    Cr-Branched-From: 010ddcfda246975d194964ccf20038ebbdec6084-refs/heads/master@{#561733}

--- third_party/blink/renderer/core/css/css_style_declaration.cc
+++ third_party/blink/renderer/core/css/css_style_declaration.cc
@@ -173,9 +173,10 @@ void CSSStyleDeclaration::AnonymousNamedGetter(const AtomicString& name,
 bool CSSStyleDeclaration::AnonymousNamedSetter(ScriptState* script_state,
                                                const AtomicString& name,
                                                const String& value) {
-  if (!script_state->ContextIsValid())
+  const ExecutionContext* execution_context =
+      ExecutionContext::From(script_state);
+  if (!execution_context)
     return false;
-
   CSSPropertyID unresolved_property = CssPropertyInfo(name);
   if (!unresolved_property)
     return false;
@@ -188,10 +189,9 @@ bool CSSStyleDeclaration::AnonymousNamedSetter(ScriptState* script_state,
       "CSSStyleDeclaration",
       CSSProperty::Get(resolveCSSPropertyID(unresolved_property))
           .GetPropertyName());
-  SetPropertyInternal(
-      unresolved_property, String(), value, false,
-      ExecutionContext::From(script_state)->GetSecureContextMode(),
-      exception_state);
+  SetPropertyInternal(unresolved_property, String(), value, false,
+                      execution_context->GetSecureContextMode(),
+                      exception_state);
   if (exception_state.HadException())
     return false;
   return true;
