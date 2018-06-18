e879a55985abe17015669cf358e0237d4c2cdaf4
Lukasz Anforowicz <lukasza@chromium.org>
2018-06-18 20:49:59 +0000

    Avoid spare (already initialized!) RenderProcessHost for tab discarding.
    This CL makes sure that the spare RenderProcessHost (which is already
    initialized and backed by an actual OS process) is not used in cases
    where WebContents shouldn't (yet) be associated with an OS process.
    Two most prominent examples of such scenario are: tab discarding
    and session restore.
    
    After this CL, the callers of WebContents::Create can specify 3 desired
    states of the renderer process associated with the WebContents being
    created: kNoRendererProcess, kOkayToHaveRendererProcess and
    kEnsureRenderViewInitialization.  The distinction between the last 2
    desired states is needed to accomodate WebContentsImpl::CreateNewWindow
    which needs to copy session storage state *before* the RenderView gets
    initialized.
    
    After this CL, tab discarding and session restore explicitly specify
    kNoRendererProcess (this CL also adds a regression test to ensure
    that WebContents associated with a discarded tab is not backed by an
    actual OS process).  Presence of kNoRendererProcess is recorded in a
    SiteInstanceImpl in a generic (i.e. non-spare-RPH-related) field
    and also enforced by a DCHECK in SiteInstanceImpl::GetProcess.
    
    TBR=lukasza@chromium.org
    
    (cherry picked from commit 3caa8372ab43568b1104dba3c6edff9654a4a5e9)
    
    Bug: 840409
    Change-Id: Id277343e9042195214e06bcf0b9b88412d6d6672
    Reviewed-on: https://chromium-review.googlesource.com/1080091
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#564554}
    Reviewed-on: https://chromium-review.googlesource.com/1105129
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3440@{#416}
    Cr-Branched-From: 010ddcfda246975d194964ccf20038ebbdec6084-refs/heads/master@{#561733}

--- content/browser/renderer_host/render_process_host_impl.h
+++ content/browser/renderer_host/render_process_host_impl.h
@@ -304,7 +304,8 @@ class CONTENT_EXPORT RenderProcessHostImpl
     kMismatchedStoragePartition = 2,
     kRefusedByEmbedder = 3,
     kSpareTaken = 4,
-    kMaxValue = kSpareTaken
+    kRefusedBySiteInstance = 5,
+    kMaxValue = kRefusedBySiteInstance
   };
 
   static base::MessageLoop* GetInProcessRendererThreadForTesting();
