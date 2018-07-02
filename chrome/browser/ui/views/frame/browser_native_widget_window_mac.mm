171bc7ca569bd2ce8d74ac02f3eeeb5e2eeb24d3
Sidney San Martín <sdy@chromium.org>
2018-07-02 19:40:44 +0000

    Fix a crash on early releases of macOS 10.10.
    So far, all instances of this missing selector crash linked in the bug have
    come from 10.10.0, but my copy of 10.10 has it. This change checks
    `respondsToSelector:`, instead of OS version, before calling SPI.
    
    Bug: 856211
    Change-Id: Ib2aa07e0e24b83f1766a2f6e16e19b2b123970a6
    Reviewed-on: https://chromium-review.googlesource.com/1115781
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Sidney San Martín <sdy@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#571123}(cherry picked from commit f44f7dc221536b3d72c76322f17dc04fc04d892d)
    Reviewed-on: https://chromium-review.googlesource.com/1123176
    Reviewed-by: Sidney San Martín <sdy@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3440@{#581}
    Cr-Branched-From: 010ddcfda246975d194964ccf20038ebbdec6084-refs/heads/master@{#561733}

--- chrome/browser/ui/views/frame/browser_native_widget_window_mac.mm
+++ chrome/browser/ui/views/frame/browser_native_widget_window_mac.mm
@@ -14,8 +14,10 @@ constexpr NSInteger kTitleBarHeight = 37;
 
 @interface NSWindow (PrivateAPI)
 + (Class)frameViewClassForStyleMask:(NSUInteger)windowStyle;
-- (void)beginWindowDragWithEvent:(NSEvent*)event
-    NS_DEPRECATED_MAC(10_10, 10_11, "Use performWindowDragWithEvent: instead.");
+
+// Available in later point releases of 10.10. On 10.11+, use the public
+// -performWindowDragWithEvent: instead.
+- (void)beginWindowDragWithEvent:(NSEvent*)event;
 @end
 
 // Weak lets Chrome launch even if a future macOS doesn't have NSThemeFrame.
@@ -76,7 +78,8 @@ WEAK_IMPORT_ATTRIBUTE
     ;  // Not needed on 10.12 and up.
   else if (@available(macOS 10.11, *))
     [self.window performWindowDragWithEvent:event];
-  else if (@available(macOS 10.10, *))
+  else if ([self.window
+               respondsToSelector:@selector(beginWindowDragWithEvent:)])
     [self.window beginWindowDragWithEvent:event];
   else
     NOTREACHED();
