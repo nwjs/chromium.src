nwjs/chromium.src

commit c53515cd2908ecee17a8fa334d30a5029d5ce214

[Autofill Views] Reimplement Scrolling
This CL adds the missing scroll functionality to the Views Autofill
implementation. Previously, dropdowns that were too tall for their
window would extend beyond its boundaries (Win + Mac) or even be cut
off entirely (Linux). The major changes include:
- Changes to PopupViewCommon to use window bounds, rather than display
  bounds
- The creation of a mock version of PVC for contexts where we cannot
  safely get the needed window object
- Exposing the x and y portions of the calculation logic in PVC
- Changes to the dropdown, including the addition of a scroll view,
  and new logic which determines the needed size. The new size logic
  first uses the new PVC function to determine the height, then adjusts
  for the scroll bar if present, then applies existing rules (rounding,
  minimum, etc.), and finally uses the other new PVC function to get
  the final width.

Change-Id: I4434fa045bfe010d51d582272fbc011b813d3ab0
Reviewed-on: https://chromium-review.googlesource.com/1072427
Commit-Queue: Tommy Martino <tmartino@chromium.org>
Reviewed-by: Evan Stade <estade@chromium.org>
Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
Cr-Commit-Position: refs/heads/master@{#568145}

chrome/browser/ui/views/autofill/autofill_popup_view_native_views.cc
@@ -30,6 +30,7 @@
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/menu/menu_config.h"
+#include "ui/views/controls/scroll_view.h"
 #include "ui/views/controls/separator.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/fill_layout.h"
@@ -379,9 +380,9 @@ AutofillPopupViewNativeViews::AutofillPopupViewNativeViews(
     views::Widget* parent_widget)
     : AutofillPopupBaseView(controller, parent_widget),
       controller_(controller) {
-  views::BoxLayout* layout = SetLayoutManager(
+  layout_ = SetLayoutManager(
       std::make_unique<views::BoxLayout>(views::BoxLayout::kVertical));
-  layout->set_main_axis_alignment(views::BoxLayout::MAIN_AXIS_ALIGNMENT_START);
+  layout_->set_main_axis_alignment(views::BoxLayout::MAIN_AXIS_ALIGNMENT_START);
 
   CreateChildViews();
   SetBackground(views::CreateSolidBackground(kAutofillBackgroundColor));
@@ -398,31 +399,6 @@ void AutofillPopupViewNativeViews::Hide() {
   controller_ = nullptr;
 
   DoHide();
-}
-
-gfx::Size AutofillPopupViewNativeViews::CalculatePreferredSize() const {
-  // The border of the input element should be aligned with the border of the
-  // dropdown when suggestions are not too wide.
-  int contents_width =
-      gfx::ToEnclosingRect(controller_->element_bounds()).width();
-
-  // Allow the dropdown to grow beyond the element width if it requires more
-  // horizontal space to render the suggestions.
-  gfx::Size size = AutofillPopupBaseView::CalculatePreferredSize();
-  if (contents_width < size.width()) {
-    contents_width = size.width();
-    // Use multiples of |kDropdownWidthMultiple| if the required width is larger
-    // than the element width.
-    if (contents_width % kDropdownWidthMultiple) {
-      contents_width +=
-          kDropdownWidthMultiple - (contents_width % kDropdownWidthMultiple);
-    }
-  }
-
-  // Notwithstanding all the above rules, enforce a hard minimum so the dropdown
-  // is not too small to interact with.
-  size.set_width(std::max(kDropdownMinWidth, contents_width));
-  return size;
 }
 
 void AutofillPopupViewNativeViews::VisibilityChanged(View* starting_from,
@@ -507,7 +483,12 @@ void AutofillPopupViewNativeViews::CreateChildViews() {
     line_number++;
   }
 
-  AddChildView(body_container);
+  scroll_view_ = new views::ScrollView();
+  scroll_view_->set_hide_horizontal_scrollbar(true);
+  scroll_view_->SetContents(body_container);
+  AddChildView(scroll_view_);
+  layout_->SetFlexForView(scroll_view_, 1);
+  scroll_view_->ClipHeightTo(0, body_container->GetPreferredSize().height());
 
   // All the remaining rows (where index >= |line_number|) are part of the
   // footer. This needs to be in its own container because it should not be
@@ -531,7 +512,32 @@ void AutofillPopupViewNativeViews::CreateChildViews() {
     }
 
     AddChildView(footer_container);
+    layout_->SetFlexForView(footer_container, 0);
   }
+}
+
+int AutofillPopupViewNativeViews::AdjustWidth(int width) const {
+  // The border of the input element should be aligned with the border of the
+  // dropdown when suggestions are not too wide.
+  int adjusted_width =
+      gfx::ToEnclosingRect(controller_->element_bounds()).width();
+
+  // Allow the dropdown to grow beyond the element width if it requires more
+  // horizontal space to render the suggestions.
+  if (adjusted_width < width) {
+    adjusted_width = width;
+    // Use multiples of |kDropdownWidthMultiple| if the required width is larger
+    // than the element width.
+    if (adjusted_width % kDropdownWidthMultiple) {
+      adjusted_width +=
+          kDropdownWidthMultiple - (adjusted_width % kDropdownWidthMultiple);
+    }
+  }
+
+  // Notwithstanding all the above rules, enforce a hard minimum so the dropdown
+  // is not too small to interact with.
+  adjusted_width = std::max(kDropdownMinWidth, adjusted_width);
+  return adjusted_width;
 }
 
 void AutofillPopupViewNativeViews::AddExtraInitParams(
@@ -564,16 +570,33 @@ std::unique_ptr<views::Border> AutofillPopupViewNativeViews::CreateBorder() {
 }
 
 void AutofillPopupViewNativeViews::DoUpdateBoundsAndRedrawPopup() {
-  SizeToPreferredSize();
+  gfx::Size size = CalculatePreferredSize();
+  gfx::Rect popup_bounds;
 
-  // When a bubble border is shown, the contents area (inside the shadow) is
-  // supposed to be aligned with input element boundaries.
-  gfx::Rect popup_bounds = PopupViewCommon().CalculatePopupBounds(
-      size().width(), size().height(),
-      gfx::ToEnclosingRect(controller_->element_bounds()),
-      controller_->container_view(), controller_->IsRTL());
+  PopupViewCommon().CalculatePopupVerticalBounds(
+      size.height(), gfx::ToEnclosingRect(controller_->element_bounds()),
+      controller_->container_view(), &popup_bounds);
 
-  // Expand the widget bounds to include the border.
+  // Adjust the width to compensate for a scroll bar, if necessary, and for
+  // other rules.
+  int scroll_width = 0;
+  if (size.height() > popup_bounds.height()) {
+    size.set_height(popup_bounds.height());
+
+    // Because the preferred size is greater than the bounds available, the
+    // contents will have to scroll. The scroll bar will steal width from the
+    // content and smoosh everything together. Instead, add to the width to
+    // compensate.
+    scroll_width = scroll_view_->GetScrollBarLayoutWidth();
+  }
+  size.set_width(AdjustWidth(size.width() + scroll_width));
+
+  PopupViewCommon().CalculatePopupHorizontalBounds(
+      size.width(), gfx::ToEnclosingRect(controller_->element_bounds()),
+      controller_->container_view(), controller_->IsRTL(), &popup_bounds);
+
+  SetSize(size);
+
   popup_bounds.Inset(-bubble_border_->GetInsets());
 
   GetWidget()->SetBounds(popup_bounds);

chrome/browser/ui/autofill/autofill_popup_controller_impl.cc
@@ -64,10 +64,8 @@ AutofillPopupControllerImpl::AutofillPopupControllerImpl(
     const gfx::RectF& element_bounds,
     base::i18n::TextDirection text_direction)
     : controller_common_(element_bounds, text_direction, container_view),
-      view_(NULL),
       layout_model_(this, delegate->GetPopupType() == PopupType::kCreditCards),
-      delegate_(delegate),
-      weak_ptr_factory_(this) {
+      delegate_(delegate) {
   ClearState();
   delegate->RegisterDeletionCallback(base::BindOnce(
       &AutofillPopupControllerImpl::HideViewAndDie, GetWeakPtr()));

chrome/browser/ui/autofill/autofill_popup_controller_impl.h
@@ -134,9 +134,7 @@ class AutofillPopupControllerImpl : public AutofillPopupController {
 
   base::WeakPtr<AutofillPopupControllerImpl> GetWeakPtr();
 
-  // Contains common popup functionality such as popup layout. Protected for
-  // testing.
-  PopupControllerCommon controller_common_;
+  AutofillPopupLayoutModel& LayoutModelForTesting() { return layout_model_; }
 
  private:
 #if !defined(OS_ANDROID)
@@ -157,7 +155,8 @@ class AutofillPopupControllerImpl : public AutofillPopupController {
   friend class AutofillPopupControllerUnitTest;
   void SetViewForTesting(AutofillPopupView* view) { view_ = view; }
 
-  AutofillPopupView* view_;  // Weak reference.
+  PopupControllerCommon controller_common_;
+  AutofillPopupView* view_ = nullptr;  // Weak reference.
   AutofillPopupLayoutModel layout_model_;
   base::WeakPtr<AutofillPopupDelegate> delegate_;
 
@@ -180,7 +179,7 @@ class AutofillPopupControllerImpl : public AutofillPopupController {
   // is drawn by Cocoa on macOS.
   gfx::Typesetter typesetter_ = gfx::Typesetter::HARFBUZZ;
 
-  base::WeakPtrFactory<AutofillPopupControllerImpl> weak_ptr_factory_;
+  base::WeakPtrFactory<AutofillPopupControllerImpl> weak_ptr_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(AutofillPopupControllerImpl);
 };

chrome/browser/ui/autofill/autofill_popup_controller_unittest.cc
@@ -13,6 +13,8 @@
 #include "build/build_config.h"
 #include "chrome/browser/ui/autofill/autofill_popup_controller_impl.h"
 #include "chrome/browser/ui/autofill/autofill_popup_view.h"
+#include "chrome/browser/ui/autofill/popup_view_common.h"
+#include "chrome/browser/ui/autofill/popup_view_test_helpers.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/autofill/content/browser/content_autofill_driver.h"
@@ -96,7 +98,10 @@ class TestAutofillPopupController : public AutofillPopupControllerImpl {
                                     NULL,
                                     NULL,
                                     element_bounds,
-                                    base::i18n::UNKNOWN_DIRECTION) {}
+                                    base::i18n::UNKNOWN_DIRECTION) {
+    LayoutModelForTesting().SetUpForTesting(
+        std::make_unique<MockPopupViewCommonForUnitTesting>());
+  }
   ~TestAutofillPopupController() override {}
 
   // Making protected functions public for testing

chrome/browser/ui/autofill/autofill_popup_layout_model.cc
@@ -95,6 +95,7 @@ AutofillPopupLayoutModel::AutofillPopupLayoutModel(
   smaller_font_list_ =
       normal_font_list_.DeriveWithSizeDelta(kSmallerFontSizeDelta);
   bold_font_list_ = normal_font_list_.DeriveWithWeight(gfx::Font::Weight::BOLD);
+  view_common_ = std::make_unique<PopupViewCommon>();
 #endif
 }
 
@@ -152,7 +153,7 @@ void AutofillPopupLayoutModel::UpdatePopupBounds() {
   int popup_width = GetDesiredPopupWidth();
   int popup_height = GetDesiredPopupHeight();
 
-  popup_bounds_ = view_common_.CalculatePopupBounds(
+  popup_bounds_ = view_common_->CalculatePopupBounds(
       popup_width, popup_height, RoundedElementBounds(),
       delegate_->container_view(), delegate_->IsRTL());
 }
@@ -282,6 +283,11 @@ int AutofillPopupLayoutModel::GetIconAccessibleNameResourceId(
       return kDataResources[i].accessible_string_id;
   }
   return kResourceNotFoundId;
+}
+
+void AutofillPopupLayoutModel::SetUpForTesting(
+    std::unique_ptr<PopupViewCommon> view_common) {
+  view_common_ = std::move(view_common);
 }
 
 const gfx::Rect AutofillPopupLayoutModel::RoundedElementBounds() const {

chrome/browser/ui/autofill/autofill_popup_layout_model.h
@@ -99,6 +99,10 @@ class AutofillPopupLayoutModel {
 
   bool is_credit_card_popup() const { return is_credit_card_popup_; }
 
+  // Allows the provision of another implementation of view_common, for use in
+  // unit tests where using the real thing could cause crashes.
+  void SetUpForTesting(std::unique_ptr<PopupViewCommon> view_common);
+
  private:
   // Returns the enclosing rectangle for the element_bounds.
   const gfx::Rect RoundedElementBounds() const;
@@ -116,7 +120,7 @@ class AutofillPopupLayoutModel {
   // The bounds of the Autofill popup.
   gfx::Rect popup_bounds_;
 
-  PopupViewCommon view_common_;
+  std::unique_ptr<PopupViewCommon> view_common_;
 
   AutofillPopupViewDelegate* delegate_;  // Weak reference.
 

chrome/browser/ui/autofill/popup_view_common.cc
@@ -7,11 +7,17 @@
 #include <algorithm>
 #include <utility>
 
-#include "ui/display/display.h"
-#include "ui/display/screen.h"
-#include "ui/gfx/geometry/point.h"
-#include "ui/gfx/geometry/rect.h"
+#include "build/build_config.h"
 #include "ui/gfx/geometry/vector2d.h"
+
+#if defined(OS_ANDROID)
+#include "ui/android/view_android.h"
+#include "ui/android/window_android.h"
+#else  // defined(OS_ANDROID)
+#include "chrome/browser/platform_util.h"
+#include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/browser_window.h"
+#endif  // !defined(OS_ANDROID)
 
 namespace autofill {
 
@@ -21,26 +27,23 @@ namespace {
 // the starting point and the width of the popup, taking into account the
 // direction it's supposed to grow (either to the left or to the right).
 // Components |y| and |height| of |popup_bounds| are not changed.
-void CalculatePopupXAndWidth(const display::Display& left_display,
-                             const display::Display& right_display,
+void CalculatePopupXAndWidth(int leftmost_available_x,
+                             int rightmost_available_x,
                              int popup_required_width,
                              const gfx::Rect& element_bounds,
                              bool is_rtl,
                              gfx::Rect* popup_bounds) {
-  int leftmost_display_x = left_display.bounds().x();
-  int rightmost_display_x =
-      right_display.GetSizeInPixel().width() + right_display.bounds().x();
-
   // Calculate the start coordinates for the popup if it is growing right or
   // the end position if it is growing to the left, capped to screen space.
-  int right_growth_start = std::max(
-      leftmost_display_x, std::min(rightmost_display_x, element_bounds.x()));
+  int right_growth_start =
+      std::max(leftmost_available_x,
+               std::min(rightmost_available_x, element_bounds.x()));
   int left_growth_end =
-      std::max(leftmost_display_x,
-               std::min(rightmost_display_x, element_bounds.right()));
+      std::max(leftmost_available_x,
+               std::min(rightmost_available_x, element_bounds.right()));
 
-  int right_available = rightmost_display_x - right_growth_start;
-  int left_available = left_growth_end - leftmost_display_x;
+  int right_available = rightmost_available_x - right_growth_start;
+  int left_available = left_growth_end - leftmost_available_x;
 
   int popup_width =
       std::min(popup_required_width, std::max(right_available, left_available));
@@ -66,77 +69,92 @@ void CalculatePopupXAndWidth(const display::Display& left_display,
 // the starting point and the height of the popup, taking into account the
 // direction it's supposed to grow (either up or down). Components |x| and
 // |width| of |popup_bounds| are not changed.
-void CalculatePopupYAndHeight(const display::Display& top_display,
-                              const display::Display& bottom_display,
+void CalculatePopupYAndHeight(int topmost_available_y,
+                              int bottommost_available_y,
                               int popup_required_height,
                               const gfx::Rect& element_bounds,
                               gfx::Rect* popup_bounds) {
-  int topmost_display_y = top_display.bounds().y();
-  int bottommost_display_y =
-      bottom_display.GetSizeInPixel().height() + bottom_display.bounds().y();
-
   // Calculate the start coordinates for the popup if it is growing down or
   // the end position if it is growing up, capped to screen space.
-  int top_growth_end = std::max(
-      topmost_display_y, std::min(bottommost_display_y, element_bounds.y()));
+  int top_growth_end =
+      std::max(topmost_available_y,
+               std::min(bottommost_available_y, element_bounds.y()));
   int bottom_growth_start =
-      std::max(topmost_display_y,
-               std::min(bottommost_display_y, element_bounds.bottom()));
+      std::max(topmost_available_y,
+               std::min(bottommost_available_y, element_bounds.bottom()));
 
-  int top_available = bottom_growth_start - topmost_display_y;
-  int bottom_available = bottommost_display_y - top_growth_end;
+  int top_available = top_growth_end - topmost_available_y;
+  int bottom_available = bottommost_available_y - bottom_growth_start;
 
-  // TODO(csharp): Restrict the popup height to what is available.
-  popup_bounds->set_height(popup_required_height);
   if (bottom_available >= popup_required_height ||
       bottom_available >= top_available) {
     // The popup can appear below the field.
+    popup_bounds->set_height(std::min(bottom_available, popup_required_height));
     popup_bounds->set_y(bottom_growth_start);
   } else {
     // The popup must appear above the field.
-    popup_bounds->set_y(top_growth_end - popup_required_height);
+    popup_bounds->set_height(std::min(top_available, popup_required_height));
+    popup_bounds->set_y(top_growth_end - popup_bounds->height());
   }
 }
 
 }  // namespace
+
+void PopupViewCommon::CalculatePopupHorizontalBounds(
+    int desired_width,
+    const gfx::Rect& element_bounds,
+    gfx::NativeView container_view,
+    bool is_rtl,
+    gfx::Rect* popup_bounds) {
+  const gfx::Rect bounds = GetWindowBounds(container_view);
+  CalculatePopupXAndWidth(/*leftmost_available_x=*/bounds.x(),
+                          /*rightmost_available_x=*/bounds.x() + bounds.width(),
+                          desired_width, element_bounds, is_rtl, popup_bounds);
+}
+
+void PopupViewCommon::CalculatePopupVerticalBounds(
+    int desired_height,
+    const gfx::Rect& element_bounds,
+    gfx::NativeView container_view,
+    gfx::Rect* popup_bounds) {
+  const gfx::Rect window_bounds = GetWindowBounds(container_view);
+  CalculatePopupYAndHeight(
+      /*topmost_available_y=*/window_bounds.y(),
+      /*bottommost_available_y=*/window_bounds.y() + window_bounds.height(),
+      desired_height, element_bounds, popup_bounds);
+}
 
 gfx::Rect PopupViewCommon::CalculatePopupBounds(int desired_width,
                                                 int desired_height,
                                                 const gfx::Rect& element_bounds,
                                                 gfx::NativeView container_view,
                                                 bool is_rtl) {
-  // This is the top left point of the popup if the popup is above the element
-  // and grows to the left (since that is the highest and furthest left the
-  // popup go could).
-  gfx::Point top_left_corner_of_popup =
-      element_bounds.origin() +
-      gfx::Vector2d(element_bounds.width() - desired_width, -desired_height);
-
-  // This is the bottom right point of the popup if the popup is below the
-  // element and grows to the right (since the is the lowest and furthest right
-  // the popup could go).
-  gfx::Point bottom_right_corner_of_popup =
-      element_bounds.origin() +
-      gfx::Vector2d(desired_width, element_bounds.height() + desired_height);
-
-  display::Display top_left_display =
-      GetDisplayNearestPoint(top_left_corner_of_popup, container_view);
-  display::Display bottom_right_display =
-      GetDisplayNearestPoint(bottom_right_corner_of_popup, container_view);
+  const gfx::Rect window_bounds = GetWindowBounds(container_view);
 
   gfx::Rect popup_bounds;
-  CalculatePopupXAndWidth(top_left_display, bottom_right_display, desired_width,
-                          element_bounds, is_rtl, &popup_bounds);
-  CalculatePopupYAndHeight(top_left_display, bottom_right_display,
-                           desired_height, element_bounds, &popup_bounds);
+  CalculatePopupXAndWidth(
+      /*leftmost_available_x=*/window_bounds.x(),
+      /*rightmost_available_x=*/window_bounds.x() + window_bounds.width(),
+      desired_width, element_bounds, is_rtl, &popup_bounds);
+  CalculatePopupYAndHeight(
+      /*topmost_available_y=*/window_bounds.y(),
+      /*bottommost_available_y=*/window_bounds.y() + window_bounds.height(),
+      desired_height, element_bounds, &popup_bounds);
 
   return popup_bounds;
 }
 
-display::Display PopupViewCommon::GetDisplayNearestPoint(
-    const gfx::Point& point,
-    gfx::NativeView container_view) {
-  return display::Screen::GetScreen()->GetDisplayNearestPoint(point);
+gfx::Rect PopupViewCommon::GetWindowBounds(gfx::NativeView container_view) {
+// The call to FindBrowserWithWindow will fail on Android, so we use
+// platform-specific calls.
+#if defined(OS_ANDROID)
+  return container_view->GetWindowAndroid()->bounds();
+#else
+  gfx::NativeWindow window = platform_util::GetTopLevel(container_view);
+  Browser* browser = chrome::FindBrowserWithWindow(window);
+  DCHECK(browser);
+  return browser->window()->GetBounds();
+#endif
 }
 
 }  // namespace autofill

chrome/browser/ui/autofill/popup_view_common.h
@@ -6,25 +6,37 @@
 #define CHROME_BROWSER_UI_AUTOFILL_POPUP_VIEW_COMMON_H_
 
 #include "base/macros.h"
+#include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/native_widget_types.h"
-
-namespace display {
-class Display;
-}
-
-namespace gfx {
-class Point;
-class Rect;
-}
 
 namespace autofill {
 
 // Provides utility functions for popup-style views.
 class PopupViewCommon {
  public:
-  // Returns the bounds that the popup should be placed at, given the desired
-  // width and height. By default this places the popup below |element_bounds|
-  // but it will be placed above if there isn't enough space.
+  virtual ~PopupViewCommon() = default;
+
+  // Writes the |x| and |width| properties to |popup_bounds| for the popup's
+  // placement based on the element's location, the desired width, whether or
+  // not this is RTL, and the space available in the window to the left/right
+  // of the element.
+  void CalculatePopupHorizontalBounds(int desired_width,
+                                      const gfx::Rect& element_bounds,
+                                      gfx::NativeView container_view,
+                                      bool is_rtl,
+                                      gfx::Rect* popup_bounds);
+
+  // Writes the |y| and |height| properties to |popup_bounds| for the popup's
+  // placement based on the element's location, the desired height, and the
+  // space available in the window above/below the element. The popup will be
+  // placed below the element as long as there is sufficient space.
+  void CalculatePopupVerticalBounds(int desired_height,
+                                    const gfx::Rect& element_bounds,
+                                    gfx::NativeView container_view,
+                                    gfx::Rect* popup_bounds);
+
+  // Convenience method which handles both the vertical and horizontal bounds
+  // and returns a new Rect.
   gfx::Rect CalculatePopupBounds(int desired_width,
                                  int desired_height,
                                  const gfx::Rect& element_bounds,
@@ -32,11 +44,9 @@ class PopupViewCommon {
                                  bool is_rtl);
 
  protected:
-  // A helper function to get the display closest to the given point (virtual
-  // for testing).
-  virtual display::Display GetDisplayNearestPoint(
-      const gfx::Point& point,
-      gfx::NativeView container_view);
+  // Returns the bounds of the containing window in screen space. Virtual for
+  // testing.
+  virtual gfx::Rect GetWindowBounds(gfx::NativeView container_view);
 };
 
 }  // namespace autofill

commit 03daf4e42f99ef9329ec05fc186c1936ffa718a9

Reland: [af] Adjust dropdown so it doesn't look so close to the input element
This relands commit f1c12e6, which was reverted by commit c71c2c1.

The reason for the revert was a crash introduced by c53515c, and
reverting that commit was very hard due to merge conflicts.

Bug: 847980
Change-Id: Ib1b6931d3ac697e78c27a6cbcc7e082f0da4b6ef
Reviewed-on: https://chromium-review.googlesource.com/1135446
Reviewed-by: Peter Kasting <pkasting@chromium.org>
Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
Cr-Commit-Position: refs/heads/master@{#574676}

chrome/browser/ui/views/autofill/autofill_popup_view_native_views.cc
@@ -53,6 +53,10 @@ const SkColor kAutofillPopupSelectedBackgroundColor = gfx::kGoogleGrey200;
 const SkColor kAutofillPopupFooterBackgroundColor = gfx::kGoogleGrey050;
 const SkColor kAutofillPopupSeparatorColor = gfx::kGoogleGrey200;
 
+// A space between the input element and the dropdown, so that the dropdown's
+// border doesn't look too close to the element.
+constexpr int kElementBorderPadding = 1;
+
 int GetCornerRadius() {
   return ChromeLayoutProvider::Get()->GetCornerRadiusMetric(
       views::EMPHASIS_MEDIUM);
@@ -632,9 +636,18 @@ void AutofillPopupViewNativeViews::DoUpdateBoundsAndRedrawPopup() {
   gfx::Size size = CalculatePreferredSize();
   gfx::Rect popup_bounds;
 
-  PopupViewCommon().CalculatePopupVerticalBounds(
-      size.height(), gfx::ToEnclosingRect(controller_->element_bounds()),
-      controller_->container_view(), &popup_bounds);
+  // When a bubble border is shown, the contents area (inside the shadow) is
+  // supposed to be aligned with input element boundaries.
+  gfx::Rect element_bounds =
+      gfx::ToEnclosingRect(controller_->element_bounds());
+  // Consider the element is |kElementBorderPadding| pixels larger at the top
+  // and at the bottom in order to reposition the dropdown, so that it doesn't
+  // look too close to the element.
+  element_bounds.Inset(/*horizontal=*/0, /*vertical=*/-kElementBorderPadding);
+
+  PopupViewCommon().CalculatePopupVerticalBounds(size.height(), element_bounds,
+                                                 controller_->container_view(),
+                                                 &popup_bounds);
 
   // Adjust the width to compensate for a scroll bar, if necessary, and for
   // other rules.
@@ -651,8 +664,8 @@ void AutofillPopupViewNativeViews::DoUpdateBoundsAndRedrawPopup() {
   size.set_width(AdjustWidth(size.width() + scroll_width));
 
   PopupViewCommon().CalculatePopupHorizontalBounds(
-      size.width(), gfx::ToEnclosingRect(controller_->element_bounds()),
-      controller_->container_view(), controller_->IsRTL(), &popup_bounds);
+      size.width(), element_bounds, controller_->container_view(),
+      controller_->IsRTL(), &popup_bounds);
 
   SetSize(size);
 