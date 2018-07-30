0aa2c4ee8f6f5335e6d86b0ab991b4bf9158dc70
Sammie Quon <sammiequon@google.com>
2018-07-30 21:03:16 +0000

    [merge to 69] overview: Enable swipe to close by default.
    TBR=sammiequon@google.com
    
    (cherry picked from commit 2a37c011b7da12bd886d110549bd484ff64ab974)
    
    Test: none
    Bug: 862489, 866715
    Change-Id: Iebf4ee24d693f49258b9c01bad9ac6cfcdb838b2
    Reviewed-on: https://chromium-review.googlesource.com/1142230
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#577331}
    Reviewed-on: https://chromium-review.googlesource.com/1155732
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3497@{#248}
    Cr-Branched-From: 271eaf50594eb818c9295dc78d364aea18c82ea8-refs/heads/master@{#576753}

--- ash/public/cpp/ash_features.cc
+++ ash/public/cpp/ash_features.cc
@@ -37,7 +37,7 @@ const base::Feature kNewWallpaperPicker{"NewWallpaperPicker",
 const base::Feature kNightLight{"NightLight", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kOverviewSwipeToClose{"OverviewSwipeToClose",
-                                          base::FEATURE_DISABLED_BY_DEFAULT};
+                                          base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kSystemTrayUnified{"SystemTrayUnified",
                                        base::FEATURE_ENABLED_BY_DEFAULT};
