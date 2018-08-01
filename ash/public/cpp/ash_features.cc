e120396671f07bedd909f68cc703707bd7315030
Tetsui Ohkubo <tetsui@chromium.org>
2018-08-01 04:13:34 +0000

    Disable UnifiedSystemTray by default.
    Will be disabled on M69 beta
    
    TEST=trybot
    BUG=842079
    TBR=yoshiki@chromium.org,jamescook@chromium.org
    
    Change-Id: I96dde69d25e9cb0c299084157ca38501cc018e1c
    Reviewed-on: https://chromium-review.googlesource.com/1157985
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3497@{#307}
    Cr-Branched-From: 271eaf50594eb818c9295dc78d364aea18c82ea8-refs/heads/master@{#576753}

--- ash/public/cpp/ash_features.cc
+++ ash/public/cpp/ash_features.cc
@@ -40,7 +40,7 @@ const base::Feature kOverviewSwipeToClose{"OverviewSwipeToClose",
                                           base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kSystemTrayUnified{"SystemTrayUnified",
-                                       base::FEATURE_ENABLED_BY_DEFAULT};
+                                       base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kTapVisualizerApp{"TapVisualizerApp",
                                       base::FEATURE_ENABLED_BY_DEFAULT};
