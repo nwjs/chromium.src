nwjs/chromium.src

commit bf7722f9b73432d38b6e21087e5c5a6e62274aff

Reland "Fixes ExtensionWebRequestApiTest.WebRequestBlocking with network service"
This is a reland of db56091963b87ed7121cb921a2c31e2922532bfd

Reland disables flaky ExtensionWebRequestApiTest.WebRequestTestOSDD on
Windows.

Original change's description:
> Fixes ExtensionWebRequestApiTest.WebRequestBlocking with network service
>
> Getting this test working required a few fixes:
> - Modifying a header rather than Set-Cookie for some tests, which
>   required switching to XMLHttpRequest (crbug.com/827582)
> - Handling redirect from http:// -> chrome-extension://, and bypassing
>   security checks if this is from web request
> - Plumbing through whether the request is async or not
>
> This is not handling subresource redirects because subresource
> redirects to schemes other than http/https/data are not supported
> yet with network service (crbug.com/868545).
>
> Also adds a bunch of tests for redirects.
>
> Bug: 721414
> Cq-Include-Trybots: luci.chromium.try:linux_mojo
> Reviewed-on: https://chromium-review.googlesource.com/1145956
> Reviewed-by: Nick Carter <nick@chromium.org>
> Reviewed-by: John Abd-El-Malek <jam@chromium.org>
> Reviewed-by: Reilly Grant <reillyg@chromium.org>
> Commit-Queue: Clark DuVall <cduvall@chromium.org>
> Cr-Commit-Position: refs/heads/master@{#580365}

TBR=nick@chromium.org,jam@chromium.org,reillyg@chromium.org,karandeepb@chromium.org

Cq-Include-Trybots: luci.chromium.try:linux_mojo
Change-Id: I8c37f335c96aa8f43548e9118e96f095f2ae6362
Bug: 721414
Reviewed-on: https://chromium-review.googlesource.com/1162274
Commit-Queue: Clark DuVall <cduvall@chromium.org>
Reviewed-by: Clark DuVall <cduvall@chromium.org>
Cr-Commit-Position: refs/heads/master@{#580595}

content/browser/loader/navigation_url_loader_impl.cc
@@ -52,6 +52,7 @@
 #include "content/public/browser/navigation_data.h"
 #include "content/public/browser/navigation_ui_data.h"
 #include "content/public/browser/plugin_service.h"
+#include "content/public/browser/redirect_checker.h"
 #include "content/public/browser/resource_dispatcher_host_delegate.h"
 #include "content/public/browser/ssl_status.h"
 #include "content/public/browser/url_loader_request_interceptor.h"
@@ -299,6 +300,29 @@ void UnknownSchemeCallback(bool handled_externally,
       handled_externally ? net::ERR_ABORTED : net::ERR_UNKNOWN_URL_SCHEME));
 }
 
+// Returns whether this URL can be handled by the default network service
+// URLLoader.
+bool IsURLHandledByDefaultLoader(const GURL& url) {
+  // Data URLs are only handled by the network service if
+  // |enable_data_url_support| is set in NetworkContextParams. This is set to
+  // true for the context used by NavigationURLLoaderImpl, so in addition to
+  // checking whether the URL is handled by the network service, we also need to
+  // check for the data scheme.
+  return IsURLHandledByNetworkService(url) || url.SchemeIs(url::kDataScheme);
+}
+
+// Determines whether it is safe to redirect to |url|.
+bool IsSafeRedirectTarget(const GURL& url, ResourceContext* resource_context) {
+  static base::NoDestructor<std::set<std::string>> kUnsafeSchemes({
+      url::kAboutScheme, url::kDataScheme, url::kFileScheme,
+      url::kFileSystemScheme,
+  });
+  return !HasWebUIScheme(url) &&
+         kUnsafeSchemes->find(url.scheme()) == kUnsafeSchemes->end() &&
+         GetContentClient()->browser()->IsSafeRedirectTarget(url,
+                                                             resource_context);
+}
+
 }  // namespace
 
 // Kept around during the lifetime of the navigation request, and is
@@ -320,6 +344,7 @@ class NavigationURLLoaderImpl::URLLoaderRequestController
       network::mojom::URLLoaderFactoryRequest proxied_factory_request,
       network::mojom::URLLoaderFactoryPtrInfo proxied_factory_info,
       std::set<std::string> known_schemes,
+      scoped_refptr<RedirectChecker> redirect_checker,
       const base::WeakPtr<NavigationURLLoaderImpl>& owner)
       : interceptors_(std::move(initial_interceptors)),
         resource_request_(std::move(resource_request)),
@@ -330,6 +355,7 @@ class NavigationURLLoaderImpl::URLLoaderRequestController
         proxied_factory_request_(std::move(proxied_factory_request)),
         proxied_factory_info_(std::move(proxied_factory_info)),
         known_schemes_(std::move(known_schemes)),
+        redirect_checker_(std::move(redirect_checker)),
         weak_factory_(this) {}
 
   ~URLLoaderRequestController() override {
@@ -672,9 +698,15 @@ class NavigationURLLoaderImpl::URLLoaderRequestController
     // the restarted request to use a new loader, instead of, e.g., reusing the
     // AppCache or service worker loader. For an optimization, we keep and reuse
     // the default url loader if the all |interceptors_| doesn't handle the
-    // redirected request.
-    if (!default_loader_used_)
+    // redirected request. If the network service is enabled, only certain
+    // schemes are handled by the default URL loader. We need to make sure the
+    // redirected URL is a handled scheme, otherwise reset the loader so the
+    // correct non-network service loader can be used.
+    if (!default_loader_used_ ||
+        (base::FeatureList::IsEnabled(network::features::kNetworkService) &&
+         !IsURLHandledByDefaultLoader(resource_request_->url))) {
       url_loader_.reset();
+    }
     interceptor_index_ = 0;
     received_response_ = false;
     MaybeStartLoader(nullptr /* interceptor */,
@@ -792,8 +824,7 @@ class NavigationURLLoaderImpl::URLLoaderRequestController
       return;
     }
 
-    if (!IsURLHandledByNetworkService(resource_request_->url) &&
-        !resource_request_->url.SchemeIs(url::kDataScheme)) {
+    if (!IsURLHandledByDefaultLoader(resource_request_->url)) {
       if (known_schemes_.find(resource_request_->url.scheme()) ==
           known_schemes_.end()) {
         bool handled = GetContentClient()->browser()->HandleExternalProtocol(
@@ -1085,6 +1116,19 @@ class NavigationURLLoaderImpl::URLLoaderRequestController
 
   void OnReceiveRedirect(const net::RedirectInfo& redirect_info,
                          const network::ResourceResponseHead& head) override {
+    if (base::FeatureList::IsEnabled(network::features::kNetworkService)) {
+      bool bypass_safety_checks =
+          redirect_checker_ &&
+          redirect_checker_->ShouldAllowRedirect(global_request_id_.request_id,
+                                                 redirect_info);
+      if (!bypass_safety_checks &&
+          !IsSafeRedirectTarget(redirect_info.new_url, resource_context_)) {
+        OnComplete(
+            network::URLLoaderCompletionStatus(net::ERR_UNSAFE_REDIRECT));
+        return;
+      }
+    }
+
     if (--redirect_limit_ == 0) {
       OnComplete(
           network::URLLoaderCompletionStatus(net::ERR_TOO_MANY_REDIRECTS));
@@ -1291,6 +1335,8 @@ class NavigationURLLoaderImpl::URLLoaderRequestController
   // protocol handlers.
   std::set<std::string> known_schemes_;
 
+  scoped_refptr<RedirectChecker> redirect_checker_;
+
   mutable base::WeakPtrFactory<URLLoaderRequestController> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(URLLoaderRequestController);
@@ -1340,7 +1386,7 @@ NavigationURLLoaderImpl::NavigationURLLoaderImpl(
         request_info->common_params.url,
         /* proxied_url_loader_factory_request */ nullptr,
         /* proxied_url_loader_factory_info */ nullptr, std::set<std::string>(),
-        weak_factory_.GetWeakPtr());
+        /* redirect_checker */ nullptr, weak_factory_.GetWeakPtr());
 
     BrowserThread::PostTask(
         BrowserThread::IO, FROM_HERE,
@@ -1369,6 +1415,7 @@ NavigationURLLoaderImpl::NavigationURLLoaderImpl(
 
   network::mojom::URLLoaderFactoryPtrInfo proxied_factory_info;
   network::mojom::URLLoaderFactoryRequest proxied_factory_request;
+  scoped_refptr<RedirectChecker> redirect_checker;
   auto* partition = static_cast<StoragePartitionImpl*>(storage_partition);
   if (frame_tree_node) {
     // |frame_tree_node| may be null in some unit test environments.
@@ -1385,7 +1432,7 @@ NavigationURLLoaderImpl::NavigationURLLoaderImpl(
     auto factory_request = mojo::MakeRequest(&factory_info);
     bool use_proxy = GetContentClient()->browser()->WillCreateURLLoaderFactory(
         partition->browser_context(), frame_tree_node->current_frame_host(),
-        true /* is_navigation */, &factory_request);
+        true /* is_navigation */, &factory_request, &redirect_checker);
     if (RenderFrameDevToolsAgentHost::WillCreateURLLoaderFactory(
             frame_tree_node->current_frame_host(), true, false,
             &factory_request)) {
@@ -1419,7 +1466,7 @@ NavigationURLLoaderImpl::NavigationURLLoaderImpl(
       std::move(initial_interceptors), std::move(new_request), resource_context,
       request_info->common_params.url, std::move(proxied_factory_request),
       std::move(proxied_factory_info), std::move(known_schemes),
-      weak_factory_.GetWeakPtr());
+      std::move(redirect_checker), weak_factory_.GetWeakPtr());
   BrowserThread::PostTask(
       BrowserThread::IO, FROM_HERE,
       base::BindOnce(
@@ -1517,7 +1564,7 @@ void NavigationURLLoaderImpl::BindNonNetworkURLLoaderFactoryRequest(
   auto* frame = frame_tree_node->current_frame_host();
   GetContentClient()->browser()->WillCreateURLLoaderFactory(
       frame->GetSiteInstance()->GetBrowserContext(), frame,
-      true /* is_navigation */, &factory);
+      true /* is_navigation */, &factory, nullptr /* redirect_checker */);
   it->second->Clone(std::move(factory));
 }
 

chrome/browser/chrome_content_browser_client.cc
@@ -438,6 +438,7 @@
 #include "extensions/common/extension_set.h"
 #include "extensions/common/manifest_handlers/background_info.h"
 #include "extensions/common/manifest_handlers/shared_module_info.h"
+#include "extensions/common/manifest_handlers/web_accessible_resources_info.h"
 #include "extensions/common/permissions/permissions_data.h"
 #include "extensions/common/permissions/socket_permission.h"
 #include "extensions/common/switches.h"
@@ -4366,7 +4367,8 @@ bool ChromeContentBrowserClient::WillCreateURLLoaderFactory(
     content::BrowserContext* browser_context,
     content::RenderFrameHost* frame,
     bool is_navigation,
-    network::mojom::URLLoaderFactoryRequest* factory_request) {
+    network::mojom::URLLoaderFactoryRequest* factory_request,
+    scoped_refptr<content::RedirectChecker>* redirect_checker) {
 #if BUILDFLAG(ENABLE_EXTENSIONS)
   auto* web_request_api =
       extensions::BrowserContextKeyedAPIFactory<extensions::WebRequestAPI>::Get(
@@ -4377,8 +4379,8 @@ bool ChromeContentBrowserClient::WillCreateURLLoaderFactory(
   if (!web_request_api)
     return false;
 
-  return web_request_api->MaybeProxyURLLoaderFactory(frame, is_navigation,
-                                                     factory_request);
+  return web_request_api->MaybeProxyURLLoaderFactory(
+      frame, is_navigation, factory_request, redirect_checker);
 #else
   return false;
 #endif
@@ -4618,6 +4620,23 @@ ChromeContentBrowserClient::CreateWindowForPictureInPicture(
 #else
   return nullptr;
 #endif
+}
+
+bool ChromeContentBrowserClient::IsSafeRedirectTarget(
+    const GURL& url,
+    content::ResourceContext* context) {
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+  if (url.SchemeIs(extensions::kExtensionScheme)) {
+    ProfileIOData* io_data = ProfileIOData::FromResourceContext(context);
+    const Extension* extension =
+        io_data->GetExtensionInfoMap()->extensions().GetByID(url.host());
+    if (!extension)
+      return false;
+    return extensions::WebAccessibleResourcesInfo::IsResourceWebAccessible(
+        extension, url.path());
+  }
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
+  return true;
 }
 
 // Static; handles rewriting Web UI URLs.

chrome/browser/chrome_content_browser_client.h
@@ -425,7 +425,8 @@ class ChromeContentBrowserClient : public content::ContentBrowserClient {
       content::BrowserContext* browser_context,
       content::RenderFrameHost* frame,
       bool is_navigation,
-      network::mojom::URLLoaderFactoryRequest* factory_request) override;
+      network::mojom::URLLoaderFactoryRequest* factory_request,
+      scoped_refptr<content::RedirectChecker>* redirect_checker) override;
   std::vector<std::unique_ptr<content::URLLoaderRequestInterceptor>>
   WillCreateURLLoaderRequestInterceptors(
       content::NavigationUIData* navigation_ui_data,
@@ -481,6 +482,8 @@ class ChromeContentBrowserClient : public content::ContentBrowserClient {
       bool has_user_gesture) override;
   std::unique_ptr<content::OverlayWindow> CreateWindowForPictureInPicture(
       content::PictureInPictureWindowController* controller) override;
+  bool IsSafeRedirectTarget(const GURL& url,
+                            content::ResourceContext* context) override;
 
  protected:
   static bool HandleWebUI(GURL* url, content::BrowserContext* browser_context);

chrome/browser/extensions/api/web_request/web_request_apitest.cc
@@ -498,8 +498,29 @@ IN_PROC_BROWSER_TEST_F(ExtensionWebRequestApiTest,
 #endif
 IN_PROC_BROWSER_TEST_F(ExtensionWebRequestApiTest, MAYBE_WebRequestBlocking) {
   ASSERT_TRUE(StartEmbeddedTestServer());
-  ASSERT_TRUE(RunExtensionSubtest("webrequest", "test_blocking.html")) <<
-      message_;
+  ASSERT_TRUE(RunExtensionSubtest("webrequest", "test_blocking.html"))
+      << message_;
+}
+
+IN_PROC_BROWSER_TEST_F(ExtensionWebRequestApiTest,
+                       WebRequestBlockingSetCookieHeader) {
+  ASSERT_TRUE(StartEmbeddedTestServer());
+  ASSERT_TRUE(RunExtensionSubtest("webrequest", "test_blocking_cookie.html"))
+      << message_;
+}
+
+IN_PROC_BROWSER_TEST_F(ExtensionWebRequestApiTest, WebRequestRedirects) {
+  ASSERT_TRUE(StartEmbeddedTestServer());
+  ASSERT_TRUE(RunExtensionSubtest("webrequest", "test_redirects.html"))
+      << message_;
+}
+
+IN_PROC_BROWSER_TEST_F(ExtensionWebRequestApiTest,
+                       WebRequestSubresourceRedirects) {
+  ASSERT_TRUE(StartEmbeddedTestServer());
+  ASSERT_TRUE(
+      RunExtensionSubtest("webrequest", "test_subresource_redirects.html"))
+      << message_;
 }
 
 // Fails often on Windows dbg bots. http://crbug.com/177163

chrome/test/data/extensions/api_test/webrequest/framework.js
@@ -125,7 +125,7 @@ function navigateAndWait(url, callback) {
   var done = chrome.test.listenForever(chrome.tabs.onUpdated,
       function (_, info, tab) {
     if (tab.id == tabId && info.status == "complete") {
-      if (callback) callback();
+      if (callback) callback(tab);
       done();
     }
   });

commit 2e0f0bb3edb6f5d941bb0820a9ec9fd694ad31ab

NavigationURLLoaderImpl: Fix GCC build after #580595
Commit bf7722f9 ('Reland "Fixes
ExtensionWebRequestApiTest.WebRequestBlocking with network service"') caused
the GCC build to hit https://gcc.gnu.org/bugzilla/show_bug.cgi?id=84849,
where having base::NoDestructor<T<U>> and passing an initializer list of Us
does not work.

Explicitly create an std::set<string> to pass to base::NoDestructor, just
like we did in https://chromium-review.googlesource.com/944405 before.

Bug: 819294
Change-Id: I8077b39959d8a53daef40bc33f8293f15c597ce0
Reviewed-on: https://chromium-review.googlesource.com/1165360
Reviewed-by: Matt Menke <mmenke@chromium.org>
Reviewed-by: Charlie Harrison <csharrison@chromium.org>
Commit-Queue: Raphael Kubo da Costa (CET) <raphael.kubo.da.costa@intel.com>
Cr-Commit-Position: refs/heads/master@{#581287}

content/browser/loader/navigation_url_loader_impl.cc
@@ -313,10 +313,11 @@ bool IsURLHandledByDefaultLoader(const GURL& url) {
 
 // Determines whether it is safe to redirect to |url|.
 bool IsSafeRedirectTarget(const GURL& url, ResourceContext* resource_context) {
-  static base::NoDestructor<std::set<std::string>> kUnsafeSchemes({
-      url::kAboutScheme, url::kDataScheme, url::kFileScheme,
-      url::kFileSystemScheme,
-  });
+  static base::NoDestructor<std::set<std::string>> kUnsafeSchemes(
+      std::set<std::string>({
+          url::kAboutScheme, url::kDataScheme, url::kFileScheme,
+          url::kFileSystemScheme,
+      }));
   return !HasWebUIScheme(url) &&
          kUnsafeSchemes->find(url.scheme()) == kUnsafeSchemes->end() &&
          GetContentClient()->browser()->IsSafeRedirectTarget(url,