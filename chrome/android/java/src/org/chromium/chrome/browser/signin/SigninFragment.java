dee6d02682ef5a8a03a4cfe19fb72574a68122e0
David Roger <droger@chromium.org>
2018-08-16 15:22:44 +0000

    [Dice] Split promo user actions
    The NewAccount actions and histograms are split into 3 subcases:
    - NewAccountPreDice: used on desktop pre-Dice
    - NewAccountNoExistingAccount: adding an account when Chrome has no account
    - NewAccountExistingAccount: adding an account when Chrome already has account(s).
    
    TBR=droger
    (cherry picked from commit 0ccfd704d43839e7587d3fe9927a7b797c22d514)
    
    Bug: 859142, 855754
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ifcf077341b00c70d07fcb8d2cf863384bb9b985e
    Reviewed-on: https://chromium-review.googlesource.com/1133388
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#582136}
    Reviewed-on: https://chromium-review.googlesource.com/1177747
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3497@{#675}
    Cr-Branched-From: 271eaf50594eb818c9295dc78d364aea18c82ea8-refs/heads/master@{#576753}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninFragment.java
@@ -147,7 +147,10 @@ public class SigninFragment extends SigninFragmentBase {
                 histogram = "Signin.SigninCompletedAccessPoint.NotDefault";
                 break;
             case PromoAction.NEW_ACCOUNT:
-                histogram = "Signin.SigninCompletedAccessPoint.NewAccount";
+                // On Android, the promo does not have a button to add an account when there is
+                // already an account on the device. That flow goes through the NotDefault promo
+                // instead. Always use the NoExistingAccount variant.
+                histogram = "Signin.SigninCompletedAccessPoint.NewAccountNoExistingAccount";
                 break;
             default:
                 assert false : "Unexpected sign-in flow type!";
@@ -170,7 +173,10 @@ public class SigninFragment extends SigninFragmentBase {
                 histogram = "Signin.SigninStartedAccessPoint.NotDefault";
                 break;
             case PromoAction.NEW_ACCOUNT:
-                histogram = "Signin.SigninStartedAccessPoint.NewAccount";
+                // On Android, the promo does not have a button to add an account when there is
+                // already an account on the device. That flow goes through the NotDefault promo
+                // instead. Always use the NoExistingAccount variant.
+                histogram = "Signin.SigninStartedAccessPoint.NewAccountNoExistingAccount";
                 break;
             default:
                 assert false : "Unexpected sign-in flow type!";
