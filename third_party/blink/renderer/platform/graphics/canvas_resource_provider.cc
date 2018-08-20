4174e43d14347282965dc96315d2e872bd18861f
Khushal <khushalsagar@chromium.org>
2018-08-20 18:07:16 +0000

    blink/canvas: Queue cleanup task to purge locked images for canvas2d.
    The CanvasImageProvider locks images, limited by a budget, to avoid GPU
    discardable ref churn. In the non deferral mode, these images are
    released on an external signal indicating the end of a task which
    mutated the canvas.
    
    The above mechanism is brittle, and can result in holding this memory
    for long durations depending on when the release is triggered. Instead
    queue a task each time we start adding images to this list, to ensure
    they are eventually cleared.
    
    R=​ericrk@chromium.org, fserb@chromium.org
    
    Bug: 872117
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ie9179e498ef70c78ac091a15aa871b16ae5d80ca
    Reviewed-on: https://chromium-review.googlesource.com/1179376
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#584128}(cherry picked from commit a8ae8cd395ac425d4a521cbbec134c95c3e0e2f9)
    Reviewed-on: https://chromium-review.googlesource.com/1181741
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3497@{#718}
    Cr-Branched-From: 271eaf50594eb818c9295dc78d364aea18c82ea8-refs/heads/master@{#576753}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -494,7 +494,8 @@ CanvasResourceProvider::CanvasImageProvider::CanvasImageProvider(
     const gfx::ColorSpace& target_color_space)
     : playback_image_provider_(cache,
                                target_color_space,
-                               cc::PlaybackImageProvider::Settings()) {}
+                               cc::PlaybackImageProvider::Settings()),
+      weak_factory_(this) {}
 
 CanvasResourceProvider::CanvasImageProvider::~CanvasImageProvider() = default;
 
@@ -506,7 +507,9 @@ CanvasResourceProvider::CanvasImageProvider::GetDecodedDrawImage(
   if (!scoped_decoded_image.needs_unlock())
     return scoped_decoded_image;
 
-  if (!scoped_decoded_image.decoded_image().is_budgeted()) {
+  constexpr int kMaxLockedImagesCount = 500;
+  if (!scoped_decoded_image.decoded_image().is_budgeted() ||
+      locked_images_.size() > kMaxLockedImagesCount) {
     // If we have exceeded the budget, ReleaseLockedImages any locked decodes.
     ReleaseLockedImages();
   }
@@ -526,6 +529,12 @@ void CanvasResourceProvider::CanvasImageProvider::ReleaseLockedImages() {
 
 void CanvasResourceProvider::CanvasImageProvider::CanUnlockImage(
     ScopedDecodedDrawImage image) {
+  if (locked_images_.empty()) {
+    Platform::Current()->CurrentThread()->GetTaskRunner()->PostTask(
+        FROM_HERE, base::BindOnce(&CanvasImageProvider::ReleaseLockedImages,
+                                  weak_factory_.GetWeakPtr()));
+  }
+
   locked_images_.push_back(std::move(image));
 }
 
