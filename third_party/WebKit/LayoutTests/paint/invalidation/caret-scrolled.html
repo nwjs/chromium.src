nwjs/chromium.src

commit de595e247abcd3dad4f5b3a472e97935749e9efc

[PE] Fix caret visual rect and painting when scrolled
This fixes 2 problems:
1. The caret visual rect was incorrectly added by scroll offset in
   composited scrolling block;
2. The painting of caret was not clipped.

TBR=wangxianzhu@chromium.org

(cherry picked from commit 3029f60761031c3720331a50da0835b8677d6c46)

Bug: 882794
Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
Change-Id: Ib7c6979acf6000a747e1ac8ae439633f338d3c3f
Reviewed-on: https://chromium-review.googlesource.com/c/1257892
Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
Cr-Original-Commit-Position: refs/heads/master@{#596498}
Reviewed-on: https://chromium-review.googlesource.com/c/1265836
Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
Cr-Commit-Position: refs/branch-heads/3538@{#878}
Cr-Branched-From: 79f7c91a2b2a2932cd447fa6f865cb6662fa8fa6-refs/heads/master@{#587811}

third_party/blink/renderer/core/editing/caret_display_item_client.cc
@@ -221,13 +221,6 @@ void CaretDisplayItemClient::InvalidatePaintInCurrentLayoutBlock(
     if (!local_rect_.IsEmpty()) {
       new_visual_rect = local_rect_;
       context.MapLocalRectToVisualRect(*layout_block_, new_visual_rect);
-
-      if (layout_block_->UsesCompositedScrolling()) {
-        // The caret should use scrolling coordinate space.
-        DCHECK(layout_block_ == context.paint_invalidation_container);
-        new_visual_rect.Move(
-            LayoutSize(layout_block_->ScrolledContentOffset()));
-      }
     }
   } else {
     new_visual_rect = visual_rect_;

third_party/blink/renderer/core/paint/block_painter.cc
@@ -65,6 +65,20 @@ void BlockPainter::Paint(const PaintInfo& paint_info) {
   // any painting order steps within the CSS spec.
   if (original_phase == PaintPhase::kForeground &&
       layout_block_.ShouldPaintCarets()) {
+    // Apply overflow clip if needed. TODO(wangxianzhu): Move PaintCarets()
+    // under |contents_paint_state| in the above block and let the caret
+    // painters paint in the space of scrolling contents.
+    base::Optional<ScopedPaintChunkProperties> paint_chunk_properties;
+    if (const auto* fragment = paint_state.FragmentToPaint()) {
+      if (const auto* properties = fragment->PaintProperties()) {
+        if (const auto* overflow_clip = properties->OverflowClip()) {
+          paint_chunk_properties.emplace(
+              paint_info.context.GetPaintController(), overflow_clip,
+              layout_block_, DisplayItem::kCaret);
+        }
+      }
+    }
+
     PaintCarets(paint_info, paint_offset);
   }
 

third_party/WebKit/LayoutTests/flag-specific/enable-blink-features=LayoutNG/paint/invalidation/clip/caret-ancestor-clip-change-expected.txt
@@ -28,7 +28,7 @@
           "reason": "incremental"
         },
         {
-          "object": "Caret",
+          "object": "LayoutBlockFlow DIV",
           "rect": [10, 58, 1, 57],
           "reason": "incremental"
         }

third_party/WebKit/LayoutTests/flag-specific/enable-slimming-paint-v2/paint/invalidation/caret-subpixel-expected.txt
@@ -12,7 +12,7 @@
           "reason": "subtree"
         },
         {
-          "object": "Caret",
+          "object": "LayoutBlockFlow DIV",
           "rect": [209, 11, 2, 16],
           "reason": "chunk appeared"
         }

third_party/WebKit/LayoutTests/flag-specific/enable-slimming-paint-v2/paint/invalidation/clip/caret-ancestor-clip-change-expected.txt
@@ -17,7 +17,7 @@
           "reason": "incremental"
         },
         {
-          "object": "Caret",
+          "object": "LayoutBlockFlow DIV",
           "rect": [10, 58, 1, 57],
           "reason": "incremental"
         }

third_party/WebKit/LayoutTests/paint/invalidation/caret-composited-scrolled-expected.html
@@ -1,0 +1,13 @@
+<!DOCTYPE html>
+<div style="position: relative; top: 100px; width: 200px; height: 200px; overflow: hidden; border: 1px solid black">
+  <div id="target" contenteditable
+       style="position: relative; top: -50px; font-size: 100px; color: green; outline-style: none">
+    <br>
+    <br>
+    <br>
+    <br>
+  </div>
+</div>
+<script>
+target.focus();
+</script>

third_party/WebKit/LayoutTests/paint/invalidation/caret-composited-scrolled.html
@@ -1,0 +1,20 @@
+<!DOCTYPE html>
+<style>
+::-webkit-scrollbar { display: none; }
+</style>
+<div id="target" contenteditable
+     style="position: relative; top: 100px; width: 200px; height: 200px; overflow: scroll; will-change: transform;
+            font-size: 100px; border: 1px solid black; outline-style: none">
+<br>
+<br>
+<br>
+<br>
+</div>
+<script src="../../resources/run-after-layout-and-paint.js"></script>
+<script>
+target.focus();
+target.scrollTop = 50;
+runAfterLayoutAndPaint(function() {
+  target.style.color = 'green';
+}, true);
+</script>

third_party/WebKit/LayoutTests/paint/invalidation/caret-scrolled-expected.html
@@ -1,0 +1,13 @@
+<!DOCTYPE html>
+<div style="position: relative; top: 100px; width: 200px; height: 200px; overflow: hidden; border: 1px solid black">
+  <div id="target" contenteditable
+       style="position: relative; top: -50px; font-size: 100px; color: green; outline-style: none">
+    <br>
+    <br>
+    <br>
+    <br>
+  </div>
+</div>
+<script>
+target.focus();
+</script>

commit f7677282b347b2dff625b485ec342b959942e580

Reland "[PE] Fix caret visual rect and painting when scrolled"
This is a reland of de595e247abcd3dad4f5b3a472e97935749e9efc which broke
build because it used a renamed variable in M71. Fixed.

Original change's description:
> [PE] Fix caret visual rect and painting when scrolled
>
> This fixes 2 problems:
> 1. The caret visual rect was incorrectly added by scroll offset in
>    composited scrolling block;
> 2. The painting of caret was not clipped.
>
> TBR=wangxianzhu@chromium.org
>
> (cherry picked from commit 3029f60761031c3720331a50da0835b8677d6c46)
>
> Bug: 882794
> Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
> Change-Id: Ib7c6979acf6000a747e1ac8ae439633f338d3c3f
> Reviewed-on: https://chromium-review.googlesource.com/c/1257892
> Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
> Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
> Cr-Original-Commit-Position: refs/heads/master@{#596498}
> Reviewed-on: https://chromium-review.googlesource.com/c/1265836
> Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
> Cr-Commit-Position: refs/branch-heads/3538@{#878}
> Cr-Branched-From: 79f7c91a2b2a2932cd447fa6f865cb6662fa8fa6-refs/heads/master@{#587811}

Bug: 882794
Change-Id: I079ec770bf276de2d8674e21c0425809d782f0b3
Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
Reviewed-on: https://chromium-review.googlesource.com/c/1266603
Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
Cr-Commit-Position: refs/branch-heads/3538@{#882}
Cr-Branched-From: 79f7c91a2b2a2932cd447fa6f865cb6662fa8fa6-refs/heads/master@{#587811}

third_party/blink/renderer/core/editing/caret_display_item_client.cc
@@ -221,13 +221,6 @@ void CaretDisplayItemClient::InvalidatePaintInCurrentLayoutBlock(
     if (!local_rect_.IsEmpty()) {
       new_visual_rect = local_rect_;
       context.MapLocalRectToVisualRect(*layout_block_, new_visual_rect);
-
-      if (layout_block_->UsesCompositedScrolling()) {
-        // The caret should use scrolling coordinate space.
-        DCHECK(layout_block_ == context.paint_invalidation_container);
-        new_visual_rect.Move(
-            LayoutSize(layout_block_->ScrolledContentOffset()));
-      }
     }
   } else {
     new_visual_rect = visual_rect_;

third_party/blink/renderer/core/paint/block_painter.cc
@@ -65,6 +65,20 @@ void BlockPainter::Paint(const PaintInfo& paint_info) {
   // any painting order steps within the CSS spec.
   if (original_phase == PaintPhase::kForeground &&
       layout_block_.ShouldPaintCarets()) {
+    // Apply overflow clip if needed. TODO(wangxianzhu): Move PaintCarets()
+    // under |contents_paint_state| in the above block and let the caret
+    // painters paint in the space of scrolling contents.
+    base::Optional<ScopedPaintChunkProperties> paint_chunk_properties;
+    if (const auto* fragment = paint_info_with_offset.FragmentToPaint()) {
+      if (const auto* properties = fragment->PaintProperties()) {
+        if (const auto* overflow_clip = properties->OverflowClip()) {
+          paint_chunk_properties.emplace(
+              paint_info.context.GetPaintController(), overflow_clip,
+              layout_block_, DisplayItem::kCaret);
+        }
+      }
+    }
+
     PaintCarets(paint_info, paint_offset);
   }
 

third_party/WebKit/LayoutTests/flag-specific/enable-blink-features=LayoutNG/paint/invalidation/clip/caret-ancestor-clip-change-expected.txt
@@ -28,7 +28,7 @@
           "reason": "incremental"
         },
         {
-          "object": "Caret",
+          "object": "LayoutBlockFlow DIV",
           "rect": [10, 58, 1, 57],
           "reason": "incremental"
         }

third_party/WebKit/LayoutTests/flag-specific/enable-slimming-paint-v2/paint/invalidation/caret-subpixel-expected.txt
@@ -12,7 +12,7 @@
           "reason": "subtree"
         },
         {
-          "object": "Caret",
+          "object": "LayoutBlockFlow DIV",
           "rect": [209, 11, 2, 16],
           "reason": "chunk appeared"
         }

third_party/WebKit/LayoutTests/flag-specific/enable-slimming-paint-v2/paint/invalidation/clip/caret-ancestor-clip-change-expected.txt
@@ -17,7 +17,7 @@
           "reason": "incremental"
         },
         {
-          "object": "Caret",
+          "object": "LayoutBlockFlow DIV",
           "rect": [10, 58, 1, 57],
           "reason": "incremental"
         }

third_party/WebKit/LayoutTests/paint/invalidation/caret-composited-scrolled-expected.html
@@ -1,0 +1,13 @@
+<!DOCTYPE html>
+<div style="position: relative; top: 100px; width: 200px; height: 200px; overflow: hidden; border: 1px solid black">
+  <div id="target" contenteditable
+       style="position: relative; top: -50px; font-size: 100px; color: green; outline-style: none">
+    <br>
+    <br>
+    <br>
+    <br>
+  </div>
+</div>
+<script>
+target.focus();
+</script>

third_party/WebKit/LayoutTests/paint/invalidation/caret-composited-scrolled.html
@@ -1,0 +1,20 @@
+<!DOCTYPE html>
+<style>
+::-webkit-scrollbar { display: none; }
+</style>
+<div id="target" contenteditable
+     style="position: relative; top: 100px; width: 200px; height: 200px; overflow: scroll; will-change: transform;
+            font-size: 100px; border: 1px solid black; outline-style: none">
+<br>
+<br>
+<br>
+<br>
+</div>
+<script src="../../resources/run-after-layout-and-paint.js"></script>
+<script>
+target.focus();
+target.scrollTop = 50;
+runAfterLayoutAndPaint(function() {
+  target.style.color = 'green';
+}, true);
+</script>

third_party/WebKit/LayoutTests/paint/invalidation/caret-scrolled-expected.html
@@ -1,0 +1,13 @@
+<!DOCTYPE html>
+<div style="position: relative; top: 100px; width: 200px; height: 200px; overflow: hidden; border: 1px solid black">
+  <div id="target" contenteditable
+       style="position: relative; top: -50px; font-size: 100px; color: green; outline-style: none">
+    <br>
+    <br>
+    <br>
+    <br>
+  </div>
+</div>
+<script>
+target.focus();
+</script>

third_party/WebKit/LayoutTests/paint/invalidation/caret-scrolled.html
@@ -1,0 +1,20 @@
+<!DOCTYPE html>
+<style>
+::-webkit-scrollbar { display: none; }
+</style>
+<div id="target" contenteditable
+     style="position: relative; top: 100px; width: 200px; height: 200px; overflow: scroll;
+            font-size: 100px; border: 1px solid black; outline-style: none">
+<br>
+<br>
+<br>
+<br>
+</div>
+<script src="../../resources/run-after-layout-and-paint.js"></script>
+<script>
+target.focus();
+target.scrollTop = 50;
+runAfterLayoutAndPaint(function() {
+  target.style.color = 'green';
+}, true);
+</script>

third_party/WebKit/LayoutTests/platform/mac/paint/invalidation/caret-subpixel-expected.txt
@@ -23,7 +23,7 @@
           "reason": "subtree"
         },
         {
-          "object": "Caret",
+          "object": "LayoutBlockFlow DIV",
           "rect": [210, 11, 2, 13],
           "reason": "chunk appeared"
         }

third_party/WebKit/LayoutTests/platform/mac/paint/invalidation/clip/caret-ancestor-clip-change-expected.txt
@@ -28,7 +28,7 @@
           "reason": "incremental"
         },
         {
-          "object": "Caret",
+          "object": "LayoutBlockFlow DIV",
           "rect": [11, 58, 1, 61],
           "reason": "incremental"
         }

third_party/WebKit/LayoutTests/platform/mac/paint/invalidation/scroll/caret-with-composited-scroll-expected.txt
@@ -44,7 +44,7 @@
           "reason": "chunk appeared"
         },
         {
-          "object": "Caret",
+          "object": "LayoutBlockFlow DIV",
           "rect": [3, 1003, 1, 13],
           "reason": "chunk appeared"
         }

third_party/WebKit/LayoutTests/platform/win/paint/invalidation/caret-subpixel-expected.txt
@@ -23,7 +23,7 @@
           "reason": "subtree"
         },
         {
-          "object": "Caret",
+          "object": "LayoutBlockFlow DIV",
           "rect": [209, 11, 2, 16],
           "reason": "chunk appeared"
         }

third_party/WebKit/LayoutTests/platform/win/paint/invalidation/clip/caret-ancestor-clip-change-expected.txt
@@ -28,7 +28,7 @@
           "reason": "incremental"
         },
         {
-          "object": "Caret",
+          "object": "LayoutBlockFlow DIV",
           "rect": [10, 58, 1, 57],
           "reason": "incremental"
         }

third_party/WebKit/LayoutTests/platform/win/paint/invalidation/scroll/caret-with-composited-scroll-expected.txt
@@ -44,7 +44,7 @@
           "reason": "chunk appeared"
         },
         {
-          "object": "Caret",
+          "object": "LayoutBlockFlow DIV",
           "rect": [3, 1003, 1, 16],
           "reason": "chunk appeared"
         }