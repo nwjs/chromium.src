nwjs/chromium.src

commit 43b8facd3a2fbfb8f75b455f07d64acc2c13fb5c

[OOR-CORS] Implement redirect location checks
This CL:
 - fixes CheckRedirectLocation implementation so that we can use it
   from CORSURLLoader.
 - fixes CORS flag handling in ResourceLoader so that it can use
   CheckRedirectLocation,
 - removes WebCORS::HandleRedirect, and has it implemented in
   blink::ResourceLoader instead,
 - integrates CheckRedirectLocation with CORSURLLoader, and
 - removes mojom::CORSError::kRedirectDisallowedScheme in favor of
   mojom::CORSError::kCORSDisabledScheme.

Bug: 736308, 800669, 870173
Cq-Include-Trybots: luci.chromium.try:linux_mojo
Change-Id: Ib31ab135b03dcc0beca137cbc39592fe3f314790
Reviewed-on: https://chromium-review.googlesource.com/1158089
Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
Cr-Commit-Position: refs/heads/master@{#581495}

third_party/blink/renderer/platform/loader/fetch/resource_fetcher.cc
@@ -41,6 +41,7 @@
 #include "third_party/blink/renderer/platform/instance_counters.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/traced_value.h"
+#include "third_party/blink/renderer/platform/loader/cors/cors.h"
 #include "third_party/blink/renderer/platform/loader/fetch/fetch_context.h"
 #include "third_party/blink/renderer/platform/loader/fetch/fetch_initiator_type_names.h"
 #include "third_party/blink/renderer/platform/loader/fetch/memory_cache.h"
@@ -59,6 +60,7 @@
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread_scheduler.h"
 #include "third_party/blink/renderer/platform/weborigin/known_ports.h"
+#include "third_party/blink/renderer/platform/weborigin/scheme_registry.h"
 #include "third_party/blink/renderer/platform/weborigin/security_origin.h"
 #include "third_party/blink/renderer/platform/weborigin/security_policy.h"
 #include "third_party/blink/renderer/platform/weborigin/security_violation_reporting_policy.h"
@@ -746,11 +748,23 @@ base::Optional<ResourceRequestBlockedReason> ResourceFetcher::PrepareRequest(
   if (!params.Url().IsValid())
     return ResourceRequestBlockedReason::kOther;
 
-  params.MutableOptions().cors_flag =
-      !origin || !origin->CanRequest(params.Url());
-
-  if (options.cors_handling_by_resource_fetcher ==
-      kEnableCORSHandlingByResourceFetcher) {
+  if (!RuntimeEnabledFeatures::OutOfBlinkCORSEnabled() &&
+      options.cors_handling_by_resource_fetcher ==
+          kEnableCORSHandlingByResourceFetcher) {
+    if (CORS::IsCORSEnabledRequestMode(
+            resource_request.GetFetchRequestMode())) {
+      DCHECK(origin);
+      if (!origin->CanRequest(params.Url())) {
+        params.MutableOptions().cors_flag = true;
+        // Cross-origin requests are only allowed certain registered schemes.
+        if (!SchemeRegistry::ShouldTreatURLSchemeAsCORSEnabled(
+                url.Protocol())) {
+          // This won't create a CORS related console error.
+          // TODO(yhirano): Fix this.
+          return ResourceRequestBlockedReason::kOther;
+        }
+      }
+    }
     bool allow_stored_credentials = false;
     switch (resource_request.GetFetchCredentialsMode()) {
       case network::mojom::FetchCredentialsMode::kOmit:

services/network/cors/cors_url_loader.cc
@@ -239,6 +239,12 @@ void CORSURLLoader::OnReceiveRedirect(
   DCHECK(forwarding_client_);
   DCHECK(!is_waiting_follow_redirect_call_);
 
+  if (request_.fetch_redirect_mode == mojom::FetchRedirectMode::kManual) {
+    is_waiting_follow_redirect_call_ = true;
+    forwarding_client_->OnReceiveRedirect(redirect_info, response_head);
+    return;
+  }
+
   // If |CORS flag| is set and a CORS check for |request| and |response| returns
   // failure, then return a network error.
   if (fetch_cors_flag_ &&
@@ -270,15 +276,13 @@ void CORSURLLoader::OnReceiveRedirect(
     return;
   }
 
-  // TODO(yhirano): Implement the following:
-  // If |request|’s mode is "cors", |actualResponse|’s location URL includes
-  // credentials, and either |request|’s tainted origin flag is set or
-  // |request|’s origin is not same origin with |actualResponse|’s location
-  // URL’s origin, then return a network error.
-
-  // TODO(yhirano): Implement the following:
-  // If |CORS flag| is set and |actualResponse|’s location URL includes
-  // credentials, then return a network error.
+  const auto error_status = CheckRedirectLocation(
+      redirect_info.new_url, request_.fetch_request_mode,
+      request_.request_initiator, fetch_cors_flag_, tainted_);
+  if (error_status) {
+    HandleComplete(URLLoaderCompletionStatus(*error_status));
+    return;
+  }
 
   // TODO(yhirano): Implement the following (Note: this is needed when upload
   // streaming is implemented):

services/network/public/cpp/cors/cors.cc
@@ -258,26 +258,33 @@ base::Optional<CORSErrorStatus> CheckPreflightAccess(
   return CORSErrorStatus(error, error_status->failed_parameter);
 }
 
-base::Optional<mojom::CORSError> CheckRedirectLocation(const GURL& redirect_url,
-                                                       bool skip_scheme_check) {
-  if (!skip_scheme_check) {
-    // Block non HTTP(S) schemes as specified in the step 4 in
-    // https://fetch.spec.whatwg.org/#http-redirect-fetch. Chromium also allows
-    // the data scheme.
-    auto& schemes = url::GetCORSEnabledSchemes();
-    if (std::find(std::begin(schemes), std::end(schemes),
-                  redirect_url.scheme()) == std::end(schemes)) {
-      return mojom::CORSError::kRedirectDisallowedScheme;
-    }
+base::Optional<CORSErrorStatus> CheckRedirectLocation(
+    const GURL& url,
+    mojom::FetchRequestMode request_mode,
+    const base::Optional<url::Origin>& origin,
+    bool cors_flag,
+    bool tainted) {
+  // If |actualResponse|’s location URL’s scheme is not an HTTP(S) scheme,
+  // then return a network error.
+  // This should be addressed in //net.
+
+  // Note: The redirect count check is done elsewhere.
+
+  const bool url_has_credentials = url.has_username() || url.has_password();
+  // If |request|’s mode is "cors", |actualResponse|’s location URL includes
+  // credentials, and either |request|’s tainted origin flag is set or
+  // |request|’s origin is not same origin with |actualResponse|’s location
+  // URL’s origin, then return a network error.
+  DCHECK(!IsCORSEnabledRequestMode(request_mode) || origin);
+  if (IsCORSEnabledRequestMode(request_mode) && url_has_credentials &&
+      (tainted || !origin->IsSameOriginWith(url::Origin::Create(url)))) {
+    return CORSErrorStatus(mojom::CORSError::kRedirectContainsCredentials);
   }
 
-  // Block URLs including credentials as specified in the step 9 in
-  // https://fetch.spec.whatwg.org/#http-redirect-fetch.
-  //
-  // TODO(tyoshino): This check should be performed also when request's
-  // origin is not same origin with the redirect destination's origin.
-  if (redirect_url.has_username() || redirect_url.has_password())
-    return mojom::CORSError::kRedirectContainsCredentials;
+  // If CORS flag is set and |actualResponse|’s location URL includes
+  // credentials, then return a network error.
+  if (cors_flag && url_has_credentials)
+    return CORSErrorStatus(mojom::CORSError::kRedirectContainsCredentials);
 
   return base::nullopt;
 }

services/network/public/cpp/cors/cors.h
@@ -75,12 +75,13 @@ base::Optional<CORSErrorStatus> CheckPreflightAccess(
 // according to CORS. That is:
 // - the URL has a CORS supported scheme and
 // - the URL does not contain the userinfo production.
-// TODO(toyoshim): Remove |skip_scheme_check| that is used when customized
-// scheme check runs in Blink side in the legacy mode.
-// See https://crbug.com/800669.
 COMPONENT_EXPORT(NETWORK_CPP)
-base::Optional<mojom::CORSError> CheckRedirectLocation(const GURL& redirect_url,
-                                                       bool skip_scheme_check);
+base::Optional<CORSErrorStatus> CheckRedirectLocation(
+    const GURL& url,
+    mojom::FetchRequestMode request_mode,
+    const base::Optional<url::Origin>& origin,
+    bool cors_flag,
+    bool tainted);
 
 // Performs the required CORS checks on the response to a preflight request.
 // Returns |kPreflightSuccess| if preflight response was successful.

services/network/public/cpp/cors/cors_unittest.cc
@@ -167,43 +167,119 @@ TEST_F(CORSTest, CheckAccessDetectsInvalidAllowCredential) {
   EXPECT_EQ("fuga", error2->failed_parameter);
 }
 
-// Tests if cors::CheckRedirectLocation detects kRedirectDisallowedScheme and
+// Tests if cors::CheckRedirectLocation detects kCORSDisabledScheme and
 // kRedirectContainsCredentials errors correctly.
-TEST_F(CORSTest, CheckRedirectLocationDetectsErrors) {
-  // Following URLs should pass.
-  EXPECT_FALSE(cors::CheckRedirectLocation(GURL("http://example.com/"), false));
-  EXPECT_FALSE(
-      cors::CheckRedirectLocation(GURL("https://example.com/"), false));
-  EXPECT_FALSE(cors::CheckRedirectLocation(GURL("data:,Hello"), false));
-  EXPECT_FALSE(
-      cors::CheckRedirectLocation(GURL("file:///not_allow_scheme"), true));
+TEST_F(CORSTest, CheckRedirectLocation) {
+  struct TestCase {
+    GURL url;
+    mojom::FetchRequestMode request_mode;
+    bool cors_flag;
+    bool tainted;
+    base::Optional<CORSErrorStatus> expectation;
+  };
 
-  // Following URLs should result in kRedirectDisallowedScheme.
-  base::Optional<mojom::CORSError> error1 =
-      cors::CheckRedirectLocation(GURL("file:///not_allow_scheme"), false);
-  ASSERT_TRUE(error1);
-  EXPECT_EQ(mojom::CORSError::kRedirectDisallowedScheme, *error1);
+  const auto kCORS = mojom::FetchRequestMode::kCORS;
+  const auto kCORSWithForcedPreflight =
+      mojom::FetchRequestMode::kCORSWithForcedPreflight;
+  const auto kNoCORS = mojom::FetchRequestMode::kNoCORS;
 
-  // Following checks should result in the kRedirectContainsCredentials error.
-  base::Optional<mojom::CORSError> error2 = cors::CheckRedirectLocation(
-      GURL("http://yukari:tamura@example.com/"), false);
-  ASSERT_TRUE(error2);
-  EXPECT_EQ(mojom::CORSError::kRedirectContainsCredentials, *error2);
+  const url::Origin origin = url::Origin::Create(GURL("http://example.com/"));
+  const GURL same_origin_url("http://example.com/");
+  const GURL cross_origin_url("http://example2.com/");
+  const GURL data_url("data:,Hello");
+  const GURL same_origin_url_with_user("http://yukari@example.com/");
+  const GURL same_origin_url_with_pass("http://:tamura@example.com/");
+  const GURL cross_origin_url_with_user("http://yukari@example2.com/");
+  const GURL cross_origin_url_with_pass("http://:tamura@example2.com/");
+  const auto ok = base::nullopt;
+  const CORSErrorStatus kCORSDisabledScheme(
+      mojom::CORSError::kCORSDisabledScheme);
+  const CORSErrorStatus kRedirectContainsCredentials(
+      mojom::CORSError::kRedirectContainsCredentials);
 
-  base::Optional<mojom::CORSError> error3 = cors::CheckRedirectLocation(
-      GURL("http://yukari:tamura@example.com/"), true);
-  ASSERT_TRUE(error3);
-  EXPECT_EQ(mojom::CORSError::kRedirectContainsCredentials, *error3);
+  TestCase cases[] = {
+      // "cors", no credentials information
+      {same_origin_url, kCORS, false, false, ok},
+      {cross_origin_url, kCORS, false, false, ok},
+      {data_url, kCORS, false, false, ok},
+      {same_origin_url, kCORS, true, false, ok},
+      {cross_origin_url, kCORS, true, false, ok},
+      {data_url, kCORS, true, false, ok},
+      {same_origin_url, kCORS, false, true, ok},
+      {cross_origin_url, kCORS, false, true, ok},
+      {data_url, kCORS, false, true, ok},
+      {same_origin_url, kCORS, true, true, ok},
+      {cross_origin_url, kCORS, true, true, ok},
+      {data_url, kCORS, true, true, ok},
 
-  base::Optional<mojom::CORSError> error4 =
-      cors::CheckRedirectLocation(GURL("http://tamura@example.com/"), true);
-  ASSERT_TRUE(error4);
-  EXPECT_EQ(mojom::CORSError::kRedirectContainsCredentials, *error4);
+      // "cors" with forced preflight, no credentials information
+      {same_origin_url, kCORSWithForcedPreflight, false, false, ok},
+      {cross_origin_url, kCORSWithForcedPreflight, false, false, ok},
+      {data_url, kCORSWithForcedPreflight, false, false, ok},
+      {same_origin_url, kCORSWithForcedPreflight, true, false, ok},
+      {cross_origin_url, kCORSWithForcedPreflight, true, false, ok},
+      {data_url, kCORSWithForcedPreflight, true, false, ok},
+      {same_origin_url, kCORSWithForcedPreflight, false, true, ok},
+      {cross_origin_url, kCORSWithForcedPreflight, false, true, ok},
+      {data_url, kCORSWithForcedPreflight, false, true, ok},
+      {same_origin_url, kCORSWithForcedPreflight, true, true, ok},
+      {cross_origin_url, kCORSWithForcedPreflight, true, true, ok},
+      {data_url, kCORSWithForcedPreflight, true, true, ok},
 
-  base::Optional<mojom::CORSError> error5 =
-      cors::CheckRedirectLocation(GURL("http://yukari:@example.com/"), true);
-  ASSERT_TRUE(error5);
-  EXPECT_EQ(mojom::CORSError::kRedirectContainsCredentials, *error5);
+      // "no-cors", no credentials information
+      {same_origin_url, kNoCORS, false, false, ok},
+      {cross_origin_url, kNoCORS, false, false, ok},
+      {data_url, kNoCORS, false, false, ok},
+      {same_origin_url, kNoCORS, false, true, ok},
+      {cross_origin_url, kNoCORS, false, true, ok},
+      {data_url, kNoCORS, false, true, ok},
+
+      // with credentials information (same origin)
+      {same_origin_url_with_user, kCORS, false, false, ok},
+      {same_origin_url_with_user, kCORS, true, false,
+       kRedirectContainsCredentials},
+      {same_origin_url_with_user, kCORS, true, true,
+       kRedirectContainsCredentials},
+      {same_origin_url_with_user, kNoCORS, false, false, ok},
+      {same_origin_url_with_user, kNoCORS, false, true, ok},
+      {same_origin_url_with_pass, kCORS, false, false, ok},
+      {same_origin_url_with_pass, kCORS, true, false,
+       kRedirectContainsCredentials},
+      {same_origin_url_with_pass, kCORS, true, true,
+       kRedirectContainsCredentials},
+      {same_origin_url_with_pass, kNoCORS, false, false, ok},
+      {same_origin_url_with_pass, kNoCORS, false, true, ok},
+
+      // with credentials information (cross origin)
+      {cross_origin_url_with_user, kCORS, false, false,
+       kRedirectContainsCredentials},
+      {cross_origin_url_with_user, kCORS, true, false,
+       kRedirectContainsCredentials},
+      {cross_origin_url_with_user, kCORS, true, true,
+       kRedirectContainsCredentials},
+      {cross_origin_url_with_user, kNoCORS, false, true, ok},
+      {cross_origin_url_with_user, kNoCORS, false, false, ok},
+      {cross_origin_url_with_pass, kCORS, false, false,
+       kRedirectContainsCredentials},
+      {cross_origin_url_with_pass, kCORS, true, false,
+       kRedirectContainsCredentials},
+      {cross_origin_url_with_pass, kCORS, true, true,
+       kRedirectContainsCredentials},
+      {cross_origin_url_with_pass, kNoCORS, false, true, ok},
+      {cross_origin_url_with_pass, kNoCORS, false, false, ok},
+  };
+
+  for (const auto& test : cases) {
+    SCOPED_TRACE(testing::Message()
+                 << "url: " << test.url
+                 << ", request mode: " << test.request_mode
+                 << ", origin: " << origin << ", cors_flag: " << test.cors_flag
+                 << ", tainted: " << test.tainted);
+
+    EXPECT_EQ(test.expectation,
+              cors::CheckRedirectLocation(test.url, test.request_mode, origin,
+                                          test.cors_flag, test.tainted));
+  }
 }
 
 TEST_F(CORSTest, CheckPreflightDetectsErrors) {

services/network/public/mojom/cors.mojom
@@ -90,7 +90,6 @@ enum CORSError {
   // Not allowed by Access-Control-Allow-Headers in CORS-preflight response.
   kHeaderDisallowedByPreflightResponse,
 
-  // Redirect
-  kRedirectDisallowedScheme,
+  // Cross origin redirect location contains credentials such as 'user:pass'.
   kRedirectContainsCredentials,
 };

third_party/WebKit/LayoutTests/TestExpectations
@@ -1685,7 +1685,6 @@ crbug.com/736308 virtual/outofblink-cors-ns/external/wpt/xhr/send-redirect-bogus
 
 # Failing tests in dictionary order.
 crbug.com/867834 virtual/outofblink-cors/external/wpt/fetch/api/cors/sandboxed-iframe.html [ Failure ]
-crbug.com/736308 virtual/outofblink-cors/external/wpt/xhr/access-control-and-redirects-async.htm [ Failure ]
 crbug.com/736308 virtual/outofblink-cors/external/wpt/xhr/access-control-sandboxed-iframe-denied-without-wildcard.htm [ Failure ]
 crbug.com/736308 virtual/outofblink-cors/external/wpt/xhr/access-control-sandboxed-iframe-denied.htm [ Failure ]
 crbug.com/736308 virtual/outofblink-cors/external/wpt/xhr/data-uri.htm [ Failure ]
@@ -1700,7 +1699,6 @@ crbug.com/736308 virtual/outofblink-cors/http/tests/xmlhttprequest/origin-whitel
 crbug.com/736308 virtual/outofblink-cors/http/tests/xmlhttprequest/origin-whitelisting-subdomains.html [ Failure ]
 crbug.com/736308 virtual/outofblink-cors/http/tests/xmlhttprequest/origin-whitelisting-ip-addresses.html [ Failure ]
 crbug.com/867834 virtual/outofblink-cors-ns/external/wpt/fetch/api/cors/sandboxed-iframe.html [ Failure ]
-crbug.com/736308 virtual/outofblink-cors-ns/external/wpt/xhr/access-control-and-redirects-async.htm [ Failure ]
 crbug.com/736308 virtual/outofblink-cors-ns/external/wpt/xhr/access-control-sandboxed-iframe-denied-without-wildcard.htm [ Failure ]
 crbug.com/736308 virtual/outofblink-cors-ns/external/wpt/xhr/access-control-sandboxed-iframe-denied.htm [ Failure ]
 crbug.com/736308 virtual/outofblink-cors-ns/external/wpt/xhr/data-uri.htm [ Failure ]

third_party/blink/public/platform/web_cors.h
@@ -39,22 +39,8 @@
 namespace blink {
 
 class WebURLResponse;
-class WebSecurityOrigin;
-struct ResourceLoaderOptions;
 
 namespace WebCORS {
-
-// TODO(toyoshim): Using platform/loader/fetch/ResourceLoaderOptions violates
-// the DEPS rule. This will be fixed soon by making HandleRedirect() not
-// depending on ResourceLoaderOptions.
-BLINK_PLATFORM_EXPORT base::Optional<network::CORSErrorStatus> HandleRedirect(
-    WebSecurityOrigin&,
-    WebURLRequest&,
-    const WebURL,
-    const int redirect_response_status_code,
-    const WebHTTPHeaderMap&,
-    network::mojom::FetchCredentialsMode,
-    ResourceLoaderOptions&);
 
 BLINK_PLATFORM_EXPORT WebHTTPHeaderSet
 ExtractCorsExposedHeaderNamesList(network::mojom::FetchCredentialsMode,

commit 14314b070e1e92dcae7fe4ccfa4531c6a128060e

Remove CORS scheme check temporarily
The scheme check added at
https://crrev.com/43b8facd3a2fbfb8f75b455f07d64acc2c13fb5c caused some
issues on WebView. This change deletes the check temporarily.

Bug: 898589
Change-Id: I6126b3b25f8700fc0547fa2c70b62bd183cc5e23
Reviewed-on: https://chromium-review.googlesource.com/c/1298828
Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
Commit-Queue: Richard Coles <torne@chromium.org>
Cr-Commit-Position: refs/heads/master@{#602736}

third_party/blink/renderer/platform/loader/fetch/resource_fetcher.cc
@@ -753,13 +753,8 @@ base::Optional<ResourceRequestBlockedReason> ResourceFetcher::PrepareRequest(
     DCHECK(!options.cors_flag);
     params.MutableOptions().cors_flag = CORS::CalculateCORSFlag(
         params.Url(), origin.get(), resource_request.GetFetchRequestMode());
-    // Cross-origin requests are only allowed certain registered schemes.
-    if (options.cors_flag && !SchemeRegistry::ShouldTreatURLSchemeAsCORSEnabled(
-                                 params.Url().Protocol())) {
-      // This won't create a CORS related console error.
-      // TODO(yhirano): Fix this.
-      return ResourceRequestBlockedReason::kOther;
-    }
+    // TODO(yhirano): Reject requests for non CORS-enabled schemes.
+    // See https://crrev.com/c/1298828.
     resource_request.SetAllowStoredCredentials(CORS::CalculateCredentialsFlag(
         resource_request.GetFetchCredentialsMode(),
         CORS::CalculateResponseTainting(