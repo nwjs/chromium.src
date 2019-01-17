667244f3b9da2390d9fcca34ff9c1e6501402b4f
Roger Wang <roger@nwjs.io>
2019-01-17 04:40:27 -0700

    nw2: disable ExtensionDevToolsInfoBar

--- chrome/browser/extensions/api/debugger/debugger_api.cc
+++ chrome/browser/extensions/api/debugger/debugger_api.cc
@@ -226,10 +226,12 @@ bool ExtensionDevToolsClientHost::Attach() {
   if (Manifest::IsPolicyLocation(extension_->location()))
     return true;
 
+#if 0
   infobar_ = ExtensionDevToolsInfoBar::Create(
       extension_id(), extension_->name(), this,
       base::Bind(&ExtensionDevToolsClientHost::InfoBarDismissed,
                  base::Unretained(this)));
+#endif
   return true;
 }
 
