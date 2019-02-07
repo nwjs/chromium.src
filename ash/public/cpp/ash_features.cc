2b74d17cfc1a0e4937c53464bec802ddba4256de
yoshiki iguchi <yoshiki@chromium.org>
2019-02-07 11:51:54 +0000

    Disable lock screen notification
    We have decided not to ship this for M73, so this CL disables it by default.
    
    I will revert this CL after it gets merged in the M73 branch.
    
    R=tetsui@chromium.org
    TBR=manucornet@chromium.org, oshima@chromium.org, yoshiki@chromium.org
    
    (cherry picked from commit efdbf88dc001ad0fb2109344b612d04dfedeef8d)
    
    Bug: 928666
    Test: none
    Change-Id: I496b0c2c312b96ce6b8a7dd3fd1bd4a66a478bf2
    Reviewed-on: https://chromium-review.googlesource.com/c/1454063
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Auto-Submit: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#629080}
    Reviewed-on: https://chromium-review.googlesource.com/c/1457887
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3683@{#268}
    Cr-Branched-From: e51029943e0a38dd794b73caaf6373d5496ae783-refs/heads/master@{#625896}

--- ash/public/cpp/ash_features.cc
+++ ash/public/cpp/ash_features.cc
@@ -17,7 +17,7 @@ const base::Feature kDragTabsInTabletMode{"DragTabsInTabletMode",
                                           base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kLockScreenNotifications{"LockScreenNotifications",
-                                             base::FEATURE_ENABLED_BY_DEFAULT};
+                                             base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kLockScreenInlineReply{"LockScreenInlineReply",
                                            base::FEATURE_DISABLED_BY_DEFAULT};
