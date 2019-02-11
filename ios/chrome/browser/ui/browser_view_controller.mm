8d25bb4a926ec9003bb344bcd00c57bc9dad5a22
Justin Cohen <justincohen@google.com>
2019-02-11 17:58:31 +0000

    [ios] Always keep the webState view in the view hierarchy.
    It seems when a WKWebView instance (in this case |contentView|) is quickly
    removed and re-added to the view hierarchy the WebKit process may quickly
    suspend and restore itself.  This edge case leads to some webpages to failing
    to properly load. (crbug.com/848789).  Keeping the webState view always in the
    view hierarchy seems to prevent this from happening.
    
    This is a very similar fix to crbug.com/739390, which we thought was fixed in
    iOS 11.3.  The revert for this old fix happened a few milestones before 848789
    was filed so it's likely they are related.
    
    TBR=justincohen@google.com
    
    (cherry picked from commit cd633aff8c9423ed0fdf6199264b5aca9269bd1d)
    
    Bug: 848789
    Change-Id: Ia079fee20892e9faefe2ff0e10baa543cdfa7b1f
    Reviewed-on: https://chromium-review.googlesource.com/c/1435965
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#627127}
    Reviewed-on: https://chromium-review.googlesource.com/c/1464423
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3683@{#352}
    Cr-Branched-From: e51029943e0a38dd794b73caaf6373d5496ae783-refs/heads/master@{#625896}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -2370,8 +2370,18 @@ NSString* const kBrowserViewControllerSnackbarCategory =
       // not be restored until LoadIfNecessary call. Remove when fixed.
       tab.webState->GetNavigationManager()->LoadIfNecessary();
 
+      // Always show the webState view under the NTP, to work around
+      // crbug.com/848789
+      if (base::FeatureList::IsEnabled(kBrowserContainerKeepsContentView)) {
+        if (self.browserContainerViewController.contentView == nil)
+          self.browserContainerViewController.contentView =
+              tab.webState->GetView();
+      } else {
+        self.browserContainerViewController.contentView = nil;
+      }
       self.browserContainerViewController.contentViewController =
           viewController;
+
     } else {
       self.browserContainerViewController.contentView = [self viewForTab:tab];
     }
