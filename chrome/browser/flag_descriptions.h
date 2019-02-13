ef65536b57839354ff9c377d8389292901be06db
Daniele Castagna <dcastagna@chromium.org>
2019-02-13 18:28:15 +0000

    cros: Disable explicit DMA fences
    On Intel devices we noticed glitches when pageflipping that seem to
    be caused by a race between GPU and display controller.
    The glitches started appearing when we enabled explicit synchronization
    for the primary plane.
    
    Investigating further we discovered that we are currently inserting the
    fence after GLRenderer draws all the contents, but before the bufferqueue
    copies from the previous framebuffer, to the current framebuffer, to
    implement partial swap. This means we can end up scanning out from an fb
    while the GPU is copying parts of the previous fb to it.
    
    This CL inverts the flag disable-explicit-dma-fences to a flag to enable
    them, effectively disabling them by default while we fix the issue we
    discovered.
    
    Bug: 889530
    Change-Id: Iea1e9d163228fc7df6389fbe730196af01ab3a51
    Reviewed-on: https://chromium-review.googlesource.com/c/1462204
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Daniele Castagna <dcastagna@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#631272}(cherry picked from commit 787f41564acc6fbabed08976282b8907d6af7534)
    Reviewed-on: https://chromium-review.googlesource.com/c/1470990
    Cr-Commit-Position: refs/branch-heads/3683@{#396}
    Cr-Branched-From: e51029943e0a38dd794b73caaf6373d5496ae783-refs/heads/master@{#625896}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1906,8 +1906,8 @@ extern const char kCrostiniUsbSupportDescription[];
 extern const char kCryptAuthV2EnrollmentName[];
 extern const char kCryptAuthV2EnrollmentDescription[];
 
-extern const char kDisableExplicitDmaFencesName[];
-extern const char kDisableExplicitDmaFencesDescription[];
+extern const char kEnableExplicitDmaFencesName[];
+extern const char kEnableExplicitDmaFencesDescription[];
 
 extern const char kDisableSystemTimezoneAutomaticDetectionName[];
 extern const char kDisableSystemTimezoneAutomaticDetectionDescription[];
