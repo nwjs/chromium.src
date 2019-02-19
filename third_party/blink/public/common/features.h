b6ee3c564b43b196744379a99067b0a28e72b2f5
Tarun Bansal <tbansal@chromium.org>
2019-02-19 18:57:04 +0000

    Limit resource blocking hints by resource type
    Add a field trial to limit resource blocking hints by
    resource type. By default, resource blocking hints
    would now apply to only raw resource types, Scripts and CSS.
    
    Field trial allows us to additionally apply the
    hints to images and/or other resource types.
    
    Change-Id: I688bd228f11ddaad45e1db4976fa447c4c8fbad9
    Bug: 926905
    Reviewed-on: https://chromium-review.googlesource.com/c/1462138
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#632517}(cherry picked from commit 8df12b5c17d45b37b181f0985620a78d10f9339d)
    Reviewed-on: https://chromium-review.googlesource.com/c/1477753
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3683@{#496}
    Cr-Branched-From: e51029943e0a38dd794b73caaf6373d5496ae783-refs/heads/master@{#625896}

--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -30,6 +30,8 @@ BLINK_COMMON_EXPORT extern const base::Feature kNavigationPredictor;
 BLINK_COMMON_EXPORT extern const base::Feature kOnionSoupDOMStorage;
 BLINK_COMMON_EXPORT extern const base::Feature kPlzDedicatedWorker;
 BLINK_COMMON_EXPORT extern const base::Feature kPortals;
+BLINK_COMMON_EXPORT extern const base::Feature
+    kPreviewsResourceLoadingHintsSpecificResourceTypes;
 BLINK_COMMON_EXPORT extern const base::Feature kRTCGetDisplayMedia;
 BLINK_COMMON_EXPORT extern const base::Feature kRTCUnifiedPlanByDefault;
 BLINK_COMMON_EXPORT extern const base::Feature kRTCOfferExtmapAllowMixed;
