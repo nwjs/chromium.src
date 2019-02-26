c5bfcab9fb434e25c89c3f8a55e4f55104315d4e
Eric Karl <ericrk@chromium.org>
2019-02-26 21:39:16 +0000

    Revert "Reland "Android OOP-D: Enable by default""
    This feature did not launch in M72, so leaving it as disabled by default
    for M73.
    
    This reverts commit ba5be349d4f0f8b516cc256f33e284af8e3fa1a6.
    
    Bug: 732555
    Change-Id: If854fa1dc27f2ee7fbd0b922d44a50d4103e94f0
    Reviewed-on: https://chromium-review.googlesource.com/c/1490054
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3683@{#652}
    Cr-Branched-From: e51029943e0a38dd794b73caaf6373d5496ae783-refs/heads/master@{#625896}

--- components/viz/common/features.cc
+++ components/viz/common/features.cc
@@ -28,7 +28,7 @@ const base::Feature kEnableSurfaceSynchronization{
 // (OOP-D).
 // TODO(dnicoara): Look at enabling Chromecast support when ChromeOS support is
 // ready.
-#if defined(OS_CHROMEOS) || defined(IS_CHROMECAST)
+#if defined(OS_CHROMEOS) || defined(IS_CHROMECAST) || defined(OS_ANDROID)
 const base::Feature kVizDisplayCompositor{"VizDisplayCompositor",
                                           base::FEATURE_DISABLED_BY_DEFAULT};
 #else
