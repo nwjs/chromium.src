acd05540698c36b6d889177ed7b066ee65fc1882
Joshua Peraza <jperaza@chromium.org>
2019-03-08 20:53:39 +0000

    Propogate kEnableCrashReporterForTesting to child processes
    Bug: 923475
    Change-Id: I70227807b1169000defc78c4e1366fd33dcf2c3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1512338
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Joshua Peraza <jperaza@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#639152}(cherry picked from commit c664a83be80711d590aa19e8da08811e6aca9b88)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1512498
    Reviewed-by: Ben Mason <benmason@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3683@{#794}
    Cr-Branched-From: e51029943e0a38dd794b73caaf6373d5496ae783-refs/heads/master@{#625896}

--- android_webview/browser/aw_content_browser_client.cc
+++ android_webview/browser/aw_content_browser_client.cc
@@ -413,6 +413,10 @@ void AwContentBrowserClient::AppendExtraCommandLineSwitches(
             ::switches::kEnableCrashReporter)) {
       command_line->AppendSwitch(::switches::kEnableCrashReporter);
     }
+    if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+            ::switches::kEnableCrashReporterForTesting)) {
+      command_line->AppendSwitch(::switches::kEnableCrashReporterForTesting);
+    }
   }
 }
 
