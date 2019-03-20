a0f4fdfc54fd0e48d187a6be1f6dda653b38a4ad
Kent Tamura <tkent@chromium.org>
2019-03-20 00:50:30 +0000

    Merge "Fix a crash on FileChooserImpl" to M73 branch
    If a renderer process is compromised, and it calls both of
    FileChooserImpl::OpenFileChooser() and EnumerateChosenDirectory() via
    Mojo, the browser process could crash because ResetOwner() for
    the first FileChooserImpl::proxy_ instance was not called.  We
    should check nullness of proxy_ before updating it.
    
    TBR=tkent@chromium.org
    
    Bug: 941008
    Change-Id: Ie0c1895ec46ce78d40594b543e49e43b420af675
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1520509
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#640580}(cherry picked from commit 4a3482693491ac6bb3dd27d591efa3de1d1f1fcf)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1530225
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3683@{#857}
    Cr-Branched-From: e51029943e0a38dd794b73caaf6373d5496ae783-refs/heads/master@{#625896}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -548,6 +548,10 @@ class FileChooserImpl : public blink::mojom::FileChooser,
 
   void OpenFileChooser(blink::mojom::FileChooserParamsPtr params,
                        OpenFileChooserCallback callback) override {
+    if (proxy_) {
+      std::move(callback).Run(nullptr);
+      return;
+    }
     callback_ = std::move(callback);
     auto listener = std::make_unique<ListenerProxy>(this);
     proxy_ = listener.get();
@@ -567,6 +571,10 @@ class FileChooserImpl : public blink::mojom::FileChooser,
   void EnumerateChosenDirectory(
       const base::FilePath& directory_path,
       EnumerateChosenDirectoryCallback callback) override {
+    if (proxy_) {
+      std::move(callback).Run(nullptr);
+      return;
+    }
     callback_ = std::move(callback);
     auto listener = std::make_unique<ListenerProxy>(this);
     proxy_ = listener.get();
