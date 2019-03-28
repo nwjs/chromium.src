d43bcbaf1c9c5ead6a6259be71a2963e1c469116
Victor Costan <pwnall@chromium.org>
2019-03-28 17:16:30 +0000

    AppCache: Fetch TLS information when populating the disk cache.
    https://crrev.com/c/599359 attempted to migrate AppCache to the Network
    Service infrastructure.
    
    Unfortunately, the CL updated the implementation of
    AppCacheUpdateJob::UpdateURLLoaderRequest::Start() to use the network
    service's URLLoaderFactory::CreateLoaderAndStart() with |options| set to
    kURLLoadOptionNone.
    
    However, AppCache assumes that TLS certificate information is passed
    into network::ResourceResponseHead::ssl_info member of the |head|
    argument to URLLoaderClient::OnReceiveResponse(), and uses this
    information to populate its disk cache (data flow assertion traced
    below). The certificate information is only populated if
    CreateLoaderAndStart() has its |options| argument set to
    kURLLoadOptionsSendSSLInfoWithResponse.
    
    This CL fixes the AppCacheUpdateJob logic, so newly fetched AppCaches
    are populated correctly. However, it does not retroactively populate the
    missing information in users' disk caches. A follow-up CL will address
    this issue, most likely by performing a one-time erase of the users'
    caches.
    
    The following steps trace the data flow for the assertion above.
    
    1) AppCacheUpdateJob::UpdateURLLoaderRequest implements the
    URLLoaderClient interface.
    AppCacheUpdateJob::UpdateURLLoaderRequest::OnReceiveResponse() copies
    the |head| argument into the |response_| member, and populates the
    HttpResponseInfo::ssl_info field of the |http_response_info_| member
    based on the network::ResourceResponseHead::ssl_info of the |head|
    argument.
    
    2) AppCacheUpdateJob::UpdateURLLoaderRequest's |http_response_info|
    member is exposed via the GetResponseInfo() getter. This getter is used
    for an SSL certificate error check in
    AppCacheUpdateJob::URLFetcher::OnResponseStarted(), and then used to
    copy the HttpResponseInfo into a HttpResponseInfoIOBuffer instance that
    is passed as the |info_buf| argument into
    AppCacheResponseWriter::WriteInfo().
    
    3) AppCacheResponseWriter::WriteInfo() copies the |info_buf| argument
    into the |info_buffer_| member, and calls into
    AppCacheResponseWriter::CreateEntryIfNeededAndContinue(). In turn, this
    method calls into AppCacheResponseWriter::OnCreateEntryComplete(), which
    eventually calls into AppCacheResponseWriter::ContinueWriteInfo().
    
    4) AppCacheResponseWriter::ContinueWriteInfo() calls
    HttpResponseInfo::Persist() on the HttpResponseInfo stored in the
    |info_buffer_| member. HttpResponseInfo::Persist() delegates to
    X509Certificate::Persist() to serialize the SSLInfo, and its serialized
    output contains the certificate information.
    
    5) The serialized output is wrapped into a
    content::WrappedPickleIOBuffer and passed to
    AppCacheResponseIO::WriteRaw(), which calls
    AppCacheDiskCacheEntry::Write(), which finally calls into disk cache's
    net::Entry::WriteData().
    
    Bug: 937761
    Change-Id: I30869c255cdee977f82c34fb8042b523906faab0
    Tested: verified the bug repro steps with new/existing Chrome profiles
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531326
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Chris Mumford <cmumford@google.com>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#642548}(cherry picked from commit 22d95e289c08817a9d2c12cd9598528e59fd6a05)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1543870
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3683@{#884}
    Cr-Branched-From: e51029943e0a38dd794b73caaf6373d5496ae783-refs/heads/master@{#625896}

--- content/browser/appcache/appcache_update_url_loader_request.cc
+++ content/browser/appcache/appcache_update_url_loader_request.cc
@@ -23,7 +23,8 @@ void AppCacheUpdateJob::UpdateURLLoaderRequest::Start() {
 
   loader_factory_getter_->GetNetworkFactory()->CreateLoaderAndStart(
       mojo::MakeRequest(&url_loader_), -1, -1,
-      network::mojom::kURLLoadOptionNone, request_, std::move(client),
+      network::mojom::kURLLoadOptionSendSSLInfoWithResponse, request_,
+      std::move(client),
       net::MutableNetworkTrafficAnnotationTag(GetTrafficAnnotation()));
 }
 
