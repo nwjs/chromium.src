a37a6024ba7b3167e872a00459c5310c3090f4d9
Joshua Pawlicki <waffles@chromium.org>
2019-04-29 19:35:06 +0000

    Catch RuntimeException from runSynchronously.
    TBR=waffles@chromium.org
    
    (cherry picked from commit 07563bc24f29310bb8f03f14f1191bb15486b442)
    
    Bug: 931177
    Change-Id: Ie699786336d84ccde017466606d26073f9d1eb8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1585961
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#654505}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588940
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3729@{#948}
    Cr-Branched-From: d4a8972e30b604f090aeda5dfff68386ae656267-refs/heads/master@{#638880}

--- chrome/android/java/src/org/chromium/chrome/browser/omaha/RequestGenerator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omaha/RequestGenerator.java
@@ -197,10 +197,11 @@ public abstract class RequestGenerator {
     public int getNumGoogleAccountsOnDevice() {
         // RequestGenerator may be invoked from JobService or AlarmManager (through OmahaService),
         // so have to make sure AccountManagerFacade instance is initialized.
-        ThreadUtils.runOnUiThreadBlocking(
-                () -> ProcessInitializationHandler.getInstance().initializePreNative());
         int numAccounts = 0;
         try {
+            // TODO(waffles@chromium.org): Ideally, this should be asynchronous.
+            ThreadUtils.runOnUiThreadBlocking(
+                    () -> ProcessInitializationHandler.getInstance().initializePreNative());
             numAccounts = AccountManagerFacade.get().getGoogleAccounts().size();
         } catch (Exception e) {
             Log.e(TAG, "Can't get number of accounts.", e);
