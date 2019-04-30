7606d59db96e6da6047afa1a603349dce3e02f64
rajendrant <rajendrant@chromium.org>
2019-04-30 00:26:01 +0000

    Reland "Disable DRP proxies that fail warmup fetch in config"
    This is a reland of 0ee0c8f9fdd149e40e2522fcb17ec9614e5342dc
    after disabling the test that failed when network service is off
    
    Original change's description:
    > Disable DRP proxies that fail warmup fetch in config
    >
    > Warmup url fetch happens on startup and depending on whether it succeeds
    > the http/https proxy should be enabled or disabled in the custom proxy
    > config in network service. However the proxies are not getting disabled.
    > This CL fixes that and also adds a feature flag to turn off this
    > behavior.
    >
    > Bug: 954958
    > Change-Id: I22f0dc4cfb0ef58b4922ecfe9ba57009744aba8b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1579026
    > Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    > Commit-Queue: rajendrant <rajendrant@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#653378}
    
    (cherry picked from commit 3caa9adf63cd80dc29786f44749965a8c3ae95fb)
    
    Bug: 954958
    Change-Id: I77d71c4c42f38dc2c6ba8b8bc55e4b0971c410d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1580760
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#653465}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1585348
    Cr-Commit-Position: refs/branch-heads/3729@{#952}
    Cr-Branched-From: d4a8972e30b604f090aeda5dfff68386ae656267-refs/heads/master@{#638880}

--- components/data_reduction_proxy/core/common/data_reduction_proxy_features.h
+++ components/data_reduction_proxy/core/common/data_reduction_proxy_features.h
@@ -19,6 +19,7 @@ extern const base::Feature kDataReductionProxyEnabledWithNetworkService;
 extern const base::Feature kDataSaverUseOnDeviceSafeBrowsing;
 extern const base::Feature kDataReductionProxyBlockOnBadGatewayResponse;
 extern const base::Feature kDataReductionProxyPopulatePreviewsPageIDToPingback;
+extern const base::Feature kDataReductionProxyDisableProxyFailedWarmup;
 
 }  // namespace features
 }  // namespace data_reduction_proxy
