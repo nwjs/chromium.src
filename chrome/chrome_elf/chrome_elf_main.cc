cf6cee0cfbe54e3619ab1bfd54441d7229c5d9fa
Roger Wang <roger@nwjs.io>
2020-02-16 05:58:13 -0700

    Fix windows build

--- chrome/chrome_elf/chrome_elf_main.cc
+++ chrome/chrome_elf/chrome_elf_main.cc
@@ -81,7 +81,7 @@ BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID reserved) {
       } __except (elf_crash::GenerateCrashDump(GetExceptionInformation())) {
       }
     } else if (!install_static::IsCrashpadHandlerProcess()) {
-      SignalInitializeCrashReporting();
+      SignalInitializeCrashReporting(nullptr, nullptr);
       // CRT on initialization installs an exception filter which calls
       // TerminateProcess. We need to hook CRT's attempt to set an exception.
 #if 0 ////disable this or NW will fail with Enigma VB
