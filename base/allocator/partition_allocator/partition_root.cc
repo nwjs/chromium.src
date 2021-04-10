60acf99e90d76dc38903ac498cf6f3e5b833e273
Roger Wang <roger@nwjs.io>
2021-04-10 05:38:58 -0600

    Fix normal and windows build

--- base/allocator/partition_allocator/partition_root.cc
+++ base/allocator/partition_allocator/partition_root.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#pragma clang diagnostic ignored "-Wmisleading-indentation"
 #include "base/allocator/partition_allocator/partition_root.h"
 
 #include "base/allocator/partition_allocator/oom.h"
