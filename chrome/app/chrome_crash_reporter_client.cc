fe0d010d4f0cd8ebba819fe9ffbcd838dca70718
Roger Wang <roger@nwjs.io>
2022-02-22 23:55:04 +0000

    Fix mac build

--- chrome/app/chrome_crash_reporter_client.cc
+++ chrome/app/chrome_crash_reporter_client.cc
@@ -131,6 +131,7 @@ void ChromeCrashReporterClient::GetProductNameAndVersion(
   *channel = chrome::GetChannelName(chrome::WithExtendedStable(true));
 }
 
+#if !BUILDFLAG(IS_MAC)
 base::FilePath ChromeCrashReporterClient::GetReporterLogFilename() {
   return base::FilePath(CrashUploadList::kReporterLogFilename);
 }
@@ -138,6 +139,7 @@ base::FilePath ChromeCrashReporterClient::GetReporterLogFilename() {
 bool ChromeCrashReporterClient::GetShouldDumpLargerDumps() {
   return chrome::GetChannel() != version_info::Channel::STABLE;
 }
+#endif
 #endif  // BUILDFLAG(IS_POSIX) && !BUILDFLAG(IS_MAC)
 
 bool ChromeCrashReporterClient::GetCrashDumpLocation(
